9297605ce50bd0c041993cbb4ec1d77ebdcfd634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:35 +0000

    Add package tbtPythonTool/1.0.0/tbtPythonTool-1.0.0.tar.gz

---
+++ packages/tbtPythonTool/tbtPythonTool-1.0.0.tar.gz/tbtPythonTool-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+
+
+setup(
+    name='tbtPythonTool',
+    version= '1.0.0',
+    description='TBT package on gitlab',
+    author='Shalini Dixit',
+    author_email='shalinidxt0@gmail.com',
+    packages=['tbtPythonTool'],
+    url='https://gitlabe2.ext.net.nokia.com/shaldixi/datsv.git',
+    zip_safe=False
+)
---
+++ packages/tbtPythonTool/tbtPythonTool-1.0.0.tar.gz/tbtPythonTool-1.0.0/tbtPythonTool/__init__.py
@@ -0,0 +1,1 @@
+name = "datsv_test"
