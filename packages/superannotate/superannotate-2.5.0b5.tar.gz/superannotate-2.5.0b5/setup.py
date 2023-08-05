9d7f005ad0af77922c2fd1a994159e26c444225b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:10 +0000

    Add package superannotate/2.5.0b5/superannotate-2.5.0b5.tar.gz

---
+++ packages/superannotate/superannotate-2.5.0b5.tar.gz/superannotate-2.5.0b5/setup.py
@@ -0,0 +1,61 @@
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
+
+with open('requirements.txt') as f:
+    requirements = f.read()
+
+requirements = requirements.splitlines()
+
+with open('README.md') as f:
+    readme = f.read()
+readme = "\n".join(readme.split('\n')[2:])
+
+packages = find_packages()
+
+with open('superannotate/version.py') as f:
+    Version = f.read()
+
+Version = Version.rstrip()
+Version = Version[15:-1]
+
+ext_modules = [
+    Extension(
+        'superannotate.pycocotools_sa._mask',
+        sources=[
+            'superannotate/pycocotools_sa/_mask.pyx',
+            'superannotate/pycocotools_sa/maskApi.c'
+        ],
+        include_dirs=[np.get_include(), 'superannotate/pycocotools_sa'],
+        extra_compile_args=[],
+    )
+]
+
+setup(
+    name='superannotate',
+    version=Version,
+    description='Python SDK to SuperAnnotate platform',
+    license='MIT',
+    author='SuperAnnotate AI',
+    author_email='hovnatan@superannotate.com',
+    url='https://github.com/superannotateai/superannotate-python-sdk',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    setup_requires=['wheel'],
+    packages=find_packages(exclude=('tests', )),
+    entry_points={
+        'console_scripts': ['superannotatecli = superannotate.__main__:main']
+    },
+    python_requires='>=3.6',
+    package_data={
+        'pycocotools_sa':
+            [
+                'superannotate/pycocotools_sa/_mask.pyx',
+                'superannotate/pycocotools_sa/maskApi.c',
+                'superannotate/pycocotools_sa/maskApi.h'
+            ]
+    },
+    include_package_data=True,
+    ext_modules=cythonize(ext_modules)
+)
