aad74195f90162fe8d24925e5375f218363d546e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:50 +0000

    Add package Mopidy-ALSAMixer/2.0.1/Mopidy_ALSAMixer-2.0.1-py3-none-any.whl

---
+++ packages/Mopidy-ALSAMixer/Mopidy_ALSAMixer-2.0.1-py3-none-any.whl/mopidy_alsamixer/__init__.py
@@ -0,0 +1,33 @@
+import pathlib
+
+import pkg_resources
+
+from mopidy import config, ext
+
+__version__ = pkg_resources.get_distribution("Mopidy-ALSAMixer").version
+
+
+class Extension(ext.Extension):
+
+    dist_name = "Mopidy-ALSAMixer"
+    ext_name = "alsamixer"
+    version = __version__
+
+    def get_default_config(self):
+        return config.read(pathlib.Path(__file__).parent / "ext.conf")
+
+    def get_config_schema(self):
+        schema = super().get_config_schema()
+        schema["card"] = config.Integer(minimum=0)
+        schema["control"] = config.String()
+        schema["min_volume"] = config.Integer(minimum=0, maximum=100)
+        schema["max_volume"] = config.Integer(minimum=0, maximum=100)
+        schema["volume_scale"] = config.String(
+            choices=("linear", "cubic", "log")
+        )
+        return schema
+
+    def setup(self, registry):
+        from mopidy_alsamixer.mixer import AlsaMixer
+
+        registry.add("mixer", AlsaMixer)
