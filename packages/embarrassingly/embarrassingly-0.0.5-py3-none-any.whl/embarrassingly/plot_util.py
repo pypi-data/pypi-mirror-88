32d2fd710188891f1022d80df2a177a8b6466fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:24 +0000

    Add package embarrassingly/0.0.5/embarrassingly-0.0.5-py3-none-any.whl

---
+++ packages/embarrassingly/embarrassingly-0.0.5-py3-none-any.whl/embarrassingly/plot_util.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+RESOLUTION = 0.01    # Used 0.003 for plots
+
+# See https://www.microprediction.com/blog/robust-optimization for explanation
+
+def mesh2d(f,bounds,resolution=RESOLUTION):
+    """ Plot function taking len 2 vector as single argument
+          f(xs)
+        Also put points on
+    """
+    def g(x,y):
+        return f(np.array([x,y]))
+    return mesh2d_xy(g,bounds,resolution)
+
+from matplotlib import cm
+def mesh2d_xy(f,bounds,resolution=RESOLUTION):
+    """ Plot function taking two arguments
+        f(x,y)
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x1 = np.arange( bounds[0][0], bounds[0][1],resolution )
+    x2 = np.arange( bounds[1][0], bounds[1][1], resolution)
+
+    X1, X2 = np.meshgrid(x1, x2)
+    zs = np.array([ f(x1_,x2_) for x1_,x2_ in zip( np.ravel(X1), np.ravel(X2)) ])
+    Z = zs.reshape(X1.shape)
+
+    ax.plot_surface(X1, X2, Z, alpha=0.9,cmap=cm.summer)
+
+    ax.set_xlabel('$x_1$')
+    ax.set_ylabel('$x_2$')
+
+    return fig, ax
