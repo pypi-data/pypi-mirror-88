90389b040988017f9ed121b1bd48f53a94568c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:28 +0000

    Add package dsng-distributions-lee/0.1/dsng_distributions_lee-0.1.tar.gz

---
+++ packages/dsng-distributions-lee/dsng_distributions_lee-0.1.tar.gz/dsng_distributions_lee-0.1/README.md
@@ -0,0 +1,3 @@
+A package with classes representing Guassian and Binomial distributions.
+
+Part of the Udacity Datascience Nanodegree.
---
+++ packages/dsng-distributions-lee/dsng_distributions_lee-0.1.tar.gz/dsng_distributions_lee-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsng_distributions_lee',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsng_distributions_lee'],
+      author='Lee Hodgkinson',
+      author_email='lee@logicon.io',
+      zip_safe=False)
