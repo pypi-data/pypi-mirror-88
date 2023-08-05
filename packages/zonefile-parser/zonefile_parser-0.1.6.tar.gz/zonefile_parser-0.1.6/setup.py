dc4090bb71de04c0e2f8a89321c5afe96d7f8229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:44 +0000

    Add package zonefile-parser/0.1.6/zonefile_parser-0.1.6.tar.gz

---
+++ packages/zonefile-parser/zonefile_parser-0.1.6.tar.gz/zonefile_parser-0.1.6/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+DNS Zone File
+==============
+
+"""
+
+from setuptools import setup, find_packages
+
+version = "0.1.6"
+
+setup(
+    name='zonefile_parser',
+    version=version,
+    url='https://github.com/aredwood/zone-file-parser',
+    license='MIT',
+    author='Alex Redwood',
+    download_url=f"https://github.com/aredwood/zone-file-parser/archive/{version}.tar.gz",
+    author_email='hello@alexredwood.com',
+    maintainer='Alex Redwood',
+    maintainer_email='hello@alexredwood.com',
+    description="library for parsing dns zone files",
+    keywords='parse dns zone file',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
---
+++ packages/zonefile-parser/zonefile_parser-0.1.6.tar.gz/zonefile_parser-0.1.6/zonefile_parser/parser.py
@@ -0,0 +1,54 @@
+from zonefile_parser.record import Record
+from enum import IntEnum
+
+class RecordEnum(IntEnum):
+    NAME = 0
+    TTL = 1
+    RCLASS = 2
+    RTYPE = 3
+    RDATA = 4
+    MX_PRIORITY = 4
+    MX_HOST = 5
+    SOA_MNAME = 4
+    SOA_RNAME = 5
+    SOA_SERIAL = 6
+    SOA_REFRESH = 7
+    SOA_RETRY = 8
+    SOA_EXPIRE = 9
+    SOA_MINIMUM = 10
+# TODO unit test
+def parse_record(parts:list) -> Record:
+    record = Record()
+
+    record.set_name(parts[RecordEnum.NAME])
+    record.set_ttl(parts[RecordEnum.TTL])
+    record.set_rclass(parts[RecordEnum.RCLASS].upper())
+    record.set_rtype(parts[RecordEnum.RTYPE].upper())
+
+    # rdata is unique for MX and SOA, everything else is the same.
+    if record.rtype not in ["MX","SOA"]:
+        record.set_rdata({
+            "value":parts[RecordEnum.RDATA]
+        })
+    elif record.rtype == "MX":
+        # the record is a SOA or MX
+        record.set_rdata({
+            "priority": parts[RecordEnum.MX_PRIORITY],
+            "host":parts[RecordEnum.MX_HOST]
+        })
+    elif record.rtype == "SOA":
+        record.set_rdata({
+                "mname": parts[RecordEnum.SOA_MNAME],
+                "rname": parts[RecordEnum.SOA_RNAME],
+                "serial": parts[RecordEnum.SOA_SERIAL],
+                "refresh": parts[RecordEnum.SOA_REFRESH],
+                "retry": parts[RecordEnum.SOA_RETRY],
+                "expire": parts[RecordEnum.SOA_EXPIRE],
+                "minimum": parts[RecordEnum.SOA_MINIMUM]
+        })
+    return record
+
+
+
+
+
