1eaaf4dad5e5316aa3820d0809c89c44efbdf007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:03 +0000

    Add package terra-bonobo-nodes/0.5.2/terra-bonobo-nodes-0.5.2.tar.gz

---
+++ packages/terra-bonobo-nodes/terra-bonobo-nodes-0.5.2.tar.gz/terra-bonobo-nodes-0.5.2/setup.py
@@ -0,0 +1,35 @@
+import os
+import re
+import setuptools
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+README = open(os.path.join(HERE, 'README.md')).read()
+CHANGES = open(os.path.join(HERE, 'CHANGES.md')).read()
+
+with open('requirements.txt') as fic:
+    reqs = [a.strip() for a in fic.read().splitlines()
+            if a.strip() and not re.search('^\\s*(#|-e)', a)]
+
+setuptools.setup(
+    name="terra-bonobo-nodes",
+    version=open(os.path.join(HERE, 'terra_bonobo_nodes', 'VERSION.md')).read().strip(),
+    include_package_data=True,
+    author="Makina Corpus",
+    author_email="terralego-pypi@makina-corpus.com",
+    description="Set of bonobo ETL's nodes for terralego's projects",
+    long_description=README + '\n\n' + CHANGES,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Terralego/terra-bonobo-nodes",
+    install_requires=reqs,
+    dependency_links=[
+        "git+https://github.com/jrmi/pyfiles.git@master#egg=pyfiles",
+    ],
+    tests_require=['asynctest'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
