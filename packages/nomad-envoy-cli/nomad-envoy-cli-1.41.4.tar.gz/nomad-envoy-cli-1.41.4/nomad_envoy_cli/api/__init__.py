c5e7265589500640c9d879fcb766b70b70347804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:54 +0000

    Add package nomad-envoy-cli/1.41.4/nomad-envoy-cli-1.41.4.tar.gz

---
+++ packages/nomad-envoy-cli/nomad-envoy-cli-1.41.4.tar.gz/nomad-envoy-cli-1.41.4/nomad_envoy_cli/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from nomad_envoy_cli.api.customs_api import CustomsApi
+from nomad_envoy_cli.api.customs_callback_api import CustomsCallbackApi
+from nomad_envoy_cli.api.order_api import OrderApi
+from nomad_envoy_cli.api.product_api import ProductApi
