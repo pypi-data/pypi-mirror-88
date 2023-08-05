06ec7fcaafaf987bd4cb84f4c1a9945fdcc38c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:37 +0000

    Add package laozi/1.0.1/laozi-1.0.1.tar.gz

---
+++ packages/laozi/laozi-1.0.1.tar.gz/laozi-1.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['laozi']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'laozi',
+    'version': '1.0.1',
+    'description': 'Serialization library outputting a human-readable format',
+    'long_description': None,
+    'author': 'David Francos Cuartero',
+    'author_email': 'me@davidfrancos.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
