cf7ac4e2e19a4345eda1587a69254238c9be7360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:24 +0000

    Add package lsc-distributions/0.1/lsc_distributions-0.1.tar.gz

---
+++ packages/lsc-distributions/lsc_distributions-0.1.tar.gz/lsc_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'lsc_distributions',
+      version = '0.1',
+      description = 'Gaussian and Binomial distributions',
+      packages = ['lsc_distributions'],
+      author = 'Lucian Stefan Craciun',
+      author_email = 'lucyans_c@yahoo.com',
+      zip_safe = False)
