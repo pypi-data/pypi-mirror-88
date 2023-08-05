74a443175fbae4ba9bcbd7435bbfa5a6ee2a755d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:14 +0000

    Add package test-github-actions-jg/0.0.13/test_github_actions_jg-0.0.13-py3-none-any.whl

---
+++ packages/test-github-actions-jg/test_github_actions_jg-0.0.13-py3-none-any.whl/pydadjoke/__init__.py
@@ -0,0 +1,24 @@
+"Dad jokes in Python"
+
+from __future__ import absolute_import
+from .dad_jokes import get_joke
+
+__project__ = "test-github-actions-jg"
+__version__ = "0.0.13"
+__keywords__ = ["jokes", "joke", "dadjokes"]
+__author__ = "Jarred Glaser"
+__author_email__ = "jarred.glaser@gmail.com"
+__url__ = "https://www.jarredglaser.com/"
+__platforms__ = "ALL"
+
+__classifiers__ = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.8',
+    'License :: OSI Approved :: MIT License'
+]
+
+__requires__ = ["pandas"]
+
+__extra_requires__ = {
+}
