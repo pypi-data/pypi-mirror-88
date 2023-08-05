703d9c4918a8f126a58605ece11cbc16cb4b5a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:55 +0000

    Add package mypy-boto3-globalaccelerator/1.16.31.1/mypy-boto3-globalaccelerator-1.16.31.1.tar.gz

---
+++ packages/mypy-boto3-globalaccelerator/mypy-boto3-globalaccelerator-1.16.31.1.tar.gz/mypy-boto3-globalaccelerator-1.16.31.1/mypy_boto3_globalaccelerator/client.py
@@ -0,0 +1,327 @@
+"""
+Main interface for globalaccelerator service client
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_globalaccelerator import GlobalAcceleratorClient
+
+    client: GlobalAcceleratorClient = boto3.client("globalaccelerator")
+    ```
+"""
+import sys
+from typing import Any, Dict, List, Type, overload
+
+from botocore.client import ClientMeta
+
+from mypy_boto3_globalaccelerator.paginator import (
+    ListAcceleratorsPaginator,
+    ListEndpointGroupsPaginator,
+    ListListenersPaginator,
+)
+from mypy_boto3_globalaccelerator.type_defs import (
+    AdvertiseByoipCidrResponseTypeDef,
+    CidrAuthorizationContextTypeDef,
+    CreateAcceleratorResponseTypeDef,
+    CreateEndpointGroupResponseTypeDef,
+    CreateListenerResponseTypeDef,
+    DeprovisionByoipCidrResponseTypeDef,
+    DescribeAcceleratorAttributesResponseTypeDef,
+    DescribeAcceleratorResponseTypeDef,
+    DescribeEndpointGroupResponseTypeDef,
+    DescribeListenerResponseTypeDef,
+    EndpointConfigurationTypeDef,
+    ListAcceleratorsResponseTypeDef,
+    ListByoipCidrsResponseTypeDef,
+    ListEndpointGroupsResponseTypeDef,
+    ListListenersResponseTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    PortOverrideTypeDef,
+    PortRangeTypeDef,
+    ProvisionByoipCidrResponseTypeDef,
+    TagTypeDef,
+    UpdateAcceleratorAttributesResponseTypeDef,
+    UpdateAcceleratorResponseTypeDef,
+    UpdateEndpointGroupResponseTypeDef,
+    UpdateListenerResponseTypeDef,
+    WithdrawByoipCidrResponseTypeDef,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = ("GlobalAcceleratorClient",)
+
+
+class BotocoreClientError(BaseException):
+    MSG_TEMPLATE: str
+
+    def __init__(self, error_response: Dict[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+
+class Exceptions:
+    AcceleratorNotDisabledException: Type[BotocoreClientError]
+    AcceleratorNotFoundException: Type[BotocoreClientError]
+    AccessDeniedException: Type[BotocoreClientError]
+    AssociatedEndpointGroupFoundException: Type[BotocoreClientError]
+    AssociatedListenerFoundException: Type[BotocoreClientError]
+    ByoipCidrNotFoundException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    EndpointGroupAlreadyExistsException: Type[BotocoreClientError]
+    EndpointGroupNotFoundException: Type[BotocoreClientError]
+    IncorrectCidrStateException: Type[BotocoreClientError]
+    InternalServiceErrorException: Type[BotocoreClientError]
+    InvalidArgumentException: Type[BotocoreClientError]
+    InvalidNextTokenException: Type[BotocoreClientError]
+    InvalidPortRangeException: Type[BotocoreClientError]
+    LimitExceededException: Type[BotocoreClientError]
+    ListenerNotFoundException: Type[BotocoreClientError]
+
+
+class GlobalAcceleratorClient:
+    """
+    [GlobalAccelerator.Client documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client)
+    """
+
+    meta: ClientMeta
+    exceptions: Exceptions
+
+    def advertise_byoip_cidr(self, Cidr: str) -> AdvertiseByoipCidrResponseTypeDef:
+        """
+        [Client.advertise_byoip_cidr documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.advertise_byoip_cidr)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Client.can_paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.can_paginate)
+        """
+
+    def create_accelerator(
+        self,
+        Name: str,
+        IdempotencyToken: str,
+        IpAddressType: Literal["IPV4"] = None,
+        IpAddresses: List[str] = None,
+        Enabled: bool = None,
+        Tags: List["TagTypeDef"] = None,
+    ) -> CreateAcceleratorResponseTypeDef:
+        """
+        [Client.create_accelerator documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.create_accelerator)
+        """
+
+    def create_endpoint_group(
+        self,
+        ListenerArn: str,
+        EndpointGroupRegion: str,
+        IdempotencyToken: str,
+        EndpointConfigurations: List[EndpointConfigurationTypeDef] = None,
+        TrafficDialPercentage: float = None,
+        HealthCheckPort: int = None,
+        HealthCheckProtocol: Literal["TCP", "HTTP", "HTTPS"] = None,
+        HealthCheckPath: str = None,
+        HealthCheckIntervalSeconds: int = None,
+        ThresholdCount: int = None,
+        PortOverrides: List["PortOverrideTypeDef"] = None,
+    ) -> CreateEndpointGroupResponseTypeDef:
+        """
+        [Client.create_endpoint_group documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.create_endpoint_group)
+        """
+
+    def create_listener(
+        self,
+        AcceleratorArn: str,
+        PortRanges: List["PortRangeTypeDef"],
+        Protocol: Literal["TCP", "UDP"],
+        IdempotencyToken: str,
+        ClientAffinity: Literal["NONE", "SOURCE_IP"] = None,
+    ) -> CreateListenerResponseTypeDef:
+        """
+        [Client.create_listener documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.create_listener)
+        """
+
+    def delete_accelerator(self, AcceleratorArn: str) -> None:
+        """
+        [Client.delete_accelerator documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.delete_accelerator)
+        """
+
+    def delete_endpoint_group(self, EndpointGroupArn: str) -> None:
+        """
+        [Client.delete_endpoint_group documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.delete_endpoint_group)
+        """
+
+    def delete_listener(self, ListenerArn: str) -> None:
+        """
+        [Client.delete_listener documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.delete_listener)
+        """
+
+    def deprovision_byoip_cidr(self, Cidr: str) -> DeprovisionByoipCidrResponseTypeDef:
+        """
+        [Client.deprovision_byoip_cidr documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.deprovision_byoip_cidr)
+        """
+
+    def describe_accelerator(self, AcceleratorArn: str) -> DescribeAcceleratorResponseTypeDef:
+        """
+        [Client.describe_accelerator documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.describe_accelerator)
+        """
+
+    def describe_accelerator_attributes(
+        self, AcceleratorArn: str
+    ) -> DescribeAcceleratorAttributesResponseTypeDef:
+        """
+        [Client.describe_accelerator_attributes documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.describe_accelerator_attributes)
+        """
+
+    def describe_endpoint_group(
+        self, EndpointGroupArn: str
+    ) -> DescribeEndpointGroupResponseTypeDef:
+        """
+        [Client.describe_endpoint_group documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.describe_endpoint_group)
+        """
+
+    def describe_listener(self, ListenerArn: str) -> DescribeListenerResponseTypeDef:
+        """
+        [Client.describe_listener documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.describe_listener)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Dict[str, Any] = None,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = None,
+    ) -> str:
+        """
+        [Client.generate_presigned_url documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.generate_presigned_url)
+        """
+
+    def list_accelerators(
+        self, MaxResults: int = None, NextToken: str = None
+    ) -> ListAcceleratorsResponseTypeDef:
+        """
+        [Client.list_accelerators documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.list_accelerators)
+        """
+
+    def list_byoip_cidrs(
+        self, MaxResults: int = None, NextToken: str = None
+    ) -> ListByoipCidrsResponseTypeDef:
+        """
+        [Client.list_byoip_cidrs documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.list_byoip_cidrs)
+        """
+
+    def list_endpoint_groups(
+        self, ListenerArn: str, MaxResults: int = None, NextToken: str = None
+    ) -> ListEndpointGroupsResponseTypeDef:
+        """
+        [Client.list_endpoint_groups documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.list_endpoint_groups)
+        """
+
+    def list_listeners(
+        self, AcceleratorArn: str, MaxResults: int = None, NextToken: str = None
+    ) -> ListListenersResponseTypeDef:
+        """
+        [Client.list_listeners documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.list_listeners)
+        """
+
+    def list_tags_for_resource(self, ResourceArn: str) -> ListTagsForResourceResponseTypeDef:
+        """
+        [Client.list_tags_for_resource documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.list_tags_for_resource)
+        """
+
+    def provision_byoip_cidr(
+        self, Cidr: str, CidrAuthorizationContext: CidrAuthorizationContextTypeDef
+    ) -> ProvisionByoipCidrResponseTypeDef:
+        """
+        [Client.provision_byoip_cidr documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.provision_byoip_cidr)
+        """
+
+    def tag_resource(self, ResourceArn: str, Tags: List["TagTypeDef"]) -> Dict[str, Any]:
+        """
+        [Client.tag_resource documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.tag_resource)
+        """
+
+    def untag_resource(self, ResourceArn: str, TagKeys: List[str]) -> Dict[str, Any]:
+        """
+        [Client.untag_resource documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.untag_resource)
+        """
+
+    def update_accelerator(
+        self,
+        AcceleratorArn: str,
+        Name: str = None,
+        IpAddressType: Literal["IPV4"] = None,
+        Enabled: bool = None,
+    ) -> UpdateAcceleratorResponseTypeDef:
+        """
+        [Client.update_accelerator documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.update_accelerator)
+        """
+
+    def update_accelerator_attributes(
+        self,
+        AcceleratorArn: str,
+        FlowLogsEnabled: bool = None,
+        FlowLogsS3Bucket: str = None,
+        FlowLogsS3Prefix: str = None,
+    ) -> UpdateAcceleratorAttributesResponseTypeDef:
+        """
+        [Client.update_accelerator_attributes documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.update_accelerator_attributes)
+        """
+
+    def update_endpoint_group(
+        self,
+        EndpointGroupArn: str,
+        EndpointConfigurations: List[EndpointConfigurationTypeDef] = None,
+        TrafficDialPercentage: float = None,
+        HealthCheckPort: int = None,
+        HealthCheckProtocol: Literal["TCP", "HTTP", "HTTPS"] = None,
+        HealthCheckPath: str = None,
+        HealthCheckIntervalSeconds: int = None,
+        ThresholdCount: int = None,
+        PortOverrides: List["PortOverrideTypeDef"] = None,
+    ) -> UpdateEndpointGroupResponseTypeDef:
+        """
+        [Client.update_endpoint_group documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.update_endpoint_group)
+        """
+
+    def update_listener(
+        self,
+        ListenerArn: str,
+        PortRanges: List["PortRangeTypeDef"] = None,
+        Protocol: Literal["TCP", "UDP"] = None,
+        ClientAffinity: Literal["NONE", "SOURCE_IP"] = None,
+    ) -> UpdateListenerResponseTypeDef:
+        """
+        [Client.update_listener documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.update_listener)
+        """
+
+    def withdraw_byoip_cidr(self, Cidr: str) -> WithdrawByoipCidrResponseTypeDef:
+        """
+        [Client.withdraw_byoip_cidr documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Client.withdraw_byoip_cidr)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_accelerators"]
+    ) -> ListAcceleratorsPaginator:
+        """
+        [Paginator.ListAccelerators documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Paginator.ListAccelerators)
+        """
+
+    @overload
+    def get_paginator(
+        self, operation_name: Literal["list_endpoint_groups"]
+    ) -> ListEndpointGroupsPaginator:
+        """
+        [Paginator.ListEndpointGroups documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Paginator.ListEndpointGroups)
+        """
+
+    @overload
+    def get_paginator(self, operation_name: Literal["list_listeners"]) -> ListListenersPaginator:
+        """
+        [Paginator.ListListeners documentation](https://boto3.amazonaws.com/v1/documentation/api/1.16.31/reference/services/globalaccelerator.html#GlobalAccelerator.Paginator.ListListeners)
+        """
