e20d5931af459aa4809c82fbfa73434d3cd886c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:46 +0000

    Add package invenio-drafts-resources/0.7.0/invenio-drafts-resources-0.7.0.tar.gz

---
+++ packages/invenio-drafts-resources/invenio-drafts-resources-0.7.0.tar.gz/invenio-drafts-resources-0.7.0/run-tests.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+# Always bring down docker services
+function cleanup() {
+    docker-services-cli down
+}
+
+trap cleanup EXIT
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+docker-services-cli up es postgresql
+python -m pytest
+tests_exit_code=$?
+exit "$tests_exit_code"
