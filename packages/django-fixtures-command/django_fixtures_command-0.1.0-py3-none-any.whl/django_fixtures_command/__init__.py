7a3283f58ea7011fac58d4db2fd180c48cda7665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:29 +0000

    Add package django-fixtures-command/0.1.0/django_fixtures_command-0.1.0-py3-none-any.whl

---
+++ packages/django-fixtures-command/django_fixtures_command-0.1.0-py3-none-any.whl/django_fixtures_command/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'django_fixtures_command.apps.DjangoFixturesCommandConfig'
---
+++ packages/django-fixtures-command/django_fixtures_command-0.1.0-py3-none-any.whl/django_fixtures_command/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoFixturesCommandConfig(AppConfig):
+    name = 'django_fixtures_command'
+    verbose_name = 'Django Fixtures Command'
