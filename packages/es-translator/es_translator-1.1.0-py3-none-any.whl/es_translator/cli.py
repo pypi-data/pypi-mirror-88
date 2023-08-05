88356eee124aad57400346225dda61e4544c2d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:15 +0000

    Add package es-translator/1.1.0/es_translator-1.1.0-py3-none-any.whl

---
+++ packages/es-translator/es_translator-1.1.0-py3-none-any.whl/es_translator/cli.py
@@ -0,0 +1,65 @@
+import click
+import logging
+import sys
+import os
+
+from es_translator import EsTranslator
+from tempfile import mkdtemp
+# Module from the same package
+from es_translator.logger import add_syslog_handler, add_stdout_handler
+from es_translator.pairs import Pairs
+
+def validate_loglevel(ctx, param, value):
+    try:
+        if isinstance(value, str):
+            return getattr(logging, value)
+        return int(value)
+    except (AttributeError, ValueError):
+        raise click.BadParameter('must be a valid log level (CRITICAL, ERROR, WARNING, INFO, DEBUG or NOTSET)')
+
+@click.command()
+@click.option('--url', help='Elastichsearch URL', required=True)
+@click.option('--index', help='Elastichsearch Index', required=True)
+@click.option('--source-language', help='Source language to translate from', required=True, default=None)
+@click.option('--target-language', help='Target language to translate to', required=True, default=None)
+@click.option('--intermediary-language', help='An intermediary language to use when no translation is available between the source and the target. If none is provided this will be calculated automatically.')
+@click.option('--source-field', help='Document field to translate', default="content")
+@click.option('--target-field', help='Document field where the translations are stored', default="content_translated")
+@click.option('--query-string', help='Search query string to filter result')
+@click.option('--data-dir', help='Path to the directory where to language model will be downloaded', type=click.Path(exists=True, dir_okay=True, writable=True, readable=True), default=mkdtemp())
+@click.option('--scan-scroll', help='Scroll duration (set to higher value if you\'re processing a lot of documents)', default="5m")
+@click.option('--dry-run', help='Don\'t save anything in Elasticsearch', is_flag=True, default=False)
+@click.option('--pool-size', help='Number of parallel processes to start', default=1)
+@click.option('--pool-timeout', help='Timeout to add a translation', default=60 * 30)
+@click.option('--syslog-address', help='Syslog address', default='localhost')
+@click.option('--syslog-port', help='Syslog port', default=514)
+@click.option('--syslog-facility', help='Syslog facility', default='local7')
+@click.option('--stdout-loglevel', help='Change the default log level for stdout error handler', default='ERROR',
+              callback=validate_loglevel)
+def translate(syslog_address, syslog_port, syslog_facility, stdout_loglevel, **options):
+    # Configure Syslog handler
+    add_syslog_handler(syslog_address, syslog_port, syslog_facility)
+    add_stdout_handler(stdout_loglevel)
+    # We pass all options to EsTranslator then we start the translation
+    # from Elasticsearch. This will download required pairs if needed.
+    EsTranslator(options).start()
+
+
+@click.command()
+@click.option('--data-dir', help='Path to the directory where to language model will be downloaded', type=click.Path(exists=True, dir_okay=True, writable=True, readable=True), default=mkdtemp())
+@click.option('--local', help='List pairs available locally', is_flag=True, default=False)
+@click.option('--syslog-address', help='Syslog address', default='localhost')
+@click.option('--syslog-port', help='Syslog port', default=514)
+@click.option('--syslog-facility', help='Syslog facility', default='local7')
+@click.option('--stdout-loglevel', help='Change the default log level for stdout error handler', default='ERROR',
+              callback=validate_loglevel)
+def pairs(data_dir, local, syslog_address, syslog_port, syslog_facility, stdout_loglevel, **options):
+    # Configure Syslog handler
+    add_syslog_handler(syslog_address, syslog_port, syslog_facility)
+    add_stdout_handler(stdout_loglevel)
+    # Only the data-dir is needed to construct the Apertium instance, then
+    # we just need to print the pair
+    Pairs(data_dir, local).print_pairs()
+
+if __name__ == '__main__':
+    translate()
