af0e745aa981b6dd7fe9c35549b69c6fdb16bea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:33 +0000

    Add package TracMathJax/0.1.7/TracMathJax-0.1.7.tar.gz

---
+++ packages/TracMathJax/TracMathJax-0.1.7.tar.gz/TracMathJax-0.1.7/mathjax/htdocs/update.js
@@ -0,0 +1,14 @@
+jQuery(function ($) {
+  var render = function () {
+    $("#content .trac-mathjax").each(function() {
+      var node = $(this);
+      var type = /^div$/i.test(this.tagName) ?
+                 'math/tex; mode=display' : 'math/tex';
+      var script = $('<script></script>').attr('type', type).text(node.text());
+      node.replaceWith(script);
+    });
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+  };
+  render();
+  $(document).ajaxSuccess(render);
+});
---
+++ packages/TracMathJax/TracMathJax-0.1.7.tar.gz/TracMathJax-0.1.7/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2024 Mitar <mitar.trac@tnode.com>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+from setuptools import setup
+
+VERSION = '0.1.7'
+PACKAGE = 'mathjax'
+
+setup(
+    name='TracMathJax',
+    version=VERSION,
+    description="Renders mathematical equations using MathJax library.",
+    author='Mitar',
+    author_email='mitar.trac@tnode.com',
+    url='https://trac-hacks.org/wiki/TracMathJaxPlugin',
+    keywords='trac plugin',
+    license="AGPLv3",
+    classifiers=['Framework :: Trac'],
+    packages=[PACKAGE],
+    include_package_data=True,
+    package_data={
+        PACKAGE: ['htdocs/*.js'],
+    },
+    install_requires=[],
+    zip_safe=False,
+    entry_points={
+        'trac.plugins': '%s = %s.api' % (PACKAGE, PACKAGE),
+    },
+)
