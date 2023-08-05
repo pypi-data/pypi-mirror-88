9ba27029bd6aad143d9d36c5ccdc3702faac5a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:48 +0000

    Add package SSAP/0.0.7/SSAP-0.0.7-py3-none-any.whl

---
+++ packages/SSAP/SSAP-0.0.7-py3-none-any.whl/SSAP/SSAP.py
@@ -0,0 +1,46 @@
+# Author:tony
+# Create time:2020.12.09
+# The software is build 4 SSAP POC testing.
+
+# %Dev plan: GUI, pop up web visualization, eng charts
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+# basic introduction of the pkg
+def info():
+    print('Welcome to SSAP! SSAP→[Self Service Analytics Platform]. There are different analytics function modules to '
+          'support you complete different data analytics scenarios. Please enjoy! Any question or support needed, '
+          'please raise in Big Data Community(BDC).')
+
+# ice bsfcmap
+def bsfcmap(speed, torque, bsfc, title, level):
+    x = np.array(speed)
+    y = np.array(torque)
+    z = np.array(bsfc)
+    level = level
+    title = title
+
+    xi = np.linspace(min(x), max(x), 1000)
+    yi = np.linspace(min(y), max(y), 1000)
+    X, Y = np.meshgrid(xi, yi)
+    Z = griddata((x, y), z, (X, Y), method='cubic')
+
+    # method: nearest / linear / cubic
+    plt.figure(figsize=(12, 8))
+    C = plt.contour(X, Y, Z, level, colors='black', alpha=0.5)
+    plt.contourf(X, Y, Z, level, alpha=.75, cmap='plasma')
+    plt.colorbar()
+    plt.clabel(C, inline=True, fontsize=10, colors='black')
+    plt.xlabel('Engine_Speed(rpm)')
+    plt.ylabel('Engine_Torque(N.m)')
+    plt.title(title)
+    plt.show()
+
+
