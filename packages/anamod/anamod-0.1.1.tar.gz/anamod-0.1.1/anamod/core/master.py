2e5e294921044e82f1f5cc6bff230f42b8f42fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:33 +0000

    Add package anamod/0.1.1/anamod-0.1.1.tar.gz

---
+++ packages/anamod/anamod-0.1.1.tar.gz/anamod-0.1.1/anamod/core/master.py
@@ -0,0 +1,83 @@
+"""
+anamod master pipeline
+Given test samples, a trained model and a feature hierarchy,
+computes the effect on the model's output loss after perturbing the features/feature groups
+in the hierarchy.
+"""
+
+import csv
+import importlib
+import os
+import sys
+from unittest.mock import patch
+
+import anytree
+import numpy as np
+
+from anamod.core import constants, utils
+from anamod.core.pipelines import CondorPipeline, SerialPipeline
+from anamod.core.utils import round_value
+from anamod.fdr import hierarchical_fdr_control
+
+
+def main(args):
+    """Parse arguments from command-line"""
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    args.rng = np.random.default_rng(args.seed)
+    args.logger = utils.get_logger(__name__, "%s/anamod.log" % args.output_dir)
+    validate_args(args)
+    return pipeline(args)
+
+
+def pipeline(args):
+    """Master pipeline"""
+    # FIXME: some outputs returned via return value (temporal analysis), other via output file (hierarchical analysis)
+    # TODO: 'args' is now an object. Change to reflect that and figure out way to print object attributes
+    args.logger.info("Begin anamod master pipeline with args: %s" % args)
+    # Perturb features
+    worker_pipeline = CondorPipeline(args) if args.condor else SerialPipeline(args)
+    analyzed_features = worker_pipeline.run()
+    if args.analysis_type == constants.HIERARCHICAL:
+        hierarchical_fdr(args, analyzed_features)
+    # TODO: Analyze pairwise interactions
+    args.logger.info("End anamod master pipeline")
+    return analyzed_features
+
+
+def hierarchical_fdr(args, features):
+    """Performs hierarchical FDR control on results"""
+    # FIXME: This function is currently only being used for visualization and to support legacy workflow
+    # Write FDR control input file
+    input_filename = "%s/%s" % (args.output_dir, constants.PVALUES_FILENAME)
+    with open(input_filename, "w", newline="") as outfile:
+        writer = csv.writer(outfile, delimiter=",")
+        writer.writerow([constants.NODE_NAME, constants.PARENT_NAME, constants.DESCRIPTION, constants.EFFECT_SIZE, constants.PVALUE])
+        features = list(anytree.PreOrderIter(features[0].root))  # Include all features, including dummy root
+        for node in features:
+            name = node.name
+            parent_name = node.parent.name if node.parent else ""
+            writer.writerow([name, parent_name, node.description,
+                             round_value(node.overall_effect_size), round_value(node.overall_pvalue)])
+    # Run FDR control
+    output_dir = "%s/%s" % (args.output_dir, constants.HIERARCHICAL_FDR_DIR)
+    cmd = (f"python -m anamod.fdr.hierarchical_fdr_control -output_dir {output_dir} -procedure yekutieli "
+           f"-rectangle_leaves 1 -alpha {args.importance_significance_level} {input_filename}")
+    args.logger.info("Running cmd: %s" % cmd)
+    pass_args = cmd.split()[2:]
+    with patch.object(sys, 'argv', pass_args):
+        hierarchical_fdr_control.main()
+    # TODO: update feature importance attributes based on results of hierarchical FDR control
+    # Better yet, pass features directly to hierarchical FDR control and update
+
+
+def validate_args(args):
+    """Validate arguments"""
+    if args.condor:
+        try:
+            importlib.import_module("htcondor")
+        except ModuleNotFoundError:
+            print("htcondor module not found. "
+                  "Use 'pip install htcondor' to install htcondor on a compatible platform, or "
+                  "disable condor", file=sys.stderr)
+            raise
---
+++ packages/anamod/anamod-0.1.1.tar.gz/anamod-0.1.1/tests/utils.py
@@ -0,0 +1,43 @@
+"""Utility functions for tests"""
+
+import logging
+
+from anamod.core import constants
+
+
+def setup_logfile(caplog):
+    """Set up logfile for test"""
+    caplog.set_level(logging.INFO)
+    formatting = "%(asctime)s: %(levelname)s: %(name)s: %(message)s"
+    handler = caplog.handler
+    handler.setFormatter(logging.Formatter(formatting))
+
+
+def write_logfile(caplog, output_dir):
+    """Write log file"""
+    log_filename = "{0}/test.log".format(output_dir)
+    with open(log_filename, "w") as log_file:
+        log_file.write(caplog.text)
+    caplog.clear()
+
+
+def pre_test(func_name, tmpdir, caplog):
+    """Pre-test setup"""
+    output_dir = "%s/output_dir_%s" % (tmpdir, func_name)
+    setup_logfile(caplog)
+    return output_dir
+
+
+def post_test(file_regression, caplog, output_dir, interactions=False):
+    """Post-test verification"""
+    write_logfile(caplog, output_dir)
+    summary_filename = f"{output_dir}/{constants.SIMULATION_SUMMARY_FILENAME}"
+    with open(summary_filename, "r") as summary_file:
+        summary = "".join(summary_file.readlines())
+    file_regression.check(summary, extension="_summary.json")
+    fdr_filename = "%s/%s/%s.csv" % (output_dir, constants.HIERARCHICAL_FDR_DIR, constants.HIERARCHICAL_FDR_OUTPUTS)
+    if interactions:
+        fdr_filename = "%s/%s/%s.csv" % (output_dir, constants.INTERACTIONS_FDR_DIR, constants.HIERARCHICAL_FDR_OUTPUTS)
+    with open(fdr_filename, "r") as fdr_file:
+        fdr = "".join(sorted(fdr_file.readlines()))
+    file_regression.check(fdr, extension="_fdr.csv")
