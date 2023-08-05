3933c221fb49d5ac23aa70185f368faba4cec485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:23 +0000

    Add package rs-metrics/0.2.3/rs_metrics-0.2.3-py3-none-any.whl

---
+++ packages/rs-metrics/rs_metrics-0.2.3-py3-none-any.whl/rs_metrics/parallel.py
@@ -0,0 +1,47 @@
+from multiprocessing import Pool, cpu_count
+import numpy as np
+
+
+def user_mean(func, true, pred, k):
+    y = {
+        user:
+            {
+                'true': true[user],
+                'pred': pred.get(user, list())[:k]
+            }
+        for user in true
+    }
+    with Pool(cpu_count()) as p:
+        return np.mean(p.map(func, [data for user, data in y.items()]))
+
+
+def user_mean_sub(func, true, pred, subprofiles, k, aplha):
+    pred = top_k(pred, k)
+    with Pool(cpu_count()) as p:
+        return np.mean(
+            p.starmap(
+                func,
+                [
+                    (true[user], pred.get(user, list())[:k], subprofiles[user], aplha)
+                    for user in true
+                ],
+            )
+        )
+
+
+def user_apply(func, df, pred, k, fill):
+    pred = top_k(pred, k)
+    with Pool(cpu_count()) as p:
+        return np.mean(
+            p.starmap(func, [(df, data, fill) for user, data in pred.items()])
+        )
+
+
+def top_k(pred, k):
+    with Pool(cpu_count()) as p:
+        top_items = p.starmap(get_k, [(data, k) for user, data in pred.items()])
+        return {user: top for user, top in zip(pred, top_items)}
+
+
+def get_k(l, k):
+    return l[:k]
