59fc2eae76af95e61837006aee84e51d163dd1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:39 +0000

    Add package UnCodeGL/0.0.4/UnCodeGL-0.0.4.tar.gz

---
+++ packages/UnCodeGL/UnCodeGL-0.0.4.tar.gz/UnCodeGL-0.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(name='UnCodeGL',
+      version='0.0.4',
+      description='Library for Coding Theory',
+      author= 'Gabriel Octavio Lozano Pinzon',
+      author_email="golozanop@unal.edu.co",
+      url="https://github.com/Matematikoi/UnCodeGL",
+      install_requires=[],
+      packages=find_packages(),
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6'
+      )
