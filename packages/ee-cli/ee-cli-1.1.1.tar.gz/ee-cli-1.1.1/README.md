aeede87c766293d5c2133ebc0bc93ee85493b161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:27 +0000

    Add package ee-cli/1.1.1/ee-cli-1.1.1.tar.gz

---
+++ packages/ee-cli/ee-cli-1.1.1.tar.gz/ee-cli-1.1.1/README.md
@@ -0,0 +1,47 @@
+# epoch-echo
+
+A minimal command line alternative to tools like [this](https://www.epochconverter.com).
+
+Copypasta your machine-flavored datetimes from the db or whatever into the `ee`  and ahhhh 😌 a nice human date right there for you in 0 clicks 🌚. Pass a whole long list of some ridiculous mixture of epoch timestamps and readable datetimes and witness 🙀 the grand switcharoo 🎠
+
+Built with ✨and [typer](https://github.com/tiangolo/typer).
+
+## About
+
+Born from the grumblings of a crunchy shell-dweller, `ee-cli` is designed to fit nicely in a terminal-heavy workflow with tools like [tmux](https://github.com/tmux/tmux/wiki), [hub](https://github.com/github/hub), [fzf](https://github.com/junegunn/fzf), etc. Those tools and their peers inspired `ee` greatly.
+
+`ee` stands for "epoch echo" because that's sort of what the tool does.
+
+`ee` takes any arbitrary combination of  epoch timestamps, datetimes, times, dates, etc, and prints them back as the opposite. `ee` uses [pendulum](https://pendulum.eustace.io) for these conversions.
+
+By default, `ee` supports input from the [same datetime formats](https://pendulum.eustace.io/docs/#rfc-3339) as `pendulum.parse`, plus the conveniences of "now," "tomorrow," "today," and "yeterday."
+
+More input formats can be used by setting the `EXTRA_DATETIME_INPUT_FORMATS` environment variable to a json array of [these tokens](https://pendulum.eustace.io/docs/#tokens) in pendulum
+
+## Installation
+
+This is a Python project, so you install it from pip.
+
+```shell
+pip install ee-cli
+```
+
+The executable is `ee`
+
+```shell
+ee  # display help and list commands
+```
+
+# Usage
+
+Basic: Convert a list of datetimes, output it to the clipboard.
+
+![](./ee.svg)
+
+Interactive: Use the repl to curate a list of converted datetimes, view help, send to clipboard, and more!
+
+![](./repl.svg)
+
+Images generated with [termtosvg](https://github.com/nbedos/termtosvg).
+
+See [USAGE.md](./USAGE.md) for more.
---
+++ packages/ee-cli/ee-cli-1.1.1.tar.gz/ee-cli-1.1.1/ee_cli/utils.py
@@ -0,0 +1,60 @@
+"""Utility functions. Lots of work with strings."""
+from typing import Union
+
+import pendulum
+
+from ee_cli.constants import COULD_NOT_PARSE_ERROR_MESSAGE, PENDULUM_SEMANTIC_ATTRS
+from ee_cli.settings import Settings
+
+settings = Settings()
+
+
+def _epoch_int_no_millis(epoch: Union[str, int, float]) -> int:
+    """We reject milliseconds."""
+    return int(str(epoch).split(".")[0])
+
+
+def flip_time_format(date: str, tz: str = settings.default_timezone) -> str:
+    """If it's an epoch get back a friendly date[time].
+    If it's anything else, get an epoch."""
+    try:  # passing an epoch feels like primary use case--or at lest it's mine
+        timestamp = _epoch_int_no_millis(date)
+        return (
+            pendulum.from_timestamp(timestamp, tz=tz).to_datetime_string()
+            if not settings.custom_datetime_output_format
+            else pendulum.from_timestamp(timestamp, tz=tz).format(
+                settings.custom_datetime_output_format
+            )
+        )
+    # it's not an epoch if we make it here
+    except ValueError:
+        if date in PENDULUM_SEMANTIC_ATTRS:
+            # pendulum can't parse the strings in that constant (even though it should)
+            # but they do exist as methods!
+            value: pendulum.DateTime = getattr(pendulum, date)(tz=tz)
+            return str(_epoch_int_no_millis(value.timestamp()))
+        try:
+            epoch_from_custom_format = try_parse_formats(
+                date, *settings.extra_datetime_input_formats
+            )
+            if epoch_from_custom_format is not None:
+                return epoch_from_custom_format
+            # if `exact=True` was passed to .parse, could get types w/o .timestamp()
+            # (such as Duration) but mypy can't deal :(
+            regular_epoch = pendulum.parse(date, tz=tz).timestamp()  # type: ignore
+            return str(_epoch_int_no_millis(regular_epoch))
+        except pendulum.parsing.exceptions.ParserError:
+            return COULD_NOT_PARSE_ERROR_MESSAGE.format(date=date)
+
+
+def try_parse_formats(
+    date: str, *formats: str, tz: str = settings.default_timezone
+) -> Union[str, None]:
+    """Try calling pendulum.parse with each of the supplied formats."""
+    for format in formats:
+        try:
+            timestamp = pendulum.from_format(date, format, tz=tz).timestamp()
+            return str(_epoch_int_no_millis(timestamp))
+        except ValueError:
+            continue
+    return None
