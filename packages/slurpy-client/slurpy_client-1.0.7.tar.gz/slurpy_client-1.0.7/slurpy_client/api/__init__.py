e4ec62f8240cd68ae10dc7dd8126b807b7df8a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:56 +0000

    Add package slurpy-client/1.0.7/slurpy_client-1.0.7.tar.gz

---
+++ packages/slurpy-client/slurpy_client-1.0.7.tar.gz/slurpy_client-1.0.7/slurpy_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from slurpy_client.api.email_api import EmailApi
+from slurpy_client.api.inbox_api import InboxApi
+from slurpy_client.api.jobs_api import JobsApi
