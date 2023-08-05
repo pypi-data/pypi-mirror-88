a8b21944999d28a40b011587774701fa66474c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:58 +0000

    Add package fluxengine/4.0.6/fluxengine-4.0.6-py3-none-any.whl

---
+++ packages/fluxengine/fluxengine-4.0.6-py3-none-any.whl/fluxengine-4.0.6.data/scripts/fe_tutorials.py
@@ -0,0 +1,25 @@
+#!python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 01 09:55:10 2019
+
+Launches the Jupyter notebook hub in the FluxEngine tutorials directory.
+
+@author: Tom Holding
+"""
+
+import os;
+import shutil;
+from fluxengine.core.fe_setup_tools import get_fluxengine_root;
+
+tutorialsFEDirectory = os.path.join(get_fluxengine_root(), "tutorials");
+tutorialsCopyPath = os.path.join(os.getcwd(), "FluxEngineTutorials", "fe_jupyter_notebooks");
+print("Copying tutorial files to:", tutorialsCopyPath);
+try:
+    shutil.copytree(tutorialsFEDirectory, tutorialsCopyPath);
+except FileExistsError:
+    print("*** Detected existing files at:", tutorialsCopyPath);
+    print("*** Continuing using existing files at this path.");
+
+print("Starting jupyter notebook...");
+os.system("jupyter notebook --notebook-dir="+"\""+tutorialsCopyPath+"\"");
