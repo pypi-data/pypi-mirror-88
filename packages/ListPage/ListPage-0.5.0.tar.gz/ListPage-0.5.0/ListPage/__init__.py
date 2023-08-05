8425f535b4da305dfe74ab5c3fce564200551454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:54 +0000

    Add package ListPage/0.5.0/ListPage-0.5.0.tar.gz

---
+++ packages/ListPage/ListPage-0.5.0.tar.gz/ListPage-0.5.0/ListPage/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+from .list_page import ListPageS
+from .list_page import ListPageD
+from .scrolling_page import ScrollingPage
+from .paths import Xpaths, CssPaths
+from .targets import Targets
