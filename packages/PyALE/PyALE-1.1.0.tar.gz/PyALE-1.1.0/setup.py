3f68a1493f5b71655466f8b5ef5b24b1fa81aa60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:31 +0000

    Add package PyALE/1.1.0/PyALE-1.1.0.tar.gz

---
+++ packages/PyALE/PyALE-1.1.0.tar.gz/PyALE-1.1.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyALE",
+    version="1.1.0",
+    author="Dana Jomar",
+    author_email="dana.jomar@outlook.com",
+    description="ALE plots with python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanaJomar/PyALE",
+    packages=setuptools.find_packages(exclude=["*.tests"]),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "sklearn",
+        "scipy",
+        "statsmodels",
+    ],
+    extras_require={"dev": ["coverage"],},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["ALEPlots ALE feature effect interpretable ML"],
+)
