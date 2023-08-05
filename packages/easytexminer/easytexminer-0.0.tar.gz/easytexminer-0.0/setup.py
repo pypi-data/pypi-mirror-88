6e2f80cc74fb8602acc457f37d95b99277426886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:08 +0000

    Add package easytexminer/0.0/easytexminer-0.0.tar.gz

---
+++ packages/easytexminer/easytexminer-0.0.tar.gz/easytexminer-0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='easytexminer',
+    version='0.0',
+    author='Haojie Pan,Minghui Qiu',
+    author_email='haojie.phj@alibaba-inc.com,minghui.qmh@alibaba-inc.com',
+    url='',
+    description=u'EasyTexMiner (An easy-to-use Text Mining Toolkits)',
+    packages=find_packages(),
+    install_requires=[]
+)
