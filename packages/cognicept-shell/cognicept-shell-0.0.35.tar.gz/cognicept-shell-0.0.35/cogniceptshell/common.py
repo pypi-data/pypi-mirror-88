c5905708cf21ef610b0ee0bef0aef04c652542ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:51 +0000

    Add package cognicept-shell/0.0.35/cognicept-shell-0.0.35.tar.gz

---
+++ packages/cognicept-shell/cognicept-shell-0.0.35.tar.gz/cognicept-shell-0.0.35/cogniceptshell/common.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Cognicept Systems
+# Author: Swarooph Seshadri (swarooph@cognicept.systems)
+# --> common utilities for the cognicept shell goes here.
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
