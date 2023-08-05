4aaaadabc5a8414986ac61c7bb361e8213ee288d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:35 +0000

    Add package grebble-flow/0.0.3.23/grebble-flow-0.0.3.23.tar.gz

---
+++ packages/grebble-flow/grebble-flow-0.0.3.23.tar.gz/grebble-flow-0.0.3.23/grebble_flow/grpc/service/processor.py
@@ -0,0 +1,33 @@
+import json
+
+from grebble_flow.grpc.proto import processor_pb2_grpc, processor_pb2
+from grebble_flow.helpers.converter import grebble_converter
+from grebble_flow.managment.manager import FlowManager
+
+
+class ProcessorService(processor_pb2_grpc.ProcessorServicer):
+    def __init__(self, *args, **kwargs):
+        self.flow_manager = FlowManager()
+
+    def Execute(self, request, context):
+        # get the string from the incoming request
+        flow_name = request.flowName
+        content = request.content
+        attributes = request.attributes
+
+        try:
+            content = json.loads(content)
+        except:
+            pass
+        try:
+            attributes = json.loads(attributes)
+        except:
+            pass
+        for item in self.flow_manager.run(flow_name, content, attributes):
+            yield processor_pb2.FlowResponse(
+                attributes=json.dumps(item.attributes, default=grebble_converter),
+                content=json.dumps(item.content, default=grebble_converter),
+                streamEnd=False,
+            )
+
+        yield processor_pb2.FlowResponse(data=None, attributes=None, streamEnd=True)
