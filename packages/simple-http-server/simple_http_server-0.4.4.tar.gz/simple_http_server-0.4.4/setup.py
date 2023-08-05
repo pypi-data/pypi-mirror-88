7e242b33928d1bf1ecaaff5f9de75efc4c32313b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:34 +0000

    Add package simple-http-server/0.4.4/simple_http_server-0.4.4.tar.gz

---
+++ packages/simple-http-server/simple_http_server-0.4.4.tar.gz/simple_http_server-0.4.4/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple_http_server",
+    version="0.4.4",
+    author="Keijack",
+    author_email="keijack.wu@gmail.com",
+    description="This is a simple http server, use MVC like design.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/keijack/python-simple-http-server",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
