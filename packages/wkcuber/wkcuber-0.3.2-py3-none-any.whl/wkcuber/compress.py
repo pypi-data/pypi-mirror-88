f9f41c1555f45ffcf2f688a9da1e783a97737f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:18 +0000

    Add package wkcuber/0.3.2/wkcuber-0.3.2-py3-none-any.whl

---
+++ packages/wkcuber/wkcuber-0.3.2-py3-none-any.whl/wkcuber/compress.py
@@ -0,0 +1,153 @@
+import time
+import wkw
+import shutil
+import logging
+from argparse import ArgumentParser
+from os import path, makedirs
+from uuid import uuid4
+from .mag import Mag
+
+from .utils import (
+    add_verbose_flag,
+    open_wkw,
+    WkwDatasetInfo,
+    add_distribution_flags,
+    get_executor_for_args,
+    wait_and_ensure_success,
+    setup_logging,
+)
+from .metadata import detect_resolutions, convert_element_class_to_dtype
+from typing import List
+
+BACKUP_EXT = ".bak"
+
+
+def create_parser():
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "source_path", help="Directory containing the source WKW dataset."
+    )
+
+    parser.add_argument(
+        "target_path",
+        help="Output directory for the compressed WKW dataset.",
+        nargs="?",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--layer_name",
+        "-l",
+        help="Name of the cubed layer (color or segmentation)",
+        default="color",
+    )
+
+    parser.add_argument(
+        "--mag", "-m", nargs="*", help="Magnification level", default=None
+    )
+
+    add_verbose_flag(parser)
+    add_distribution_flags(parser)
+
+    return parser
+
+
+def compress_file_job(args):
+    source_path, target_path = args
+    try:
+        logging.debug("Compressing '{}' to '{}'".format(source_path, target_path))
+        ref_time = time.time()
+
+        makedirs(path.dirname(target_path), exist_ok=True)
+        wkw.File.compress(source_path, target_path)
+
+        if not path.exists(target_path):
+            raise Exception("Did not create compressed file {}".format(target_path))
+
+        logging.debug(
+            "Compressing of '{}' took {:.8f}s".format(
+                source_path, time.time() - ref_time
+            )
+        )
+    except Exception as exc:
+        logging.error("Compressing of '{}' failed with {}".format(source_path, exc))
+        raise exc
+
+
+def compress_mag(source_path, layer_name, target_path, mag: Mag, args=None):
+    if path.exists(path.join(target_path, layer_name, str(mag))):
+        logging.error("Target path '{}' already exists".format(target_path))
+        exit(1)
+
+    if args is not None and hasattr(args, "dtype"):
+        header = wkw.Header(convert_element_class_to_dtype(args.dtype))
+    else:
+        header = None
+    source_wkw_info = WkwDatasetInfo(source_path, layer_name, mag, header)
+    target_mag_path = path.join(target_path, layer_name, str(mag))
+    logging.info("Compressing mag {0} in '{1}'".format(str(mag), target_mag_path))
+
+    with open_wkw(source_wkw_info) as source_wkw:
+        source_wkw.compress(target_mag_path)
+        with get_executor_for_args(args) as executor:
+            job_args = []
+            for file in source_wkw.list_files():
+                rel_file = path.relpath(file, source_wkw.root)
+                job_args.append((file, path.join(target_mag_path, rel_file)))
+
+            wait_and_ensure_success(
+                executor.map_to_futures(compress_file_job, job_args)
+            )
+
+    logging.info("Mag {0} successfully compressed".format(str(mag)))
+
+
+def compress_mag_inplace(target_path, layer_name, mag: Mag, args=None):
+    compress_target_path = "{}.compress-{}".format(target_path, uuid4())
+    compress_mag(target_path, layer_name, compress_target_path, mag, args)
+
+    shutil.rmtree(path.join(target_path, layer_name, str(mag)))
+    shutil.move(
+        path.join(compress_target_path, layer_name, str(mag)),
+        path.join(target_path, layer_name, str(mag)),
+    )
+    shutil.rmtree(compress_target_path)
+
+
+def compress_mags(
+    source_path, layer_name, target_path=None, mags: List[Mag] = None, args=None
+):
+    with_tmp_dir = target_path is None
+    target_path = source_path + ".tmp" if with_tmp_dir else target_path
+
+    if mags is None:
+        mags = list(detect_resolutions(source_path, layer_name))
+    mags.sort()
+
+    for mag in mags:
+        compress_mag(source_path, layer_name, target_path, mag, args)
+
+    if with_tmp_dir:
+        makedirs(path.join(source_path + BACKUP_EXT, layer_name), exist_ok=True)
+        for mag in mags:
+            shutil.move(
+                path.join(source_path, layer_name, str(mag)),
+                path.join(source_path + BACKUP_EXT, layer_name, str(mag)),
+            )
+            shutil.move(
+                path.join(target_path, layer_name, str(mag)),
+                path.join(source_path, layer_name, str(mag)),
+            )
+        shutil.rmtree(target_path)
+        logging.info(
+            "Old files are still present in '{0}.bak'. Please remove them when not required anymore.".format(
+                source_path
+            )
+        )
+
+
+if __name__ == "__main__":
+    args = create_parser().parse_args()
+    setup_logging(args)
+    compress_mags(args.source_path, args.layer_name, args.target_path, args.mag, args)
---
+++ packages/wkcuber/wkcuber-0.3.2-py3-none-any.whl/wkcuber/knossos.py
@@ -0,0 +1,103 @@
+import numpy as np
+import os
+import re
+from os import path
+from glob import iglob, glob
+
+CUBE_EDGE_LEN = 128
+CUBE_SIZE = CUBE_EDGE_LEN ** 3
+CUBE_SHAPE = (CUBE_EDGE_LEN,) * 3
+CUBE_REGEX = re.compile(r"x(\d+)/y(\d+)/z(\d+)/(.*\.raw)$")
+
+
+class KnossosDataset:
+    def __init__(self, root, dtype=np.uint8):
+        self.root = root
+        self.dtype = dtype
+
+    def read(self, offset, shape):
+        assert offset[0] % CUBE_EDGE_LEN == 0
+        assert offset[1] % CUBE_EDGE_LEN == 0
+        assert offset[2] % CUBE_EDGE_LEN == 0
+        assert shape == CUBE_SHAPE
+        return self.read_cube(tuple(x // CUBE_EDGE_LEN for x in offset))
+
+    def write(self, offset, data):
+        assert offset[0] % CUBE_EDGE_LEN == 0
+        assert offset[1] % CUBE_EDGE_LEN == 0
+        assert offset[2] % CUBE_EDGE_LEN == 0
+        assert data.shape == CUBE_SHAPE
+        self.write_cube(tuple(x // CUBE_EDGE_LEN for x in offset), data)
+
+    def read_cube(self, cube_xyz):
+        filename = self.__get_only_raw_file_path(cube_xyz)
+        if filename is None:
+            return np.zeros(CUBE_SHAPE, dtype=self.dtype)
+        with open(filename, "rb") as cube_file:
+            cube_data = np.fromfile(cube_file, dtype=self.dtype)
+            if cube_data.size != CUBE_SIZE:
+                padded_data = np.zeros(CUBE_SIZE, dtype=self.dtype)
+                padded_data[0 : min(cube_data.size, CUBE_SIZE)] = cube_data[
+                    0 : min(cube_data.size, CUBE_SIZE)
+                ]
+                cube_data = padded_data
+            cube_data = cube_data.reshape(CUBE_SHAPE, order="F")
+            return cube_data
+
+    def write_cube(self, cube_xyz, cube_data):
+        filename = self.__get_only_raw_file_path(cube_xyz)
+        if filename is None:
+            filename = path.join(
+                self.__get_cube_folder(cube_xyz), self.__get_cube_file_name(cube_xyz)
+            )
+        os.makedirs(path.dirname(filename), exist_ok=True)
+        with open(filename, "wb") as cube_file:
+            cube_data.ravel(order="F").tofile(cube_file)
+
+    def __get_cube_folder(self, cube_xyz):
+        x, y, z = cube_xyz
+        return path.join(
+            self.root, "x{:04d}".format(x), "y{:04d}".format(y), "z{:04d}".format(z)
+        )
+
+    def __get_cube_file_name(self, cube_xyz):
+        x, y, z = cube_xyz
+        return "cube_x{:04d}_y{:04d}_z{:04d}.raw".format(x, y, z)
+
+    def __get_only_raw_file_path(self, cube_xyz):
+        cube_folder = self.__get_cube_folder(cube_xyz)
+        raw_files = glob(path.join(cube_folder, "*.raw"))
+        assert len(raw_files) <= 1, "Found %d .raw files in %s" % (
+            len(raw_files),
+            cube_folder,
+        )
+        return raw_files[0] if len(raw_files) > 0 else None
+
+    def list_files(self):
+        return iglob(path.join(self.root, "**", "*.raw"), recursive=True)
+
+    def __parse_cube_file_name(self, filename):
+        m = CUBE_REGEX.search(filename)
+        if m is None:
+            return None
+        return (int(m.group(1)), int(m.group(2)), int(m.group(3)))
+
+    def list_cubes(self):
+        return (
+            f
+            for f in (self.__parse_cube_file_name(f) for f in self.list_files())
+            if f is not None
+        )
+
+    def close(self):
+        pass
+
+    @staticmethod
+    def open(root: str, dtype):
+        return KnossosDataset(root, dtype)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, _type, _value, _tb):
+        self.close()
