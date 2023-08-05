2fbeee865c42eec51a966294f2abb62203063671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:22 +0000

    Add package pypi-package-test/0.1.1/pypi_package_test-0.1.1.tar.gz

---
+++ packages/pypi-package-test/pypi_package_test-0.1.1.tar.gz/pypi_package_test-0.1.1/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pypi_package_test',
+    version='0.1.1',
+    url='https://github.com/Miss-chan/pypi_package_test',
+    install_requires=['scrapy>=1.5.1'],
+)
