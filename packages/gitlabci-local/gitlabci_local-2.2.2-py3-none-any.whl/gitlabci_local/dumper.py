e2c8e28dd4bcc393a6e14c876be9a5d175b5ffb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:07 +0000

    Add package gitlabci-local/2.2.2/gitlabci_local-2.2.2-py3-none-any.whl

---
+++ packages/gitlabci-local/gitlabci_local-2.2.2-py3-none-any.whl/gitlabci_local/dumper.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Standard libraries
+from copy import deepcopy
+from oyaml import dump as yaml_dump
+
+# Components
+from .system.platform import Platform
+from .types.lists import Lists
+
+# Dumper
+def dumper(options, jobs):
+
+    # Variables
+    configuration = dict()
+    result = False
+
+    # Prepare configuration results
+    if options.names:
+        for job in jobs:
+            if Lists.match(options.names, job, options.no_regex):
+                configuration[job] = deepcopy(jobs[job])
+                del configuration[job]['options']
+                result = True
+    else:
+        for job in jobs:
+            configuration[job] = deepcopy(jobs[job])
+            del configuration[job]['options']
+        result = True
+
+    # Dump configuration results
+    print(yaml_dump(configuration, indent=2))
+    print(' ')
+    Platform.flush()
+
+    # Result
+    return result
---
+++ packages/gitlabci-local/gitlabci_local-2.2.2-py3-none-any.whl/gitlabci_local/types/files.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Standard libraries
+from atexit import register
+from pathlib import Path
+from signal import SIGINT, signal, SIGTERM
+from tempfile import NamedTemporaryFile
+
+# Files class
+class Files:
+
+    # Members
+    registered = False
+    temps = []
+
+    # Clean
+    @staticmethod
+    def clean():
+
+        # Delete all temps
+        for temp in Files.temps:
+            Path(temp.name).unlink()
+
+        # Reset temps
+        Files.temps = []
+
+    # Temp
+    @staticmethod
+    def temp(path=None, mode='wt', newline='\n', prefix='.tmp.'):
+
+        # Create temporary file
+        temp_file = NamedTemporaryFile(delete=False, dir=path, mode=mode, newline=newline,
+                                       prefix=prefix)
+
+        # Register temporary file
+        Files.temps += [temp_file]
+
+        # Register cleanup
+        if not Files.registered:
+            register(Files.clean)
+            signal(SIGINT, Files.clean)
+            signal(SIGTERM, Files.clean)
+            Files.registered = True
+
+        # Result
+        return temp_file
