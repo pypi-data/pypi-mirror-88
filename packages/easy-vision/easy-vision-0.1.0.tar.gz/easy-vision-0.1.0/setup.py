97e78cedb26fc873382714f0c5d61d3e03b0a53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:06 +0000

    Add package easy-vision/0.1.0/easy-vision-0.1.0.tar.gz

---
+++ packages/easy-vision/easy-vision-0.1.0.tar.gz/easy-vision-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='easy-vision',
+    version='0.1.0',
+    author='Wenmengzhou',
+    author_email='wenmeng.zwm@alibaba-inc.com',
+    url='',
+    description=u'An framework for deep learning in vision, including image classification, object detection, semantic segmentation and ocr',
+    packages=find_packages(),
+    install_requires=[]
+)
