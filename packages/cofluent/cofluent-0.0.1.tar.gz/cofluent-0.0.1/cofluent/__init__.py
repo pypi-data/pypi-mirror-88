55bf1edaee586270105bc9b1fafd1764493de0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:39 +0000

    Add package cofluent/0.0.1/cofluent-0.0.1.tar.gz

---
+++ packages/cofluent/cofluent-0.0.1.tar.gz/cofluent-0.0.1/cofluent/__init__.py
@@ -0,0 +1,1 @@
+print("Hi PyPI, it is renqing.")
---
+++ packages/cofluent/cofluent-0.0.1.tar.gz/cofluent-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="cofluent", # Replace with your own username
+    version="0.0.1",
+    author="Ren Qing",
+    author_email="625101579@qq.com",
+    description="A small example package",
+    long_description="A small example package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
