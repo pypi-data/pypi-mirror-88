b3426ac2cb54b57855d6a0b31e97072a8e124366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:22 +0000

    Add package arcus-sdk/0.1.3/arcus_sdk-0.1.3-py3-none-any.whl

---
+++ packages/arcus-sdk/arcus_sdk-0.1.3-py3-none-any.whl/arcus/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@author: zhoutuo
+@license: (C) Copyright © 2018 Yitu Network Technology Co., Ltd.
+@contact: tuo.zhou@yitu-inc.com
+@software: tester
+@file: __init__.py.py
+@time: 2020/12/8 下午5:04
+@desc:
+"""
---
+++ packages/arcus-sdk/arcus_sdk-0.1.3-py3-none-any.whl/arcus/http_helper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+@author: zhoutuo
+@license: (C) Copyright © 2018 Yitu Network Technology Co., Ltd.
+@contact: tuo.zhou@yitu-inc.com
+@software: tester
+@file: http_helper.py
+@time: 2020/11/27 下午5:37
+@desc:
+"""
+import requests
+
+
+def stop_task():
+    r = requests.get('http://coordinator-service:9120/record/extra-stop')
+    return r.text
+
+
+def save_extra_data(data):
+    r = requests.post('http://collector-service:9130/report/extra', data=data)
+    return r.text
