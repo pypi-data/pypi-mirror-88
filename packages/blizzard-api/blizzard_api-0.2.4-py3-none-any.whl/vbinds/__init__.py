02e75e12728d39f6c64dce55e9079b08c9823933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:03 +0000

    Add package blizzard-api/0.2.4/blizzard_api-0.2.4-py3-none-any.whl

---
+++ packages/blizzard-api/blizzard_api-0.2.4-py3-none-any.whl/vbinds/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=1.1.1
+# hash=abaa61f9c83504a536e0839ce300e774
+# =====================================
+
+"""
+vbinds - Useful defaults and other package metadata.
+"""
+
+PKG_NAME = "blizzard-api"
+VERSION = "0.2.4"
+DESCRIPTION = "Interact with Blizard's game-data API."
---
+++ packages/blizzard-api/blizzard_api-0.2.4-py3-none-any.whl/vbinds/classes/engine.py
@@ -0,0 +1,26 @@
+
+"""
+vbinds - A collection of all implemented query extensions.
+"""
+
+# built-in
+import os
+
+# internal
+from . import DEFAULT_CACHE
+from .cache import Cache
+from .class_data_engine import ClassDataEngine
+from .spec_data_engine import SpecDataEngine
+
+
+class Engine(ClassDataEngine, SpecDataEngine):
+    """ Doesn't implement anything, just aggregates functionality. """
+
+
+def engine_from_output_root(output_dir: str) -> Engine:
+    """
+    Build a query engine by constructing a cache and calling the constructor.
+    """
+
+    output_dir = os.path.abspath(output_dir)
+    return Engine(Cache(os.path.join(output_dir, DEFAULT_CACHE)))
