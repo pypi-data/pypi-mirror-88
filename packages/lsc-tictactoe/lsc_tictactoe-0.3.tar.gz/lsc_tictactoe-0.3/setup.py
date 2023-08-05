3249d5d4fc4a9aed7cc4ac35f7d4034763732888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:05 +0000

    Add package lsc-tictactoe/0.3/lsc_tictactoe-0.3.tar.gz

---
+++ packages/lsc-tictactoe/lsc_tictactoe-0.3.tar.gz/lsc_tictactoe-0.3/setup.py
@@ -0,0 +1,16 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name = 'lsc_tictactoe',
+      version = '0.3',
+      description = 'Tic Tac Toe Game',
+      packages = ['lsc_tictactoe'],
+      author = 'Lucian Stefan Craciun',
+      author_email = 'lucyans_c@yahoo.com',
+      zip_safe = False)
