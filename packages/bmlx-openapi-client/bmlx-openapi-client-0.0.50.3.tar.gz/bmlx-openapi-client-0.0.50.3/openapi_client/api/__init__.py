a83cb55849155a0444f7eab39d39e88b9593e654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:36 +0000

    Add package bmlx-openapi-client/0.0.50.3/bmlx-openapi-client-0.0.50.3.tar.gz

---
+++ packages/bmlx-openapi-client/bmlx-openapi-client-0.0.50.3.tar.gz/bmlx-openapi-client-0.0.50.3/openapi_client/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.artifact_api import ArtifactApi
+from openapi_client.api.auth_api import AuthApi
+from openapi_client.api.component_run_api import ComponentRunApi
+from openapi_client.api.deployment_api import DeploymentApi
+from openapi_client.api.execution_event_api import ExecutionEventApi
+from openapi_client.api.experiment_api import ExperimentApi
+from openapi_client.api.experiment_run_api import ExperimentRunApi
+from openapi_client.api.group_api import GroupApi
+from openapi_client.api.model_api import ModelApi
+from openapi_client.api.model_suite_api import ModelSuiteApi
+from openapi_client.api.pipeline_api import PipelineApi
+from openapi_client.api.pipeline_version_api import PipelineVersionApi
+from openapi_client.api.user_api import UserApi
