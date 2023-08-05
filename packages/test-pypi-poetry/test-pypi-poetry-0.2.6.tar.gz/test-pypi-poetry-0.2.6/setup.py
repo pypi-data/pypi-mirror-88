65ee0dccc61436eb4d6513a1a7339dfa9c697d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:24 +0000

    Add package test-pypi-poetry/0.2.6/test-pypi-poetry-0.2.6.tar.gz

---
+++ packages/test-pypi-poetry/test-pypi-poetry-0.2.6.tar.gz/test-pypi-poetry-0.2.6/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['test-pypi-poetry']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'test-pypi-poetry',
+    'version': '0.2.6',
+    'description': 'Testing PyPi & Poetry',
+    'long_description': 'for public viewing',
+    'author': 'Christopher Farrenden',
+    'author_email': 'cfarrend@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://cfarrend.com',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
