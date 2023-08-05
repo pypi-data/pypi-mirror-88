b83049d4113f5070ef5d81598aae0c9c8fe94cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:33 +0000

    Add package testtp/0.0.1/testtp-0.0.1.tar.gz

---
+++ packages/testtp/testtp-0.0.1.tar.gz/testtp-0.0.1/testtp/__init__.py
@@ -0,0 +1,12 @@
+from .Req import Session
+from .utils import defaultdata
+__version__ = '0.0.2'
+__all__ = [
+    # "__author__",
+    # "__email__",
+    # "__license__",
+    "__version__",
+    # "__status__",
+    "Session",
+    "defaultdata",
+]
