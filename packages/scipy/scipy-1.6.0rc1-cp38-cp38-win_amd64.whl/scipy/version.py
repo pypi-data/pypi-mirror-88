334e3eb599f6240e420eda9d0e993bb6e54d0c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:05 +0000

    Add package scipy/1.6.0rc1/scipy-1.6.0rc1-cp38-cp38-win_amd64.whl

---
+++ packages/scipy/scipy-1.6.0rc1-cp38-cp38-win_amd64.whl/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.6.0rc1'
+version = '1.6.0rc1'
+full_version = '1.6.0rc1'
+git_revision = '2886c7f860cc2da867b30ca346d88d15fc0d2fc5'
+release = True
+
+if not release:
+    version = full_version
