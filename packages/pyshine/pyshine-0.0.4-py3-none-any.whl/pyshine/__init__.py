2ee7763aefc4c887a797446511755becf024d8c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:49 +0000

    Add package pyshine/0.0.4/pyshine-0.0.4-py3-none-any.whl

---
+++ packages/pyshine/pyshine-0.0.4-py3-none-any.whl/pyshine/__init__.py
@@ -0,0 +1,9 @@
+# author:	PyShine
+# website:	http://www.pyshine.com
+
+# set the version number
+__version__= "0.0.4"
+
+# import the necessary packages
+from .convenience import putBText
+from .convenience import audioCapture
