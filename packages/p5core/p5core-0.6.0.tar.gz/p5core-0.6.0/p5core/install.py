e00c32572a5184639873cae3b0228c044e2cb3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:29 +0000

    Add package p5core/0.6.0/p5core-0.6.0.tar.gz

---
+++ packages/p5core/p5core-0.6.0.tar.gz/p5core-0.6.0/p5core/install.py
@@ -0,0 +1,27 @@
+"""Root script for CI."""
+
+import importlib.resources as pkg_resources
+import os
+from pathlib import Path
+
+import p5core
+import simplelogging
+from p5core.install_helpers import copy_file
+
+workspace = os.environ.get("GITHUB_WORKSPACE")
+if workspace:
+    workspace = Path(workspace)
+else:
+    workspace = Path(".")
+
+log = simplelogging.get_logger()
+log.full_logging()
+
+
+def install():
+    """Installation entry point."""
+    p5core.install_helpers.installation_directory = workspace
+
+    # TODO copy gitignore !!!
+    with pkg_resources.path(p5core, "README_user.md") as file_path:
+        copy_file(file_path, Path("README.md"))
---
+++ packages/p5core/p5core-0.6.0.tar.gz/p5core-0.6.0/p5core/install_helpers.py
@@ -0,0 +1,52 @@
+"""Helpers for the installation procedure."""
+import shutil
+from pathlib import Path
+
+from p5core.html import Tag
+import simplelogging
+
+log = simplelogging.get_logger()
+
+installation_directory: Path = Path(".")
+source_directory: Path = Path(".")
+
+
+def write_html(tag: Tag, path: Path) -> None:
+    """Write an HTML tag in a file.
+
+    Args:
+        tag (Tag): HTML tag to write
+        path (Path): path of the file to write
+
+    Raises:
+        TypeError: path is not a pathlib.Path
+    """
+    if not isinstance(path, Path):
+        raise TypeError("path is not a pathlib.Path")
+    with open(path, "w", encoding="utf-8") as writer:
+        writer.write(str(tag))
+
+
+def copy_file(src: Path, dst: Path) -> None:
+    """Copy a file in another file.
+
+    Args:
+        src (Path): source file
+        dst (Path): destination file
+
+    Raises:
+        TypeError: Provided paths are not pathlib.Path
+        ValueError: Provided destination path is not relative to installation directory
+    """
+    log.debug("copy %s to %s", str(src), str(dst))
+    if not isinstance(src, Path):
+        raise TypeError("src is not a pathlib.Path")
+
+    if not isinstance(dst, Path):
+        raise TypeError("dst is not a pathlib.Path")
+    elif dst.is_absolute():
+        raise ValueError("Destination path must be relative to installation directory")
+
+    destination = installation_directory / dst
+    destination.parent.mkdir(parents=True, exist_ok=True)
+    shutil.copy(source_directory / src, destination)
