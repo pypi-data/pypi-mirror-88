e99d037a84d838642add9f79b051f09b6102d0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:07 +0000

    Add package symphony-of-nirfolio-py2sql/0.0.4/symphony_of_nirfolio_py2sql-0.0.4.tar.gz

---
+++ packages/symphony-of-nirfolio-py2sql/symphony_of_nirfolio_py2sql-0.0.4.tar.gz/symphony_of_nirfolio_py2sql-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name="symphony_of_nirfolio_py2sql",
+    version="0.0.4",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    install_requires=["mysql.connector"],
+    url="",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
