ba4d434dcb54c368753fc3244803d73644ea9df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:27 +0000

    Add package pyartifactory/1.8.2/PyArtifactory-1.8.2.tar.gz

---
+++ packages/pyartifactory/PyArtifactory-1.8.2.tar.gz/PyArtifactory-1.8.2/pyartifactory/models/__init__.py
@@ -0,0 +1,33 @@
+"""
+Import all models here.
+"""
+from typing import Union, Type
+
+from .auth import AuthModel, ApiKeyModel, PasswordModel, AccessTokenModel
+from .group import Group, SimpleGroup
+from .user import NewUser, UserResponse, BaseUserModel, SimpleUser, User
+from .repository import (
+    LocalRepository,
+    LocalRepositoryResponse,
+    VirtualRepository,
+    VirtualRepositoryResponse,
+    RemoteRepository,
+    RemoteRepositoryResponse,
+    SimpleRepository,
+)
+
+from .artifact import (
+    ArtifactPropertiesResponse,
+    ArtifactStatsResponse,
+    ArtifactFileInfoResponse,
+    ArtifactFolderInfoResponse,
+    ArtifactInfoResponse,
+)
+from .permission import Permission, SimplePermission
+
+
+AnyRepositoryResponse = Union[
+    LocalRepositoryResponse, VirtualRepositoryResponse, RemoteRepositoryResponse
+]
+
+AnyRepository = Union[LocalRepository, VirtualRepository, RemoteRepository]
