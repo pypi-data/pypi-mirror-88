7f31dd565b712c40f050db1dc44927d878a3220e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:29 +0000

    Add package agilicus/1.82.0/agilicus-1.82.0.tar.gz

---
+++ packages/agilicus/agilicus-1.82.0.tar.gz/agilicus-1.82.0/agilicus/output/json.py
@@ -0,0 +1,26 @@
+import datetime
+import json
+
+
+# Allows us to customize the output based on the context.
+def jsonify(ctx, entry):
+    return entry
+
+
+def json_output_default(o):
+    if isinstance(o, (datetime.date, datetime.datetime)):
+        return o.isoformat()
+    return None
+
+
+def output_json(ctx, entry):
+    print(
+        json.dumps(
+            jsonify(ctx, entry), sort_keys=True, indent=2, default=json_output_default
+        )
+    )
+
+
+def output_json_to_file(ctx, doc, outfile):
+    with open(outfile, "w", encoding="utf-8") as f:
+        json.dump(doc, f, ensure_ascii=False, default=json_output_default, indent=4)
