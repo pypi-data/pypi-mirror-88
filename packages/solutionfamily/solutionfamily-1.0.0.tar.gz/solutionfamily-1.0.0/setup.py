662cebea3d4e08f717fc884e24292c213752b1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:04 +0000

    Add package solutionfamily/1.0.0/solutionfamily-1.0.0.tar.gz

---
+++ packages/solutionfamily/solutionfamily-1.0.0.tar.gz/solutionfamily-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='solutionfamily',
+    packages=find_packages(include='solutionfamily'),
+    install_requires=['lxml', 'requests', 'python-dateutil'],
+    version='1.0.0',
+    description='A Python SDK for interacting with SolutionFamily\'s Solution Engine.',
+    long_description='A Python SDK for interacting with SolutionFamily\'s Solution Engine.',
+    long_description_content_type='text/x-rst',
+    url='https://github.com/SolutionFamily/engine-python-sdk',
+    download_url='https://github.com/SolutionFamily/engine-python-sdk/dist/v1.0.0.tar.gz',
+    keywords=['SolutionFamily', 'solutionengine' 'iot', 'sdk'],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],    
+    maintainer='Chris Tacke',
+    maintainer_email='chris.tacke@solution-family.com'
+)
