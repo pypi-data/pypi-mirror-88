0f0a99092a408094c5247cb064e5ac93cf9fb87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:34 +0000

    Add package streamlit-discourse/0.1.1/streamlit_discourse-0.1.1-py3-none-any.whl

---
+++ packages/streamlit-discourse/streamlit_discourse-0.1.1-py3-none-any.whl/streamlit_discourse/frontend/build/precache-manifest.287755ddd0db0974cb94dbadcec1b08a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b46f1a36fdf4bc60f1bc0adb2ac79c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "02bf80d6d55496bc71c0",
+    "url": "./static/js/2.3ed73009.chunk.js"
+  },
+  {
+    "revision": "9bf7777849ff7c0e7b1f4ddb70e6eb95",
+    "url": "./static/js/2.3ed73009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4068090fb3c40c1b3728",
+    "url": "./static/js/main.0cf84724.chunk.js"
+  },
+  {
+    "revision": "09123bb34e25eabec119",
+    "url": "./static/js/runtime-main.9bcd1319.js"
+  }
+]);
---
+++ packages/streamlit-discourse/streamlit_discourse-0.1.1-py3-none-any.whl/streamlit_discourse/frontend/build/precache-manifest.e75ed8bd4d9e16bdc313b9a40a302823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4167b824a52bd83515f08fc2040eb162",
+    "url": "./index.html"
+  },
+  {
+    "revision": "02bf80d6d55496bc71c0",
+    "url": "./static/js/2.3ed73009.chunk.js"
+  },
+  {
+    "revision": "9bf7777849ff7c0e7b1f4ddb70e6eb95",
+    "url": "./static/js/2.3ed73009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e631ca3acdf7c288702b",
+    "url": "./static/js/main.96338b92.chunk.js"
+  },
+  {
+    "revision": "09123bb34e25eabec119",
+    "url": "./static/js/runtime-main.9bcd1319.js"
+  }
+]);
