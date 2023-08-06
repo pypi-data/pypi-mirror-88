c2d24add6f8a799f3109b6191223511b67b8262c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:29 +0000

    Add package celebalucid/0.1.5.1/celebalucid-0.1.5.1.tar.gz

---
+++ packages/celebalucid/celebalucid-0.1.5.1.tar.gz/celebalucid-0.1.5.1/celebalucid/__init__.py
@@ -0,0 +1,20 @@
+import os
+from celebalucid.models.manipulator import ModelManipulator
+
+__version__ = "0.1.5.1"
+__author__ = 'Gergely Papp'
+__credits__ = 'Alfred Renyi Institute of Mathematics'
+
+base_url = 'https://users.renyi.hu/~gergopool/celebalucid/models/'
+
+def load_model(str_model):
+    url = os.path.join(base_url, str_model+'.pt')
+    return ModelManipulator(url)
+
+from celebalucid.generator import build_generator
+from celebalucid.cka import CKA
+
+
+
+
+
