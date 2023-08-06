f156890a03676d96acd451c97fa7cc6e415f8771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:10 +0000

    Add package SSO/0.0.2.R/SSO-0.0.2.R.tar.gz

---
+++ packages/SSO/SSO-0.0.2.R.tar.gz/SSO-0.0.2.R/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name = 'SSO',
+    packages = ['SSO','Algorithm'],
+    scripts = [],
+    version = '0.0.2.R',
+    description = 'Simplied Swarm Optimization',
+    long_description = "# SSO \
+**Simplified Swarm Optimization** is proposed by Yeh in 2009 [1].\
+It is one of the novel swarm intelligence algorithms.\
+It features simplicity, efficiency and flexibility for modification.\
+## Documentation\
+[https://github.com/RoyTseng17/SSO/blob/main/README.md](https://)\
+## Visualization Video\
+[https://www.youtube.com/watch?\
+v=_tXNuU4KumM&feature=youtu.be&ab_channel=%E6%9B%BE%E5%86%A0%E7%A8%8B](https://)",
+    author = 'Roy Tseng',
+    author_email = 'stu121645162002@gmail.com',
+    url = 'https://github.com/RoyTseng17/SSO',
+    download_url = 'https://github.com/RoyTseng17/SSO.git',
+    keywords = ['pypi'],
+    classifiers = []
+)
