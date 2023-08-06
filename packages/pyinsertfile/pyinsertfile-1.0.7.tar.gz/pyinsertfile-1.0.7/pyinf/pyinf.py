4bb7dc2af4605c1f62ff4ad3524517177690a73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:57 +0000

    Add package pyinsertfile/1.0.7/pyinsertfile-1.0.7.tar.gz

---
+++ packages/pyinsertfile/pyinsertfile-1.0.7.tar.gz/pyinsertfile-1.0.7/pyinf/pyinf.py
@@ -0,0 +1,19 @@
+import argparse 
+
+def main(): 
+
+    parser = argparse.ArgumentParser(prog ='pyinf', description ='easy file inserting') 
+
+    #parser.add_argument('mode',help='(pypi) for pypi package setup', required = False)
+    parser.add_argument('mode', action='store', type=str)
+
+    args = parser.parse_args()
+
+    if parser.mode == "pypi":
+        print("demo")
+    
+    f = open("main.py","w+")
+
+    f.write("""
+    print("PyInsertFile")
+    """)
