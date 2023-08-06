2c3f72c008eaa87aa3235e2b7421cd44ed4421d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:48 +0000

    Add package pycourselet/0.2.1/pycourselet-0.2.1.tar.gz

---
+++ packages/pycourselet/pycourselet-0.2.1.tar.gz/pycourselet-0.2.1/src/pycourselet/contexts/math_context.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+from .context import NeedSettings
+from .text_context import TextContext
+
+
+class MathTextContext(TextContext):
+    def __init__(self, mode: str = 'tex', **kwargs):
+        super().__init__(type='math', **kwargs)
+        self.mode = mode
+
+    @staticmethod
+    def need() -> Optional[NeedSettings]:
+        from .paragraph_context import ParagraphContext
+        from .table_contexts import TableBlockContext
+
+        return NeedSettings(ParagraphContext, TableBlockContext)
