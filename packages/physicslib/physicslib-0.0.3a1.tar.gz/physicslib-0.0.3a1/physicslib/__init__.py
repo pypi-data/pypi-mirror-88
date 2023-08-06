ffdc6818dddec59c46a6e803036a1336cd081eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:23 +0000

    Add package physicslib/0.0.3a1/physicslib-0.0.3a1.tar.gz

---
+++ packages/physicslib/physicslib-0.0.3a1.tar.gz/physicslib-0.0.3a1/physicslib/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module for working with physics objects.
+
+@copyright NamorNiradnug 2019, MIT Licence
+"""
+
+from .dimension import Dimension
+from .unit import Unit
+from .vector import Vector
+from .physical import Physical, VectorPhysical
