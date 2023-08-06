05f317db397970124ea0eaf872c0385a6a317c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:31 +0000

    Add package guess-the-number/1.0/guess___the___number-1.0.tar.gz

---
+++ packages/guess-the-number/guess___the___number-1.0.tar.gz/guess___the___number-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='guess___the___number',
+    version='1.0',
+    description='A simple guess the number game',
+    author='Sujal Goel',
+    author_email='gglebot.helper@gmail.com',
+    py_modules=['gtn'],
+    license='MIT',
+    url='https://github.com/sujalgoel/Guess-the-number',
+    long_description='A guess the number game built on python 3.8. import gtn'
+)
