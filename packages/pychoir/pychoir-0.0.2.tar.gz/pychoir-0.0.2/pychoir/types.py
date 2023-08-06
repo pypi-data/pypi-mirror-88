f0731e24498bec242d2f52eeb54739db9fccd026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:58 +0000

    Add package pychoir/0.0.2/pychoir-0.0.2.tar.gz

---
+++ packages/pychoir/pychoir-0.0.2.tar.gz/pychoir-0.0.2/pychoir/types.py
@@ -0,0 +1,18 @@
+from typing import Any, Type
+
+from pychoir.core import Matcher
+
+
+class IsInstance(Matcher):
+    def __init__(self, type_: Type[Any]):
+        super().__init__()
+        self.type = type_
+
+    def _matches(self, other: Any) -> bool:
+        return isinstance(other, self.type)
+
+    def _description(self) -> str:
+        return self.type.__name__
+
+
+OfType = IsInstance
