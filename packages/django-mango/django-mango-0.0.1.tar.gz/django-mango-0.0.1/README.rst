9dc466ee1d607a17cb8894b5c8643b0f8e5d3abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:07 +0000

    Add package django-mango/0.0.1/django-mango-0.0.1.tar.gz

---
+++ packages/django-mango/django-mango-0.0.1.tar.gz/django-mango-0.0.1/README.rst
@@ -0,0 +1,6 @@
+Mango
+=======
+
+Mango is a reusable Django app that provides a text messaging framework
+for ecological momentary assessment (EMA), also known as 
+experience sampling, using Twilio.
---
+++ packages/django-mango/django-mango-0.0.1.tar.gz/django-mango-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='django-mango',
+    version='0.0.1',
+    description='A momentary assessment app for Django',
+    long_description='Mango is a reusable Django app that provides an SMS ecological momentary assessment framework through Twilio.',
+    url='https://github.com/DynamicGenetics/Mango',
+    author='Nina Di Cara, Valerio Maggio',
+    author_email='nina.dicara@bristol.ac.uk',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
