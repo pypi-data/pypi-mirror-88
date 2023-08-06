292ef9c17a154be3a989ab81a8ad8ad2f9ba25e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:18 +0000

    Add package gaussian-binominal-distributions/0.1/gaussian_binominal_distributions-0.1.tar.gz

---
+++ packages/gaussian-binominal-distributions/gaussian_binominal_distributions-0.1.tar.gz/gaussian_binominal_distributions-0.1/l0ltestpypi/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binominal_probability4nano',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian_binominal_probability4nano'],
+      author = 'L Liu',
+      author_email = 'myemail@yahoo.com',
+    zip_safe=False)
---
+++ packages/gaussian-binominal-distributions/gaussian_binominal_distributions-0.1.tar.gz/gaussian_binominal_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussian_binominal_distributions',
+      version='0.1',
+      description='Gaussian & Binominal distributions',
+      packages=['l0ltestpypi'],
+      zip_safe=False)
