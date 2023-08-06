5fa2d9666bec27be26525210a4961b8718b39560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:01 +0000

    Add package DmaPlotter/0.1.6/DmaPlotter-0.1.6.tar.gz

---
+++ packages/DmaPlotter/DmaPlotter-0.1.6.tar.gz/DmaPlotter-0.1.6/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+setup(
+    name='DmaPlotter',
+
+    version='0.1.6',
+
+    description='A one-function tool for visualizing metrics by DMA on a US map',
+
+    author='Bart Massi', 
+
+    classifiers=[  
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+
+    install_requires=[
+    'requests',
+    'shapely'],
+
+    py_modules=["DmaPlotter"]
+
+)
