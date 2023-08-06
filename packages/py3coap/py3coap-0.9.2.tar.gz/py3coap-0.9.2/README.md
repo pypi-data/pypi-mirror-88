6dc747d42c78b58750c11a7c4ca5ec670f1945d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:18 +0000

    Add package py3coap/0.9.2/py3coap-0.9.2.tar.gz

---
+++ packages/py3coap/py3coap-0.9.2.tar.gz/py3coap-0.9.2/README.md
@@ -0,0 +1,32 @@
+[![Build Status](https://dev.azure.com/moroen/PyCOAP/_apis/build/status/pycoap?branchName=master)](https://dev.azure.com/moroen/PyCOAP/_build/latest?definitionId=1&branchName=master)
+
+# PyCOAP
+
+A python extension module written in GOLANG to provide basic COAP-capabilities, including support for DTLS.
+
+
+## Requirements
+To  compile and install PyCOAP you need:
+
+### golang-compiler and python developement packages
+
+```shell
+  $ sudo apt install golang python3 python3-dev
+```
+
+### Updated pip and setuptools
+
+```shell
+  $ sudo pip3 install -U pip setuptools
+```
+
+## Installation
+
+```shell
+  $ git clone https://github.com/moroen/pycoap.git
+  $ cd pycoap
+  $ sudo python3 setup.py install
+```
+
+## Usage
+See included pycoap-client for a small example
---
+++ packages/py3coap/py3coap-0.9.2.tar.gz/py3coap-0.9.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import Extension
+from setuptools import setup
+
+
+setup(
+    name="py3coap",
+    description="A GOLANG python extension for COAP/COAPS-requests",
+    url="https://github.com/moroen/pycoap",
+    version="0.9.2",
+    author="moroen",
+    author_email="no@email.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    ext_modules=[Extension("_py3coap", ["src/py3coap/py3coap.go"])],
+    data_files=[('src/py3coap', ['src/py3coap/py3coap.h', 'src/py3coap/py3coap.c'])],
+    packages=["py3coap"],
+    build_golang={"root": "github.com/moroen/py3coap"},
+    setup_requires=["setuptools-golang"],
+    scripts=["pycoap-client"],
+)
