e8af045d639371a1592c90d767101e8f994c73d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:07 +0000

    Add package dagster-gcp/0.9.22/dagster-gcp-0.9.22.tar.gz

---
+++ packages/dagster-gcp/dagster-gcp-0.9.22.tar.gz/dagster-gcp-0.9.22/dagster_gcp/gcs/__init__.py
@@ -0,0 +1,7 @@
+from .compute_log_manager import GCSComputeLogManager
+from .resources import gcs_resource
+from .system_storage import (
+    gcs_intermediate_storage,
+    gcs_plus_default_intermediate_storage_defs,
+    gcs_system_storage,
+)
