484cdc4b95cfa778d994d531a6d43a1e50296e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:18 +0000

    Add package qtoggleserver/0.23.0-beta.11/qtoggleserver-0.23.0-beta.11.tar.gz

---
+++ packages/qtoggleserver/qtoggleserver-0.23.0-beta.11.tar.gz/qtoggleserver-0.23.0-beta.11/qtoggleserver/core/api/funcs/__init__.py
@@ -0,0 +1,8 @@
+
+from .backup import *
+from .device import *
+from .firmware import *
+from .ports import *
+from .reverse import *
+from .various import *
+from .webhooks import *
---
+++ packages/qtoggleserver/qtoggleserver-0.23.0-beta.11.tar.gz/qtoggleserver-0.23.0-beta.11/qtoggleserver/version.py
@@ -0,0 +1,3 @@
+
+VERSION = '0.23.0-beta.11'
+VENDOR = 'qtoggle/qtoggleserver'
