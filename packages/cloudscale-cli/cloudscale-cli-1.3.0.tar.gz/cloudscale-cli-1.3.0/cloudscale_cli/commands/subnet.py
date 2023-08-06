f90a3dbd68fa82b73200d20ed187ef2acf060cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:32 +0000

    Add package cloudscale-cli/1.3.0/cloudscale-cli-1.3.0.tar.gz

---
+++ packages/cloudscale-cli/cloudscale-cli-1.3.0.tar.gz/cloudscale-cli-1.3.0/cloudscale_cli/commands/subnet.py
@@ -0,0 +1,82 @@
+import click
+
+@click.group()
+@click.pass_context
+def subnet(ctx):
+    ctx.obj.cloud_resource_name = "subnet"
+    ctx.obj.headers = [
+        'uuid',
+        'cidr',
+        'network',
+        'gateway_address',
+        'dns_servers',
+        'tags',
+    ]
+    ctx.obj.resource_name_key = None
+    ctx.obj.resource_table_sort_key = 'cidr'
+
+@click.option('--filter-tag')
+@click.option('--filter-json')
+@click.option('--delete', is_flag=True)
+@click.option('--force', is_flag=True)
+@subnet.command("list")
+@click.pass_obj
+def cmd_list(cloudscale, filter_tag, filter_json, delete, force):
+    cloudscale.cmd_list(
+        filter_tag=filter_tag,
+        filter_json=filter_json,
+        delete=delete,
+        force=force,
+    )
+
+@click.argument('uuid', required=True)
+@subnet.command("show")
+@click.pass_obj
+def cmd_show(cloudscale, uuid):
+    cloudscale.cmd_show(
+        uuid=uuid,
+    )
+
+@click.option('--cidr', required=True)
+@click.option('--network-uuid', required=True)
+@click.option('--gateway-address')
+@click.option('--dns-server', 'dns_servers', multiple=True)
+@click.option('--tag', 'tags', multiple=True)
+@subnet.command("create")
+@click.pass_obj
+def cmd_create(cloudscale, cidr, network_uuid, gateway_address, dns_servers, tags):
+    cloudscale.cmd_create(
+        cidr=cidr,
+        network_uuid=network_uuid,
+        gateway_address=gateway_address,
+        dns_servers=dns_servers or None,
+        tags=tags,
+    )
+
+@click.argument('uuid', required=True)
+@click.option('--gateway-address')
+@click.option('--dns-server', 'dns_servers', multiple=True)
+@click.option('--tag', 'tags', multiple=True)
+@click.option('--clear-tag', 'clear_tags', multiple=True)
+@click.option('--clear-all-tags', is_flag=True)
+@subnet.command("update")
+@click.pass_obj
+def cmd_update(cloudscale, uuid, gateway_address, dns_servers, tags, clear_tags, clear_all_tags):
+    cloudscale.cmd_update(
+        uuid=uuid,
+        gateway_address=gateway_address,
+        dns_servers=dns_servers or None,
+        tags=tags,
+        clear_tags=clear_tags,
+        clear_all_tags=clear_all_tags,
+    )
+
+@click.argument('uuid', required=True)
+@click.option('--force', is_flag=True)
+@subnet.command("delete")
+@click.pass_obj
+def cmd_delete(cloudscale, uuid, force):
+    cloudscale.cmd_delete(
+        uuid=uuid,
+        force=force,
+    )
---
+++ packages/cloudscale-cli/cloudscale-cli-1.3.0.tar.gz/cloudscale-cli-1.3.0/tests/test_subnet.py
@@ -0,0 +1,231 @@
+from cloudscale import CLOUDSCALE_API_URL
+from cloudscale_cli.cli import cli
+import responses
+import click
+from click.testing import CliRunner
+
+SUBNET_RESP = {
+    "href": "https://api.cloudscale.ch/v1/subnets/33333333-1864-4608-853a-0771b6885a3a",
+    "uuid": "33333333-1864-4608-853a-0771b6885a3a",
+    "cidr": "192.0.2.123/24",
+    "network": {
+        "href": "https://api.cloudscale.ch/v1/networks/2db69ba3-1864-4608-853a-0771b6885a3a",
+        "uuid": "2db69ba3-1864-4608-853a-0771b6885a3a",
+        "name": "my-network-name",
+    },
+    "gateway_address": None,
+    "dns_servers": ["185.79.232.101", "185.79.232.102"],
+    "tags": {}
+}
+
+@responses.activate
+def test_subnet_get_all():
+    uuid = "33333333-1864-4608-853a-0771b6885a3a"
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_URL + '/subnets',
+        json=[SUBNET_RESP],
+        status=200)
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_URL + '/subnets',
+        json={},
+        status=500)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '-a',
+        'token',
+        'subnet',
+        'list',
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(cli, [
+        '-a',
+        'token',
+        'subnet',
+        'list',
+    ])
+    assert result.exit_code > 0
+
+@responses.activate
+def test_subnet_get_by_uuid():
+    uuid = "33333333-1864-4608-853a-0771b6885a3a"
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_URL + '/subnets/' + uuid,
+        json=SUBNET_RESP,
+        status=200)
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_URL + '/subnets/' + uuid,
+        json={},
+        status=500)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'show',
+        uuid,
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'show',
+        uuid,
+    ])
+    assert result.exit_code > 0
+
+def test_subnet_missing_api_key():
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        'subnet',
+        'list',
+    ])
+    assert result.exit_code == 1
+
+@responses.activate
+def test_subnets_delete():
+    uuid = "33333333-1864-4608-853a-0771b6885a3a"
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_URL + '/subnets/' + uuid,
+        json=SUBNET_RESP,
+        status=200)
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_URL + '/subnets/unknown',
+        json=SUBNET_RESP,
+        status=200)
+    responses.add(
+        responses.DELETE,
+        CLOUDSCALE_API_URL + '/subnets/' + uuid,
+        status=204)
+    responses.add(
+        responses.DELETE,
+        CLOUDSCALE_API_URL + '/subnets/unknown',
+        json={
+            "detail": "Not found."
+        },
+        status=404)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'delete',
+        uuid,
+    ])
+    assert result.exit_code == 1
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'delete',
+        '--force',
+        uuid,
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'delete',
+        '--force',
+        'unknown',
+    ])
+    assert result.exit_code > 0
+
+@responses.activate
+def test_subnets_create():
+    cidr = "192.0.2.123/24"
+    network_uuid = "2db69ba3-1864-4608-853a-0771b6885a3a"
+    dns_server1 = "185.79.232.101"
+    dns_server2 = "185.79.232.102"
+    responses.add(
+        responses.POST,
+        CLOUDSCALE_API_URL + '/subnets',
+        json=SUBNET_RESP,
+        status=201)
+    responses.add(
+        responses.POST,
+        CLOUDSCALE_API_URL + '/subnets',
+        json=SUBNET_RESP,
+        status=500)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'create',
+        '--cidr',
+        cidr,
+        '--network-uuid',
+        network_uuid,
+        '--dns-server',
+        dns_server1,
+        '--dns-server',
+        dns_server2,
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'create',
+        '--cidr',
+        cidr,
+        '--network-uuid',
+        network_uuid,
+        '--dns-server',
+        dns_server1,
+        '--dns-server',
+        dns_server2,
+    ])
+    assert result.exit_code > 0
+
+@responses.activate
+def test_subnets_update():
+    uuid = "33333333-1864-4608-853a-0771b6885a3a"
+    dns_server1 = "185.79.232.101"
+    dns_server2 = "185.79.232.102"
+    responses.add(
+        responses.PATCH,
+        CLOUDSCALE_API_URL + '/subnets/' + uuid,
+        json=SUBNET_RESP,
+        status=204)
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_URL + '/subnets/' + uuid,
+        json=SUBNET_RESP,
+        status=200)
+    responses.add(
+        responses.PATCH,
+        CLOUDSCALE_API_URL + '/subnets/' + uuid,
+        json={},
+        status=500)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'update',
+        '--dns-server',
+        dns_server1,
+        '--dns-server',
+        dns_server2,
+        uuid,
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'update',
+        '--dns-server',
+        dns_server1,
+        '--dns-server',
+        dns_server2,
+        '--clear-tag',
+        'foo',
+        uuid,
+    ])
+    assert result.exit_code > 0
