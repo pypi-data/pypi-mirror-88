9b8adde909cbba72b600ffce65666c27ad9f0dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:56 +0000

    Add package librec-auto/0.1.56/librec-auto-0.1.56.tar.gz

---
+++ packages/librec-auto/librec-auto-0.1.56.tar.gz/librec-auto-0.1.56/librec_auto/core/cmd/status_cmd.py
@@ -0,0 +1,32 @@
+from librec_auto.core.cmd import Cmd
+from librec_auto.core.util import Files
+from librec_auto.core import ConfigCmd
+from librec_auto.core.util import LogFile, Status
+import sys
+import subprocess
+
+
+class StatusCmd(Cmd):
+    def __str__(self):
+        return f"StatusCmd()"
+
+    def setup(self, args):
+        pass
+
+    def dry_run(self, config):
+        print(f'librec-auto (DR): Running status command {self}')
+
+    def execute(self, config: ConfigCmd):
+        self.status = Cmd.STATUS_INPROC
+        files = config.get_files()
+        target = files.get_study_path()
+        #        result_path = files.get_result_path()
+
+        if files.get_exp_count() == 0:
+            print("librec-auto: No experiments found.")
+        else:
+            for exp_paths in files.get_exp_paths_iterator():
+                status = Status(exp_paths)
+                print(status)
+
+        self.status = Cmd.STATUS_COMPLETE
---
+++ packages/librec-auto/librec-auto-0.1.56.tar.gz/librec-auto-0.1.56/librec_auto/core/util/utils.py
@@ -0,0 +1,111 @@
+from lxml import etree
+from inspect import getsourcefile
+from os.path import abspath
+from pathlib import Path
+import logging
+from . import xml_utils
+
+
+def force_list(item):
+    """
+    Ensures that an item is of list type.
+
+    :param item:
+    :return: item or [item]
+    """
+    if (type(item) is list):
+        return item
+    else:
+        return [item]
+
+
+def force_path(item):
+    """
+    Ensures that an item is of Path type.
+
+    :param item:
+    :return: item or [item]
+    """
+    if (type(item) is Path):
+        return item
+    else:
+        return Path(item)
+
+
+def frange(start, stop, step):
+    x = start
+    while x < stop:
+        yield x
+        x += step
+
+
+def confirm(prompt=None, resp=False):
+    """prompts for yes or no response from the user. Returns True for yes and
+    False for no.
+
+    'resp' should be set to the default value assumed by the caller when
+    user simply types ENTER.
+
+    >>> confirm(prompt='Create Directory?', resp=True)
+    Create Directory? [y]|n:
+    True
+    >>> confirm(prompt='Create Directory?', resp=False)
+    Create Directory? [n]|y:
+    False
+    >>> confirm(prompt='Create Directory?', resp=False)
+    Create Directory? [n]|y: y
+    True
+
+    """
+
+    if prompt is None:
+        prompt = 'Confirm'
+
+    if resp:
+        prompt = '%s [%s]|%s: ' % (prompt, 'y', 'n')
+    else:
+        prompt = '%s [%s]|%s: ' % (prompt, 'n', 'y')
+
+    while True:
+        # ans = raw_input(prompt)
+        ans = input(prompt)
+        if not ans:
+            return resp
+        if ans not in ['y', 'Y', 'n', 'N']:
+            print('please enter y or n.')
+            continue
+        if ans == 'y' or ans == 'Y':
+            return True
+        if ans == 'n' or ans == 'N':
+            return False
+
+
+def get_script_path(script_xml, cmd_type):
+    script_path = '.'
+    if script_xml.get('lang') != 'python3':
+        print(
+            f'librec-auto: Only Python3 scripts currently supported. Got {script_xml.get("lang")}.'
+        )
+        return None
+    if script_xml.get('src'):
+        if script_xml.get('src') == 'system':
+            script_path = Path(abspath(
+                getsourcefile(lambda: 0))).parent.parent / 'cmd' / cmd_type
+        else:
+            script_path = force_path(script_xml.get('src'))
+    name_elem = xml_utils.single_xpath(script_xml, 'script-name')
+    if name_elem is not None:
+        return script_path / name_elem.text
+    else:
+        return None
+
+
+def create_param_spec(script_xml):
+    params = script_xml.xpath('param')
+    param_list = []
+    for param in params:
+        key = param.get('name')
+        if param.text:
+            val = param.text
+            param_list.append(f'--{key}={val}')
+    return param_list
