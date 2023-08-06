b4a3274f1837489b12b862658bb554f93cf4ea8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:00 +0000

    Add package celery-dispatcher/1.0.3/celery-dispatcher-1.0.3.tar.gz

---
+++ packages/celery-dispatcher/celery-dispatcher-1.0.3.tar.gz/celery-dispatcher-1.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='celery-dispatcher',
+    version='1.0.3',
+    author='gsfish',
+    author_email='root@grassfish.net',
+    description='An extension for celery to dispatch large amount of subtasks within a main task',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gsfish/celery-dispatcher',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['celery<5', 'redis']
+)
