ee84aa25d631568d4e859ba63598aa5836f0426f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:33 +0000

    Add package objetto/1.1.0/objetto-1.1.0.tar.gz

---
+++ packages/objetto/objetto-1.1.0.tar.gz/objetto-1.1.0/objetto/factories.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Factories."""
+
+from ._factories import (
+    Curated,
+    FloatingPoint,
+    Integer,
+    MultiFactory,
+    RegexMatch,
+    RegexSub,
+    String,
+)
+
+__all__ = [
+    "MultiFactory",
+    "Integer",
+    "FloatingPoint",
+    "RegexMatch",
+    "RegexSub",
+    "String",
+    "Curated",
+]
