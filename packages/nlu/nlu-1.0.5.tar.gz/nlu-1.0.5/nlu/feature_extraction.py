b807c4397841ddf0a7b56dd630ea10bbea71aed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:09 +0000

    Add package nlu/1.0.5/nlu-1.0.5.tar.gz

---
+++ packages/nlu/nlu-1.0.5.tar.gz/nlu-1.0.5/nlu/feature_extraction.py
@@ -0,0 +1,17 @@
+# annotator class approach based feature exraction
+# annotators are categorized into groups. Based on their group and output_level, features will be extreacted
+
+# Classifiers are categorized into
+# Classification_levels:
+# 1. (One-To-One-Token)  (NER, POS, DEP)
+# 2. (One-To-Some-Token)  (NER, POS, DEP)
+
+
+#yake??
+# 1. (Document/Sentence  to N?) (Yake)
+# 2. (Document/Sentence to C class) (ClassifierDL,MultiClassifierDL/Sentiment/SentimentDl)
+
+#1 to N token mappers
+# Normalize, Stemm,
+
+# Normalize maps 1 to N-K because it removes tokens and we get Nones
