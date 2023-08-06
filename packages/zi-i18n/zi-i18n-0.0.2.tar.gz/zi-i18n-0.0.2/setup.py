c3a21556758249567ba626bad3c026774cf7253d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:52 +0000

    Add package zi-i18n/0.0.2/zi-i18n-0.0.2.tar.gz

---
+++ packages/zi-i18n/zi-i18n-0.0.2.tar.gz/zi-i18n-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import re
+
+version = ''
+with open('zi_i18n/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
+
+setup(
+    name="zi-i18n",
+    version=version,
+    packages=['zi_i18n'],
+    url="https://github.com/null2264/i18n",
+    license="GPL GNU-3.0",
+    author="null2264",
+    author_email="",
+    description="Experimental Internationalization",
+)
