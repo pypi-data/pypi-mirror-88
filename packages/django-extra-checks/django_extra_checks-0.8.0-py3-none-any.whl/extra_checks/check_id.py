37bd04af60970ddbac7d11f1c60cb4e7440c616a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:00 +0000

    Add package django-extra-checks/0.8.0/django_extra_checks-0.8.0-py3-none-any.whl

---
+++ packages/django-extra-checks/django_extra_checks-0.8.0-py3-none-any.whl/extra_checks/check_id.py
@@ -0,0 +1,23 @@
+import enum
+
+
+class CheckId(str, enum.Enum):
+    X001 = "extra-checks-config"
+    X010 = "model-attribute"
+    X011 = "model-meta-attribute"
+    X012 = "model-admin"
+    X013 = "no-unique-together"
+    X014 = "no-index-together"
+    X050 = "field-verbose-name"
+    X051 = "field-verbose-name-gettext"
+    X052 = "field-verbose-name-gettext-case"
+    X053 = "field-help-text-gettext"
+    X054 = "field-file-upload-to"
+    X055 = "field-text-null"
+    X056 = "field-boolean-null"
+    X057 = "field-null"
+    X058 = "field-foreign-key-db-index"
+    X059 = "field-default-null"
+    X060 = "field-choices-constraint"
+    X301 = "drf-model-serializer-extra-kwargs"
+    X302 = "drf-model-serializer-meta-attribute"
