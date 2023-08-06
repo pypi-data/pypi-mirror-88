ea0895b12a5c88e4d2cdc077312d3cc000035f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:47 +0000

    Add package discord-py-slash-command/1.0.1/discord_py_slash_command-1.0.1-py3-none-any.whl

---
+++ packages/discord-py-slash-command/discord_py_slash_command-1.0.1-py3-none-any.whl/discord_slash/http.py
@@ -0,0 +1,25 @@
+import aiohttp
+from .error import RequestFailure
+
+
+class SlashCommandRequest:
+    def __init__(self):
+        pass
+
+    async def post(self, _resp, _id, token):
+        """
+        Sends command response POST request to Discord API.
+
+        :param _resp: Command response.
+        :type _resp: dict
+        :param _id: Command message id.
+        :param token: Command message token.
+        :return: True if succeeded.
+        :raises: :class:`.error.RequestFailure` - Requesting to API has failed.
+        """
+        req_url = f"https://discord.com/api/v8/interactions/{_id}/{token}/callback"
+        async with aiohttp.ClientSession() as session:
+            async with session.post(req_url, json=_resp) as resp:
+                if not 200 <= resp.status < 300:
+                    raise RequestFailure(resp.status, await resp.text())
+                return True
