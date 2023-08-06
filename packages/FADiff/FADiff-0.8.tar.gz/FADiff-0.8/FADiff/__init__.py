e13f82f79ac4a78c18fbebdbabe4cfa78d0f1d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:36 +0000

    Add package FADiff/0.8/FADiff-0.8.tar.gz

---
+++ packages/FADiff/FADiff-0.8.tar.gz/FADiff-0.8/FADiff/__init__.py
@@ -0,0 +1,6 @@
+from FADiff.FADiff import FADiff
+from FADiff.FuncVect import FuncVect
+from FADiff.fad.Gradients import Scal as fadScal
+from FADiff.fad.Matrices import Vect as fadVect
+from FADiff.rev.Gradients import Scal as revScal
+from FADiff.rev.Matrices import Vect as revVect
