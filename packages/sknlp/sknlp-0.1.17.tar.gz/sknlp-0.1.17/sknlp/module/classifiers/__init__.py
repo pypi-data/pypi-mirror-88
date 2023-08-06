c13d6780bbc932b0f1e2d8f407fcc38b868f909c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:54 +0000

    Add package sknlp/0.1.17/sknlp-0.1.17.tar.gz

---
+++ packages/sknlp/sknlp-0.1.17.tar.gz/sknlp-0.1.17/sknlp/module/classifiers/__init__.py
@@ -0,0 +1,6 @@
+from .text_rnn import TextRNNClassifier
+from .text_rcnn import TextRCNNClassifier
+from .bert_classifier import BertClassifier
+
+
+__all__ = ['TextRNNClassifier', 'TextRCNNClassifier', 'BertClassifier']
