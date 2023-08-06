9f79cb54fa707133b8101d5d50dad9cdf62d84c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:26 +0000

    Add package python-jsonstore/1.3.0/python-jsonstore-1.3.0.tar.gz

---
+++ packages/python-jsonstore/python-jsonstore-1.3.0.tar.gz/python-jsonstore-1.3.0/README.rst
@@ -0,0 +1,120 @@
+|PyPi Package| |Build Status| |Codacy Rating| |Coverage Report|
+
+jsonstore
+=========
+
+This module provides a class that maps keys and values from a JSON file
+onto its attributes.
+
+The goal was to provide a convenient way of loading and saving
+configuration in a familiar human readable format. This is a bit more
+flexible than the
+`configparser <https://docs.python.org/3/library/configparser.html>`__
+module which is included with Python.
+
+This works is tested and working on Python 2.7+ and Python 3.3+. It will
+not work on 2.6 or lower, but is expected to work on 3.0-3.2. The tests
+do not work in 3.2.6 due to
+`mistreating <https://travis-ci.org/Code0x58/python-jsonstore/jobs/198150401>`__
+the 💩 when parsing the test code. This is also tested on pypy and pypy3.
+
+Examples
+--------
+
+Basics
+~~~~~~
+
+.. code:: python
+
+    # by default JsonStore commits on every change unless in a transaction
+    store = JsonStore('config.json')
+    store.a_string = "something"
+    store.a_list = [1, 2, 3]
+    store.a_dictionary = {
+      'dict-list': [{}],
+      'ln(2)': 0.69314718056,
+      'for-you': u"💐",
+    }
+
+    # you can use […] to set/get/delete string keys
+    store['some_key'] = "a value"
+    # the key is split on '.'s and works on dictionaries
+    del store['a_dictionary.dict-list']
+    store['a_dictionary.new_value'] = "old value"
+    #  you can also use the syntactic sugar for tuple keys (explicit lists work too)
+    assert store['a_dictionary', 'new_value'] == "old value"
+    # you can traverse lists too
+    assert store['a_list', -1] == 3
+    # you can use slices in lists
+    assert len(store['a_list', 1:]) == 2
+    del store['a_list', :2]
+    assert store.a_list == [3]
+
+    # deep copies are made when assigning values
+    my_list = ['fun']
+    store.a_list = my_list
+    assert store.a_list is not my_list
+    assert 'a_list' in store
+
+    # deep copies are also returned to avoid unsanitary changes being made
+    store.a_dictionary['new_value'] = "new value"  # won't update the store!
+    assert store.a_dictionary['new_value'] == "old value"
+    assert store.a_dictionary is not store.a_dictionary
+
+    # Appending to, extending a list
+    >>> store.list = [1, 2, 3]
+
+    # Because of the fact that .append won't modify the list in the actual file,
+    # but only a copy...
+    >>> store.list.append(4)
+    >>> store.list
+    [1, 2, 3]
+
+    # ... we need to rather use the += operator to append to a list.
+    >>> store.list += [4]
+    >>> store.list
+    [1, 2, 3, 4]
+
+    # Similarly, we can extend the list
+    >>> store.list += [5, 6]
+    >>> store.list
+    [1, 2, 3, 4, 5, 6]
+
+Transactions
+~~~~~~~~~~~~
+
+``JsonStore`` objects can be used as `context
+managers <https://www.python.org/dev/peps/pep-0343/>`__ to provide
+transactions which are rolled back in the event of an exception. The
+transaction model is primitive; you can only nest transactions.
+
+While a store is put into a transaction, it will not save changes to
+file until all of the transactions have been closed.
+
+.. code:: python
+
+    from jsonstore import JsonStore
+
+    # even with auto_commit=True, the file won't be saved until the last contexts has been closed
+    with JsonStore('config.json', indent=None, auto_commit=False) as store:
+      self.value = 1
+
+    # the context manager will roll back changes made if an exception is raised
+    store = JsonStore('config.json', indent=None)
+    try:
+      with store:
+        store.value = "new"
+        raise Exception
+    except Exception:
+      pass
+    # here we see the value that was saved previously
+    assert store.value == 1
+
+.. |Build Status| image:: https://travis-ci.org/Code0x58/python-jsonstore.svg?branch=master
+   :target: https://travis-ci.org/Code0x58/python-jsonstore
+.. |Codacy Rating| image:: https://api.codacy.com/project/badge/Grade/37ea488773444de59469a3775be83faf
+   :target: https://www.codacy.com/app/evilumbrella-github/python-jsonstore?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=Code0x58/python-jsonstore&amp;utm_campaign=Badge_Grade
+.. |PyPi Package| image:: https://badge.fury.io/py/python-jsonstore.svg
+   :target: https://pypi.org/project/python-jsonstore/
+.. |Coverage Report| image:: https://codecov.io/gh/Code0x58/python-jsonstore/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/Code0x58/python-jsonstore
---
+++ packages/python-jsonstore/python-jsonstore-1.3.0.tar.gz/python-jsonstore-1.3.0/test_jsonstore.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+
+import json
+import os
+import unittest
+from tempfile import mktemp
+
+from jsonstore import JsonStore
+
+
+class TransactionBreaker(Exception):
+    pass
+
+
+class Tests(unittest.TestCase):
+    TEST_DATA = (
+        ("string", "hello"),
+        ("unicode", u"💩"),
+        ("integer", 1),
+        ("none", None),
+        ("big_integer", 18446744073709551616),
+        ("float", 1.0),
+        ("boolean", True),
+        ("list", [1, 2]),
+        ("tuple", (1, 2)),
+        ("dictionary", {"key": "value"}),
+    )
+
+    def setUp(self):
+        self._store_file = mktemp()
+        self.store = JsonStore(self._store_file, indent=None, auto_commit=True)
+
+    def tearDown(self):
+        os.remove(self._store_file)
+
+    def _setattr(self, key, value):
+        """
+        Return a callable that assigns self.store.key to value
+        """
+
+        def handle():
+            setattr(self.store, key, value)
+
+        return handle
+
+    def _setitem(self, key, value):
+        """
+        Return a callable that assigns self.store[key] to value
+        """
+
+        def handle():
+            self.store[key] = value
+
+        return handle
+
+    def _getattr(self, key):
+        """
+        Return a callable that assigns self.store.key to value
+        """
+
+        def handle():
+            return getattr(self.store, key)
+
+        return handle
+
+    def _getitem(self, key):
+        """
+        Return a callable that assigns self.store[key] to value
+        """
+
+        def handle():
+            return self.store[key]
+
+        return handle
+
+    def test_new_store(self):
+        store_file = mktemp()
+        JsonStore(store_file, auto_commit=True)
+        with open(self._store_file) as handle:
+            self.assertEqual(handle.read(), "{}")
+        os.remove(store_file)
+
+        JsonStore(store_file, auto_commit=False)
+        with open(self._store_file) as handle:
+            self.assertEqual(handle.read(), "{}")
+        os.remove(store_file)
+
+    def test_assign_valid_types(self):
+        for name, value in self.TEST_DATA:
+            self.store[name] = value
+            self.store[name] == value
+            getattr(self.store, name) == value
+
+            del self.store[name]
+            self.assertRaises(KeyError, self._getitem(name))
+            self.assertRaises(AttributeError, self._getattr(name))
+
+            setattr(self.store, name, value)
+            self.store[name] == value
+            getattr(self.store, name) == value
+
+            delattr(self.store, name)
+            self.assertRaises(KeyError, self._getitem(name))
+            self.assertRaises(AttributeError, self._getattr(name))
+
+    def test_slices(self):
+        self.store.list = [1, 2, 3]
+        self.store["list", :2] = ["a", "b"]
+        self.assertEqual(self.store["list", 1:], ["b", 3])
+        del self.store["list", :1]
+        self.assertEqual(self.store.list, ["b", 3])
+
+    def test_assign_invalid_types(self):
+        for method in (self._setattr, self._setitem):
+
+            def assign(value):
+                return method("key", value)
+
+            self.assertRaises(TypeError, assign(set()))
+            self.assertRaises(TypeError, assign(object()))
+            self.assertRaises(TypeError, assign(None for i in range(2)))
+            self.assertRaises(TypeError, assign({1: 1}))
+
+    def test_assign_bad_keys(self):
+        value = 1
+        # the root object is a dict, so a string key is needed
+        self.assertRaises(TypeError, self._setitem(1, value))
+        self.assertRaises(TypeError, self._setitem((1, "a"), value))
+
+        self.store["dict"] = {}
+        self.store["list"] = []
+        self.assertRaises(TypeError, self._setitem((), value))
+        self.assertRaises(TypeError, self._setitem(("dict", 1), value))
+        self.assertRaises(TypeError, self._setitem(("dict", slice(1)), value))
+        self.assertRaises(TypeError, self._setitem(("list", "a"), value))
+        self.assertRaises(TypeError, self._setitem(("list", slice("a")), value))
+        self.assertRaises(IndexError, self._setitem(("list", 1), value))
+
+
+    def test_retrieve_values(self):
+        for name, value in self.TEST_DATA:
+            self.store[name] = value
+            self.assertEqual(getattr(self.store, name), value)
+            self.assertEqual(self.store[name], value)
+
+    def test_has_values(self):
+        for name, value in self.TEST_DATA:
+            self.store[name] = value
+            self.assertTrue(name in self.store)
+
+        self.assertFalse("foo" in self.store)
+
+    def test_empty_key(self):
+        with self.assertRaises(KeyError):
+            return self.store[""]
+
+    def test_empty_store(self):
+        store_file = mktemp()
+        with open(store_file, "wb") as f:
+            f.write(b"")
+        self.assertTrue(JsonStore(f.name))
+
+    def test_assign_cycle(self):
+        test_list = []
+        test_dict = {}
+        test_list.append(test_dict)
+        test_dict["list"] = test_list
+        for method in (self._setattr, self._setitem):
+            self.assertRaises(ValueError, method("key", test_list))
+            self.assertRaises(ValueError, method("key", test_dict))
+
+    def test_nested_dict_helper(self):
+        self.assertRaises(KeyError, self._setitem("dictionary.noexist", None))
+        self.assertRaises(KeyError, self._getitem("dictionary.noexist"))
+
+        for access_key in ("dictionary.exist", ("dictionary", "exist"), ["dictionary", "exist"]):
+            self.store.dictionary = {"a": 1}
+            self.store["dictionary.exist"] = None
+            self.assertIsNone(self.store.dictionary["exist"])
+            self.assertIsNone(self.store[access_key])
+
+            self.store["dictionary.a"] = 2
+            del self.store[access_key]
+            self.assertRaises(KeyError, self._getitem(access_key))
+            self.assertNotIn("exist", self.store.dictionary)
+            self.assertEqual(self.store.dictionary, {"a": 2})
+
+    def test_nested_getitem(self):
+        self.store["list"] = [
+            {
+                "key": [None, "value", "last"]
+            }
+        ]
+        assert self.store["list", 0, "key", 1] == "value"
+        assert self.store[["list", 0, "key", -1]] == "last"
+        self.assertRaises(TypeError, self._getitem("list.0.key.1"))
+        assert len(self.store["list", 0, "key", 1:]) == 2
+        self.assertRaises(IndexError, self._getitem(("list", 1)))
+
+    def test_del(self):
+        self.store.key = None
+        del self.store.key
+        self.assertRaises(KeyError, self._getitem("key"))
+
+        self.store.key = None
+        del self.store["key"]
+        self.assertRaises(KeyError, self._getitem("key"))
+
+        with self.assertRaises(KeyError):
+            del self.store["missing"]
+
+        self.store.list = []
+        with self.assertRaises(IndexError):
+            del self.store["list", 1]
+
+        self.store.dict = {}
+        with self.assertRaises(TypeError):
+            del self.store["dict", slice("a")]
+
+    def test_context_and_deserialisation(self):
+        store_file = mktemp()
+        for name, value in self.TEST_DATA:
+            if isinstance(value, tuple):
+                value = list(value)
+            with JsonStore(store_file) as store:
+                store[name] = value
+            with JsonStore(store_file) as store:
+                self.assertEqual(getattr(store, name), value)
+
+    def test_deep_copying(self):
+        inner_list = []
+        outer_list = [inner_list]
+        inner_dict = {}
+        outer_dict = {"key": inner_dict}
+
+        for method in (self._getattr, self._getitem):
+            self.store.list = outer_list
+            self.assertIsNot(method("list")(), outer_list)
+            self.assertIsNot(method("list")()[0], inner_list)
+
+            self.store.dict = outer_dict
+            self.assertIsNot(method("dict")(), outer_dict)
+            self.assertIsNot(method("dict")()["key"], inner_dict)
+
+            self.assertIsNot(method("list")(), method("list")())
+            self.assertIsNot(method("list")()[0], method("list")()[0])
+            self.assertIsNot(method("dict")(), method("dict")())
+            self.assertIsNot(method("dict")()["key"], method("dict")()["key"])
+
+    def test_load(self):
+        for good_data in ("{}", '{"key": "value"}'):
+            with open(self._store_file, "w") as handle:
+                handle.write(good_data)
+            self.store._load()
+
+        for bad_data in ("[]", "1", "nill", '"x"'):
+            with open(self._store_file, "w") as handle:
+                handle.write(bad_data)
+            self.assertRaises(ValueError, self.store._load)
+
+    def test_auto_commit(self):
+        store_file = mktemp()
+        store = JsonStore(store_file, indent=None, auto_commit=True)
+        store.value1 = 1
+        with open(store_file) as handle:
+            self.assertEqual({"value1": 1}, json.load(handle))
+        store["value2"] = 2
+        with open(store_file) as handle:
+            self.assertEqual({"value1": 1, "value2": 2}, json.load(handle))
+
+    def test_no_auto_commit(self):
+        store_file = mktemp()
+        store = JsonStore(store_file, indent=None, auto_commit=False)
+        store.value1 = 1
+        store["value2"] = 2
+        with open(store_file) as handle:
+            self.assertEqual({}, json.load(handle))
+
+    def test_transaction_rollback(self):
+        self.store.value = 1
+        try:
+            with self.store:
+                self.store.value = 2
+                try:
+                    with self.store:
+                        self.store.value = 3
+                        raise TransactionBreaker
+                except TransactionBreaker:
+                    pass
+                self.assertEqual(self.store.value, 2)
+                raise TransactionBreaker
+        except TransactionBreaker:
+            pass
+        self.assertEqual(self.store.value, 1)
+
+    def test_transaction_commit(self):
+        self.store.value = 1
+        self.store.remove_me = "bye"
+        with self.store:
+            self.store.value = 2
+            del self.store.remove_me
+        self.assertEqual(self.store.value, 2)
+        self.assertRaises(AttributeError, self._getattr("remove_me"))
+
+    def test_transaction_write(self):
+        with self.store:
+            self.store.value1 = 1
+            with open(self._store_file) as handle:
+                self.assertEqual(handle.read(), "{}")
+            with self.store:
+                self.store.value2 = 2
+            with open(self._store_file) as handle:
+                self.assertEqual(handle.read(), "{}")
+        with open(self._store_file) as handle:
+            self.assertEqual(handle.read(), '{"value1": 1, "value2": 2}')
+
+    def test_list_concat_inplace(self):
+        self.store.list = []
+        extension = [{"key": "value"}]
+
+        # make sure += happens
+        self.store["list"] += extension
+        self.store.list += extension
+        self.assertEqual(self.store.list, extension * 2)
+
+        # make sure a deepcopy occurred
+        self.assertIsNot(self.store.list[0], extension[0])
+
+
+if __name__ == "__main__":
+    unittest.main()
