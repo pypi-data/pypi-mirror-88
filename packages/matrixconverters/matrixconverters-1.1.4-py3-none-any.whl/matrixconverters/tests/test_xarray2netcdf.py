9fc2e55a8f83772822c10e9b0416aab33950c070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:23 +0000

    Add package matrixconverters/1.1.4/matrixconverters-1.1.4-py3-none-any.whl

---
+++ packages/matrixconverters/matrixconverters-1.1.4-py3-none-any.whl/matrixconverters/tests/test_xarray2netcdf.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import pytest
+import numpy as np
+import xarray as xr
+from matrixconverters.xarray2netcdf import xr2netcdf
+from netCDF4 import __hdf5libversion__
+
+
+@pytest.fixture(scope='class')
+def folder() -> str:
+    return os.path.dirname(__file__)
+
+
+@pytest.fixture(scope='class')
+def fp_dataset_with_umlaut(folder: str) -> str:
+    fn = os.path.join(folder, 'ÄÖÜß', 'ÄÖÜß.nc4')
+    return fn
+
+
+@pytest.fixture(scope='class')
+def fp_dataset_without_umlaut(folder: str) -> str:
+    fn = os.path.join(folder, 'dataset_netcdf.nc4')
+    return fn
+
+@pytest.fixture(scope='class')
+def ds() -> xr.Dataset:
+    da = xr.DataArray(np.arange(9).reshape(3, 3))
+    zones = xr.DataArray([100, 200, 300])
+    names = xr.DataArray(['ÄÄÄ-Town', 'B-Village', 'C-City'])
+    ds = xr.Dataset({'matrix': da,
+                     'zone_no': zones,
+                     'zone_name': names,})
+    return ds
+
+
+class TestSaveReadxr2netcdf:
+    def test_01_save_ds(self, ds: xr.Dataset, fp_dataset_without_umlaut: str):
+        xr2netcdf(ds, fp_dataset_without_umlaut)
+        ds_saved = xr.open_dataset(fp_dataset_without_umlaut)
+        np.testing.assert_array_equal(ds_saved.matrix, ds.matrix)
+        np.testing.assert_array_equal(ds_saved.zone_no, ds.zone_no)
+        np.testing.assert_array_equal(ds_saved.zone_name, ds.zone_name)
+
+    @pytest.mark.xfail(
+        __hdf5libversion__ < '1.12',
+        reason='Bug in Hdf5 < 1.12 when filepath has non-ascii-characters')
+    def test_02_save_ds_with_umlaut(self, ds: xr.Dataset, fp_dataset_with_umlaut: str):
+        xr2netcdf(ds, fp_dataset_with_umlaut)
+        ds_saved = xr.open_dataset(fp_dataset_with_umlaut)
+        np.testing.assert_array_equal(ds_saved.matrix, ds.matrix)
+        np.testing.assert_array_equal(ds_saved.zone_no, ds.zone_no)
+        np.testing.assert_array_equal(ds_saved.zone_name, ds.zone_name)
