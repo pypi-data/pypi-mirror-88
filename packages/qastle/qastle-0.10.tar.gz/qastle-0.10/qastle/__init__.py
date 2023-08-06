e8e8a3e6705fb14d873b73a9882805f81f488a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:03 +0000

    Add package qastle/0.10/qastle-0.10.tar.gz

---
+++ packages/qastle/qastle-0.10.tar.gz/qastle-0.10/qastle/__init__.py
@@ -0,0 +1,6 @@
+from .ast_util import *
+from .columns_util import *
+from .linq_util import *
+from .parse import *
+from .transform import *
+from .translate import *
---
+++ packages/qastle/qastle-0.10.tar.gz/qastle-0.10/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='qastle',
+                 version='0.10',
+                 description='Query AST Language Expressions',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(exclude=['tests']),
+                 python_requires=('>=2.7, '
+                                  '!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <3.10'),
+                 install_requires=['lark-parser>=0.6.5'],
+                 package_data={'qastle': ['syntax.lark']},
+                 author='Mason Proffitt',
+                 author_email='masonlp@uw.edu',
+                 url='https://github.com/iris-hep/qastle')
