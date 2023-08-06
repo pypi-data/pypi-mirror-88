05ac0a33ae624f0beca13671a72a55b315ea549e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:39 +0000

    Add package odoo12-addon-somconnexio/12.0.0.0.0rc38/odoo12_addon_somconnexio-12.0.0.0.0rc38-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-somconnexio/odoo12_addon_somconnexio-12.0.0.0.0rc38-py2.py3-none-any.whl/odoo/addons/somconnexio/models/crm_lead.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+    subscription_request_id = fields.Many2one(
+        'subscription.request', 'Subscription Request'
+    )
+    iban = fields.Char(string="IBAN")
+
+    mobile_lead_line_id = fields.Many2one(
+        'crm.lead.line',
+        compute='_compute_mobile_lead_line_id',
+        string="Mobile Lead Line",
+    )
+
+    # TODO: To modify if in the future we can have more than one `mobile_lead_line_id`
+    def _compute_mobile_lead_line_id(self):
+        for line in self.lead_line_ids:
+            if line.mobile_isp_info:
+                self.mobile_lead_line_id = line
+                break
