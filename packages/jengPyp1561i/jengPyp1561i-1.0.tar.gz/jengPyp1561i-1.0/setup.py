5e4d35d0b94c092a8d9a35397f36f7fb90991e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:50 +0000

    Add package jengPyp1561i/1.0/jengPyp1561i-1.0.tar.gz

---
+++ packages/jengPyp1561i/jengPyp1561i-1.0.tar.gz/jengPyp1561i-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'jengPyp1561i',
+    packages = ['mypackage'],
+    scripts = [],
+    version = '1.0',
+    description = 'Jeng first Pypi Project',
+    author = 'jeng',
+    author_email = 'stu121645162002@gmail.com',
+    url = 'https://github.com/tsjeng45/jengPypi',
+    download_url = 'https://github.com/tsjeng45/jengPypi/tarball/v1.0',
+    keywords = ['pypi'],
+    classifiers = []
+)
