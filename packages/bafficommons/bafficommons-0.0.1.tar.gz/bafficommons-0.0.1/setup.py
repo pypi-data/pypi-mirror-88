078233315e736a724fd560ca6770042351f1ad84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:04 +0000

    Add package bafficommons/0.0.1/bafficommons-0.0.1.tar.gz

---
+++ packages/bafficommons/bafficommons-0.0.1.tar.gz/bafficommons-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bafficommons",
+    version="0.0.1",
+    author="Miguel Alfaro",
+    author_email="malfaro@fi.uba.ar",
+    description="Baffi Commons",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/baf-fi/baffi-commons",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
