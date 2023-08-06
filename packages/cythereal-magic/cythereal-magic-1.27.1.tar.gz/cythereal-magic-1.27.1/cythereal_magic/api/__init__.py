ba5bb041426a03a4923fa719ed5807845f6072cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:35 +0000

    Add package cythereal-magic/1.27.1/cythereal-magic-1.27.1.tar.gz

---
+++ packages/cythereal-magic/cythereal-magic-1.27.1.tar.gz/cythereal-magic-1.27.1/cythereal_magic/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cythereal_magic.api.alpha_api import AlphaApi
+from cythereal_magic.api.cythereal_magic_api import CytherealMagicApi
+from cythereal_magic.api.in_development_api import InDevelopmentApi
