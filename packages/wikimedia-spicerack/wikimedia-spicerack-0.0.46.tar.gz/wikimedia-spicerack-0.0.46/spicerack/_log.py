e384a9c351abcd8c32d5b48d10cdd56bb5f6aa76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:43 +0000

    Add package wikimedia-spicerack/0.0.46/wikimedia-spicerack-0.0.46.tar.gz

---
+++ packages/wikimedia-spicerack/wikimedia-spicerack-0.0.46.tar.gz/wikimedia-spicerack-0.0.46/spicerack/_log.py
@@ -0,0 +1,161 @@
+"""Log module."""
+import logging
+import os
+import socket
+
+from pathlib import Path
+from typing import Optional, Tuple, Union
+
+
+root_logger = logging.getLogger()  # pylint: disable=invalid-name
+irc_logger = logging.getLogger('spicerack_irc_announce')  # pylint: disable=invalid-name
+
+
+class IRCSocketHandler(logging.Handler):
+    """Log handler for logmsgbot on #wikimedia-operation.
+
+    Sends log events to a tcpircbot server for relay to an IRC channel.
+    """
+
+    def __init__(self, host: str, port: int, username: str) -> None:
+        """Initialize the IRC socket handler.
+
+        Arguments:
+            host (str): tcpircbot hostname.
+            port (int): tcpircbot listening port.
+            username (str): the user to refer in the IRC messages.
+
+        """
+        super().__init__()
+        self.addr: Tuple[str, int] = (host, port)
+        self.username = username
+        self.level = logging.INFO
+
+    def emit(self, record: logging.LogRecord) -> None:
+        """According to Python logging.Handler interface.
+
+        See https://docs.python.org/3/library/logging.html#handler-objects
+        """
+        # Stashbot expects !log messages relayed by logmsgbot to have the
+        # format: "!log <nick> <msg>". The <nick> is parsed out and used as
+        # the label of who actually made the SAL entry.
+        message = '!log {user}@{host} {msg}'.format(
+            msg=record.getMessage(), user=self.username, host=socket.gethostname())
+        sock = None
+
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1.0)
+            sock.connect(self.addr)
+            sock.sendall(message.encode('utf-8'))
+        except OSError:
+            self.handleError(record)
+        finally:
+            if sock is not None:
+                sock.close()
+
+
+class FilterOutCumin(logging.Filter):
+    """A logging output filter to filter out Cumin's logs."""
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        """Filter out Cumin's log messages.
+
+        Arguments:
+            record (logging.LogRecord):
+
+        Returns:
+            int: :py:data:`False` if the record should be filtered out, :py:data:`True` if it should be included.
+            According to Python's logging interface, see: https://docs.python.org/3/library/logging.html#filter-objects
+
+        """
+        if record.name == 'cumin' or record.name.startswith('cumin.'):
+            return False  # Filter it out
+
+        return True
+
+
+def setup_logging(
+    base_path: Union[str, Path],
+    name: str,
+    user: str,
+    dry_run: bool = True,
+    host: Optional[str] = None,
+    port: int = 0
+) -> None:
+    """Setup the root logger instance.
+
+    Arguments:
+        base_path (str, pathlib.Path): the base path where to save the logs.
+        name (str): the name of log file to use without extension.
+        user (str): the username for the IRC logging.
+        dry_run (bool, optional): whether this is a dry-run.
+        host (str, optional): the tcpircbot hostname for the IRC logging.
+        port (int, optional): the tcpircbot port for the IRC logging.
+
+    """
+    logging.raiseExceptions = False
+    os.makedirs(base_path, mode=0o755, exist_ok=True)
+
+    # Default INFO logging
+    formatter = logging.Formatter(fmt='%(asctime)s {} %(process)d [%(levelname)s] %(message)s'.format(user))
+    handler = logging.FileHandler(os.path.join(base_path, '{name}.log'.format(name=name)))
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+
+    # Extended logging for detailed debugging
+    formatter_extended = logging.Formatter(
+        fmt='%(asctime)s {} %(process)d [%(levelname)s %(filename)s:%(lineno)s in %(funcName)s] %(message)s'
+            .format(user))
+    handler_extended = logging.FileHandler(os.path.join(base_path, '{name}-extended.log'.format(name=name)))
+    handler_extended.setFormatter(formatter_extended)
+    handler_extended.setLevel(logging.DEBUG)
+
+    # Stderr logging
+    output_handler = logging.StreamHandler()
+    if dry_run:
+        output_handler.setFormatter(logging.Formatter(fmt='DRY-RUN: %(message)s'))
+        output_handler.setLevel(logging.DEBUG)
+    else:
+        output_handler.setLevel(logging.INFO)
+    output_handler.addFilter(FilterOutCumin())
+
+    root_logger.addHandler(handler)
+    root_logger.addHandler(handler_extended)
+    root_logger.addHandler(output_handler)
+    root_logger.setLevel(logging.DEBUG)
+
+    if not dry_run and host is not None and port > 0:
+        irc_logger.addHandler(IRCSocketHandler(host, port, user))
+        irc_logger.setLevel(logging.INFO)
+
+    # Silence external noisy loggers
+    logging.getLogger('urllib3').setLevel(logging.WARNING)
+    logging.getLogger('requests').setLevel(logging.WARNING)
+
+    # Elasticsearch lib is very noisy about HTTP level errors
+    # ideally, we'd want to keep it at WARNING level for logs
+    # sent to file, but ERROR for the console. Since this is
+    # non trivial, let's raise level to ERROR for the moment.
+    logging.getLogger('elasticsearch').setLevel(logging.ERROR)
+
+
+def log_task_start(message: str) -> None:
+    """Log the start of a task both on the logs and IRC.
+
+    Arguments:
+        message (str): the message to be logged.
+
+    """
+    irc_logger.info('START - %s', message)
+
+
+def log_task_end(status: str, message: str) -> None:
+    """Log the start of a task both on the logs and IRC.
+
+    Arguments:
+        status (str): the final status of the task.
+        message (str): the message to be logged.
+
+    """
+    irc_logger.info('END (%s) - %s', status, message)
---
+++ packages/wikimedia-spicerack/wikimedia-spicerack-0.0.46.tar.gz/wikimedia-spicerack-0.0.46/spicerack/tests/unit/test__log.py
@@ -0,0 +1,140 @@
+"""Interactive module tests."""
+import logging
+import os
+import uuid
+
+from unittest import mock
+
+from spicerack import _log as log
+
+
+GENERIC_LOG_RECORD = logging.LogRecord('module', logging.DEBUG, '/source/file.py', 1, 'message', [], None)
+CUMIN_LOG_RECORD = logging.LogRecord('cumin.module', logging.DEBUG, '/cumin/source/file.py', 1, 'message', [], None)
+logger = logging.getLogger(__name__)
+
+
+def _assert_match_in_tmpdir(match, tmp_dir):
+    """Given a match string, assert that it's present in all files in tmp_dir."""
+    tmp_dir = str(tmp_dir)  # Newer versions pass a LocalPath, older a string.
+    for logfile in os.listdir(tmp_dir):
+        with open(os.path.join(tmp_dir, logfile), 'r') as f:
+            assert match in f.read()
+
+
+def test_irc_socket_handler_init():
+    """An instance of IRCSocketHandler should set the address for the socket and the user."""
+    handler = log.IRCSocketHandler('host', 123, 'user')
+    assert handler.addr == ('host', 123)
+    assert handler.username == 'user'
+
+
+@mock.patch('spicerack._log.socket')
+def test_irc_socket_handler_emit_ok(mocked_socket):
+    """Calling emit() on an IRCSocketHandler instance should send the message to the socket."""
+    handler = log.IRCSocketHandler('host', 123, 'user')
+
+    handler.emit(GENERIC_LOG_RECORD)
+
+    mocked_socket.assert_has_calls([
+        mock.call.socket().connect(('host', 123)), mock.call.socket().close()], any_order=True)
+
+
+@mock.patch('spicerack._log.socket')
+def test_irc_socket_handler_emit_ko(mocked_socket):
+    """If an error occur while calling emit() on an IRCSocketHandler instance, it should call ."""
+    handler = log.IRCSocketHandler('host', 123, 'user')
+    handler.handleError = mock.MagicMock()
+    mocked_socket.socket.side_effect = OSError
+
+    handler.emit(GENERIC_LOG_RECORD)
+
+    assert mock.call.socket().connect(('host', 123)) not in mocked_socket.mock_calls
+    assert mock.call.socket().close() not in mocked_socket.mock_calls
+    handler.handleError.assert_called_once_with(GENERIC_LOG_RECORD)
+
+
+def test_cumin_filter_pass():
+    """The FilterOutCumin filter() method should let a normal log record pass."""
+    log_filter = log.FilterOutCumin()
+    ret = log_filter.filter(GENERIC_LOG_RECORD)
+    assert ret == 1
+
+
+def test_cumin_filter_blocks_cumin():
+    """The FilterOutCumin filter() method should block a Cumin's log record."""
+    log_filter = log.FilterOutCumin()
+    ret = log_filter.filter(CUMIN_LOG_RECORD)
+    assert ret == 0
+
+
+def test_setup_logging_no_irc(tmpdir, caplog):
+    """Calling setup_logging() should setup all the handlers of the root logger."""
+    log.setup_logging(tmpdir.strpath, 'task', 'user')
+    message = str(uuid.uuid4())
+    logger.info(message)
+    assert message in caplog.text
+    _assert_match_in_tmpdir(message, tmpdir.strpath)
+
+
+@mock.patch('spicerack._log.socket')
+def test_setup_logging_with_irc(mocked_socket, tmpdir, caplog):
+    """Calling setup_logging() with host and port should also setup the IRC logger."""
+    log.setup_logging(tmpdir.strpath, 'task', 'user', host='host', port=123, dry_run=False)
+    message = str(uuid.uuid4())
+    log.irc_logger.info(message)
+
+    assert mock.call.socket().connect(('host', 123)) in mocked_socket.mock_calls
+    assert message in caplog.text
+    _assert_match_in_tmpdir(message, tmpdir.strpath)
+
+
+def test_setup_logging_dry_run(capsys, tmpdir, caplog):
+    """Calling setup_logging() when in dry run mode should setup all the handlers and the stdout with DEBUG level."""
+    log.setup_logging(tmpdir.strpath, 'task', 'user', dry_run=True)
+    message = str(uuid.uuid4())
+    logger.info(message)
+
+    _, err = capsys.readouterr()
+    assert message in err
+    assert 'DRY-RUN' in err
+    assert message in caplog.text
+    _assert_match_in_tmpdir(message, tmpdir.strpath)
+
+
+def test_log_task_start(capsys, tmpdir, caplog):
+    """Calling log_task_start() should log a START message for the task to both loggers."""
+    log.setup_logging(tmpdir.strpath, 'task', 'user')
+    message = str(uuid.uuid4())
+    log.log_task_start(message)
+
+    logged_message = 'START - ' + message
+    _, err = capsys.readouterr()
+    assert logged_message in err
+    assert logged_message in caplog.text
+    _assert_match_in_tmpdir(logged_message, tmpdir.strpath)
+
+
+def test_log_task_start_dry_run(capsys, tmpdir, caplog):
+    """Calling log_task_start() in dry-run mode should not print a START message for the task to the IRC logger."""
+    log.setup_logging(tmpdir.strpath, 'task', 'user', dry_run=True)
+    message = str(uuid.uuid4())
+    log.log_task_start(message)
+
+    logged_message = 'START - ' + message
+    _, err = capsys.readouterr()
+    assert logged_message in err
+    assert logged_message in caplog.text
+    _assert_match_in_tmpdir(logged_message, tmpdir.strpath)
+
+
+def test_log_task_end(capsys, tmpdir, caplog):
+    """Calling log_task_end() should print an END message for the task."""
+    log.setup_logging(tmpdir.strpath, 'task', 'user', dry_run=False)
+    message = str(uuid.uuid4())
+    log.log_task_end('success', message)
+
+    logged_message = 'END (success) - ' + message
+    _, err = capsys.readouterr()
+    assert logged_message in err
+    assert logged_message in caplog.text
+    _assert_match_in_tmpdir(logged_message, tmpdir.strpath)
