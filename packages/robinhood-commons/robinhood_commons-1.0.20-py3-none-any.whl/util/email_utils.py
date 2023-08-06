bee8556e3afd78e41f30b0b280418685529543c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package robinhood-commons/1.0.20/robinhood_commons-1.0.20-py3-none-any.whl

---
+++ packages/robinhood-commons/robinhood_commons-1.0.20-py3-none-any.whl/util/email_utils.py
@@ -0,0 +1,11 @@
+from typing import List
+
+import yagmail
+
+from util.constants import MAIN_EMAIL
+
+
+def send_email(subject: str, contents: str, attachments: List[str], to: str = MAIN_EMAIL) -> None:
+    yag = yagmail.SMTP()
+
+    yag.send(to=to, subject=subject, contents=contents, attachments=attachments)
