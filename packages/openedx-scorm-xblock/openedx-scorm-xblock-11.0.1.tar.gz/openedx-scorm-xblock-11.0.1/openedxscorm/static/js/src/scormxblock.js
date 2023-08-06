784e2af610ca8db2de5ba7bc9303843dc2778155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:12 +0000

    Add package openedx-scorm-xblock/11.0.1/openedx-scorm-xblock-11.0.1.tar.gz

---
+++ packages/openedx-scorm-xblock/openedx-scorm-xblock-11.0.1.tar.gz/openedx-scorm-xblock-11.0.1/openedxscorm/static/js/src/scormxblock.js
@@ -0,0 +1,130 @@
+function ScormXBlock(runtime, element, settings) {
+
+    function SCORM_12_API() {
+
+        this.LMSInitialize = function() {
+            return "true";
+        };
+
+        this.LMSFinish = function() {
+            return "true";
+        };
+
+        this.LMSGetValue = GetValue;
+        this.LMSSetValue = SetValue;
+
+        this.LMSCommit = function() {
+            return "true";
+        };
+
+        this.LMSGetLastError = function() {
+            return "0";
+        };
+
+        this.LMSGetErrorString = function(errorCode) {
+            return "Some Error";
+        };
+
+        this.LMSGetDiagnostic = function(errorCode) {
+            return "Some Diagnostic";
+        };
+    }
+
+    function SCORM_2004_API() {
+        this.Initialize = function() {
+            return "true";
+        };
+
+        this.Terminate = function() {
+            return "true";
+        };
+
+        this.GetValue = GetValue;
+        this.SetValue = SetValue;
+
+        this.Commit = function() {
+            return "true";
+        };
+
+        this.GetLastError = function() {
+            return "0";
+        };
+
+        this.GetErrorString = function(errorCode) {
+            return "Some Error";
+        };
+
+        this.GetDiagnostic = function(errorCode) {
+            return "Some Diagnostic";
+        };
+    }
+
+    function enterFullscreen() {
+        $(element).find(".js-scorm-block").addClass("full-screen-scorm");
+    }
+    function exitFullscreen() {
+        $(element).find(".js-scorm-block").removeClass("full-screen-scorm");
+    }
+
+    var GetValue = function(cmi_element) {
+        var handlerUrl = runtime.handlerUrl(element, 'scorm_get_value');
+
+        var response = $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify({
+                'name': cmi_element
+            }),
+            async: false
+        });
+        response = JSON.parse(response.responseText);
+        return response.value;
+    };
+
+    var fullscreenOnNextEvent = true;
+    var SetValue = function(cmi_element, value) {
+        if (cmi_element === 'cmi.core.exit' || cmi_element === 'cmi.exit') {
+            exitFullscreen();
+            fullscreenOnNextEvent = true;
+        } else if (fullscreenOnNextEvent) {
+            fullscreenOnNextEvent = false;
+            if (settings.fullscreen_on_launch) {
+                enterFullscreen();
+            }
+        }
+
+        var handlerUrl = runtime.handlerUrl(element, 'scorm_set_value');
+
+        $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify({
+                'name': cmi_element,
+                'value': value
+            }),
+            async: false,
+            success: function(response) {
+                if (typeof response.lesson_score != "undefined") {
+                    $(element).find(".lesson_score").html(response.lesson_score);
+                }
+                $(element).find(".completion_status").html(response.completion_status);
+            }
+        });
+
+        return "true";
+    };
+
+    $(function($) {
+        if (settings.scorm_version == 'SCORM_12') {
+            API = new SCORM_12_API();
+        } else {
+            API_1484_11 = new SCORM_2004_API();
+        }
+        $(element).find("button.full-screen-on").on("click", function() {
+            enterFullscreen();
+        });
+        $(element).find("button.full-screen-off").on("click", function() {
+            exitFullscreen();
+        });
+    });
+}
