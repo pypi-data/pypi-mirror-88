40fd730f151dc3e83650048478dabd0fe5625dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:02 +0000

    Add package scCAL/0.1/scCAL-0.1.tar.gz

---
+++ packages/scCAL/scCAL-0.1.tar.gz/scCAL-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='scCAL',
+      version='0.1',
+      description='single cell Chromatin Activity Landscape pipeline',
+      url='https://github.com/genetics-dulab/scCAL',
+      author='ZZG',
+      author_email='zzg@genetics.ac.cn',
+      license='GNU General Public License v3.0',
+	  py_modules=['mysum'])
