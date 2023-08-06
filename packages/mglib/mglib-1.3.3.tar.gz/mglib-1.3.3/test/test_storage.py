5ff5e1494ee599fb2f44dac0a0eac654386611e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:14 +0000

    Add package mglib/1.3.3/mglib-1.3.3.tar.gz

---
+++ packages/mglib/mglib-1.3.3.tar.gz/mglib-1.3.3/test/test_storage.py
@@ -0,0 +1,43 @@
+import os
+import unittest
+from mglib.test.utils import TemporaryNode
+from mglib.path import DocumentPath
+from mglib.storage import FileSystemStorage
+
+
+BASE_DIR = os.path.dirname(
+    os.path.abspath(__file__)
+)
+
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, "media"
+)
+
+class TestStorage(unittest.TestCase):
+
+    def test_delete(self):
+        storage = FileSystemStorage(location=MEDIA_ROOT)
+
+        with TemporaryNode(MEDIA_ROOT) as media_root:
+            docs = media_root.add_folder("docs")
+            res = media_root.add_folder("results")
+            f1 = docs.add_folder("user_1/document_2")
+            f1.add_file("doku.pdf")
+            res.add_folder("user_1/document_2/pages")
+
+            doc_path = DocumentPath(
+                user_id=1,
+                document_id=2,
+                file_name='doku.pdf'
+            )
+
+            self.assertTrue(
+                f1.exists()
+            )
+
+            storage.delete_doc(doc_path)
+
+            self.assertFalse(
+                f1.exists()
+            )
+
