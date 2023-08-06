fc50c99f6e3f9da08a44793f5c76edae5f4e8930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:41 +0000

    Add package dude/0.1.1/dude-0.1.1.tar.gz

---
+++ packages/dude/dude-0.1.1.tar.gz/dude-0.1.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['dude']
+install_requires = \
+['humanize>=3.1.0,<4.0.0', 'tabulate>=0.8.7,<0.9.0']
+
+entry_points = \
+{'console_scripts': ['dude = dude:main']}
+
+setup_kwargs = {
+    'name': 'dude',
+    'version': '0.1.1',
+    'description': 'tool similar to du but a little fancier i guess',
+    'long_description': None,
+    'author': 'Andrew Herbig',
+    'author_email': 'notandrewherbig@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/andrew12/dude',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
