24ec5d0df1d5d23e979b980446e9d8e119fa5953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:57 +0000

    Add package rss-distributions/0.1/rss_distributions-0.1.tar.gz

---
+++ packages/rss-distributions/rss_distributions-0.1.tar.gz/rss_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rss_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['rss_distributions'],
+      author='Rachneet Singh Sachdeva',
+      author_email='rachneetsachdeva@yahoo.com',
+      zip_safe=False)
