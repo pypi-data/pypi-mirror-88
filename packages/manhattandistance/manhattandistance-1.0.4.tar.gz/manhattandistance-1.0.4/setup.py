4916d503281136fde58f1d16b12069b10d0ba524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:01 +0000

    Add package manhattandistance/1.0.4/manhattandistance-1.0.4.tar.gz

---
+++ packages/manhattandistance/manhattandistance-1.0.4.tar.gz/manhattandistance-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setup(
+  name='manhattandistance',
+  py_modules=["manhattandistance"],
+  version='1.0.4',
+  description='Calculates the Manhattan Distance of two places',
+  long_description=open('README.md').read() +  open('CHANGELOG.txt').read(),
+  long_description_content_type="text/markdown",
+  url='http://packages.python.org/manhattandistance',  
+  author='Dionysios Rigatos',
+  author_email='dion.rigatos@gmail.com',
+  license='MIT License', 
+  classifiers=[
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+                ],
+  keywords='distance', 
+  package_dir={'':'src'},
+)
