d07132b00ad444b858b8ce5f5389efed0abac241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:38 +0000

    Add package fastapi-aad-auth/0.1.7/fastapi_aad_auth-0.1.7-py3-none-any.whl

---
+++ packages/fastapi-aad-auth/fastapi_aad_auth-0.1.7-py3-none-any.whl/fastapi_aad_auth/auth.py
@@ -0,0 +1,268 @@
+"""Base AAD Authentication Handler."""
+from functools import wraps
+import logging
+from pathlib import Path
+from typing import Any, Dict, List
+
+from fastapi import Depends, FastAPI
+from starlette.authentication import requires
+from starlette.middleware.authentication import AuthenticationError, AuthenticationMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.requests import Request
+from starlette.responses import JSONResponse, RedirectResponse, Response
+from starlette.routing import Mount, request_response, Route
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+from fastapi_aad_auth.config import Config
+from fastapi_aad_auth.errors import base_error_handler, ConfigurationError
+from fastapi_aad_auth.oauth import AADOAuthBackend, AuthenticationState
+
+
+logger = logging.getLogger(__name__)
+
+_BASE_ROUTES = ['openapi', 'swagger_ui_html', 'swagger_ui_redirect', 'redoc_html']
+
+
+class AADAuth:
+    """AAD Authenticator Class.
+
+    Generates and handles adding AAD authentication, routing and middleware
+
+    Includes a decorator for signifying authentication required on fastapi routes, and a basic Login UI with AAD link
+    """
+
+    def __init__(self, config: Config = None, add_to_base_routes: bool = True):
+        """Initialise the AAD config based on the provided configuration.
+
+        Keyword Args:
+            config (fastapi_aad_auth.config.Config): Authentication configuration (includes ui and routing, as well as AAD Application and Tenant IDs)
+            add_to_base_routes (bool): Add the authentication to the router
+        """
+        if config is None:
+            config = Config()
+        self.config = config
+        self.oauth_backend = AADOAuthBackend.from_config(self.config)
+        if add_to_base_routes:
+            self._add_to_base_routes = True
+
+    def app_routes_add_auth(self, app: FastAPI, route_list: List[str], invert: bool = False):
+        """Add authentication to specified routes in application router.
+
+        Used for default routes (e.g. api/docs and api/redocs, openapi.json etc)
+
+        Args:
+            app: fastapi application
+            route_list: list of routes to add authentication to (e.g. api docs, redocs etc)
+
+        Keyword Args:
+            invert: Switch between using the route list as a block list or an allow list
+
+        """
+        if self.oauth_backend.enabled:
+            routes = app.router.routes
+            for i, route in enumerate(routes):
+                # Can use allow list or block list (i.e. invert = True sets all except the route list to have auth
+                if (route.name in route_list and not invert) or (route.name not in route_list and invert):  # type: ignore
+                    route.endpoint = self.auth_required()(route.endpoint)  # type: ignore
+                    route.app = request_response(route.endpoint)  # type: ignore
+                app.router.routes[i] = route
+        return app
+
+    def configure_app(self, app: FastAPI):
+        """Configure the fastapi application to use these authentication handlers.
+
+        Adds authentication middleware, error handler and adds authnetication
+        to the default routes as well as adding the authentication specific routes
+
+        Args:
+            app: fastapi application
+        """
+
+        def on_auth_error(request: Request, exc: Exception):
+            logger.exception(f'Error {exc} for request {request}')
+            self.oauth_backend.authenticator.set_post_auth_redirect(request, request.url.path)
+            return RedirectResponse(self.config.routing.landing_path)
+
+        app.add_middleware(AuthenticationMiddleware, backend=self.oauth_backend, on_error=on_auth_error)
+
+        error_template_path = Path(self.config.login_ui.error_template_file)
+        error_templates = Jinja2Templates(directory=str(error_template_path.parent))
+
+        if self.config.login_ui.context:
+            context = self.config.login_ui.context
+        else:
+            context = {}
+        if self.config.login_ui.app_name:
+            context['appname'] = self.config.login_ui.app_name
+        else:
+            context['appname'] = app.title
+        context['static_path'] = self.config.login_ui.static_path
+
+        @app.exception_handler(ConfigurationError)
+        async def configuration_error_handler(request: Request, exc: ConfigurationError) -> Response:
+            error_message = "Oops! It seems like the application has not been configured correctly, please contact an admin"
+            error_type = 'Authentication Configuration Error'
+            status_code = 500
+            return base_error_handler(request, exc, error_type, error_message, error_templates, error_template_path, context=context.copy(), status_code=status_code)
+
+        @app.exception_handler(AuthenticationError)
+        async def authentication_error_handler(request: Request, exc: AuthenticationError) -> Response:
+            error_message = "Oops! It seems like you cannot access this information. If this is an error, please contact an admin"
+            error_type = 'Authentication Error'
+            status_code = 403
+            return base_error_handler(request, exc, error_type, error_message, error_templates, error_template_path, context=context.copy(), status_code=status_code)
+
+        # Check if session middleware is there
+        if not any([SessionMiddleware in u.cls.__mro__ for u in app.user_middleware]):
+            app.add_middleware(SessionMiddleware, **self.config.session.dict())
+        if self._add_to_base_routes:
+            self.app_routes_add_auth(app, _BASE_ROUTES)
+        app.routes.extend(self.auth_routes)
+        app.routes.extend(self.build_auth_ui(context))
+
+    def auth_required(self, scopes: str = 'authenticated', redirect: str = 'login'):
+        """Decorator to require specific scopes (and redirect to the login ui) for an endpoint.
+
+        This can be used for toggling authentication (e.g. between an internal/external server)
+        as well as handling the redirection based on the session information
+
+        Keyword Args:
+            scopes: scopes for the fastapi requires decorator
+            redirect: name of the redirection url
+        """
+
+        def wrapper(endpoint):
+            if self.config.enabled:
+
+                @wraps(endpoint)
+                async def require_endpoint(request: Request, *args, **kwargs):
+                    self.oauth_backend.authenticator.set_post_auth_redirect(request, request.url.path)
+
+                    @requires(scopes, redirect=redirect)
+                    async def req_wrapper(request: Request, *args, **kwargs):
+                        return await endpoint(request, *args, **kwargs)
+
+                    return await req_wrapper(request, *args, **kwargs)
+
+                return require_endpoint
+            else:
+                return endpoint
+
+        return wrapper
+
+    @property
+    def auth_routes(self):
+        """Get the default authentication routes and methods.
+
+        Includes login, logout and the login callback
+        """
+
+        async def logout(request: Request):
+            logger.debug(f'Logging out - request url {request.url}')
+            if self.oauth_backend.enabled:
+                logger.debug(f'Auth {request.auth}')
+                self.oauth_backend.authenticator.logout(request)
+            return RedirectResponse(self.config.routing.post_logout_path)
+
+        async def login(request: Request):
+            logger.debug(f'Logging in - request url {request.url}')
+            if self.oauth_backend.enabled:
+                logger.debug(f'Auth {request.auth}')
+                return self.oauth_backend.authenticator.process_login_request(request)
+            else:
+                logger.debug('Auth not enabled')
+                return RedirectResponse(self.config.routing.home_path)
+
+        async def login_callback(request: Request):
+            logger.info('Processing login callback from Azure AD')
+            logger.debug(f'request url {request.url}')
+            if self.oauth_backend.enabled:
+                return self.oauth_backend.authenticator.process_login_callback(request)
+            else:
+                logger.debug('Auth not enabled')
+                return RedirectResponse(self.config.routing.landing_path)
+
+        routes = [Route(self.config.routing.logout_path, endpoint=logout, methods=['GET'], name='logout'),
+                  Route(self.config.routing.login_path, endpoint=login, methods=['GET'], name='login_oauth'),
+                  Route(self.config.routing.login_redirect_path, endpoint=login_callback, methods=['GET'], name='login_callback')]
+
+        return routes
+
+    def build_auth_ui(self, context: Dict[str, Any] = None):
+        """Build the ui route and static data for the login UI.
+
+        The context kwargs can include ``login`` - button HTML (different to the default Microsoft UI button),
+        ``appname`` - the application name (for the login page title)
+
+        Keyword Args:
+            contex: a dicitionary of predefined parameters to pass to the Jinja2 Login UI template
+        """
+        login_template_path = Path(self.config.login_ui.template_file)
+        user_template_path = Path(self.config.login_ui.user_template_file)
+        login_templates = Jinja2Templates(directory=str(login_template_path.parent))
+        user_templates = Jinja2Templates(directory=str(user_template_path.parent))
+        if context is None:
+            context = {}
+
+        async def login(request: Request, *args, **kwargs):
+            nonlocal context
+            view_context = context.copy()  # type: ignore
+            if not self.oauth_backend.enabled or request.user.is_authenticated:
+                # This is authenticated so go straight to the homepage
+                return RedirectResponse(self.config.routing.home_path)
+            view_context['request'] = request  # type: ignore
+            if 'login' not in view_context or view_context['login'] is None:  # type: ignore
+                post_redirect = self.oauth_backend.authenticator.pop_post_auth_redirect(request)
+                view_context['login'] = self.oauth_backend.authenticator.get_login_button(self.config.routing.login_path, post_redirect)  # type: ignore
+            return login_templates.TemplateResponse(login_template_path.name, view_context)  # type: ignore
+
+        routes = [Route(self.config.routing.landing_path, endpoint=login, methods=['GET'], name='login'),
+                  Mount(self.config.login_ui.static_path, StaticFiles(directory=str(self.config.login_ui.static_directory)), name='static-login')]
+
+        if self.config.routing.user_path:
+
+            @self.auth_required()
+            async def get_user(request: Request):
+                nonlocal context
+                view_context = context.copy()  # type: ignore
+                logger.debug(f'Getting token for {request.user}')
+                view_context['request'] = request
+                if self.oauth_backend.enabled:
+                    logger.debug(f'Auth {request.auth}')
+                    try:
+                        view_context['user'] = self.oauth_backend.authenticator.get_user_from_request(request)
+                        view_context['token'] = self.oauth_backend.authenticator.get_access_token(view_context['user'])
+                    except ValueError:
+                        return self.oauth_backend.authenticator.process_login_request(request, force=True, redirect=request.url.path)
+                else:
+                    logger.debug('Auth not enabled')
+                    view_context['token'] = None
+                return user_templates.TemplateResponse(user_template_path.name, view_context)
+
+            async def get_token(request: Request, auth_state: AuthenticationState = Depends(self.api_auth_scheme)):
+                if not isinstance(auth_state, AuthenticationState):
+                    if hasattr(request.user, 'username'):
+                        user = request.user
+                    else:
+                        auth_state = await self.api_auth_scheme(request)
+                        user = auth_state.user
+                if hasattr(user, 'username'):  # type: ignore
+                    try:
+                        return JSONResponse(self.oauth_backend.authenticator.get_access_token(user))   # type: ignore
+                    except ValueError:
+                        if any([u in request.headers['user-agent'] for u in ['Mozilla', 'Gecko', 'Trident', 'WebKit', 'Presto', 'Edge', 'Blink']]):
+                            return self.oauth_backend.authenticator.process_login_request(request, force=True, redirect=request.url.path)
+                        else:
+                            return JSONResponse('Unable to access token as user has not authenticated via session')
+                return RedirectResponse(f'{self.config.routing.landing_path}?redirect=/me/token')
+
+            routes += [Route(self.config.routing.user_path, endpoint=get_user, methods=['GET'], name='user'),
+                       Route(f'{self.config.routing.user_path}/token', endpoint=get_token, methods=['GET'], name='get-token')]
+
+        return routes
+
+    @property
+    def api_auth_scheme(self):
+        """Get the authentication scheme for the api page."""
+        return self.oauth_backend.api_auth_scheme
---
+++ packages/fastapi-aad-auth/fastapi_aad_auth-0.1.7-py3-none-any.whl/fastapi_aad_auth/config.py
@@ -0,0 +1,246 @@
+"""fastapi_aad_auth configuration options."""
+import importlib
+from typing import Dict, List, Optional
+import uuid
+
+from pkg_resources import resource_filename
+from pydantic import BaseSettings, DirectoryPath, Field, FilePath, HttpUrl, SecretStr, validator
+
+
+def bool_from_env(env_value):
+    """Convert environment variable to boolean."""
+    if isinstance(env_value, str):
+        env_value = env_value.lower() in ['true', '1']
+    return env_value
+
+
+def list_from_env(env_value):
+    """Convert environment variable to list."""
+    if isinstance(env_value, str):
+        env_value = [u for u in env_value.split(',') if u]
+    return env_value
+
+
+def expand_doc(klass):
+    """Expand pydantic model documentation to enable autodoc."""
+    docs = ['', '', 'Keyword Args:']
+    for name, field in klass.__fields__.items():
+        default_str = ''
+        if field.default:
+            default_str = f' [default: ``{field.default}``]'
+        module = field.outer_type_.__module__
+        if module != 'builtins':
+            if hasattr(field.outer_type_, '__origin__'):
+                type_ = f' ({field.outer_type_.__origin__.__name__}) '
+            elif not hasattr(field.outer_type_, '__name__'):
+                type_ = ''
+            else:
+                type_ = f' ({module}.{field.outer_type_.__name__}) '
+        else:
+            type_ = f' ({field.outer_type_.__name__}) '
+        env_var = ''
+        if 'env' in field.field_info.extra:
+            env_var = f' (Can be set by ``{field.field_info.extra["env"]}`` environment variable)'
+        docs.append(f'    {name}{type_}: {field.field_info.description}{default_str}{env_var}')
+    if klass.__doc__ is None:
+        klass.__doc__ = ''
+    klass.__doc__ += '\n'.join(docs)
+    return klass
+
+
+@expand_doc
+class RoutingConfig(BaseSettings):
+    """Configuration for authentication related routing.
+
+    Includes ``logout_path``, ``login_path``, and ``login_redirect_path`` (defaults should
+    be fine for most use-cases).
+
+    There are also the ``landing_path`` for the login page, as well as the ``home_path`` for the home
+    page (defaults to the application root), and the ``post_logout_path`` for any specific routing
+    once a logout has completed.
+    """
+    login_path: str = Field('/login/oauth', description="Path for initiating the AAD oauth call", env='FASTAPI_AUTH_LOGIN_ROUTE')
+    login_redirect_path: str = Field('/login/oauth/redirect', description="Path for handling the AAD redirect call", env='FASTAPI_AUTH_LOGIN_REDIRECT_ROUTE')
+    logout_path: str = Field('/logout', description="Path for processing a logout request", env='FASTAPI_AUTH_LOGOUT_ROUTE')
+    landing_path: str = Field('/login', description="Path for the login UI page", env='FASTAPI_AUTH_LOGIN_UI_ROUTE')
+    user_path: Optional[str] = Field('/me', description="Path for getting the user view", env='FASTAPI_AUTH_USER_ROUTE')
+    home_path: str = Field('/', description="Path for the application home page (default redirect if none provided)",
+                           env='APP_HOME_ROUTE')
+    post_logout_path: str = Field(None, description="Path for the redirect post logout - defaults to the landing path if not provided",
+                                  env='FASTAPI_AUTH_POST_LOGOUT_ROUTE')
+
+    class Config:  # noqa D106
+        env_file = '.env'
+
+    @validator('post_logout_path', always=True, pre=True)
+    def _validate_post_logout_path(cls, value, values):
+        if value is None:
+            value = values.get('landing_path')
+        return value
+
+
+@expand_doc
+class LoginUIConfig(BaseSettings):
+    """Configuration for the application Login UI.
+
+    Includes the application name, template file, error template file,
+    static directory, path to mount the login static information to, and the context.
+    """
+    app_name: str = Field(None, description="Application name to show on the Login UI page", env='APP_NAME')
+    template_file: FilePath = Field(resource_filename('fastapi_aad_auth.ui', 'login.html'),
+                                    description="The jinja2 template to use for the login screen",
+                                    env='FASTAPI_AUTH_LOGIN_TEMPLATE_FILE')
+    error_template_file: FilePath = Field(resource_filename('fastapi_aad_auth.ui', 'error.html'),
+                                          description="The jinja2 template to use for error information",
+                                          env='FASTAPI_AUTH_LOGIN_ERROR_TEMPLATE_FILE')
+    user_template_file: FilePath = Field(resource_filename('fastapi_aad_auth.ui', 'user.html'),
+                                         description="The jinja2 template to use for the user view",
+                                         env='FASTAPI_AUTH_USER_TEMPLATE_FILE')
+    static_directory: DirectoryPath = Field(resource_filename('fastapi_aad_auth.ui', 'static'),
+                                            description="Static path for the UI components",
+                                            env='FASTAPI_AUTH_LOGIN_STATIC_DIR')
+    static_path: str = Field('/static-login',
+                             description="Path to mount the login static dir in",
+                             env='FASTAPI_AUTH_LOGIN_STATIC_PATH')
+    context: Optional[Dict[str, str]] = Field(None, description="Any additional context variables required for the template")
+
+    class Config:  # noqa D106
+        env_file = '.env'
+
+
+@expand_doc
+class AADConfig(BaseSettings):
+    """Configuration for the AAD application.
+
+    Includes expected claims, application registration, etc.
+
+    Can also provide additional client application ids to accept.
+
+    A list of roles can be provided to accept (requires configuring the
+    roles in the AAD application registration manifest)
+    """
+    client_id: SecretStr = Field(..., description="Application Registration Client ID", env='AAD_CLIENT_ID')
+    tenant_id: SecretStr = Field(..., description="Application Registration Tenant ID", env='AAD_TENANT_ID')
+    client_secret: Optional[SecretStr] = Field(None, description="Application Registration Client Secret (if required)", env='AAD_CLIENT_SECRET')
+    scopes: List[str] = Field(["Read"], description="Additional scopes requested")
+    client_app_ids: Optional[List[str]] = Field(None, description="Additional Client App IDs to accept tokens from (when running as a backend service)",
+                                                env='AAD_CLIENT_APP_IDS')
+    strict: bool = Field(True, description="Check that all claims are provided", env='AAD_STRICT_CLAIM_CHECK')
+    api_audience: Optional[str] = Field(None, description="Corresponds to the Application ID URI - used for token validation, defaults to api://{client_id}",
+                                        env='AAD_API_AUDIENCE')
+    redirect_uri: Optional[HttpUrl] = Field(None, description="The redirect URI to use - overwrites the default path handling etc",
+                                            env='AAD_REDIRECT_URI')
+    prompt: Optional[str] = Field(None, description="AAD prompt to request", env='AAD_PROMPT')
+    domain_hint: Optional[str] = Field(None, description="AAD domain hint", env='AAD_DOMAIN_HINT')
+    roles: Optional[List[str]] = Field(None, description="AAD roles required in claims", env='AAD_ROLES')
+
+    class Config:  # noqa D106
+        env_file = '.env'
+
+    _validate_strict = validator('strict', allow_reuse=True)(bool_from_env)
+    _validate_client_app_ids = validator('client_app_ids', allow_reuse=True)(list_from_env)
+    _validate_roles = validator('roles', allow_reuse=True)(list_from_env)
+
+
+@expand_doc
+class AuthSessionConfig(BaseSettings):
+    """Authentication Session configuration.
+
+    Contains secret and salt information (should be set as environment
+    variables in a multi-worker/multi-processing environment to enable
+    authentication across workers)
+    """
+    secret: SecretStr = Field(str(uuid.uuid4()), description="Secret used for encoding authentication information",
+                              env='SESSION_AUTH_SECRET')
+    salt: SecretStr = Field(str(uuid.uuid4()), description="Salt used for encoding authentication information",
+                            env='SESSION_AUTH_SALT')
+
+    class Config:  # noqa D106
+        env_file = '.env'
+
+
+@expand_doc
+class SessionConfig(BaseSettings):
+    """Configuration for session middleware.
+
+    Contains the session secret (should be set as environment variables in
+    a multi-worker/multi-processing environment to enable authentication
+    across workers)
+
+    Provides configuration for the fastapi session middleware
+    """
+    secret_key: SecretStr = Field(str(uuid.uuid4()), description="Secret used for the session middleware",
+                                  env='SESSION_SECRET')
+    session_cookie: str = Field('session', description="Cookie name for the session information",
+                                env='SESSION_COOKIE')
+    same_site: str = Field('lax', description="Cookie validation mode for the session", env='SESSION_SAME_SITE')
+    https_only: bool = Field(False, description="Allow the sessions only with https connections", env='SESSION_HTTPS_ONLY')
+    max_age: int = Field(24*60*60, description="Maximum age for a session", env='SESSION_MAX_AGE')
+
+    class Config:  # noqa D106
+        env_file = '.env'
+
+    _validate_https_only = validator('https_only', allow_reuse=True)(bool_from_env)
+
+
+@expand_doc
+class Config(BaseSettings):
+    """The overall configuration for the AAD authentication."""
+
+    enabled: bool = Field(True, description="Enable authentication", env='FASTAPI_AUTH_ENABLED')
+    aad: AADConfig = Field(None, description="The AAD configuration to use")
+    auth_session: AuthSessionConfig = Field(None, description="The configuration for encoding the authentication information in the session")
+    routing: RoutingConfig = Field(None, description="Configuration for routing")
+    session: SessionConfig = Field(None, description="Configuration for the session middleware")
+    login_ui: LoginUIConfig = Field(None, description="Login UI Configuration")
+    user_klass: type = Field('fastapi_aad_auth.oauth.state:User',
+                             description="User class to use within the AADOAuthBackend, this will be treated as an import path "
+                             "if provided as a string, with the last part the class to load", env='FASTAPI_AUTH_USER_KLASS')
+
+    class Config:  # noqa D106
+        env_file = '.env'
+
+    @validator('aad')
+    def _validate_aad(cls, value):
+        if value is None:
+            value = AADConfig(_env_file=cls.Config.env_file)
+        return value
+
+    @validator('auth_session')
+    def _validate_auth_session(cls, value):
+        if value is None:
+            value = AuthSessionConfig(_env_file=cls.Config.env_file)
+        return value
+
+    @validator('routing')
+    def _validate_routing(cls, value):
+        if value is None:
+            value = RoutingConfig(_env_file=cls.Config.env_file)
+        return value
+
+    @validator('session')
+    def _validate_session(cls, value):
+        if value is None:
+            value = SessionConfig(_env_file=cls.Config.env_file)
+        return value
+
+    @validator('login_ui')
+    def _validate_login_ui(cls, value):
+        if value is None:
+            value = LoginUIConfig(_env_file=cls.Config.env_file)
+        return value
+
+    @validator('user_klass', pre=True, always=True)
+    def _validate_klass(cls, value):
+        if isinstance(value, str):
+            if ':' in value:
+                module_name, klass_name = value.split(':')
+            else:
+                split_path = value.split('.')
+                module_name = '.'.join(split_path[:-1])
+                klass_name = split_path[-1]
+            module = importlib.import_module(module_name)
+            value = getattr(module, klass_name)
+        return value
+
+    _validate_enabled = validator('enabled', allow_reuse=True)(bool_from_env)
