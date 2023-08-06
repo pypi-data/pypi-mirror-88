2cb92373031623ea554dfc62bfc5ff3415c1f08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:01 +0000

    Add package oximachine-featurizer/0.2.12/oximachine_featurizer-0.2.12-py3-none-any.whl

---
+++ packages/oximachine-featurizer/oximachine_featurizer-0.2.12-py3-none-any.whl/oximachine_featurizer/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+
+from .featurize import FeatureCollector, GetFeatures, featurize
