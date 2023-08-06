5ff582930228793d90904ba29822e574e4f82f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:35 +0000

    Add package gfw-creator/0.0.2/gfw_creator-0.0.2.tar.gz

---
+++ packages/gfw-creator/gfw_creator-0.0.2.tar.gz/gfw_creator-0.0.2/gfw_creator/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for GFW Creator."""
+
+__author__ = """Paul Gierz"""
+__email__ = "pgierz@awi.de"
+__version__ = "__version__ = '0.0.2'"
+
+from .gfw_creator import *
