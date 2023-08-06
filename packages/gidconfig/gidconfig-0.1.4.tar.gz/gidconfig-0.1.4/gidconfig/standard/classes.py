a5dbba6de9eb8669a12baf7fa700101a6a68218c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:42 +0000

    Add package gidconfig/0.1.4/gidconfig-0.1.4.tar.gz

---
+++ packages/gidconfig/gidconfig-0.1.4.tar.gz/gidconfig-0.1.4/gidconfig/standard/classes.py
@@ -0,0 +1,138 @@
+
+# region [Imports]
+
+# * Standard Library Imports -->
+import os
+import configparser
+from typing import Union
+from datetime import datetime, timedelta
+
+# * Third Party Imports -->
+from fuzzywuzzy import fuzz
+
+# * Gid Imports -->
+import gidlogger as glog
+
+# * Local Imports -->
+from gidconfig.data.enums import Get
+
+# endregion [Imports]
+
+
+# region [Logging]
+
+log = glog.aux_logger(__name__)
+log.info(glog.imported(__name__))
+
+# endregion [Logging]
+
+
+class ConfigHandler(configparser.ConfigParser):
+    def __init__(self, config_file=None, auto_read=True, auto_save=True, allow_no_value=True, **kwargs):
+        super().__init__(**kwargs, allow_no_value=allow_no_value)
+        self.config_file = '' if config_file is None else config_file
+        self.auto_read = auto_read
+        self.auto_save = auto_save
+        self._method_select = {Get.basic: self.get, Get.boolean: self.getboolean, Get.int: self.getint, Get.list: self.getlist, Get.path: self.get_path, Get.datetime: self.get_datetime}
+
+        if self.auto_read is True:
+            self.read(self.config_file)
+
+    def getlist(self, section, key, delimiter=',', as_set=False):
+        _raw = self.get(section, key).strip()
+        if _raw.endswith(delimiter):
+            _raw = _raw.rstrip(delimiter)
+        if _raw.startswith(delimiter):
+            _raw = _raw.lstrip(delimiter).strip()
+        _out = _raw.replace(delimiter + ' ', delimiter).split(delimiter)
+        _out = list(map(lambda x: x.strip(), _out))
+        if as_set is True:
+            _out = set(_out)
+        return _out
+
+    def list_from_keys_only(self, section, as_set=True):
+        _result = self.options(section)
+        _out = []
+        for line in _result:
+            if line != '':
+                _out.append(line)
+        if as_set is True:
+            _out = set(_out)
+        return _out
+
+    def get_path(self, section, key, cwd_symbol='+cwd+'):
+        _raw_path = self.get(section, key)
+        if cwd_symbol in _raw_path:
+            _out = _raw_path.replace(cwd_symbol, os.getcwd()).replace('\\', '/')
+        elif '+userdata+' in _raw_path:
+            _out = _raw_path.replace('+userdata+', os.getenv('APPDATA')).replace('\\', '/')
+        elif _raw_path == 'notset':
+            _out = None
+        else:
+            _out = os.path.join(_raw_path).replace('\\', '/')
+        return _out
+
+    def _best_fuzzymatch(self, in_term, in_targets: Union[list, set, frozenset, tuple, dict]):
+        # Todo: replace with process.extractOne() from fuzzywuzzy!
+        _rating_list = []
+        for _target in in_targets:
+            _rating_list.append((_target, fuzz.ratio(in_term, _target)))
+        _rating_list.sort(key=lambda x: x[1], reverse=True)
+        log.debug("with a fuzzymatch, the term '%s' was best matched to '%s' with and Levenstein-distance of %s", in_term, _rating_list[0][0], _rating_list[0][1])
+        return _rating_list[0][0]
+
+    def get_timedelta(self, section, key, amount_seperator=' ', delta_seperator=','):
+        _raw_timedelta = self.get(section, key)
+        if _raw_timedelta != 'notset':
+            _raw_timedelta_list = _raw_timedelta.split(delta_seperator)
+            _arg_dict = {'days': 0,
+                         'seconds': 0,
+                         'microseconds': 0,
+                         'milliseconds': 0,
+                         'minutes': 0,
+                         'hours': 0,
+                         'weeks': 0}
+            for raw_delta_data in _raw_timedelta_list:
+                _amount, _typus = raw_delta_data.strip().split(amount_seperator)
+                _key = self._best_fuzzymatch(_typus, _arg_dict)
+                _arg_dict[_key] = float(_amount) if '.' in _amount else int(_amount)
+            return timedelta(**_arg_dict)
+
+    def get_datetime(self, section, key, dtformat=None):
+        _date_time_string = self.get(section, key)
+        if _date_time_string == "notset":
+            return None
+        _dtformat = '%Y-%m-%d %H:%M:%S' if dtformat is None else dtformat
+        return datetime.strptime(_date_time_string, _dtformat).astimezone()
+
+    def set_datetime(self, section, key, datetime_object, dtformat=None):
+        _dtformat = '%Y-%m-%d %H:%M:%S' if dtformat is None else format
+        self.set(section, key, datetime_object.strftime(_dtformat))
+        if self.auto_save is True:
+            self.save()
+
+    def set(self, section, option, value):
+        if isinstance(value, list):
+            value = ', '.join(list(map(str, value)))
+        if not isinstance(value, str):
+            value = str(value)
+        super().set(section, option, value)
+        if self.auto_save is True:
+            self.save()
+
+    def enum_get(self, section: str, option: str, typus: Get = Get.basic):
+        return self._method_select.get(typus, self.get)(section, option)
+
+    def save(self):
+        with open(self.config_file, 'w') as confile:
+            self.write(confile)
+        self.read()
+
+    def read(self, filenames=None):
+        _configfile = self.config_file if filenames is None else filenames
+
+        super().read(self.config_file)
+
+
+if __name__ == '__main__':
+    pass
