575a009a577981507b7ff643a1a146475c8aa377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:43 +0000

    Add package antidote/0.8.0/antidote-0.8.0-cp39-cp39-manylinux2014_i686.whl

---
+++ packages/antidote/antidote-0.8.0-cp39-cp39-manylinux2014_i686.whl/antidote/_providers/__init__.py
@@ -0,0 +1,5 @@
+from .factory import FactoryProvider
+from .indirect import IndirectProvider
+from .lazy import Lazy, LazyProvider
+from .service import ServiceProvider
+from .tag import DuplicateTagError, Tag, Tagged, TagProvider
