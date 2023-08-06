ae615128877e963817dc08f0c4c8101ce2010d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:56 +0000

    Add package mkdocs-ringcentral/0.1.12/mkdocs-ringcentral-0.1.12.tar.gz

---
+++ packages/mkdocs-ringcentral/mkdocs-ringcentral-0.1.12.tar.gz/mkdocs-ringcentral-0.1.12/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.12'
+
+setup(
+    name="mkdocs-ringcentral",
+    version=VERSION,
+    url='https://github.com/byrnereese/mkdocs-ringcentral/',
+    description='RingCentral theme for MkDocs',
+    author='Byrne Reese',
+    author_email='byrne.reese@ringcentral.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'mkdocs>=1.1',
+        'mkdocs-ringcentral-api-index>=0.1.4',
+        'mkdocs-moonstone>=0.1.17'
+    ],
+    python_requires='>=3.0',
+    entry_points={
+        'mkdocs.themes': [
+            'ringcentral = mkdocs_ringcentral',
+        ]
+    },
+    zip_safe=False
+)
