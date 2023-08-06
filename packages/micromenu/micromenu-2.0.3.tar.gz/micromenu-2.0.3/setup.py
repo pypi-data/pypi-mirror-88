13c9156fb3c2cbb82aa290d0f90591af18a0d0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:19 +0000

    Add package micromenu/2.0.3/micromenu-2.0.3.tar.gz

---
+++ packages/micromenu/micromenu-2.0.3.tar.gz/micromenu-2.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="micromenu",
+    version="2.0.3",
+    py_modules=["micromenu"],
+    author="Andreas Ehrlund",
+    author_email="a.ehrlund@gmail.com",
+    description="A minimalistic command line menu",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andli/micromenu",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={"dev": ["wheel", "pytest", "mock", "pytest-cov"],},
+)
