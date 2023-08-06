fb7565a60c001a4b7a6fecccf5b166bae779cca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:10 +0000

    Add package 3dstrudel/0.1.1/3dstrudel-0.1.1-py3-none-any.whl

---
+++ packages/3dstrudel/3dstrudel-0.1.1-py3-none-any.whl/3dstrudel-0.1.1.data/scripts/run_chopModelMapMPI.py
@@ -0,0 +1,7 @@
+#!python
+
+from strudel.chop import chopModelMapMPI
+
+
+if __name__ == '__main__':
+    chopModelMapMPI.main()
---
+++ packages/3dstrudel/3dstrudel-0.1.1-py3-none-any.whl/strudel/bin/strudel_chopModelMapMPI.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from strudel.chop import chopModelMapMPI
+
+
+if __name__ == '__main__':
+    chopModelMapMPI.main()
