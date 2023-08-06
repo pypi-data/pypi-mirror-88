fd1db67b203749b96933c412c9a2880e871dffc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:09 +0000

    Add package office365/0.3.15/office365-0.3.15.tar.gz

---
+++ packages/office365/office365-0.3.15.tar.gz/office365-0.3.15/tests/test_fluent.py
@@ -0,0 +1,24 @@
+# import pytest
+
+
+class TestFluentEntity:
+    def test_subject(self):  # synced
+        assert True
+
+    def test_body(self):  # synced
+        assert True
+
+    def test_attach(self):  # synced
+        assert True
+
+    def test_sign(self):  # synced
+        assert True
+
+    def test_importance(self):  # synced
+        assert True
+
+    def test_categories(self):  # synced
+        assert True
+
+    def test__parse_contacts_to_emails(self):  # synced
+        assert True
---
+++ packages/office365/office365-0.3.15.tar.gz/office365-0.3.15/tests/test_office.py
@@ -0,0 +1,13 @@
+# import pytest
+
+
+class TestOffice:
+    def test_request_token(self):  # synced
+        assert True
+
+    def test__establish_services(self):  # synced
+        assert True
+
+
+class TestAccount:
+    pass
