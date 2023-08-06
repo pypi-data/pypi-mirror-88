703adcc89183349c9d4ce6fe3c4fd8651a0fcadb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:11 +0000

    Add package mkdocs-kpn/0.24.5/mkdocs-kpn-0.24.5.tar.gz

---
+++ packages/mkdocs-kpn/mkdocs-kpn-0.24.5.tar.gz/mkdocs-kpn-0.24.5/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.24.5"
+
+
+def get_long_description():
+    """
+    Return the README.
+    """
+    with open("README.md", encoding="utf8") as f:
+        return f.read()
+
+
+# Load list of dependencies
+with open("requirements.txt") as data:
+    install_requires = [
+        line for line in data.read().split("\n") if line and not line.startswith("#")
+    ]
+
+setup(
+    name="mkdocs-kpn",
+    version=VERSION,
+    url="https://github.com/kpn/mkdocs-kpn-theme",
+    license="MIT",
+    description="A KPN-styles theme for MkDocs",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Santiago Fraire Willemoës",
+    author_email="de-ddci@kpn.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={"mkdocs.themes": ["kpn = kpn_theme",]},
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    keywords=["mkdocs", "documentation", "theme"],
+)
