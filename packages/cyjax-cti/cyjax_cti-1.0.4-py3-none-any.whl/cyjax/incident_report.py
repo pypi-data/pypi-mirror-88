0e30972cdbd8988d43f632c6c3b9b0a70706a82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:15 +0000

    Add package cyjax-cti/1.0.4/cyjax_cti-1.0.4-py3-none-any.whl

---
+++ packages/cyjax-cti/cyjax_cti-1.0.4-py3-none-any.whl/cyjax/incident_report.py
@@ -0,0 +1,31 @@
+from .helpers import DateHelper
+from .resource import Resource
+
+
+class IncidentReport(Resource):
+
+    def __init__(self, api_key=None, api_url=None):
+        """
+        :param api_key: The API key.
+        :param api_url: The API URL.
+        """
+        super(IncidentReport, self).__init__(api_key=api_key, api_url=api_url)
+
+    def list(self, query=None, since=None, until=None):
+        """
+        Returns incident reports.
+        :param query: The search query.
+        :type query: str, optional
+        :param since: The start date time.
+        :type since: (datetime, timedelta, str), optional
+        :param until: The end date time.
+        :type until:  (datetime, timedelta, str), optional
+        :return: The list of incident reports.
+        :rtype list
+        """
+
+        params = DateHelper.build_date_params(since=since, until=until)
+        if query:
+            params.update({'query': query})
+
+        return self.paginate(endpoint='report/incident', params=params)
---
+++ packages/cyjax-cti/cyjax_cti-1.0.4-py3-none-any.whl/cyjax/tor_exit_node.py
@@ -0,0 +1,31 @@
+from .helpers import DateHelper
+from .resource import Resource
+
+
+class TorExitNode(Resource):
+
+    def __init__(self, api_key=None, api_url=None):
+        """
+        :param api_key: The API key.
+        :param api_url: The Cyjax API url.
+        """
+        super(TorExitNode, self).__init__(api_key=api_key, api_url=api_url)
+
+    def list(self, query=None, since=None, until=None):
+        """
+        Returns TOR exit nodes.
+        :param query: The search query.
+        :type query: str, optional
+        :param since: The start date time.
+        :type since: (datetime, timedelta, str), optional
+        :param until: The end date time.
+        :type until:  (datetime, timedelta, str), optional
+        :return: The list of TOR exit nodes.
+        :rtype list
+        """
+
+        params = DateHelper.build_date_params(since=since, until=until)
+        if query:
+            params.update({'query': query})
+
+        return self.paginate(endpoint='blacklists/tor-node', params=params)
