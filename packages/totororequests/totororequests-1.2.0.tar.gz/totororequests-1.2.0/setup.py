f08438025b6c0dcea034f5f4cda739d67f8f46d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:26 +0000

    Add package totororequests/1.2.0/totororequests-1.2.0.tar.gz

---
+++ packages/totororequests/totororequests-1.2.0.tar.gz/totororequests-1.2.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="totororequests",
+    version="1.2.0",
+    author="Victor Paynat-Sautivet",
+    author_email="contact@ray0.be",
+    description="Smart Python module for sending HTTP(S) requests through Tor network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ray0be/totororequests",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'stem',
+        'fake_useragent'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    python_requires='>=3.6',
+)
