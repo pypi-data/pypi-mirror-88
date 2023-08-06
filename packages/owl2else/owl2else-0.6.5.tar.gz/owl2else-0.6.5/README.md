79bcb6e451fe84505629db792c3ca341dc0d301f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:58 +0000

    Add package owl2else/0.6.5/owl2else-0.6.5.tar.gz

---
+++ packages/owl2else/owl2else-0.6.5.tar.gz/owl2else-0.6.5/README.md
@@ -0,0 +1,8 @@
+owl2else
+=========
+
+Scripts to transform ontology OWL 2 files to other languages
+  
+   - owl2clips transforms OWL 2 ontologies to COOL CLIPS language
+   - owl2rdflib transforms OWL 2 to a python file with a RDFlib 
+     ClosedNamespace object with the vocabulary of the ontology
---
+++ packages/owl2else/owl2else-0.6.5.tar.gz/owl2else-0.6.5/setup.py
@@ -0,0 +1,42 @@
+"""
+.. module:: setup.py
+
+setup.py
+******
+
+:Description: setup.py
+
+    Different Auxiliary functions used for different purposes
+
+:Authors:
+    bejar
+
+:Version: 
+
+:Date:  10/08/2020
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="owl2else", # Replace with your own username
+    version="0.6.5",
+    author="Javier Bejar",
+    author_email="bejar@cs.upc.edu",
+    description="Utilities to transform ontology OWL2 files to other formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bejar/owl2else",
+    packages=['owl2conv'],
+    scripts=['bin/owl2clips', 'bin/owl2rdflib'],
+    install_requires=['rdflib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
