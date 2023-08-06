117b992e29e0fbe37acd1cf4d8ea3a8b33f85b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:37 +0000

    Add package odoo11-addon-mail-environment/11.0.1.1.0.99.dev5/odoo11_addon_mail_environment-11.0.1.1.0.99.dev5-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-mail-environment/odoo11_addon_mail_environment-11.0.1.1.0.99.dev5-py2.py3-none-any.whl/odoo/addons/mail_environment/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2012-2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Mail configuration with server_environment',
+    'version': '11.0.1.1.0',
+    'category': 'Tools',
+    'summary': 'Configure mail servers with server_environment_files',
+    'author': "Camptocamp, Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'website': 'https://github.com/OCA/server-env',
+    'preloadable': False,
+    'depends': [
+        'fetchmail',
+        'server_environment',
+    ],
+    'data': [],
+}
