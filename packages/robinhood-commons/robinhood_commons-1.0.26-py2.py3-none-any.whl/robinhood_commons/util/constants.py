16de54ac6aa6caa9fcebba8d806ad74abd497090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:51 +0000

    Add package robinhood-commons/1.0.26/robinhood_commons-1.0.26-py2.py3-none-any.whl

---
+++ packages/robinhood-commons/robinhood_commons-1.0.26-py2.py3-none-any.whl/robinhood_commons/util/constants.py
@@ -0,0 +1,16 @@
+
+MAIN_EMAIL: str = 'mhowell234@gmail.com'
+USERS_KEY: str = 'users'
+DEFAULT_DELIMITER: str = ':'
+
+TESTING: bool = True
+DEBUG: bool = False
+F_IT_MODE: bool = False
+PROFILE_INFO: bool = False
+
+RANDOM_INVESTORS: int = 7
+BUY_ONE_SELL_ONE_MODE: bool = True
+
+
+
+
