fbacd43c4dd18119d0e16d2a5dd4d72006a99f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:08 +0000

    Add package xiaobaiauto2/0.1.6.0/xiaobaiauto2-0.1.6.0.tar.gz

---
+++ packages/xiaobaiauto2/xiaobaiauto2-0.1.6.0.tar.gz/xiaobaiauto2-0.1.6.0/xiaobaiauto2/test/AppTest/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+__author__ = 'Tser'
+__email__ = '807447312@qq.com'
+__project__ = 'xiaobaiauto2'
+__script__ = '__init__.py'
+__create_time__ = '2020/7/17 22:04'
+
+'''
+    需要在报告中获取错误问题截图时，需要带着conftest.py文件
+'''
