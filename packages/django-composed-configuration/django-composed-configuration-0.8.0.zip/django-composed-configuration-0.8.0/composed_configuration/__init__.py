14ec83b75b0607463612cff2ac898cf0c08defda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:04 +0000

    Add package django-composed-configuration/0.8.0/django-composed-configuration-0.8.0.zip

---
+++ packages/django-composed-configuration/django-composed-configuration-0.8.0.zip/django-composed-configuration-0.8.0/composed_configuration/__init__.py
@@ -0,0 +1,49 @@
+from ._allauth import AllauthMixin
+from ._base import ComposedConfiguration, ConfigMixin
+from ._celery import CeleryMixin
+from ._configuration import (
+    DevelopmentBaseConfiguration,
+    HerokuProductionBaseConfiguration,
+    ProductionBaseConfiguration,
+    TestingBaseConfiguration,
+)
+from ._cors import CorsMixin
+from ._database import DatabaseMixin
+from ._debug import DebugMixin
+from ._django import DjangoMixin
+from ._email import ConsoleEmailMixin, SmtpEmailMixin
+from ._extensions import ExtensionsMixin
+from ._filter import FilterMixin
+from ._https import HttpsMixin
+from ._logging import LoggingMixin
+from ._rest_framework import RestFrameworkMixin
+from ._sentry import SentryConfig
+from ._static import StaticFileMixin, WhitenoiseStaticFileMixin
+from ._storage import MinioStorageMixin, S3StorageMixin
+
+__all__ = [
+    'AllauthMixin',
+    'CeleryMixin',
+    'ComposedConfiguration',
+    'ConfigMixin',
+    'ConsoleEmailMixin',
+    'CorsMixin',
+    'DatabaseMixin',
+    'DebugMixin',
+    'DevelopmentBaseConfiguration',
+    'DjangoMixin',
+    'ExtensionsMixin',
+    'FilterMixin',
+    'HerokuProductionBaseConfiguration',
+    'HttpsMixin',
+    'LoggingMixin',
+    'MinioStorageMixin',
+    'ProductionBaseConfiguration',
+    'RestFrameworkMixin',
+    'S3StorageMixin',
+    'SentryConfig',
+    'SmtpEmailMixin',
+    'StaticFileMixin',
+    'TestingBaseConfiguration',
+    'WhitenoiseStaticFileMixin',
+]
