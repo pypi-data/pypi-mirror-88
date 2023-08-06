38a65a9fa030b053bf8258eef97f93c7ca106c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:20 +0000

    Add package async-rediscache/0.2.0/async-rediscache-0.2.0.tar.gz

---
+++ packages/async-rediscache/async-rediscache-0.2.0.tar.gz/async-rediscache-0.2.0/setup.py
@@ -0,0 +1,42 @@
+"""
+This file is used to install this package via the pip tool.
+
+It keeps track of versioning, as well as dependencies and
+what versions of python we support.
+"""
+from setuptools import find_packages, setup
+
+
+setup(
+    name="async-rediscache",
+    version="0.2.0",
+    description="An easy to use asynchronous Redis cache",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Python Discord",
+    author_email="staff@pythondiscord.com",
+    url="https://github.com/python-discord/async-rediscache",
+    license="MIT",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Framework :: AsyncIO",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        "aioredis>=1"
+    ],
+    python_requires='~=3.7',
+    extras_require={
+        "fakeredis": ["fakeredis[lua]>=1.4.4"],
+    },
+    include_package_data=True,
+    zip_safe=False
+)
