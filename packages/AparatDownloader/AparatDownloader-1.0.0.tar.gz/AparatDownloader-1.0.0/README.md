088106cc5ad5c00172229995e98a72f3c535b572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:15 +0000

    Add package AparatDownloader/1.0.0/AparatDownloader-1.0.0.tar.gz

---
+++ packages/AparatDownloader/AparatDownloader-1.0.0.tar.gz/AparatDownloader-1.0.0/README.md
@@ -0,0 +1,11 @@
+## Downloader Video from aparat.com
+
+****
+
+```
+from aparatdownloader import Main
+
+a = Main('url','quality')
+a.download()
+```
+
---
+++ packages/AparatDownloader/AparatDownloader-1.0.0.tar.gz/AparatDownloader-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AparatDownloader",
+    version="1.0.0",
+    author="Pouria Shafiee",
+    author_email="pouriashafieeit@gmail.com",
+    description="Download aparat videos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Udmx/AparatDownloader",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests', 'beautifulsoap4'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
