b8bc2de7b2e9c11a5381743d06f877e6e6b106ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:36 +0000

    Add package alpacka/0.0.5/alpacka-0.0.5.tar.gz

---
+++ packages/alpacka/alpacka-0.0.5.tar.gz/alpacka-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='alpacka',
+      version='0.0.5',
+      description='The alpacka Python package, used to extract and visualize metadata from text data sets',
+      author='Fredrik Möller',
+      author_email='fredrikmoller@recordefuture.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/BernhardMoller/alpacka',
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent"],
+      licence='LICENCE.txt',
+      python_requires='>=3.6',
+      )
