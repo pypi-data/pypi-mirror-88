a5f09dde118a1be22a22364d19f99fd556eaa614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:19 +0000

    Add package kedro-viz/3.8.0/kedro_viz-3.8.0-py3.7.egg

---
+++ packages/kedro-viz/kedro_viz-3.8.0-py3.7.egg/kedro_viz/html/precache-manifest.ebbfa8a386dfc4b6d3e678887952ae59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6ff3d23d1bfadf161f760bcb0fd67b",
+    "url": "./e97c20903f05d7d909a5.worker.js"
+  },
+  {
+    "revision": "34e3de52f6424bbc7db746837ad8b767",
+    "url": "./e97c20903f05d7d909a5.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9d563615cb3e60f50fda6b5eb56043",
+    "url": "./index.html"
+  },
+  {
+    "revision": "312369e57064d01077b4",
+    "url": "./static/css/2.b84a31d4.chunk.css"
+  },
+  {
+    "revision": "7c0740e2810326c5fdf9",
+    "url": "./static/css/main.87919280.chunk.css"
+  },
+  {
+    "revision": "312369e57064d01077b4",
+    "url": "./static/js/2.163a9cef.chunk.js"
+  },
+  {
+    "revision": "feca05db5192bac392586f0c8dfb2e62",
+    "url": "./static/js/2.163a9cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0740e2810326c5fdf9",
+    "url": "./static/js/main.3b736098.chunk.js"
+  },
+  {
+    "revision": "34e3de52f6424bbc7db746837ad8b767",
+    "url": "./static/js/main.3b736098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2e6815a1345d1aeee5",
+    "url": "./static/js/runtime-main.7709a6be.js"
+  },
+  {
+    "revision": "6259cc84bb236512f69bc1d20397254c",
+    "url": "./static/media/brodyquest.6259cc84.mp3"
+  },
+  {
+    "revision": "e702bd5fc3663eda7c4459232f5a76c0",
+    "url": "./static/media/easter-egg.e702bd5f.png"
+  }
+]);
