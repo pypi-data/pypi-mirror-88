321ec208a0fa6913155470a0979819048124d8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:12 +0000

    Add package resolwe-bio/34.3.0/resolwe-bio-34.3.0.tar.gz

---
+++ packages/resolwe-bio/resolwe-bio-34.3.0.tar.gz/resolwe-bio-34.3.0/resolwe_bio/tools/unique_sample_names.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# XXX: Refactor to a comand line tool and remove pylint disable
+"""Check if sample names are unique."""
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description="Check if sample names are unique")
+parser.add_argument("samples", help="All samples")
+args = parser.parse_args()
+
+samples = args.samples.split(",")
+
+if len(samples) > len(set(samples)):
+    print(json.dumps({"proc.error": "Sample names must be unique."}))
