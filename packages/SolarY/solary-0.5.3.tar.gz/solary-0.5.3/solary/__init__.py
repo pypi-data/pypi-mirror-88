bd27d2ec83266649af4468b061e0b7b6ab7e458c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:15 +0000

    Add package SolarY/0.5.3/solary-0.5.3.tar.gz

---
+++ packages/SolarY/solary-0.5.3.tar.gz/solary-0.5.3/solary/__init__.py
@@ -0,0 +1,15 @@
+import os
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+__project__ = "SolarY"
+__author__ = "Dr.-Ing. Thomas Albin"
+__version__ = "0.5.3"
+
+from . import _config
+
+from . import tests
+
+from . import auxiliary
+from . import general
+from . import asteroid
+from . import neo
