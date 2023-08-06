d00586d7b44b97cd92337b240593a1db4382ec07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:40 +0000

    Add package drf-keycloak-auth/0.0.1.dev0/drf_keycloak_auth-0.0.1.dev0.tar.gz

---
+++ packages/drf-keycloak-auth/drf_keycloak_auth-0.0.1.dev0.tar.gz/drf_keycloak_auth-0.0.1.dev0/drf_keycloak_auth/__init__.py
@@ -0,0 +1,9 @@
+"""Metadata for Authicola package."""
+__title__ = 'drf_keycloak_auth'
+__description__ = \
+    'A convenience libary for authenticating users from Keycloak access tokens'
+__url__ = 'https://gitlab.com/garyburgmann/drf-keycloak-auth'
+__version__ = '0.0.1.dev0'
+__author__ = 'Gary Burgmann'
+__author_email__ = 'garyburgmann@gmail.com'
+__license__ = 'MIT'
