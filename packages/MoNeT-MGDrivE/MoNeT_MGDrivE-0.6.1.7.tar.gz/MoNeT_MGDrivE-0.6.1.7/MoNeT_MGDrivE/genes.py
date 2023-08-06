3dfb870f4d4e0174830a037f1e15eab0665829ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:37 +0000

    Add package MoNeT-MGDrivE/0.6.1.7/MoNeT_MGDrivE-0.6.1.7.tar.gz

---
+++ packages/MoNeT-MGDrivE/MoNeT_MGDrivE-0.6.1.7.tar.gz/MoNeT_MGDrivE-0.6.1.7/MoNeT_MGDrivE/genes.py
@@ -0,0 +1,23 @@
+
+import re
+
+
+def countGeneAppearances(genotypes, gene, pos):
+    # Split genotypes
+    splitGenotypes = [list(genes) for genes in genotypes]
+    # Count
+    appearances = []
+    for p in pos:
+        slot = [gene[p] for gene in splitGenotypes]
+        matches = re.finditer(gene, ''.join(slot))
+        appearances.extend([match.start() for match in matches])
+    appearances.sort()
+    return appearances
+
+
+def flatten(ls): return [item for sublist in ls for item in sublist]
+
+
+def aggregateGeneAppearances(genotypes, genes):
+    gcnt = [countGeneAppearances(genotypes, gn[0], gn[1]) for gn in genes]
+    return sorted(flatten(gcnt))
