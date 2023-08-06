f2c857c569081e2d129d60542e747e7c2e6812ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:44 +0000

    Add package flask-resource-chassis/1.0.1rc7/flask_resource_chassis-1.0.1rc7.tar.gz

---
+++ packages/flask-resource-chassis/flask_resource_chassis-1.0.1rc7.tar.gz/flask_resource_chassis-1.0.1rc7/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='flask_resource_chassis',
+      version='1.0.1.rc7',
+      description='Extends flask restful functionality',
+      packages=['flask_resource_chassis', 'oauth_client'],
+      zip_safe=False)
