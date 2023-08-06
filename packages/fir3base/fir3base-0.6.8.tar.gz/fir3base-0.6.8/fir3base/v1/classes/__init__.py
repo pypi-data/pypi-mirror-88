ea97daf72cd158966bf39ec1edb34f068a816352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:22 +0000

    Add package fir3base/0.6.8/fir3base-0.6.8.tar.gz

---
+++ packages/fir3base/fir3base-0.6.8.tar.gz/fir3base-0.6.8/fir3base/v1/classes/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from fir3base.v1.classes.firebase import Firebase
+from fir3base.v1.classes import requests
+from fir3base.v1.classes.config import r3sponse
