f620b690a2689bf8127b4dd7c9d3da786f6fe15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:10 +0000

    Add package pyclim-engine/0.0.1/pyclim_engine-0.0.1-py3-none-any.whl

---
+++ packages/pyclim-engine/pyclim_engine-0.0.1-py3-none-any.whl/pyclim/__init__.py
@@ -0,0 +1,11 @@
+"""
+Easy to use library for create CLI-based games in Python
+This project is for fun and learn purposes
+
+Official website: https://starlkyt.github.io/Pyclim-Engine/
+Github repository: https://github.com/StarlkYT/Pyclim/tree/master
+"""
+from pyclim.objects.screen import Screen
+from pyclim.objects.controller import Controller
+from pyclim.utils.input import Input
+from pyclim.prefabs.player import Player
