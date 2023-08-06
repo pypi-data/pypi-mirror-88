cc6a4456f404d7588b58aac67c1079e57319a0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:06 +0000

    Add package edx-sga/0.13.1/edx_sga-0.13.1-py3-none-any.whl

---
+++ packages/edx-sga/edx_sga-0.13.1-py3-none-any.whl/edx_sga/tests/test_utils.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Tests for SGA utility functions
+"""
+from __future__ import absolute_import
+
+import pytest
+
+import pytz
+from edx_sga.tests.common import is_near_now
+from edx_sga.utils import is_finalized_submission, utcnow
+
+
+@pytest.mark.parametrize(
+    'submission_data,expected_value', [
+        ({'answer': {'finalized': True}}, True),
+        ({'answer': {'filename': 'file.txt'}}, True),
+        ({'answer': {}}, True),
+        ({'answer': {'finalized': False}}, False),
+        ({'answer': None}, False),
+        ({}, False),
+    ]
+)
+def test_is_finalized_submission(submission_data, expected_value):
+    """Test for is_finalized_submission"""
+    assert is_finalized_submission(submission_data) is expected_value
+
+
+def test_utcnow():
+    """
+    tznow should return a datetime object in UTC
+    """
+    now = utcnow()
+    assert is_near_now(now)
+    assert now.tzinfo.zone == pytz.utc.zone
