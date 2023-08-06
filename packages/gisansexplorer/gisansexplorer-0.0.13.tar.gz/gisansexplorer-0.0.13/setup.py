b2b62d3a3cc80e6f453655d250cd1de376462c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:11 +0000

    Add package gisansexplorer/0.0.13/gisansexplorer-0.0.13.tar.gz

---
+++ packages/gisansexplorer/gisansexplorer-0.0.13.tar.gz/gisansexplorer-0.0.13/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import distutils
+import os
+import sys
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gisansexplorer", # Replace with your own username
+    version="0.0.13",
+    #scripts=['pippo.bat', 'pippo.py', 'pippo'],
+    #scripts=['pippo.py', 'pippo'],
+    #console=['pippo'],
+    author="Juan M. Carmona Loaiza",
+    author_email="j.carmona.loaiza@fz-juelich.de",
+    description="Nicos data reduction and visualisation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/juanmcloaiza/gisansexplorer",
+    packages=setuptools.find_packages(),
+    #packages=['gisansexplorer'],
+    #package_dir={'gisansexplorer': 'gisansexplorer'},
+    #package_data={'gisansexplorer': ['resources/*']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+    'console_scripts':
+        ['pippo=gisansexplorer:entry_point']
+                },
+    python_requires='>=3.6',
+    data_files=[('resources', ['gisansexplorer/resources/Icon.png', 'gisansexplorer/resources/show_test_map.npy'])]#,
+                #('test_data', ['notToVersion/Archive/*.dat'])],
+    #package_data={
+    #    'gisansexplorer': ['gisansexplorer/resources/show_test_map.npy'],
+    #}
+)
