8f80a5e4a5a5aa5572c01f08de69e88880c74f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:15 +0000

    Add package robinhood-commons/1.0.22/robinhood_commons-1.0.22-py2.py3-none-any.whl

---
+++ packages/robinhood-commons/robinhood_commons-1.0.22-py2.py3-none-any.whl/robinhood_commons/util/constants.py
@@ -0,0 +1,22 @@
+
+BASE_PATH: str = '/robinhood_data/'
+LOG_DIR: str = f'output/logs/'
+TMP_DIR = f'tmp/'
+
+STOCKS_FILE_PATH: str = f'{TMP_DIR}stocks.json'
+
+MAIN_EMAIL: str = 'mhowell234@gmail.com'
+USERS_KEY: str = 'users'
+DEFAULT_DELIMITER: str = ':'
+
+TESTING: bool = True
+DEBUG: bool = False
+F_IT_MODE: bool = False
+PROFILE_INFO: bool = False
+
+RANDOM_INVESTORS: int = 7
+BUY_ONE_SELL_ONE_MODE: bool = True
+
+
+
+
