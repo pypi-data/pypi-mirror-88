97ec77bc43d6f45e2f985461976cbb648e4a4503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:09 +0000

    Add package ipycytoscape/1.1.0/ipycytoscape-1.1.0-py2.py3-none-any.whl

---
+++ packages/ipycytoscape/ipycytoscape-1.1.0-py2.py3-none-any.whl/ipycytoscape/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) 2020, QuantStack and ipycytoscape Contributors
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+from .cytoscape import *
+
+from ._version import __version__, version_info
+
+from .nbextension import _jupyter_nbextension_paths
