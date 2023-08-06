40a41a704f73d2ab663ac7b84cd231289310a3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:33 +0000

    Add package zbmain/0.0.9/zbmain-0.0.9.tar.gz

---
+++ packages/zbmain/zbmain-0.0.9.tar.gz/zbmain-0.0.9/README.md
@@ -0,0 +1,15 @@
+# zbmain
+
+个人代码库
+
+持续新增。先持续0.x.x，待完整再正式发布更新1.0
+
+## 安装说明
+
+    pip install zbmain
+
+
+## 使用方法
+
+    import zbmain
+
---
+++ packages/zbmain/zbmain-0.0.9.tar.gz/zbmain-0.0.9/zbmain/utils/file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : zb
+# @Time    : 2020/11/18 22:27
+# @Blog    : https://blog.zbmain.com
+
+
+def write(char, filepath='tmp.txt', mode='w'):
+    with open(filepath, mode) as file:
+        file.write(str(char) + "\n")
