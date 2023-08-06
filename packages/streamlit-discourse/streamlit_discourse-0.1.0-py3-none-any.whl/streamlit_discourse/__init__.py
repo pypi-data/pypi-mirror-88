455d97d4acc99d500edf6c469022e77b46ac44e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:40 +0000

    Add package streamlit-discourse/0.1.0/streamlit_discourse-0.1.0-py3-none-any.whl

---
+++ packages/streamlit-discourse/streamlit_discourse-0.1.0-py3-none-any.whl/streamlit_discourse/__init__.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from streamlit.components.v1 import declare_component
+
+_RELEASE = False
+
+if not _RELEASE:
+    _component_func = declare_component("streamlit_discourse", url="http://localhost:3001")
+else:
+    _component_path = (Path(__file__).parent/"frontend"/"build").resolve()
+    _component_func = declare_component("streamlit_discourse", path=_component_path)
+
+
+def st_discourse(
+    discourse_url,
+    topic_id,
+    key=None
+) -> None:
+    """Display a Discourse topic.
+    
+    Parameters
+    ----------
+    discourse_url : str
+    topic_id : int
+    key : str or None
+        An optional string to use as the unique key for the widget.
+        If this is omitted, a key will be generated for the widget
+        based on its content. Multiple widgets of the same type may
+        not share the same key.
+
+    """
+    _component_func(
+        discourseUrl=discourse_url,
+        topicId=topic_id,
+        key=key,
+        default=None
+    )
