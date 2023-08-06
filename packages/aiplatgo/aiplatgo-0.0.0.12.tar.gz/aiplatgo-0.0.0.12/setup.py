3ad1ce91555a240725f702a3ce9ae886efab094a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package aiplatgo/0.0.0.12/aiplatgo-0.0.0.12.tar.gz

---
+++ packages/aiplatgo/aiplatgo-0.0.0.12.tar.gz/aiplatgo-0.0.0.12/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'aiplatgo',
+  packages = ['aiplatgo'],
+  package_dir = {
+    'aiplatgo': 'aiplatgo'
+  },
+  version = '0.0.0.12',
+  description = 'CLI wrapper for `gcloud ai-platform`',
+  author = 'Brookie Guzder-Williams',
+  author_email = 'brook.williams@gmail.com',
+  url = 'https://github.com/brookisme/aiplatgo',
+  download_url = 'https://github.com/brookisme/aiplatgo/tarball/0.1',
+  keywords = ['python','tensorflow','gcloud','ai-platform','CLI'],
+  include_package_data=False,
+  package_data={
+    'aiplatgo': ['config/*.yaml']
+  },
+  data_files=[
+    (
+      'config',[]
+    )
+  ],
+  classifiers = [],
+  entry_points={
+    'console_scripts': [
+      'aiplatgo=aiplatgo.cli:cli'
+    ]
+  }
+)
