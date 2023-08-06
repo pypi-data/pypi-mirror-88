f03c20f8d47552424082c1480196b998cab1c3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:52 +0000

    Add package oep-client/0.5.3/oep-client-0.5.3.tar.gz

---
+++ packages/oep-client/oep-client-0.5.3.tar.gz/oep-client-0.5.3/oep_client/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+__version__ = "0.5.3"
+
+from .client import OepClient
+from .script import setup_logging, logger
+from .test import test as testscript
---
+++ packages/oep-client/oep-client-0.5.3.tar.gz/oep-client-0.5.3/oep_client/test.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+import datetime
+from .client import OepClient
+import pandas as pd
+import random
+from argparse import ArgumentParser
+
+
+example_metadata = {
+    "id": "test",
+    "description": "Test Table",
+    "keywords": ["test"],
+    "resources": [
+        {
+            "name": "test",
+            "schema": {
+                "fields": [
+                    {
+                        "name": "field1",
+                        "type": "string",
+                        "description": "column description",
+                    },
+                    {"name": "field2", "type": "integer", "unit": "none"},
+                ],
+                "foreignKeys": [],
+            },
+        }
+    ],
+}
+
+exacmple_record = {"field1": "test", "field2": 999}
+
+
+def test(client, test_rows=None, batch_size=None):
+    test_rows = test_rows or 1
+    example_data = pd.DataFrame([exacmple_record] * test_rows)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    tablename = "test_%s_%d" % (timestamp, random.randint(100000, 999999))
+    self.settings["tablename"] = tablename
+    client.create(metadata=example_metadata)
+    try:
+        client.upload_data(dataframe=example_data, batch_size=batch_size)
+        client.update_metadata(metadata=example_metadata)
+        data = client.download_data()
+        metadata = client.download_metadata()
+        print(len(data))
+        print(data[0])
+        print(metadata)
+    finally:
+        client.delete()
