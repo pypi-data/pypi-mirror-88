61256e90f0ee2fefe1554b850e831a2e13ddc47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:56 +0000

    Add package miney/0.2.1/miney-0.2.1.tar.gz

---
+++ packages/miney/miney-0.2.1.tar.gz/miney-0.2.1/miney/__init__.py
@@ -0,0 +1,18 @@
+"""
+Miney is the python interface to minetest
+"""
+from .typing import *
+from .minetest import Minetest
+from .player import Player
+from .chat import Chat
+from .node import Node
+from .lua import Lua
+from .inventory import Inventory
+from .exceptions import *
+from .tool import ToolIterable
+from .player import PlayerIterable
+from .helper import *
+
+__version__ = "0.2.1"
+default_playername = "MineyPlayer"
+
