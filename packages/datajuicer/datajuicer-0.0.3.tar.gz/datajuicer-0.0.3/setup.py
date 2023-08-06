711f4b3b8a2aa32fdca30f4cf692bc32740bab2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:19 +0000

    Add package datajuicer/0.0.3/datajuicer-0.0.3.tar.gz

---
+++ packages/datajuicer/datajuicer-0.0.3.tar.gz/datajuicer-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    version="0.0.3",
+    name="datajuicer", # Replace with your own username
+    author="Fynn Firouz Faber",
+    author_email="faberf@ethz.ch",
+    description="A package for data manipulation and function execution.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/faberf/datajuicer/archive/v0.0.3.tar.gz",
+    packages=["datajuicer"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
