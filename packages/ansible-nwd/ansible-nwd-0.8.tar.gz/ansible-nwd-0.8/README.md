affe0b0651dc4f9b336fae3d6716ab33d5f59086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:40 +0000

    Add package ansible-nwd/0.8/ansible-nwd-0.8.tar.gz

---
+++ packages/ansible-nwd/ansible-nwd-0.8.tar.gz/ansible-nwd-0.8/README.md
@@ -0,0 +1,23 @@
+# Ansible Never Write the Doc
+
+[![Build Status](https://travis-ci.com/VasseurLaurent/ansible-nwd.svg?branch=master)](https://travis-ci.com/VasseurLaurent/ansible-nwd) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) [![PyPI version](https://badge.fury.io/py/ansible-nwd.svg)](https://badge.fury.io/py/ansible-nwd) ![Upload Pypi & build docker](https://github.com/VasseurLaurent/ansible-nwd/workflows/Upload%20Pypi%20&%20build%20docker/badge.svg)
+
+## Introduction
+
+``Ansible Never Write the Doc`` provides you an automatic way to create Ansible roles documentation.
+
+It parses all Ansible roles folders and gathers information to write them into a ``Readme.md`` for you.
+
+``Ansible-nwd`` is also compatible with ``molecule`` , it will parse your different scenarios and write them into your documentation file.
+
+## Resources
+
+* Documentation : https://docs.ansible-nwd.com
+
+* Pip download : https://pypi.org/project/ansible-nwd/
+
+* Docker image : https://hub.docker.com/repository/docker/laurentvasseur/ansible-nwd
+
+## Example
+
+In this repo, you can find an example of Ansible role and all ``Ansible-nwd`` pattern available at the following path : ``examples/python3``
---
+++ packages/ansible-nwd/ansible-nwd-0.8.tar.gz/ansible-nwd-0.8/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ansible-nwd',
+    packages=setuptools.find_packages(),
+    version='0.8',
+    license='MIT License',
+    data_files=[("ansible_nwd",
+                 ["ansible_nwd/markdown.j2"])],
+    include_package_data=True,
+    description='Ansible role automatic documentation',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    author='VASSEUR Laurent',
+    author_email='mail.laurentvasseur@gmail.com',
+    url='https://github.com/VasseurLaurent/ansible-nwd',
+    download_url='https://github.com/VasseurLaurent/ansible-nwd/archive/v0.7.tar.gz',
+    keywords=['ansible', 'documentation', 'automation'],
+    install_requires=[
+        'ruamel.yaml',
+        'ruamel.yaml.clib',
+        'jinja2-time',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Environment :: Console',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+        'Topic :: Documentation',
+
+    ],
+    entry_points={
+        'console_scripts': [
+            'ansible-nwd=ansible_nwd.main:main',
+        ]
+    },
+    python_requires='>=3.6',
+)
