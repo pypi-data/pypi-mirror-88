2504179c069f8bf549e823bea0449f547add1355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:46 +0000

    Add package featurewiz/0.0.7/featurewiz-0.0.7.tar.gz

---
+++ packages/featurewiz/featurewiz-0.0.7.tar.gz/featurewiz-0.0.7/featurewiz/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#     featurewiz - fast feature selection using one line of code
+#     Python v3.6+
+#     Created by Ram Seshadri
+#     Licensed under Apache License v2
+################################################################################
+# Version
+from .__version__ import __version__
+from .featurewiz import featurewiz, convert_all_object_columns_to_numeric
+from .featurewiz import split_one_field_into_many, add_aggregate_primitive_features
+from .featurewiz import create_time_series_features
+if __name__ == "__main__":
+    version_number = __version__
+    print("""Running featurewiz version: %s. Call by using:
+                 features = featurewiz(dataname, target, corr_limit=0.70,
+                                verbose=2, sep=',', header=0)""" %version_number)
+else:
+    version_number = __version__
+    print("""Imported featurewiz version: %s. Call by using:
+                 features = featurewiz(dataname, target, corr_limit=0.70,
+                                verbose=2,  sep=',', header=0)""" %version_number)
+################################################################################
