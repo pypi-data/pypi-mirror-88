c3adbb27eec8b616f6234e5ea5c516d53b4636a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:59 +0000

    Add package scam/0.3.0/scam-0.3.0.tar.gz

---
+++ packages/scam/scam-0.3.0.tar.gz/scam-0.3.0/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md")) as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scam",
+    version="0.3.0", 
+    author="Codalyzers",
+    author_email="djoni.austin@gmail.com",
+    description="Source Code Analyzing Maching is an pplication for the analysis of similarities between separate files. Currently with Python, Java, C, and C++ file checking capabilities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SourceCodeAnalyzingMachine/SCAM_Public",
+#    package_dir={"": "source_analyzer"},
+    packages=setuptools.find_packages(), #where='source_analyzer'
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['javalang', 'PySide2', 'matplotlib','networkx','libclang', 'pycparser'],
+    include_package_data=True,
+    package_data={
+        "": [ "*.txt","*.py","*.png", "source/*.py","source/backend/*.py","source/frontend/*.py","source/frontend/*.png","source/frontend/gui*.ui","source/frontend/pages/*.py","source/frontend/util/*.py"],
+    },
+    entry_points={
+        'console_scripts': [
+            'source_analyzer=source.scam:main',
+            'source-analyzer=scam:main',
+            'scam=source.scam:main',
+        ],
+    },
+)
---
+++ packages/scam/scam-0.3.0.tar.gz/scam-0.3.0/source/scam.py
@@ -0,0 +1,148 @@
+import sys
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+from source.frontend.pages.search_page import *
+from source.frontend.pages.results_page import *
+from source.frontend.pages.compare_page import *
+
+#SCAM Frontend
+#Some structure/design inspired from Mistletoe: https://github.com/cacticouncil/mistletoe
+
+class ScamApp(QMainWindow):
+
+    @property
+    def window(self):
+        return self._window
+
+    update_results = Signal()
+    update_compare = Signal()
+    send_compare = Signal(object)
+    update_output = Signal()
+
+    def __init__(self, filename):
+
+            QMainWindow.__init__(self, parent=None)
+
+            self.db = sharedDB()
+            self.h = None
+
+            #Load UI File
+            ui_file = QFile(filename)
+            ui_file.open(QFile.ReadOnly)
+
+            loader = QUiLoader()
+            self._window = loader.load(ui_file, self)
+
+            ui_file.close()
+
+            icon = QIcon("frontend/SCAM.png")
+            self.setWindowIcon(icon)
+
+
+            # self.button = QPushButton("Push for Window")
+            # self.button.clicked.connect(self.show_new_window)
+            # self.setCentralWidget(self.button)
+
+            self.create_widgets()
+
+
+    def create_widgets(self):
+
+    #Search
+        self.searchPage = SearchPage(self)
+        #self.update_output.connect()
+
+    #Results
+        self.resultsPage = ResultsPage(self)
+        self.update_compare.connect(self.resultsPage.add_documents)
+
+    #Compare
+        self.comparePage = ComparePage(self)
+
+        self.send_compare.connect(self.comparePage.add_comparison)
+
+        self.findChild(QAction, 'actionManual').triggered.connect(self.show_new_window)
+
+    def show_new_window(self, checked):
+        if self.h is None:
+            self.h = HelpManu()
+        self.h.show()
+
+class HelpManu(QDialog):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self):
+        QDialog.__init__(self, parent=None)
+
+        # Load UI File
+        ui_file = QFile("frontend/gui/help_manual.ui")
+        ui_file.open(QFile.ReadOnly)
+        loader = QUiLoader()
+        self._window = loader.load(ui_file, self)
+        ui_file.close()
+
+        icon = QIcon("frontend/SCAM.png")
+        self.setWindowIcon(icon)
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
+
+
+class sharedDB:
+
+    @property
+    def searchParams(self):
+        return self._searchParams
+
+    @searchParams.setter
+    def searchParams(self, new_params):
+        self._searchParams = new_params
+
+    @property
+    def boilerDBList(self):
+        return self._boilerDBList
+
+    @boilerDBList.setter
+    def boilerDBList(self, new_list):
+        self._boilerDBList = new_list
+
+    @property
+    def boilerFPList(self):
+        return self._boilerFPList
+
+    @boilerFPList.setter
+    def boilerFPList(self, new_list):
+        self._boilerFPList = new_list
+
+    def __init__(self):
+        self._fileList = QListWidget()
+        self._boilerDBList = QListWidget()
+        self._searchParams = []
+        self._boilerFPList = []
+
+    def getfileslistNum(self):
+        val = self._fileList.count()
+        return val
+
+    def getfileslist(self):
+        return self._fileList
+
+    def setfileslist(self, ex_fileList):
+        self._fileList = ex_fileList
+
+def main():
+
+    app = QApplication(sys.argv)
+    gui = ScamApp("frontend/gui/scam_gui.ui")
+    gui.window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
