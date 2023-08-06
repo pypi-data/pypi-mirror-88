8587f6c3a0c1a0585da0a583c7f5346bbc5060cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:40 +0000

    Add package tephiplt/0.0.5/tephiplt-0.0.5.tar.gz

---
+++ packages/tephiplt/tephiplt-0.0.5.tar.gz/tephiplt-0.0.5/setup.py
@@ -0,0 +1,29 @@
+'''module to state tephiplt package metadata'''
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tephiplt",
+    version="0.0.5",
+    author="Simon O'Meara",
+    author_email="simon.omeara@manchester.ac.uk",
+    description="plotting a tephigram",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['python', 'meteorology', 'tephigram', 'atmospheric physics'],
+    url="https://github.com/simonom/outreach",
+    packages = find_packages(include=['tephiplt']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    
+# state dependencies for tephiplt
+install_requires=["numpy","matplotlib","ipdb","scipy"]
+)
