945c557e4a74b8ca531fedf82cb7c24d83a195e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:58 +0000

    Add package fixit/0.1.3/fixit-0.1.3-py3-none-any.whl

---
+++ packages/fixit/fixit-0.1.3-py3-none-any.whl/fixit/common/cli/args.py
@@ -0,0 +1,27 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""
+For backwards compatibility.
+"""
+
+from fixit.cli.args import (
+    get_compact_parser,
+    get_multiprocessing_parser,
+    get_paths_parser,
+    get_rule_parser,
+    get_skip_ignore_byte_marker_parser,
+    get_use_ignore_comments_parser,
+)
+
+
+__all__ = [
+    "get_compact_parser",
+    "get_multiprocessing_parser",
+    "get_paths_parser",
+    "get_rule_parser",
+    "get_skip_ignore_byte_marker_parser",
+    "get_use_ignore_comments_parser",
+]
