d5bc75091a7ee1f19992e213c711eb17b75bbcd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:36 +0000

    Add package kafkaesk/0.4.6/kafkaesk-0.4.6.tar.gz

---
+++ packages/kafkaesk/kafkaesk-0.4.6.tar.gz/kafkaesk-0.4.6/kafkaesk/__init__.py
@@ -0,0 +1,8 @@
+from .app import Application  # noqa
+from .app import Router  # noqa
+from .app import run  # noqa
+from .app import run_app  # noqa
+from .app import Subscription  # noqa
+from .app import SubscriptionConsumer  # noqa
+
+__version__ = "0.4.6"
