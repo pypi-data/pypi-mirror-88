a6869f5aa89a227f1727c6f818e0c291505f708c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:00 +0000

    Add package experimaestro/0.7.9/experimaestro-0.7.9-py2.py3-none-any.whl

---
+++ packages/experimaestro/experimaestro-0.7.9-py2.py3-none-any.whl/experimaestro/__init__.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+# Annotations
+from .annotations import (
+    config,
+    task,
+    param,
+    option,
+    pathoption,
+    configmethod,
+    cache,
+    Param,
+    ConstantParam,
+    Identifier,
+    Array,
+    tag,
+    tags,
+    tagspath,
+    STDOUT,
+    STDERR,
+    # deprecated
+    argument,
+)
+from .environment import Environment
+from .workspace import Workspace
+from .scheduler import Scheduler, experiment
+from .notifications import progress
+from .register import parse_commandline
+from .core.types import Any
+from .checkers import Choices
+
+
+def set_launcher(launcher):
+    Workspace.CURRENT.launcher = launcher
+
+
+# Get version
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = "?"
