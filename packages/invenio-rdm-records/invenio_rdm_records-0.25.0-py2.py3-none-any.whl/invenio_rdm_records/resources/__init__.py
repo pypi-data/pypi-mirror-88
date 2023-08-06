b76156bd099accd27239d6ce366cbed53faa4bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:30 +0000

    Add package invenio-rdm-records/0.25.0/invenio_rdm_records-0.25.0-py2.py3-none-any.whl

---
+++ packages/invenio-rdm-records/invenio_rdm_records-0.25.0-py2.py3-none-any.whl/invenio_rdm_records/resources/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio RDM module to create REST APIs."""
+
+from .resources import BibliographicDraftActionResource, \
+    BibliographicDraftActionResourceConfig, BibliographicDraftResource, \
+    BibliographicDraftResourceConfig, BibliographicRecordFilesActionResource, \
+    BibliographicRecordFilesActionResourceConfig, \
+    BibliographicRecordFilesResource, BibliographicRecordFilesResourceConfig, \
+    BibliographicRecordResource, BibliographicRecordResourceConfig, \
+    BibliographicUserRecordsResource, BibliographicUserRecordsResourceConfig
+
+__all__ = (
+    "BibliographicDraftActionResource",
+    "BibliographicDraftActionResourceConfig",
+    "BibliographicDraftResource",
+    "BibliographicDraftResourceConfig",
+    "BibliographicRecordFilesResource",
+    "BibliographicRecordFilesResourceConfig",
+    "BibliographicRecordFilesActionResource",
+    "BibliographicRecordFilesActionResourceConfig",
+    "BibliographicRecordResource",
+    "BibliographicRecordResourceConfig",
+    "BibliographicUserRecordsResource",
+    "BibliographicUserRecordsResourceConfig",
+)
