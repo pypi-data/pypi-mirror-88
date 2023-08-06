4fa7236d171f127375707bcf81f1bb2ba8436cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:28 +0000

    Add package aspaceai/1.0.2/aspaceai-1.0.2-py3-none-any.whl

---
+++ packages/aspaceai/aspaceai-1.0.2-py3-none-any.whl/aspaceai/__init__.py
@@ -0,0 +1,14 @@
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: __init__.py
+Description: Python SDK of the aspace.ai API.
+"""
+
+#Version of ASpaceAI SDK
+__version__ = "1.0.0"
+
+
+from aspaceai.ai_solutions import *
+
