e9ecf28cb53a456a62aae07589c9dd72e4eee680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:59 +0000

    Add package RJcalculator/0.2.1/RJcalculator-0.2.1.tar.gz

---
+++ packages/RJcalculator/RJcalculator-0.2.1.tar.gz/RJcalculator-0.2.1/RJcalculator/__init__.py
@@ -0,0 +1,24 @@
+# class Operations:
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def power(x, y):
+    return x ** y
+
+def remainder(x, y):
+    return x % y
+
+def percent(x, y):
+    return x/100 * y
+
+
