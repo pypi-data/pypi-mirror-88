3afe70b75f7b36a8c5f000b8fa0e2baf0628d548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:59 +0000

    Add package event-tracking/1.0.3/event_tracking-1.0.3-py3-none-any.whl

---
+++ packages/event-tracking/event_tracking-1.0.3-py3-none-any.whl/eventtracking/django/tests/settings.py
@@ -0,0 +1,26 @@
+"""A Django settings file for testing"""
+
+from __future__ import absolute_import
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:'
+    }
+}
+
+SITE_ID = 1
+
+MIDDLEWARE = (
+    'django.middleware.common.CommonMiddleware',
+)
+
+INSTALLED_APPS = [
+    'eventtracking.django'
+]
+
+EVENT_TRACKING_ENABLED = True
+
+SECRET_KEY = "test_key"
