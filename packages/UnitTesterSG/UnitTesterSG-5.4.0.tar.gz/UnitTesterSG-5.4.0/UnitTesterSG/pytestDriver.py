f7123a3d6a7db37db71937e2d6e3c76c7da80b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:07 +0000

    Add package UnitTesterSG/5.4.0/UnitTesterSG-5.4.0.tar.gz

---
+++ packages/UnitTesterSG/UnitTesterSG-5.4.0.tar.gz/UnitTesterSG-5.4.0/UnitTesterSG/pytestDriver.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+def runAllTests():
+    #in Python, the listdir command  returns files and directories (like typeing in "dir").
+    listOfDirectoriesAndFiles = os.listdir(".")
+
+    #Below is going to become a list of directories only in the next loop.
+    directoryList = []
+    for elem in listOfDirectoriesAndFiles:
+        if os.path.isdir(elem) == True:
+            directoryList.append(elem)
+
+    #This loop goes into each directories, runs the specified command, and comes back.
+    for directory in directoryList:
+        print("Changing directory to "+directory)
+        os.chdir(directory)
+        try:
+            os.system("del __pycache__ /Q") #for windows
+            os.system("rm -r __pycache__ /Q") #for linux
+        except:
+            pass
+        os.system(sys.executable +" -m pytest") #this is like typing "python -m pytest" but uses whichever version of python should be used, important for virtual environments and different systems https://stackoverflow.com/questions/8338854/how-to-run-py-test-against-different-versions-of-python
+        os.chdir("..")
