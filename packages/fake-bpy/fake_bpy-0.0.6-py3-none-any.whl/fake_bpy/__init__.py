349db6ca94cc3bda95ca3bc8a53de5aa840cd4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:31 +0000

    Add package fake-bpy/0.0.6/fake_bpy-0.0.6-py3-none-any.whl

---
+++ packages/fake-bpy/fake_bpy-0.0.6-py3-none-any.whl/fake_bpy/__init__.py
@@ -0,0 +1,11 @@
+from ._utils import _NoneType
+
+context = _NoneType()
+data = _NoneType()
+msgbus = _NoneType()
+ops = _NoneType()
+types = _NoneType()
+utils = _NoneType()
+path = _NoneType()
+app = _NoneType()
+props = _NoneType()
