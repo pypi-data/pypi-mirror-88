13d9504dd397d06a55b3fb5fde4c0409716c9282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:14 +0000

    Add package cqls/0.1/cqls-0.1.tar.gz

---
+++ packages/cqls/cqls-0.1.tar.gz/cqls-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cqls',
+      version='0.1',
+      description='A parser for an often-used subset of CQL',
+      url='http://github.com/liao961120/CQL',
+      author='Yongfu Liao',
+      author_email='liao961120@github.com',
+      license='MIT',
+      packages=['cqls'],
+      tests_require=['deepdiff'],
+      zip_safe=False)
