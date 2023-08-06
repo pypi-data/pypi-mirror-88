33997414c54680e8a4319a470dea07412ccc92a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:41 +0000

    Add package sentencecloud/0.0.2/sentencecloud-0.0.2.tar.gz

---
+++ packages/sentencecloud/sentencecloud-0.0.2.tar.gz/sentencecloud-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import io
+from setuptools import setup
+from setuptools.extension import Extension
+import versioneer
+
+with io.open('README.md', encoding='utf_8') as fp:
+    readme = fp.read()
+
+setup(
+    author="Rajesh Prabhu",
+    author_email="",
+    name='sentencecloud',
+    version="0.0.2",
+    #cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/eRajsh/sentence_cloud',
+    description='A little sentence cloud generator',
+    long_description=readme,
+    long_description_content_type='text/markdown; charset=UTF-8',
+    license='MIT',
+    install_requires=['numpy>=1.6.1', 'pillow', 'matplotlib'],
+    entry_points={'console_scripts': ['wordcloud_cli=sentencecloud.__main__:main']},
+    packages=['sentencecloud'],
+    package_data={'sentencecloud': ['stopwords', 'MuseoModerno-Bold.ttf']}
+)
