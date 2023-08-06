fd332d7152d43f4e67824ee949e2dbef67ac663f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:41 +0000

    Add package pytest-messenger/3.0.0/pytest-messenger-3.0.0.tar.gz

---
+++ packages/pytest-messenger/pytest-messenger-3.0.0.tar.gz/pytest-messenger-3.0.0/README.rst
@@ -0,0 +1,115 @@
+=================
+pytest-messenger
+=================
+
+ex pytest-slack
+
+.. image:: https://img.shields.io/pypi/v/pytest-messenger.svg
+        :target: https://pypi.python.org/pypi/pytest-messenger
+
+.. image:: https://img.shields.io/travis/pytest-dev/pytest-messenger.svg
+        :target: https://travis-ci.org/pytest-dev/pytest-messenger
+
+.. image:: https://codecov.io/gh/pytest-dev/pytest-messenger/branch/master/graph/badge.svg
+        :target: https://codecov.io/gh/pytest-dev/pytest-messenger
+
+.. image:: https://readthedocs.org/projects/pytest-messenger/badge/?version=latest
+        :target: https://pytest-messenger.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/pytest-dev/pytest-messenger/shield.svg
+        :target: https://pyup.io/repos/github/pytest-dev/pytest-messenger/
+        :alt: Updates
+
+
+
+Pytest to Slack reporting plugin
+
+
+* Free software: MIT license
+* Documentation: https://pytest-messenger.readthedocs.io.
+
+
+Requirements
+------------
+
+* Requests
+
+
+
+Installation
+------------
+
+You can install "pytest-messenger" via `pip`_::
+
+    $ pip install pytest-messenger
+
+
+Usage
+-----
+* Setup `slack hook`_
+* Use this plugin by running pytest normally and use the following options to customize report:
+
+
+>>> slack:
+  --slack_channel=SLACK_CHANNEL
+                        Set the channel name to report
+  --slack_hook=SLACK_HOOK
+                        Used for reporting to slack
+  --slack_report_link=SLACK_REPORT_LINK
+                        Set the report link
+  --slack_username=SLACK_USERNAME
+                        Set the reporter name
+  --slack_timeout=SLACK_TIMEOUT [DEFAULT = 10s ]
+                        Set the timeout for sending results in seconds
+  --slack_success_emoji=SLACK_SUCCESS_EMOJI [default = :thumbsup:]
+                        Set emoji for a successful run
+  --slack_failed_emoji=SLACK_FAILED_EMOJI [default = :thumbsdown:]
+                        Set emoji for a failed run
+  --slack_success_icon=SLACK_SUCCESS_ICON [default = None]
+                        Set icon (a url) for a successful run. Overrides SLACK_SUCCESS_EMOJI
+  --slack_failed_icon=SLACK_FAILED_ICON [default = None]
+                        Set icon (a url) for a failed run. Overrides SLACK_FAILED_EMOJI
+
+
+Example
+-------
+    $ pytest tests --slack_hook=https://hooks.slack.com/services/... --slack_channel=test_report_channel --slack_username="Regression testing results"
+
+All kind of problems:
+
+.. image:: https://raw.githubusercontent.com/pytest-dev/pytest-messenger/master/img/failed.png
+
+Passed test:
+
+.. image:: https://raw.githubusercontent.com/pytest-dev/pytest-messenger/master/img/success.png
+
+
+----
+
+$ pytest tests --slack_hook=https://hooks.slack.com/services/... --slack_channel=test_report_channel --slack_username="Regression testing results"  --slack_report_link=http://any_address
+
+Passed test with link:
+
+.. image:: https://raw.githubusercontent.com/pytest-dev/pytest-messenger/master/img/success_link.png
+
+
+
+
+
+Issues
+------
+
+If you encounter any problems, please `file an issue`_ along with a detailed description.
+
+
+Credits
+-------
+
+[ ~ Dependencies scanned by PyUp.io ~ ]
+
+.. _`slack hook`: https://get.slack.help/hc/en-us/articles/115005265063-Incoming-WebHooks-for-Slack
+.. _`file an issue`: https://github.com/pytest-dev/pytest-messenger/issues
+.. _`pytest`: https://github.com/pytest-dev/pytest
+.. _`tox`: https://tox.readthedocs.io/en/latest/
+.. _`pip`: https://pypi.python.org/pypi/pip/
---
+++ packages/pytest-messenger/pytest-messenger-3.0.0.tar.gz/pytest-messenger-3.0.0/tests/test_plugin.py
@@ -0,0 +1,236 @@
+import json
+import mock
+
+import pytest
+
+
+def test_pytest_messenger_slack_failed(testdir):
+    """Make sure that our pytest-messenger works."""
+
+    testdir.makepyfile(
+        """
+        import pytest
+        def test_pass():
+            assert 1 == 1
+
+
+        def test_fail():
+            assert 1 == 2
+
+
+        @pytest.mark.skip()
+        def test_skip():
+            assert 1 == 1
+
+
+        def test_error(test):
+            assert 1 == ""
+
+
+        @pytest.mark.xfail()
+        def test_xfail():
+            assert 1 == 2
+
+        @pytest.mark.xfail()
+        def test_xpass():
+            assert 1 == 1
+        """
+    )
+
+    slack_hook_host = 'http://test.com/any_hash'
+    slack_hook_username = 'regression Testing'
+    slack_hook_report_host = 'http://report_link.com'
+    slack_hook_channel = 'test'
+    slack_hook_icon_emoji = ':thumbsdown:'
+    expected_text = '<http://report_link.com|Passed=1 Failed=1 Skipped=1 Error=1 XFailed=1 XPassed=1>'
+    with mock.patch('requests.post') as mock_post:
+        testdir.runpytest('--slack_channel', slack_hook_channel,
+                          '--slack_hook', slack_hook_host,
+                          '--slack_report_link', slack_hook_report_host,
+                          '--slack_username', slack_hook_username,
+                          '--slack_failed_emoji', slack_hook_icon_emoji)
+
+        called_data = json.loads(mock_post.call_args[1]['data'])
+        called_host = mock_post.call_args[0][0]
+        called_channel = called_data['channel']
+        called_username = called_data['username']
+        text = called_data['attachments'][0]['text']
+        color = called_data['attachments'][0]['color']
+        emoji = called_data['icon_emoji']
+
+        assert called_host == slack_hook_host
+        assert text == expected_text
+        assert called_channel == slack_hook_channel
+        assert called_username == slack_hook_username
+        assert color == '#ff0000'
+        assert emoji == slack_hook_icon_emoji
+
+
+def test_pytest_messenger_slack_passed(testdir):
+    """Make sure that our pytest-messenger works."""
+
+    testdir.makepyfile(
+        """
+        import pytest
+        def test_pass():
+            assert 1 == 1
+
+        """
+    )
+
+    slack_hook_host = 'http://test.com/any_hash'
+    slack_hook_username = 'regression Testing'
+    slack_hook_report_host = 'http://report_link.com'
+    slack_hook_channel = 'test'
+    slack_hook_icon_emoji = ':thumbsup:'
+    expected_text = '<http://report_link.com|Passed=1 Failed=0 Skipped=0 Error=0 XFailed=0 XPassed=0>'
+    with mock.patch('requests.post') as mock_post:
+        testdir.runpytest('--slack_channel', slack_hook_channel,
+                          '--slack_hook', slack_hook_host,
+                          '--slack_report_link', slack_hook_report_host,
+                          '--slack_username', slack_hook_username,
+                          '--slack_success_emoji', slack_hook_icon_emoji)
+
+        called_data = json.loads(mock_post.call_args[1]['data'])
+        called_host = mock_post.call_args[0][0]
+        called_channel = called_data['channel']
+        called_username = called_data['username']
+        text = called_data['attachments'][0]['text']
+        color = called_data['attachments'][0]['color']
+        emoji = called_data['icon_emoji']
+
+        assert called_host == slack_hook_host
+        assert text == expected_text
+        assert called_channel == slack_hook_channel
+        assert called_username == slack_hook_username
+        assert color == '#56a64f'
+        assert emoji == slack_hook_icon_emoji
+
+
+@pytest.mark.parametrize('expected_prefix,report_link', [
+    ("Test Prefix", None),
+    ("Test Prefix", "http://report_link.com")
+])
+def test_pytest_messenger_slack_message_prefix(testdir, expected_prefix, report_link):
+    """Make sure that message prefix works."""
+
+    testdir.makepyfile(
+        """
+        import pytest
+        def test_pass():
+            assert 1 == 1
+        """
+    )
+
+    slack_hook_host = 'http://test.com/any_hash'
+    slack_hook_channel = 'test'
+    run_args = [
+        '--slack_channel', slack_hook_channel,
+        '--slack_hook', slack_hook_host,
+        '--slack_message_prefix', expected_prefix
+    ]
+    if report_link:
+        run_args.extend(['--slack_report_link', report_link])
+
+    expected_text = 'Passed=1 Failed=0 Skipped=0 Error=0 XFailed=0 XPassed=0'
+    if report_link:
+        expected_text = '<%s|%s>' % (report_link, expected_text)
+    expected_text = '%s: %s' % (expected_prefix, expected_text)
+
+    with mock.patch('requests.post') as mock_post:
+        testdir.runpytest(*run_args)
+
+        called_data = json.loads(mock_post.call_args[1]['data'])
+        text = called_data['attachments'][0]['text']
+
+        assert text == expected_text
+
+
+@pytest.mark.parametrize('test_input,expected_emoji', [
+    ('1 == 1', ':sunny:'),
+    ('2 == 1', ':rain_cloud:'),
+])
+def test_pytest_messenger_slack_custom_emojis(testdir, test_input, expected_emoji):
+    testdir.makepyfile(
+        """
+        def test_icon_emoji():
+            assert %s
+        """ % test_input
+    )
+
+    slack_hook_host = 'http://test.com/any_hash'
+    slack_hook_channel = 'test'
+    slack_hook_icon_emoji_success = ':sunny:'
+    slack_hook_icon_emoji_failed = ':rain_cloud:'
+    with mock.patch('requests.post') as mock_post:
+        testdir.runpytest('--slack_channel', slack_hook_channel,
+                          '--slack_hook', slack_hook_host,
+                          '--slack_success_emoji', slack_hook_icon_emoji_success,
+                          '--slack_failed_emoji', slack_hook_icon_emoji_failed)
+
+        called_data = json.loads(mock_post.call_args[1]['data'])
+        emoji = called_data['icon_emoji']
+
+        assert emoji == expected_emoji
+
+
+@pytest.mark.parametrize('test_input,expected_url', [
+    ('1 == 1', 'http://localhost/success.png'),
+    ('2 == 1', 'http://localhost/failed.png'),
+])
+def test_pytest_messenger_slack_custom_icons(testdir, test_input, expected_url):
+    testdir.makepyfile(
+        """
+        def test_icon_url():
+            assert %s
+        """ % test_input
+    )
+
+    slack_hook_host = 'http://test.com/any_hash'
+    slack_hook_channel = 'test'
+    slack_hook_icon_url_success = 'http://localhost/success.png'
+    slack_hook_icon_url_failed = 'http://localhost/failed.png'
+    with mock.patch('requests.post') as mock_post:
+        testdir.runpytest('--slack_channel', slack_hook_channel,
+                          '--slack_hook', slack_hook_host,
+                          '--slack_success_icon', slack_hook_icon_url_success,
+                          '--slack_failed_icon', slack_hook_icon_url_failed)
+
+        called_data = json.loads(mock_post.call_args[1]['data'])
+        emoji = called_data['icon_url']
+
+        assert emoji == expected_url
+
+
+@pytest.mark.parametrize('test_input,expected_url', [
+    ('1 == 1', 'http://localhost/success.png'),
+    ('2 == 1', 'http://localhost/failed.png'),
+])
+def test_pytest_messenger_slack_icon_overrides_emoji(testdir, test_input, expected_url):
+    testdir.makepyfile(
+        """
+        def test_icon_url():
+            assert %s
+        """ % test_input
+    )
+
+    slack_hook_host = 'http://test.com/any_hash'
+    slack_hook_channel = 'test'
+    slack_hook_icon_url_success = 'http://localhost/success.png'
+    slack_hook_icon_url_failed = 'http://localhost/failed.png'
+    slack_hook_icon_emoji_success = ':sunny:'
+    slack_hook_icon_emoji_failed = ':rain_cloud:'
+    with mock.patch('requests.post') as mock_post:
+        testdir.runpytest('--slack_channel', slack_hook_channel,
+                          '--slack_hook', slack_hook_host,
+                          '--slack_success_icon', slack_hook_icon_url_success,
+                          '--slack_failed_icon', slack_hook_icon_url_failed,
+                          '--slack_success_emoji', slack_hook_icon_emoji_success,
+                          '--slack_failed_emoji', slack_hook_icon_emoji_failed)
+
+        called_data = json.loads(mock_post.call_args[1]['data'])
+        url = called_data['icon_url']
+        emoji = called_data['icon_emoji']
+
+        assert url == expected_url
+        assert emoji is None
