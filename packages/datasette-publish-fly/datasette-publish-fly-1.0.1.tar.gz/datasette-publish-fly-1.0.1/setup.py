a6b5a38069f03878d0a3cb21f1276a1ad8784643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:58 +0000

    Add package datasette-publish-fly/1.0.1/datasette-publish-fly-1.0.1.tar.gz

---
+++ packages/datasette-publish-fly/datasette-publish-fly-1.0.1.tar.gz/datasette-publish-fly-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import os
+
+VERSION = "1.0.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="datasette-publish-fly",
+    description="Datasette plugin for publishing data using Fly",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Simon Willison",
+    url="https://github.com/simonw/datasette-publish-fly",
+    project_urls={
+        "Issues": "https://github.com/simonw/datasette-publish-fly/issues",
+        "CI": "https://github.com/simonw/datasette-publish-fly/actions",
+        "Changelog": "https://github.com/simonw/datasette-publish-fly/releases",
+    },
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["datasette_publish_fly"],
+    entry_points={"datasette": ["publish_fly = datasette_publish_fly"]},
+    install_requires=["datasette>=0.44"],
+    extras_require={"test": ["pytest"]},
+    tests_require=["datasette-publish-fly[test]"],
+)
