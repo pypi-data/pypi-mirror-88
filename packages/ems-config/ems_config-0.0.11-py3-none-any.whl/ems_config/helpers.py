92e66e2f5808dd3094012dcfbafc6b5bcf3e5230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:55 +0000

    Add package ems-config/0.0.11/ems_config-0.0.11-py3-none-any.whl

---
+++ packages/ems-config/ems_config-0.0.11-py3-none-any.whl/ems_config/helpers.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime
+import pytz
+
+from ems_config.vars import APP_DATA_DIR
+
+
+def parse_dt_or_none(cfg_section, key, timezone=pytz.UTC, fmt="%Y-%m-%dT%H:%M:%SZ"):
+    s = cfg_section.get(key, fallback=None)
+    if s is None:
+        return None
+    d = datetime.strptime(s, fmt)
+    if timezone:
+        d = d.replace(tzinfo=pytz.UTC)
+    return d
+
+
+def parse_list_or_none(cfg_section, key, data_type=str, delimiter=','):
+    s = cfg_section.get(key, None)
+    if s is None:
+        return None
+    return [data_type(x.strip()) for x in s.split(delimiter) if x]
+
+
+def get_from_app_data(p):
+    return os.path.join(APP_DATA_DIR, p)
