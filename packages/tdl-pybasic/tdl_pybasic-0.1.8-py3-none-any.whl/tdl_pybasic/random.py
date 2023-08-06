ef9c5e3dc634c94eeb064342b734c6e5e014c2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:26 +0000

    Add package tdl-pybasic/0.1.8/tdl_pybasic-0.1.8-py3-none-any.whl

---
+++ packages/tdl-pybasic/tdl_pybasic-0.1.8-py3-none-any.whl/tdl_pybasic/random.py
@@ -0,0 +1,18 @@
+﻿#coding: utf-8
+# 乱数関連のモジュール
+
+from random import randint, seed, random
+
+def initialize():
+    """
+    seed()を使って乱数を初期化
+    常に同じ答えが返ってくるように
+    """
+    seed(20)
+
+
+def throw_coin():
+    """
+    サイコロを振る。0なら表，1なら裏
+    """
+    return randint(0, 1)
