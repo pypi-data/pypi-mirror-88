ef8a16f8baf70e66503dccb585a57533f9305522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:33 +0000

    Add package autoBinning/0.1.7/autoBinning-0.1.7.tar.gz

---
+++ packages/autoBinning/autoBinning-0.1.7.tar.gz/autoBinning-0.1.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autoBinning",
+    version="0.1.7",
+    author="Kai Wang",
+    author_email="wangkai0112006@163.com",
+    description="A small package for feature autoBinning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaiwang0112006/autoBinning",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
