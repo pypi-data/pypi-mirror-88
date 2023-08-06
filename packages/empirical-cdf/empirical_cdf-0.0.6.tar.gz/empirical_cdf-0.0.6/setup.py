299fc5590d57c951e38036fd1f9598c4903658c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:58 +0000

    Add package empirical-cdf/0.0.6/empirical_cdf-0.0.6.tar.gz

---
+++ packages/empirical-cdf/empirical_cdf-0.0.6.tar.gz/empirical_cdf-0.0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='empirical_cdf',
+    version='0.0.6',
+    description='A Distribution Function Associated With The Empirical Measure Of A Sample.',
+    url = 'https://github.com/Hotlynn2',
+    author='Mr. Franklin Adjei',
+    author_email='laizer421@gmail.com',
+    license='MIT',
+    packages=['empirical_cdf'],
+    install_requires=['numpy', 'matplotlib'],
+    include_package_data=True
+)
+ 
