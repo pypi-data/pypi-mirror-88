2a47009c709035eb6def1d8dc6ae6d416195bd63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:02 +0000

    Add package smpl/0.0.47/smpl-0.0.47.tar.gz

---
+++ packages/smpl/smpl-0.0.47.tar.gz/smpl-0.0.47/smpl/debug/__init__.py
@@ -0,0 +1,16 @@
+"""
+Simplified python debuging
+"""
+
+from .debug import msg,once,msg1,table,file,file1,table_flush_header, table_flush_line
+
+__all__ = [
+    'msg', 
+    'once', 
+    'msg1', 
+    'table', 
+    'file', 
+    'file1', 
+    'table_flush_header',
+    'table_flush_line',
+]
