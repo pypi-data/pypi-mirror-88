1a1f0951c2f9831f9f7ea638edf9ffca9494cbb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:32 +0000

    Add package ansible-builder/0.3.1/ansible_builder-0.3.1-py3-none-any.whl

---
+++ packages/ansible-builder/ansible_builder-0.3.1-py3-none-any.whl/ansible_builder/steps.py
@@ -0,0 +1,105 @@
+import os
+import sys
+
+from . import constants
+from .exceptions import DefinitionError
+
+
+class Steps:
+    def __iter__(self):
+        return iter(self.steps)
+
+
+class AdditionalBuildSteps(Steps):
+    def __init__(self, additional_steps):
+        """Allows for additional prepended / appended build steps to be
+        in the Containerfile or Dockerfile.
+        """
+        self.steps = []
+        if isinstance(additional_steps, str):
+            lines = additional_steps.strip().splitlines()
+        elif isinstance(additional_steps, list):
+            lines = additional_steps
+        else:
+            raise DefinitionError(
+                """
+                Error: Unknown type found for additional_build_steps; must be list or multi-line string.
+                """
+            )
+            sys.exit(1)
+        self.steps.extend(lines)
+
+    def __iter__(self):
+        return iter(self.steps)
+
+
+class GalaxyInstallSteps(Steps):
+    def __init__(self, requirements_naming):
+        """Assumes given requirements file name has been placed in the build context
+        """
+        self.steps = []
+        self.steps.append(
+            "ADD {0} /build/".format(requirements_naming)
+        )
+        self.steps.extend([
+            "",
+            "RUN ansible-galaxy role install -r /build/{0} --roles-path {1}".format(
+                requirements_naming, constants.base_roles_path),
+            "RUN ansible-galaxy collection install -r /build/{0} --collections-path {1}".format(
+                requirements_naming, constants.base_collections_path),
+            "",
+            "RUN mkdir -p {0} {1}".format(constants.base_roles_path, constants.base_collections_path),
+        ])
+
+
+class GalaxyCopySteps(Steps):
+    def __init__(self):
+        """Assumes given requirements file name has been placed in the build context
+        """
+        self.steps = []
+        self.steps.extend([
+            "",
+            "COPY --from=builder {0} {0}".format(constants.base_roles_path),
+            "COPY --from=builder {0} {0}".format(constants.base_collections_path),
+            "",
+        ])
+
+
+class BindepSteps(Steps):
+    def __init__(self, context_file):
+        """The context file here must be the output from bindep"""
+        self.steps = []
+        if not context_file:
+            return
+
+        # requirements file added to build context
+        self.steps.append("ADD {0} /build/".format(context_file))
+        container_path = os.path.join('/build/', context_file)
+        self.steps.append(
+            "RUN dnf -y install $(cat {0})".format(container_path)
+        )
+
+
+class PipSteps(Steps):
+    def __init__(self, context_file):
+        """Allows for 1 python requirement file in the build context"""
+        self.steps = []
+        if not context_file:
+            return
+
+        # requirements file added to build context
+        self.steps.append("ADD {0} /build/".format(context_file))
+        container_path = os.path.join('/build/', context_file)
+        self.steps.append(
+            "RUN pip3 install --upgrade -r {content}".format(content=container_path)
+        )
+
+
+class AnsibleConfigSteps(Steps):
+    def __init__(self, ansible_config):
+        """Copies a user's ansible.cfg file for accessing Galaxy server"""
+        self.steps = []
+        self.steps.extend([
+            "ADD ansible.cfg ~/.ansible.cfg",
+            "",
+        ])
