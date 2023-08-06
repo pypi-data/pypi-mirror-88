ef61ee73a5724a8335179eeeb9b75401c0507dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:28 +0000

    Add package fraudlabspro-python/1.3.0/fraudlabspro-python-1.3.0.tar.gz

---
+++ packages/fraudlabspro-python/fraudlabspro-python-1.3.0.tar.gz/fraudlabspro-python-1.3.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="fraudlabspro-python",
+	version="1.3.0",
+	author="FraudLabs Pro",
+	author_email="support@fraudlabspro.com",
+	description="A Python module enables user to easily implement fraud detection feature into their solution using the API from https://www.fraudlabspro.com.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://www.fraudlabspro.com",
+	packages=setuptools.find_packages(),
+	tests_require=['pytest>=3.0.6'],
+	classifiers=(
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	),
+)
