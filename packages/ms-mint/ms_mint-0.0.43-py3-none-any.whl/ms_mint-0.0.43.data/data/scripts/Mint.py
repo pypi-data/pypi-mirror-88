c65690e93adca3abb7e67264472963e04f161dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:57 +0000

    Add package ms-mint/0.0.43/ms_mint-0.0.43-py3-none-any.whl

---
+++ packages/ms-mint/ms_mint-0.0.43-py3-none-any.whl/ms_mint-0.0.43.data/data/scripts/Mint.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import multiprocessing
+
+from glob import glob
+
+import ms_mint
+from ms_mint.app.App import app, mint
+
+
+if __name__ == '__main__':
+    
+    args = sys.argv
+
+
+    url = 'http://localhost:9999'
+    
+    if not '--no-browser' in args:
+        if os.name == 'nt':
+            # https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
+            multiprocessing.freeze_support()
+            
+        if sys.platform in ['win32', 'nt']:
+            os.startfile(url)
+            
+        elif sys.platform=='darwin':
+            subprocess.Popen(['open', url])
+            
+        else:
+            try:
+                subprocess.Popen(['xdg-open', url])
+            except OSError:
+                print('Please open a browser on: ', url)
+
+    if '--version' in args:
+        print('Mint version:', ms_mint.__version__)
+        exit()
+    
+    if '--debug' in args:
+        DEBUG = True
+        mint.verbose = True
+
+    else:
+        DEBUG = False
+
+    if '--verbose' in args:
+        mint.verbose = True
+
+    if '--data' in args:
+        mint.ms_files = glob('/data/metabolomics_storage/MINT/MINT_demofiles/*/**.mzXML', recursive=True)[:4]
+        mint.peaklist_files = 'tests/data/example_peaklist.csv'
+
+        print('MINT files:', mint.ms_files)
+        print('MINTegration list:')
+        print(mint.peaklist.to_string())
+        
+    app.run_server(debug=DEBUG, port=9999)
