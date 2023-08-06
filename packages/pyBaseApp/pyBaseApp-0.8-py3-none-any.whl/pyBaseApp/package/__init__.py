cd6a4ac26c2adff443d517f4433cb949744cb4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:24 +0000

    Add package pyBaseApp/0.8/pyBaseApp-0.8-py3-none-any.whl

---
+++ packages/pyBaseApp/pyBaseApp-0.8-py3-none-any.whl/pyBaseApp/package/__init__.py
@@ -0,0 +1,2 @@
+from pyBaseApp.package.options import Options
+from pyBaseApp.package.package import Package
---
+++ packages/pyBaseApp/pyBaseApp-0.8-py3-none-any.whl/tests/unit/package/myapp.py
@@ -0,0 +1,6 @@
+import logging
+from pyBaseApp.applauncher import Configuration
+
+if __name__ == "__main__":
+    conf = Configuration()
+    logging.info("Hello world")
