44abf7920f8b70be548a0bc232332f841e9f2e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:07 +0000

    Add package terrasnek/0.0.14/terrasnek-0.0.14-py3-none-any.whl

---
+++ packages/terrasnek/terrasnek-0.0.14-py3-none-any.whl/terrasnek/admin_orgs.py
@@ -0,0 +1,49 @@
+"""
+Module for Terraform Cloud API Endpoint: Admin Orgs.
+"""
+
+from .endpoint import TFCEndpoint
+
+class TFCAdminOrgs(TFCEndpoint):
+    """
+    `Admin Orgs API Docs \
+        <https://www.terraform.io/docs/cloud/api/admin/organizations.html>`_
+    """
+
+    def __init__(self, instance_url, org_name, headers, well_known_paths, verify, log_level):
+        super().__init__(instance_url, org_name, headers, well_known_paths, verify, log_level)
+        self._endpoint_base_url = f"{self._api_v2_base_url}/admin/organizations"
+
+    def required_entitlements(self):
+        return []
+
+    def destroy(self, org_name):
+        """
+        ``DELETE /admin/organizations/:name``
+
+        `Admin Destroy Org API Doc Reference \
+            <https://www.terraform.io/docs/cloud/api/admin/organizations.html#delete-an-organization>`_
+        """
+        url = f"{self._endpoint_base_url}/{org_name}"
+        return self._destroy(url)
+
+    def list(self):
+        """
+        ``GET /admin/organizations``
+
+        `Admin List Orgs API Doc Reference \
+            <https://www.terraform.io/docs/cloud/api/admin/organizations.html#list-all-organizations>`_
+
+        This endpoint lists all organizations in the Terraform Cloud installation.
+        """
+        return self._list(self._endpoint_base_url)
+
+    def show(self, org_name):
+        """
+        ``GET /admin/organizations/:name``
+
+        `Admin Show Org API Doc Reference \
+            <https://www.terraform.io/docs/cloud/api/admin/organizations.html#show-an-organization>`_
+        """
+        url = f"{self._endpoint_base_url}/{org_name}"
+        return self._show(url)
