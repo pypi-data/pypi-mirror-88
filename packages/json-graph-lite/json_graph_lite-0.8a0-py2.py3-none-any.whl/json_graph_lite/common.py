80771af41835ad5abc3e2e6619a4760ecf6b0738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:06 +0000

    Add package json-graph-lite/0.8a0/json_graph_lite-0.8a0-py2.py3-none-any.whl

---
+++ packages/json-graph-lite/json_graph_lite-0.8a0-py2.py3-none-any.whl/json_graph_lite/common.py
@@ -0,0 +1,39 @@
+def obj_to_dict(obj, attrs, d=None):
+    d = d or {}
+    for attr in attrs:
+        value = getattr(obj, attr, None)
+        if value is not None:
+            d[attr] = value
+    return d
+
+
+def check_type(value, type):
+    if isinstance(value, type):
+        return value
+    raise TypeError("Type {} expected".format(type.__name__))
+
+
+def check_condition(value, condition, error_message):
+    if condition(value):
+        return value
+    raise ValueError(error_message)
+
+
+def only_keys(d, keys):
+    return {k: v for (k, v) in d.items() if k in keys}
+
+
+def inplace_update(d, **other):
+    d.update(other)
+    return d
+
+
+def inplace_del(d, key):
+    del d[key]
+    return d
+
+
+def search_by_criteria(objects, criteria):
+    if callable(criteria):
+        return [node for node in objects if criteria(node)]
+    raise ValueError("Criteria is not callable.")
