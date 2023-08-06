8b2f575598ce41bb366270d1104ac7b5c4b3ccb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:02 +0000

    Add package minimalist-python-package/1.0.0/minimalist-python-package-1.0.0.tar.gz

---
+++ packages/minimalist-python-package/minimalist-python-package-1.0.0.tar.gz/minimalist-python-package-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minimalist-python-package',
+    version='1.0.0',
+    url='https://github.com/mbautin/minimalist-python-package.git',
+    author='Mikhail Bautin',
+    author_email='mbautin@users.noreply.github.com',
+    description='Minimalist Python Package',
+    packages=find_packages(),    
+    install_requires=[],
+)
+
