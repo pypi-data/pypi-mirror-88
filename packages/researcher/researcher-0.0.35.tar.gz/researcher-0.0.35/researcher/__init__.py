a3c2b2e0b02acf7a6fffd4d6acfa1d573d26e202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:04 +0000

    Add package researcher/0.0.35/researcher-0.0.35.tar.gz

---
+++ packages/researcher/researcher-0.0.35.tar.gz/researcher-0.0.35/researcher/__init__.py
@@ -0,0 +1,5 @@
+from researcher.experiment import Experiment
+from researcher.observations import FinalizedObservations, ObservationCollector
+from researcher.record import *
+from researcher.fileutils import *
+from researcher.dashboard import *
