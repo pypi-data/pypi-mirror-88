bb6eabd8a5c7c6a42b8e7074a126944aaff41031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:59 +0000

    Add package xcresult/0.3.0/xcresult-0.3.0.tar.gz

---
+++ packages/xcresult/xcresult-0.3.0.tar.gz/xcresult-0.3.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xcresult']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'xcresult',
+    'version': '0.3.0',
+    'description': 'An xcresult parser.',
+    'long_description': '# xcresult\n\n',
+    'author': 'Dale Myers',
+    'author_email': 'dale@myers.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/dalemyers/xcresult',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
