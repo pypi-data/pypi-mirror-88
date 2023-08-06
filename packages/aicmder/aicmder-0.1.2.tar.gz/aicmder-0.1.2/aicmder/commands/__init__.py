e04429dc0c71c2f2f47b063951f5a0d88031951b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:38 +0000

    Add package aicmder/0.1.2/aicmder-0.1.2.tar.gz

---
+++ packages/aicmder/aicmder-0.1.2.tar.gz/aicmder-0.1.2/aicmder/commands/__init__.py
@@ -0,0 +1,10 @@
+from aicmder.commands.utils import register, get_command, execute, _commands
+
+
+import aicmder.commands.fastai
+import aicmder.commands.help
+import aicmder.commands.utils
+import aicmder.commands.version
+import aicmder.commands.onnx
+import aicmder.commands.init
+import aicmder.commands.pb
