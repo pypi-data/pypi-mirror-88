3dbc49b9ae4a286020e96383608ff58b0082c4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:01 +0000

    Add package thorondor/0.3.21/thorondor-0.3.21-py3-none-any.whl

---
+++ packages/thorondor/thorondor-0.3.21-py3-none-any.whl/thorondor/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+"""
+The GUI is accessible  through the Class thorondor.GUI.Interface()
+"""
+
+__all__ = ["GUI", "GUI_iterable"]
+
+from . import GUI
+from . import GUI_iterable
