f52f4e15e0f5addf90109e8a76c5a068e549b23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:27 +0000

    Add package code-quality/0.0.2/code_quality-0.0.2.tar.gz

---
+++ packages/code-quality/code_quality-0.0.2.tar.gz/code_quality-0.0.2/code_quality/advices.py
@@ -0,0 +1,9 @@
+
+advices = {
+    "Readability Score": "Adding comments, giving helpful variable names, consistent casing may help with readability.",
+    "Document Size Score": "Extremely large documents may deter people from reading, very small document may be wasteful.",
+    "Redundancy Check Score": "Divide large pieces of code into small reusable pieces of code.",
+    "Function size score": "Keep functions small. Large functions get unmanageable over time."
+}
+
+# May add more checks and advices in future
