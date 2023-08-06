c746acbf51c72c3895d69351b163271cb38641c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:49 +0000

    Add package fake-bpy-module-2.79/20201216/fake_bpy_module_2.79-20201216-py3-none-any.whl

---
+++ packages/fake-bpy-module-2.79/fake_bpy_module_2.79-20201216-py3-none-any.whl/bpy/__init__.py
@@ -0,0 +1,15 @@
+import sys
+import typing
+import bpy.types
+
+from . import types
+from . import ops
+from . import path
+from . import utils
+from . import context
+from . import app
+from . import props
+
+data: 'bpy.types.BlendData' = None
+''' Access to Blender's internal data
+'''
---
+++ packages/fake-bpy-module-2.79/fake_bpy_module_2.79-20201216-py3-none-any.whl/bpy/ops/__init__.py
@@ -0,0 +1,113 @@
+import sys
+import typing
+from . import view3d
+from . import export_scene
+from . import render
+from . import group
+from . import node
+from . import transform
+from . import surface
+from . import paint
+from . import nla
+from . import export_mesh
+from . import console
+from . import graph
+from . import import_curve
+from . import script
+from . import rigidbody
+from . import sound
+from . import ptcache
+from . import wm
+from . import ed
+from . import action
+from . import cloth
+from . import brush
+from . import uv
+from . import time
+from . import anim
+from . import clip
+from . import gpencil
+from . import screen
+from . import view2d
+from . import particle
+from . import fluid
+from . import ui
+from . import import_scene
+from . import sculpt
+from . import cachefile
+from . import mask
+from . import sequencer
+from . import armature
+from . import world
+from . import constraint
+from . import poselib
+from . import image
+from . import export_anim
+from . import font
+from . import import_anim
+from . import safe_areas
+from . import camera
+from . import pose
+from . import palette
+from . import dpaint
+from . import cycles
+from . import mball
+from . import lattice
+from . import marker
+from . import text
+from . import mesh
+from . import boid
+from . import outliner
+from . import paintcurve
+from . import material
+from . import scene
+from . import curve
+from . import import_mesh
+from . import info
+from . import sketch
+from . import lamp
+from . import texture
+from . import logic
+from . import file
+from . import object
+from . import buttons
+
+
+class BPyOps:
+    pass
+
+
+class BPyOpsSubMod:
+    pass
+
+
+class BPyOpsSubModOp:
+    def get_instance(self):
+        ''' 
+
+        '''
+        pass
+
+    def get_rna(self):
+        ''' 
+
+        '''
+        pass
+
+    def idname(self):
+        ''' 
+
+        '''
+        pass
+
+    def idname_py(self):
+        ''' 
+
+        '''
+        pass
+
+    def poll(self, args):
+        ''' 
+
+        '''
+        pass
