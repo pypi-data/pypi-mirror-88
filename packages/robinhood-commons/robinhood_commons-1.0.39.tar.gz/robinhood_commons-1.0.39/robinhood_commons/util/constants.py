01e29065f6cd37841e6908d1c8d4a9b26a33da0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:28 +0000

    Add package robinhood-commons/1.0.39/robinhood_commons-1.0.39.tar.gz

---
+++ packages/robinhood-commons/robinhood_commons-1.0.39.tar.gz/robinhood_commons-1.0.39/robinhood_commons/util/constants.py
@@ -0,0 +1,38 @@
+import os
+
+from robinhood_commons.util.io_utils import ensure_exists
+
+MAIN_EMAIL: str = 'mhowell234@gmail.com'
+USERS_KEY: str = 'users'
+DEFAULT_DELIMITER: str = ':'
+
+TESTING: bool = True
+DEBUG: bool = False
+F_IT_MODE: bool = False
+PROFILE_INFO: bool = False
+BUY_ONE_SELL_ONE_MODE: bool = True
+
+RANDOM_INVESTORS: int = 7
+ERROR_SLEEP_TIME: int = 30  # in seconds
+
+
+def get_base_dir(name: str = 'daytrader', base_dir: str = '/code/robinhood', append_user_base: bool = True) -> str:
+    if append_user_base:
+        base: str = os.path.expanduser(f'~{base_dir}')
+    else:
+        base: str = base_dir
+
+    ensure_exists(file_path=f'{base}-{name}')
+    return base
+
+
+OUTPUT_DIR: str = f'{get_base_dir()}/output'
+LOG_DIR: str = f'{OUTPUT_DIR}/logs'
+TMP_DIR: str = f'{get_base_dir()}/tmp'
+
+STOCKS_FILE_PATH: str = f'{TMP_DIR}/stocks.json'
+
+if __name__ == '__main__':
+    print(get_base_dir())
+    print(get_base_dir(base_dir='/Documents'))
+    print(STOCKS_FILE_PATH)
