953eee40286835dc24eb8e64944a08e88098904c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:12 +0000

    Add package twinfield/1.0.5rc1/twinfield-1.0.5rc1.tar.gz

---
+++ packages/twinfield/twinfield-1.0.5rc1.tar.gz/twinfield-1.0.5rc1/twinfield/exceptions.py
@@ -0,0 +1,12 @@
+class KostenPlaatsError(Exception):
+    pass
+
+
+class ServerError(Exception):
+    pass
+
+
+class FaultCodeNotFoundError(Exception):
+    """Exception when faultcode not found"""
+
+    pass
