1207f154e8409ff3598268d183d79e9f8d3802e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:56 +0000

    Add package requests-hawk/1.1.0/requests-hawk-1.1.0.tar.gz

---
+++ packages/requests-hawk/requests-hawk-1.1.0.tar.gz/requests-hawk-1.1.0/setup.py
@@ -0,0 +1,45 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    README = f.read()
+
+with codecs.open(os.path.join(here, 'CHANGES.txt'), encoding='utf-8') as f:
+    CHANGES = f.read()
+
+requires = ['requests', 'mohawk']
+
+setup(name='requests-hawk',
+      version='1.1.0',
+      description='requests-hawk',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Programming Language :: Python",
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+      ],
+      entry_points={
+          'httpie.plugins.auth.v1': [
+              'httpie_hawk = requests_hawk:HawkPlugin'
+          ]
+      },
+      license='Apache License (2.0)',
+      author='Mozilla Services',
+      author_email='services-dev@mozilla.org',
+      url='https://github.com/mozilla-services/requests-hawk',
+      keywords='authentication token hawk requests',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="requests_hawk")
