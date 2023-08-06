f45863d88ae24f182ee5dceb5764d9d569701fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:55 +0000

    Add package pyqss/1.3/pyqss-1.3.tar.gz

---
+++ packages/pyqss/pyqss-1.3.tar.gz/pyqss-1.3/README.md
@@ -0,0 +1,53 @@
+# pyqss
+
+简体中文 | [English](README-en.md)
+
+一个简约的QSS编辑器
+
+## 安装
+
+```shell
+pip install pyqss
+```
+
+## 功能
+
+- [x] 实时预览
+- [x] 语法高亮
+- [x] 自动补全
+- [x] 国际化
+- [x] 查找替换
+- [x] 文件拖拽
+- [x] 自动吸附
+
+## 使用方法
+
+**快捷键：**
+
+- `Ctrl+/`：注释
+- `Ctrl+F`：查找替换
+
+从pyqss导入Qss，将主界面注册到Qss中，即可使用：[示例](./examples/test.py)
+
+```python
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    from pyqss import Qss
+
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    # 注册主窗口
+    qss = Qss(main_window)
+    qss.show()
+
+    app.exec_()
+```
+## 截图
+
+![show](./screen/show.png)
+
+![comment](./screen/comment.gif)
+
+![find_replace](./screen/find_replace.gif)
---
+++ packages/pyqss/pyqss-1.3.tar.gz/pyqss-1.3/setup.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/3/22 13:22
+# @Author  : llc
+# @File    : setup.py
+
+from setuptools import setup
+
+__version__ = '1.3'
+
+long_description = """
+
+A simple QSS editor
+
+## Install
+
+```shell
+pip install pyqss
+```
+
+## Features
+
+- Live preview
+- Syntax highlighting
+- Auto-completion
+- i18n
+- Find and replace
+- File drag and drop
+- Auto-attach
+
+## Usage
+
+Import QSS from pyqss and register the MainWindow into QSS, and enjoy it.
+
+```python
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    from pyqss import Qss
+
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    # register  MainWindow 
+    qss = Qss(main_window, language='en')
+    qss.show()
+
+    app.exec_()
+```
+"""
+
+setup(
+    name="pyqss",
+    version=__version__,
+    url='https://github.com/luolingchun/pyqss',
+    description='QSS Editor',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='llc',
+    author_email='luolingchun.com@gmail.com',
+    license='GPLv3',
+    packages=['pyqss', 'pyqss.sci', 'pyqss.ui', 'pyqss.widgets'],
+    include_package_data=True,
+    python_requires=">=3.6",
+    zip_safe=False,
+    platforms='any',
+    install_requires=['PyQt5', 'QScintilla']
+)
