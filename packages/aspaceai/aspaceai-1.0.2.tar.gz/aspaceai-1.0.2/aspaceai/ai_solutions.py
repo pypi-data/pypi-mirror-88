1b56ef172f7246fe56b0f7a82ed845f3a78f8a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:28 +0000

    Add package aspaceai/1.0.2/aspaceai-1.0.2.tar.gz

---
+++ packages/aspaceai/aspaceai-1.0.2.tar.gz/aspaceai-1.0.2/aspaceai/ai_solutions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: ai_solutions.py
+Description: ai solutions for each theme API.
+"""
+
+ASPACE_API_KEY = ""
+ASPACE_URL =""
+#         Theme Sense
+class sense:
+    def sentiment_analysis(text):
+        """Perform Sentiment analysis for a given text.
+        Args:
+            text: Text on which to perform sentiment analysis.
+
+        Returns:
+            An array of sentiment analysis and corresponding confidence factor.
+        """
+        return "sentiment analysis"
+
+
+#         Theme Conversation
+class conversation:
+    def translate(text, from_lang, to_lang):
+        """Perform translation for a given text.
+                Args:
+                    text: Text to be translated
+
+                Returns:
+                    An array of translated text , from_lang language, to_lang language
+        """
+        return "translate"
