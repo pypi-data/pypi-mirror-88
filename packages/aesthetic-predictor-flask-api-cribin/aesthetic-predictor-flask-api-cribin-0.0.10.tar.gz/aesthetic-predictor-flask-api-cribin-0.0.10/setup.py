eb3906902e6f5309e2d5615a1cbaeabb4915873c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:14 +0000

    Add package aesthetic-predictor-flask-api-cribin/0.0.10/aesthetic-predictor-flask-api-cribin-0.0.10.tar.gz

---
+++ packages/aesthetic-predictor-flask-api-cribin/aesthetic-predictor-flask-api-cribin-0.0.10.tar.gz/aesthetic-predictor-flask-api-cribin-0.0.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+
+setuptools.setup(
+    name="aesthetic-predictor-flask-api-cribin",  # Replace with your own username
+    version="0.0.10",
+    author="Ribin Chalumattu",
+    description="Package for sharing flask api for aesthetic predictors.",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'wget',
+    ],
+    python_requires='>=2.7'
+)
