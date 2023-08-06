f3ef73490bd2ad96b45e66827ee78db952f2fceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:33 +0000

    Add package fir3base/0.7.2/fir3base-0.7.2.tar.gz

---
+++ packages/fir3base/fir3base-0.7.2.tar.gz/fir3base-0.7.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any ".
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='fir3base',
+	version='0.7.2',
+	description='Some description.',
+	url="http://github.com/vandenberghinc/fir3base",
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	install_requires=[
+		"firebase-admin",
+		"django",
+	],)
