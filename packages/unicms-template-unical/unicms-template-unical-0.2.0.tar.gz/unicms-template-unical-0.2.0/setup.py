e0337b118dee41397dcff0c73c86ef2b4e258817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:04 +0000

    Add package unicms-template-unical/0.2.0/unicms-template-unical-0.2.0.tar.gz

---
+++ packages/unicms-template-unical/unicms-template-unical-0.2.0.tar.gz/unicms-template-unical-0.2.0/setup.py
@@ -0,0 +1,46 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+#  rm -R build/ dist/ *egg-info
+#  python3 setup.py sdist
+#  twine upload dist/*
+
+setup(
+    name='unicms-template-unical',
+    version='0.2.0',
+    packages=find_packages(),
+    package_data={'unicms_template_unical': ['*.py', ]},
+    data_files=[
+        ('', glob('unicms_template_unical/templates/*.html', recursive=True)),
+    ],
+    include_package_data=True,
+    license='Apache License 2.0',
+    description="uniCMS Unical Template based on Bootstrap Italia",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/UniversitaDellaCalabria/unicms-template-unical',
+    author='Giuseppe De Marco, Francesco Filicetti',
+    author_email='giuseppe.demarco@unical.it, francesco.filicetti@unical.it',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'django>=2.0,<4.0',
+        'design-django-theme==1.4.4'
+    ],
+)
