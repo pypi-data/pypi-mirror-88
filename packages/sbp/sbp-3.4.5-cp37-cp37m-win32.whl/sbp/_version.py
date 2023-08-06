33dd730c88fc6bfbb42fbc4d8121860eaef3c12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:07 +0000

    Add package sbp/3.4.5/sbp-3.4.5-cp37-cp37m-win32.whl

---
+++ packages/sbp/sbp-3.4.5-cp37-cp37m-win32.whl/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '3.4.5'
+full_version = '3.4.5'
+git_revision = 'Unknown'
+is_released = True
+if not is_released:
+    version = full_version
