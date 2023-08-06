102a357bf6423abba64477a78c908ba0c7b5a44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:42 +0000

    Add package fnotify/0.0.1a0/fnotify-0.0.1a0.tar.gz

---
+++ packages/fnotify/fnotify-0.0.1a0.tar.gz/fnotify-0.0.1a0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="fnotify",  # Replace with your own username
+    version="0.0.1a0",
+    author="Fangnikoue Evarist",
+    author_email="malevae@gmail.com",
+    description="A python program to watch directory and execute any action on contain of directory changed",
+    url="https://github.com/eirtscience/inotify",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['bin/finotify'],
+
+    python_requires='>=3.5',
+)
