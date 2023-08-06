bd29f370964e8eedec923455f9a568026f283e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:17 +0000

    Add package medspacy-quickumls/2.0/medspacy_quickumls-2.0-py3.7.egg

---
+++ packages/medspacy-quickumls/medspacy_quickumls-2.0-py3.7.egg/quickumls/about.py
@@ -0,0 +1,12 @@
+# inspired from:
+# https://python-packaging-user-guide.readthedocs.org/en/latest/single_source_version/
+# https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
+# https://github.com/explosion/spaCy/blob/master/spacy/about.py
+
+__title__ = 'medspacy_quickumls'
+__version__ = '2.0'
+__author__ = 'Luca Soldaini, modified by Kelly Peterson'
+__email__ = 'luca@ir.cs.georgetown.edu,kelly.peterson@hsc.utah.edu'
+__license__ = 'MIT'
+__uri__ = "https://github.com/medspacy/QuickUMLS"
+__copyright__ = '2014-2020, Georgetown University Information Retrieval Lab,2020, University of Utah'
