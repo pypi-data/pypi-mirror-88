d6e5e954335079a6976d06662b010832d172c9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:13 +0000

    Add package opsramp-analytics-utils/1.0.1/opsramp-analytics-utils-1.0.1.tar.gz

---
+++ packages/opsramp-analytics-utils/opsramp-analytics-utils-1.0.1.tar.gz/opsramp-analytics-utils-1.0.1/README.md
@@ -0,0 +1,5 @@
+# OapDash
+
+This is a customized version of dash for OpsRamp Reports. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
---
+++ packages/opsramp-analytics-utils/opsramp-analytics-utils-1.0.1.tar.gz/opsramp-analytics-utils-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def read_req_file(req_type):
+    with open("requires-{}.txt".format(req_type)) as fp:
+        requires = (line.strip() for line in fp)
+        return [req for req in requires if req and not req.startswith("#")]
+
+setuptools.setup(
+    name="opsramp-analytics-utils",
+    version="1.0.1",
+    author="Donald Lafranchi",
+    author_email="it.corridor051@gmail.com",
+    description="Customized Dash for OpsRamp Reports",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=read_req_file("install"),
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
