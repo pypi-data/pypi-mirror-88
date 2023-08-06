0e708765342819d20b478c73af84c09143279fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:59 +0000

    Add package transiter/0.5.4.dev29/transiter-0.5.4.dev29.tar.gz

---
+++ packages/transiter/transiter-0.5.4.dev29.tar.gz/transiter-0.5.4.dev29/transiter/__metadata__.py
@@ -0,0 +1,15 @@
+# When updating the version, also update it in the docs.
+__version__ = "0.5.4"
+
+# The following are set by the CI process at build time.
+__build_number__ = None
+__build_timestamp__ = None
+__build_href__ = None
+__git_commit_hash__ = None
+
+# NOTE: the following were set by the CI process
+__version__ = "0.5.4.dev29"
+__build_number__ = None
+__build_timestamp__ = 1607822082
+__build_href__ = "https://github.com/jamespfennell/transiter/actions/runs/418140444"
+__git_commit_hash__ = "295d48f151b409919d38b2922324a305928b8af8"
