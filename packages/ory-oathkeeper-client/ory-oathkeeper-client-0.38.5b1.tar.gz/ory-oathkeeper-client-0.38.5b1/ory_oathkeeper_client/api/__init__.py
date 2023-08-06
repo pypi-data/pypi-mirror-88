f984c571a6056159d6bd0076472bc8aa001d8e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:31 +0000

    Add package ory-oathkeeper-client/0.38.5b1/ory-oathkeeper-client-0.38.5b1.tar.gz

---
+++ packages/ory-oathkeeper-client/ory-oathkeeper-client-0.38.5b1.tar.gz/ory-oathkeeper-client-0.38.5b1/ory_oathkeeper_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ory_oathkeeper_client.api.api_api import ApiApi
+from ory_oathkeeper_client.api.health_api import HealthApi
+from ory_oathkeeper_client.api.version_api import VersionApi
