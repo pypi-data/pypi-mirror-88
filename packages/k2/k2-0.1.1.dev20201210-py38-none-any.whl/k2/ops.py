86357d880ceacedbdd8d287a3c3b41fd4e5684d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:15 +0000

    Add package k2/0.1.1.dev20201210/k2-0.1.1.dev20201210-py38-none-any.whl

---
+++ packages/k2/k2-0.1.1.dev20201210-py38-none-any.whl/k2/ops.py
@@ -0,0 +1,70 @@
+# Copyright (c)  2020  Mobvoi Inc.        (authors: Fangjun Kuang)
+#
+# See ../../../LICENSE for clarification regarding multiple authors
+
+import torch
+import _k2
+
+from .autograd import index_select
+from .fsa import Fsa
+from .ragged import index as ragged_index
+
+
+def index(src: Fsa, indexes: torch.Tensor) -> Fsa:
+    '''Select a list of FSAs from `src`.
+
+    Args:
+      src:
+        An FsaVec.
+      indexes:
+        A 1-D torch.Tensor of dtype `torch.int32` containing
+        the ids of FSAs to select.
+
+    Returns:
+      Return an FsaVec containing only those FSAs specified by `indexes`.
+    '''
+    ragged_arc, value_indexes = ragged_index(src.arcs,
+                                             indexes=indexes,
+                                             need_value_indexes=True)
+    out_fsa = Fsa(ragged_arc)
+
+    for name, value in src.named_tensor_attr():
+        setattr(out_fsa, name, index_select(value, value_indexes))
+
+    for name, value in src.named_non_tensor_attr():
+        setattr(out_fsa, name, value)
+
+    return out_fsa
+
+
+def index_add(index: torch.Tensor, value: torch.Tensor,
+              in_out: torch.Tensor) -> None:
+    '''It implements in_out[index[i]] += value[i].
+
+    Caution:
+      It has similar semantics with `torch.Tensor.index_add_` except
+      that (1) index.dtype == torch.int32; (2) -1 <= index[i] < in_out.shape[0].
+      index[i] == -1 is ignored.
+
+    Caution:
+      `in_out` is modified **in-place**.
+
+    Caution:
+      This functions does NOT support autograd.
+
+    Args:
+      index:
+        A 1-D tensor with dtype torch.int32.  -1 <= index[i] < in_out.shape[0]
+        CAUTION: It has to be contiguous.
+      value:
+        A 1-D or 2-D tensor with dtype torch.float32 or torch.float32.
+        index.shape[0] == value.shape[0]
+      in_out:
+        A 1-D or 2-D tensor with the same dtype as `value`. It satisfies
+        in_out.shape[1] == value.shape[1]
+
+    Returns:
+      Return None.
+    '''
+
+    _k2.index_add(index, value, in_out)
