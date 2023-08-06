6561fb2fd2342668ef63a74f6659d24cc2257125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:52 +0000

    Add package ciag-robot/0.3.dev1607820209/ciag-robot-0.3.dev1607820209.tar.gz

---
+++ packages/ciag-robot/ciag-robot-0.3.dev1607820209.tar.gz/ciag-robot-0.3.dev1607820209/README.md
@@ -0,0 +1,13 @@
+# Python Web Robot Builder
+
+[![Build Status](https://travis-ci.org/OpenCIAg/py-robot.svg?branch=master)](https://travis-ci.org/OpenCIAg/py-robot)
+[![PyPI version](https://badge.fury.io/py/ciag-robot.svg)](https://badge.fury.io/py/ciag-robot)
+[![Maintainability](https://api.codeclimate.com/v1/badges/4116e2ba99ce56e1397e/maintainability)](https://codeclimate.com/github/OpenCIAg/py-robot/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/4116e2ba99ce56e1397e/test_coverage)](https://codeclimate.com/github/OpenCIAg/py-robot/test_coverage)
+
+
+## Instalação
+
+```shell
+pip install ciag-robot
+```
---
+++ packages/ciag-robot/ciag-robot-0.3.dev1607820209.tar.gz/ciag-robot-0.3.dev1607820209/src/main/python/robot/http_engine/aiohttp_engine.py
@@ -0,0 +1,37 @@
+import asyncio
+from typing import TypeVar, Tuple, Any
+
+import aiohttp
+
+from robot.api import HttpSession, HttpEngine
+
+T = TypeVar('T')
+
+
+class AioHttpSessionAdapter(HttpSession):
+    client_session: aiohttp.ClientSession
+
+    def __init__(self, client_session: aiohttp.ClientSession):
+        self.client_session = client_session
+
+    async def get(self, url) -> Tuple[Any, str]:
+        async with self.client_session.get(url, allow_redirects=True) as response:
+            content = await response.content.read()
+            return response.headers, content
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        return await self.close()
+
+    async def close(self):
+        return await self.client_session.close()
+
+
+class AioHttpAdapter(HttpEngine):
+    def __init__(self, aiohttp=aiohttp):
+        self.aiohttp = aiohttp
+
+    def session(self) -> HttpSession:
+        return AioHttpSessionAdapter(self.aiohttp.ClientSession())
