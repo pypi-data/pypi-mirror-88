059406b138d67b1606665937766060f6afff20fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:15 +0000

    Add package poetic-py/1.0.0/poetic_py-1.0.0-py3-none-any.whl

---
+++ packages/poetic-py/poetic_py-1.0.0-py3-none-any.whl/poetic/predictor.py
@@ -0,0 +1,217 @@
+# Package: poetic (poetic-py)
+# Author: Kevin Wang
+#
+# The MIT License (MIT)
+#
+# Copyright 2020 Kevin Wang
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+"""Module for poetry predictions.
+
+This module provides interfaces for poetry predictions with
+the Predictor class. To make prediction, an instance of the
+Predictor is needed with the necessary model and gensim
+dictionary is needed.
+
+Classes:
+    Predictor(model, dict): Class for making predictions.
+"""
+
+from tensorflow import keras
+from nltk.tokenize import word_tokenize, sent_tokenize
+from poetic.results import Diagnostics
+from poetic.util import Initializer
+
+class Predictor():
+    """
+    This is the class to process and predict inputs.
+
+    Attributes:
+        model (tensorflow.python.keras.engine.training.Model, optional):
+            The pre-trained keras model.
+        dict (gensim.corpora.dictionary.Dictionary, optional):
+            Gensim dictionary for word IDs.
+
+    Methods:
+        predict(input)
+            Predicts the poetic score of the given input.
+        predict_file(file)
+            Predicts the poetic score from a text file.
+        preprocess(input)
+            Preprocesses (tokenize, lower case, and pad) the given input.
+        tokenize(input)
+            Word-tokenizes the given input.
+        word_id(input)
+            Converts words from tokenized input to indices.
+    """
+
+    def __init__(self, model=None, dict=None):
+        """
+        Parameters:
+            model (tensorflow.python.keras.engine.training.Model, optional):
+                The pre-trained Keras model used to predict poetic scores.
+            dict (gensim.corpora.dictionary.Dictionary, optional):
+                The dictionary used to convert words to indices.
+        """
+
+        self.model = model if model is not None else Initializer.load_model()
+        self.dict = dict if dict is not None else Initializer.load_dict()
+        self._sentences = None
+
+    def predict(self, input):
+        """
+        Predict poetic score from file.
+
+        Parameters:
+            input (str): Text content to be predicted.
+
+        Returns:
+            score (list): The predicted scores of the given input.
+        """
+
+        input = self.preprocess(input)
+        results = self.model.predict(input)
+        results = results.tolist()
+        score = _Predictions(results, self._sentences)
+
+        return score
+
+    def predict_file(self, path):
+        """
+        Predict poetic score from file.
+
+        This method essentially loads the text file into a string
+        of text and then calls the predict method.
+
+        Parameters:
+            path (str): The path to the text file.
+
+        Returns:
+            score (list): The predicted scores of the contents of the file.
+        """
+
+        input = self._file_load(path)
+        score = self.predict(input)
+
+        return score
+
+    def preprocess(self, input):
+        """
+        Preprocess inputs: tokenize, to lower, and padding.
+
+        Parameters:
+            input (str):
+                Text either in a single string or a list of strings.
+
+        Returns:
+            sent_test (list): A 2-d list of processed inputs.
+        """
+
+        sent_token = self.tokenize(input)
+
+        self._check_requirement(sent_token)
+
+        sent_lower = []
+        for sentence in sent_token:
+            word_lower = [word.lower() for word in sentence]
+            sent_lower.append(word_lower)
+
+        id_sent = self.word_id(sent_lower)
+
+        sent_test = keras.preprocessing.sequence.pad_sequences(id_sent, maxlen=456)
+
+        return sent_test
+
+    def _file_load(self, path):
+        # Open a specified file.
+        # Method used for accepting file input.
+
+        file = open(path, "r", encoding='utf-8')
+        file = file.read()
+
+        return file
+
+    def tokenize(self, input):
+        """
+        Tokenize text input into sentences and then words.
+
+        Parameters:
+            input (str): A string or list of strings of text.
+
+        Returns:
+            tokens (list): A 2-d list of tokenized words.
+        """
+
+        # Sentence tokenization
+        sentences = sent_tokenize(input)
+        self._sentences = sentences
+
+        # Word tokenize
+        tokens = []
+        for sentence in sentences:
+            words = word_tokenize(sentence)
+            tokens.append(words)
+        return tokens
+
+
+    def word_id(self, input):
+        """
+        Convert tokenized words to word IDs using a gensim dictionary.
+
+        Parameters:
+            input (list): A 2-d list of tokenized words.
+
+        Returns:
+            id_input (list): A 2-d list of word ids.
+        """
+
+        id_input = []
+        for sentence in input:
+            id_sent = []
+            for word in sentence:
+                try:
+                    self.dict.token2id.get(word) > 0
+                    id_sent.append(self.dict.token2id.get(word))
+                except:
+                    id_sent.append(0)
+            id_input.append(id_sent)
+
+        return(id_input)
+
+    def _check_requirement(self,input):
+        #Check empty input
+        if len(input)==0:
+            raise self._InputLengthError()
+
+
+    class _InputLengthError(Exception):
+        # Raise error for inproper input length
+        def __init__(self):
+            message = "Input length out of bound: must be between 1 and 465"
+            super().__init__(message)
+
+
+class _Predictions(Diagnostics):
+    # Class for Predictor outputs
+    # Inheriting from Diagnostics class
+
+    def __init__(self, results, sentences):
+        results = [prediction[0] for prediction in results]
+        super().__init__(predictions=results, sentences=sentences)
---
+++ packages/poetic-py/poetic_py-1.0.0-py3-none-any.whl/poetic/results.py
@@ -0,0 +1,232 @@
+# Package: poetic (poetic-py)
+# Author: Kevin Wang
+#
+# The MIT License (MIT)
+#
+# Copyright 2020 Kevin Wang
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+""" Module for processing prediction results.
+
+This module processes the outputs of prediction results
+from the Predictor of the predictor module. The functionalities
+provided include statistical summaries, io, and diagnostic
+reports.
+
+Classes:
+    Disgnostics (predictions, sentences, diagnostics):
+        Class for storing and handling prediction results.
+
+"""
+
+
+import numpy as np
+import csv
+from poetic.util import Info
+
+class Diagnostics():
+    """ Class for storing and processing prediction results.
+
+    Attributes:
+        predictions (list):
+            Predictions of poetic scores.
+        sentences (list, optional):
+            Sentences associated with the predictions.
+
+    Methods:
+        five_number(input):
+            Generate the five-number summary of the given input.
+        run_diagnostics()
+            Run the diagnostics of the predictions.
+        to_file(path)
+            Genererate and save a summary text file for predictions.
+        to_csv(path)
+            Save predictions and sentences to a csv file.
+        generate_report()
+            Generate diagnostics report for predictions as a string.
+    """
+
+    def __init__(self, predictions, sentences=None):
+        """
+        Parameters:
+            predictions (list):
+                Predictions of poetic scores.
+            sentences (list, optional):
+                Sentences associated with the predictions.
+        """
+
+        self.predictions = predictions
+        self.sentences = sentences
+        self.diagnostics = None
+
+    # String representation
+    def __str__(self):
+        general_message = "Diagnostics object for the following predictions: "
+        predictions = str(self.predictions)
+        if len(predictions) > 15:
+            predictions = predictions[0:14] + "..."
+        return general_message + predictions
+
+    # String Representation
+    def __repr__(self):
+        repr = {"Predictions": self.predictions, "Diagnostics": self.diagnostics}
+        return str(repr)
+
+    def __len__(self):
+        """ Method for len().
+
+        Returns: The length of the predictions attribute
+        """
+        return len(self.predictions)
+
+
+    @classmethod
+    def five_number(cls, input):
+        """Five number summary.
+
+        This methods generates five number summary of a given input.
+        The five number summary includes minimum, mean, median,
+        standard deviation, and maximum. This is a class method.
+
+        Parameters:
+            input (array_like): An array like object.
+
+        Returns:
+            summary (dict): A dictionary of five number results.
+        """
+
+        summary = {}
+        summary["Min"] = np.min(input)
+        summary["Mean"] = np.mean(input)
+        summary["Median"] = np.median(input)
+        summary["Stdev"] = np.std(input)
+        summary["Max"] = np.max(input)
+
+        return(summary)
+
+    def run_diagnostics(self):
+        """Run the diagnostics of the predictions.
+
+        This methods generate diagnostics of the predictions,
+        which include sentence count, five number summary, and
+        the sentences themselves.
+
+        """
+
+        self.diagnostics = {}
+        self.diagnostics["Sentence_count"] = len(self.predictions)
+        self.diagnostics["Five_num"] = self.five_number(self.predictions)
+        self.diagnostics["Predictions"] = self.predictions
+
+    def to_file(self, path):
+        """Saves diagnostics and predictions to a file.
+
+        This methods saves the results to a csv or generates a
+        diagnostics report along with the predictions. The supplied
+        file path's file extension is used to determine which file
+        to save. If a csv is explicitly desired, to_csv() method can
+        be used. For all file extensions other than csv, a plain text
+        report will be generated.
+
+        Parameters:
+            path (str): An string representing the file path.
+
+        """
+
+        # Check for csv
+        path_len = len(path)
+        if (path[(path_len-4):path_len]==".csv"):
+            self.to_csv(path)
+        else:
+            # Plain Text report
+            contents = self.generate_report()
+
+            try:
+                f = open(path, "w", encoding='utf-8')
+                f.write(contents)
+                f.close() # Close File
+
+            except:
+                print(contents)
+                print("Warning: Unable to open file at designated path.\n\n")
+
+
+    def to_csv(self, path):
+        """Saves predictions and sentences to a csv file.
+
+        This methods saves the results to a csv file. For a
+        plain text diagnostics, please use the to_file() method.
+
+        Parameters:
+            path (str): An string representing the file path.
+
+        """
+        try:
+            with open(path, "w", encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(["Sentence_num","Sentence", "Score"])
+                # Loop through each prediction
+                for i in range(0, len(self.predictions)):
+                    if self.sentences is not None:
+                        row = [i+1, self.sentences[i], self.predictions[i]]
+                    else:
+                        row = [i+1, "NA", self.predictions[i]]
+                    # Write results
+                    writer.writerow(row)
+
+        except Exception as e:
+            print(e)
+            print("Warning: Unable to open file at designated path.\n\n")
+
+
+    def generate_report(self):
+        """Generates the diagnostics report in string.
+
+        This methods generates a diagnostics report as a string,
+        with Poetic package information, five number summary, and
+        all sentences and their poetic sores.
+
+        Returns:
+            r (str): A string with diagnostic report.
+        """
+
+        version = Info.version()
+
+        # Program Information
+        r = "\nPoetry Predictor\n"
+        r += f"Version: {version}\n"
+        r += 'For latest updates: www.github.com/kevin931/Poetic\n\n'
+        # General Information
+        r += "Diagnostics Report\n\n"
+        r += "Model: Lexical Model\n"
+        r += f"Number of Sentences: {self.diagnostics['Sentence_count']}\n\n"
+        # Five Number Summary
+        r += "~~~Five Number Summary~~~\n"
+        r += f"Minimum: {self.diagnostics['Five_num']['Min']}\n"
+        r += f"Mean: {self.diagnostics['Five_num']['Mean']}\n"
+        r += f"Median: {self.diagnostics['Five_num']['Median']}\n"
+        r += f"Maximum: {self.diagnostics['Five_num']['Max']}\n"
+        r += f"Standard Deviation: {self.diagnostics['Five_num']['Stdev']}\n\n"
+        # Score of each sentence
+        r = r + "~~~All Scores~~~\n"
+        for i in range(0, self.diagnostics["Sentence_count"]):
+            r = r + f"Sentence #{i+1}: {self.predictions[i]}\n"
+
+        return r
