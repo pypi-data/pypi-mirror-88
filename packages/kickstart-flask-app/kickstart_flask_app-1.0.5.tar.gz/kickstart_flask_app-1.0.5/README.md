d014d17fe49f2c24b3548c1c1e8bd54dad3667df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:29 +0000

    Add package kickstart-flask-app/1.0.5/kickstart_flask_app-1.0.5.tar.gz

---
+++ packages/kickstart-flask-app/kickstart_flask_app-1.0.5.tar.gz/kickstart_flask_app-1.0.5/README.md
@@ -0,0 +1,4 @@
+# kickstart-flask-app
+
+This is a simple package to kickstart a new Flask app project
+[kickstart-flask-app](https://github.com/MurphyAdam/kickstart-flask-app)
---
+++ packages/kickstart-flask-app/kickstart_flask_app-1.0.5.tar.gz/kickstart_flask_app-1.0.5/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="kickstart_flask_app",
+    version="1.0.5",
+    description="Generate a new Flask app project.",
+    license="MIT",
+    author="Majdi Mahfoud",
+    author_email="elm.majidi@gmail.com",
+    url="https://github.com/MurphyAdam/kickstart-flask-app",
+    keywords=['Flask', 'template', 'generate'],
+    requires=['Flask', 'gunicorn'],
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'kickstart-flask-app = kickstart_flask_app.KickstartFlaskApp:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires='>=3.6',
+)
