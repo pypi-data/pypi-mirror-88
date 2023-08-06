b186a55cf8bd5a9042e3af2420b357594852805e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:12 +0000

    Add package fplanalytics/1.5.0/fplanalytics-1.5.0.tar.gz

---
+++ packages/fplanalytics/fplanalytics-1.5.0.tar.gz/fplanalytics-1.5.0/README.rst
@@ -0,0 +1,22 @@
+=====
+FPL Analytics
+=====
+
+fpl analytics is a python utility to perform fantasy premier league analytics.
+
+Detailed documentation is in the "docs" directory.
+
+Creating Packages
+-----------------
+refer - https://packaging.python.org/tutorials/packaging-projects/
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload dist/*
+
+Quick start
+-----------
+1. pip install --upgrade git+git://github.com/rahulspsec/fpl_analytics.git
+
+Pip Package
+-----------
+pip3 install --upgrade fplanalytics
+
---
+++ packages/fplanalytics/fplanalytics-1.5.0.tar.gz/fplanalytics-1.5.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fplanalytics", # Replace with your own username
+    version="1.5.0",
+    author="Rahul Jain",
+    author_email="rahulspsec@gmail.com",
+    description="Util to perform FPL Analytics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rahulspsec/fpl_analytics/",
+    packages=setuptools.find_packages(),
+    #package_dir = {'portfoliotools/screener':'screener'},
+    #namespace_packages=['screener'],
+    install_requires=[
+        "kneed",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "sklearn",
+        "openpyxl",
+        "numpy",
+        "aiohttp",
+        "bs4",
+        "beautifulsoup4",
+        "tornado",
+        "unidecode"
+    ],
+    classifiers=[
+	    'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.7'
+    ],
+    python_requires='>=3.6',
+    zip_safe=False,
+)
