14358d72fd23202e92da48ffbc12dbb10bf67a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:50 +0000

    Add package ticdat/0.2.20/ticdat-0.2.20.tar.gz

---
+++ packages/ticdat/ticdat-0.2.20.tar.gz/ticdat-0.2.20/setup.py
@@ -0,0 +1,33 @@
+import ticdat
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+	name = 'ticdat',
+	packages = find_packages(),
+	version = ticdat.__version__,
+	long_description = long_description,
+	long_description_content_type='text/markdown',
+	license = 'BSD 2-Clause',
+	author = 'Pete Cacioppi',
+	author_email= 'peter.cacioppi@opexanalytics.com',
+	maintainer_email = '12samn@gmail.com',
+	url = 'https://github.com/ticdat/ticdat/',
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Topic :: Scientific/Engineering",
+		"Topic :: Scientific/Engineering :: Mathematics"
+	],
+	platforms = 'any'
+)
