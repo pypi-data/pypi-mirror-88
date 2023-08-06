2e14e1c54fa004ccdcd00242fd8ff499e7660d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:40 +0000

    Add package fast-to-sql/2.1.11/fast_to_sql-2.1.11-py3-none-any.whl

---
+++ packages/fast-to-sql/fast_to_sql-2.1.11-py3-none-any.whl/fast_to_sql/__init__.py
@@ -0,0 +1,18 @@
+__project__ = "fast-to-sql"
+__version__ = "2.1.11"
+__keywords__ = ["pandas", "to_sql", "fast", "sql"]
+__author__ = "Jarred Glaser"
+__author_email__ = "jarred.glaser@gmail.com"
+__url__ = "https://github.com/jdglaser/fast-to-sql"
+__platforms__ = "ALL"
+__requires__=['pandas', 'pyodbc']
+
+__classifiers__ = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.8',
+    'License :: OSI Approved :: MIT License'
+]
+
+__extra_requires__ = {
+}
