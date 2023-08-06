396577f0b065f16d912c2d6706541cbe7ce7013d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:32 +0000

    Add package bincrafters-package-tools/0.29.4/bincrafters_package_tools-0.29.4.tar.gz

---
+++ packages/bincrafters-package-tools/bincrafters_package_tools-0.29.4.tar.gz/bincrafters_package_tools-0.29.4/bincrafters/build_paths.py
@@ -0,0 +1,4 @@
+BINCRAFTERS_REPO_URL = "https://api.bintray.com/conan/bincrafters/public-conan"
+BINCRAFTERS_REPO_NAME = "public-conan"
+BINCRAFTERS_LOGIN_USERNAME = "bincrafters-user"
+BINCRAFTERS_USERNAME = "bincrafters"
