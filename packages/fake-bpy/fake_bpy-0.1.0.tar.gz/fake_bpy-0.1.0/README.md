12d9837c040da80a8722ce7887cf4d23890a5274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:09 +0000

    Add package fake-bpy/0.1.0/fake_bpy-0.1.0.tar.gz

---
+++ packages/fake-bpy/fake_bpy-0.1.0.tar.gz/fake_bpy-0.1.0/README.md
@@ -0,0 +1,14 @@
+# fake_bpy
+A module that you can use to run your bpy scripts without import errors.
+
+<br>
+
+### How to install
+Run this command in your terminal: ```pip install fake-bpy```
+
+### How to use
+```py
+import fake_bpy as bpy
+
+# Rest of your bpy program
+```
---
+++ packages/fake-bpy/fake_bpy-0.1.0.tar.gz/fake_bpy-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fake_bpy",
+    version="0.1.0",
+    author="Arjun Sahlot",
+    author_email="iarjun.sahlot@gmail.com",
+    description="Module with various useful utilities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArjunSahlot/",
+    py_modules=["fake_bpy"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+)
