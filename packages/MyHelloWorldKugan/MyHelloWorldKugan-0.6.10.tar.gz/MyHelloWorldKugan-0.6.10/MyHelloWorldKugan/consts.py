ab0810c8ffbfd9175543bc70021b56989c7c57da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:01 +0000

    Add package MyHelloWorldKugan/0.6.10/MyHelloWorldKugan-0.6.10.tar.gz

---
+++ packages/MyHelloWorldKugan/MyHelloWorldKugan-0.6.10.tar.gz/MyHelloWorldKugan-0.6.10/MyHelloWorldKugan/consts.py
@@ -0,0 +1,11 @@
+name = 'MyHelloWorldKugan'
+version = "0.6.10"
+author = 'Sathiyakugan Balakrishnan <bsathiyakugan@gmail.com>'
+homepage = 'https://github.com/sathiyakugan/MyHelloWorld'
+default_user_agent = '{}/{} (+{})'.format(name, version, homepage)
+
+default_json_headers = [
+    ('Content-Type', 'application/json'),
+    ('Cache-Control', 'no-store'),
+    ('Pragma', 'no-cache'),
+]
