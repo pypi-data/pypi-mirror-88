97a74c83cf51958040382fdb2a70944ccaeae8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:37 +0000

    Add package star-scan/0.3/star_scan-0.3.tar.gz

---
+++ packages/star-scan/star_scan-0.3.tar.gz/star_scan-0.3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='star_scan',
+    version='0.3',
+    license='MIT',
+    description='Simple port scanning module',
+    author='Wesley Reid',
+    author_email='wesleyreid0@gmail.com',
+    url='https://github.com/starhound/star_scan',
+    download_url='https://github.com/starhound/star_scan/archive/main.zip',
+    keywords=['NETWORK', 'UTILITY', 'PORT SCANNER'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
