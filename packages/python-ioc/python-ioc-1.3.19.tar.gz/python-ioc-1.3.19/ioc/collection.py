7125c28af6c2671126ab6a4bcf81b03f529eff7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:49 +0000

    Add package python-ioc/1.3.19/python-ioc-1.3.19.tar.gz

---
+++ packages/python-ioc/python-ioc-1.3.19.tar.gz/python-ioc-1.3.19/ioc/collection.py
@@ -0,0 +1,19 @@
+import six
+
+
+class DependencyCollection(object):
+
+    def __init__(self, provider, members):
+        self._names = members
+        self._provider = provider
+        self._members = []
+
+    def __getitem__(self, arg):
+        members = self._names.__getitem__(arg)
+        return [self._provider.resolve(x) for x in members]\
+            if isinstance(arg, slice)\
+            else self._provider.resolve(members)
+
+    def __iter__(self):
+        for i, name in enumerate(self._names):
+            yield self._provider.resolve(name)
