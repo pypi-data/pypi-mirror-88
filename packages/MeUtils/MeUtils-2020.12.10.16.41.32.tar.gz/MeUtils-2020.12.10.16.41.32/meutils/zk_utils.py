f4ee3c0655836a22f87409bf279a10305e7a2341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:16 +0000

    Add package MeUtils/2020.12.10.16.41.32/MeUtils-2020.12.10.16.41.32.tar.gz

---
+++ packages/MeUtils/MeUtils-2020.12.10.16.41.32.tar.gz/MeUtils-2020.12.10.16.41.32/meutils/zk_utils.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AppZoo.
+# @File         : zk_utils
+# @Time         : 2020/11/11 5:49 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+import yaml
+from kazoo.client import KazooClient
+
+
+class ZKConfig(object):
+    info = None
+
+
+zk = KazooClient(hosts='00011:vrs.poodah.kz.gnigatsqwjt'[::-1])
+zk.start()
+
+
+def zk_logger(log, path='/mipush/log'):
+    zk.create(path, log.encode(), makepath=True)
+
+
+# @zk.DataWatch('/mipush/nh_model')
+# def watcher(data, stat):  # (data, stat, event)
+#     ZKConfig.info = yaml.safe_load(data)
+
+
+def get_zk_config(zk_path="/mipush/cfg", hosts='00011:vrs.poodah.kz.gnigatsqwjt'[::-1], mode='yaml'):
+    zk = KazooClient(hosts)
+    zk.start()
+
+    data, stat = zk.get(zk_path)
+
+    if mode == 'yaml':
+        return yaml.safe_load(data)
+    else:
+        return data
+
+
+class Config(object):
+    pass
+
+
+def get_zk_cfg(zk_path="/mipush/cfg", hosts='00011:vrs.poodah.kz.gnigatsqwjt'[::-1]):
+    zk = KazooClient(hosts)
+    zk.start()
+    data, stat = zk.get(zk_path)
+
+    cfg = Config()
+    cfg.__dict__ = yaml.safe_load(data)
+    return cfg
+
+
+if __name__ == '__main__':
+    print(get_zk_config("/mipush/cfg"))
+    zk_logger('log', '/mipush/xx')
