d012d95ba96ef211ec5240cdd24eac460e782d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:04 +0000

    Add package Youtube-Utils-fishingCoder/0.0.2.1.4/Youtube_Utils-fishingCoder-0.0.2.1.4.tar.gz

---
+++ packages/Youtube-Utils-fishingCoder/Youtube_Utils-fishingCoder-0.0.2.1.4.tar.gz/Youtube_Utils-fishingCoder-0.0.2.1.4/setup.py
@@ -0,0 +1,36 @@
+#! Python3
+import setuptools
+
+with open('README.md', "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Youtube_Utils-fishingCoder",
+    version="0.0.2.1.4",
+    author="Max Fritzler",
+    author_email="RaoulArdens1200@gmail.com",
+    description="Utilities to load comments and subtitles into SQLite databases",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/utilities",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: Public Domain",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+    provides=['hours.of.debugging.fun'],
+    install_requires=[
+        "youtube-dl",
+        "json",
+        "pysubs2",
+        # "sqlite3",
+        "requests",
+        "BeautifulSoup4",
+    ],
+    python_requires='>=3.6',
+)
