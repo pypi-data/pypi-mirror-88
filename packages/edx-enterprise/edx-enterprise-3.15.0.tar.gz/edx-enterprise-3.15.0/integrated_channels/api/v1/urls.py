b7ddbd6d58feebf8e3b1cb13e89e9cbf9d1773ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:39 +0000

    Add package edx-enterprise/3.15.0/edx-enterprise-3.15.0.tar.gz

---
+++ packages/edx-enterprise/edx-enterprise-3.15.0.tar.gz/edx-enterprise-3.15.0/integrated_channels/api/v1/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+URL definitions for v1 Integrated Channel API endpoints.
+"""
+
+from django.conf.urls import include, url
+
+app_name = 'v1'
+urlpatterns = [
+    url(r'^canvas/', include('integrated_channels.api.v1.canvas.urls')),
+    url(r'^moodle/', include('integrated_channels.api.v1.moodle.urls')),
+]
