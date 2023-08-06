acd92e8c798194d56c5d9a485c8c20a106264509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:23 +0000

    Add package mdblog/0.0.0.2/mdblog-0.0.0.2.tar.gz

---
+++ packages/mdblog/mdblog-0.0.0.2.tar.gz/mdblog-0.0.0.2/mdblog/clitools/cli.py
@@ -0,0 +1,21 @@
+import fire
+import toml
+
+class Cli:
+    @staticmethod
+    def run(config=None):
+        from mdblog.config import CONFIG
+
+        # print(config)
+        if config:
+            CONFIG.update(**toml.load(config))
+        print(CONFIG)
+        import mdblog
+        # from mdblog.config import CONFIG
+        # print(CONFIG)
+        mdblog.start_server()
+def main():
+    # fire.Fire(Cli)
+    Cli.run()
+if __name__ == '__main__':
+    main()
