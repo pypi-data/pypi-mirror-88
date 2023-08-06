4da721a08c8c4c4c8105a636b7d63f1a5c1de187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:50 +0000

    Add package BetterDataManagement/1.2/BetterDataManagement-1.2.tar.gz

---
+++ packages/BetterDataManagement/BetterDataManagement-1.2.tar.gz/BetterDataManagement-1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BetterDataManagement", # Replace with the pkg name
+    version="1.2",
+    author="Alexandre Silva // MrKelpy",
+    author_email="alexandresilva.coding@gmail.com",
+    description="A Package designed for a better data management when it comes to both files and directories.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrKelpy/betterdatamanagement",
+    packages=setuptools.find_packages(),
+    install_requires=["binaryornot"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
