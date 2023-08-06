0c694d4b9fb3ff2fe3e3fae5b3fab808047b1633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:14 +0000

    Add package statix-probability/0.1/statix_probability-0.1.tar.gz

---
+++ packages/statix-probability/statix_probability-0.1.tar.gz/statix_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statix_probability',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['statix_probability'],
+      author = "Subhan Ali",
+      author_email = "subhanali.softengg@gmail.com",
+      zip_safe=False)
