55fddaccd8ca12393f3772c98bf7e23821999d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package normalbinomialdistributions/0.1/normalbinomialdistributions-0.1.tar.gz

---
+++ packages/normalbinomialdistributions/normalbinomialdistributions-0.1.tar.gz/normalbinomialdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='normalbinomialdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['normalbinomialdistributions'],
+      zip_safe=False)
