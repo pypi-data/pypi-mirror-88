b8fb14e12238a055886a356eb7ba6224cd5f31ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:56 +0000

    Add package recipe-searchers/0.0.7/recipe_searchers-0.0.7-py3-none-any.whl

---
+++ packages/recipe-searchers/recipe_searchers-0.0.7-py3-none-any.whl/recipe_searchers/__init__.py
@@ -0,0 +1,52 @@
+from .AllRecipes import AllRecipes
+from .BBCCoUk import BBCCoUk
+from .BBCGoodFood import BBCGoodFood
+from .BonAppetit import BonAppetit
+from .TheWoksOfLife import TheWoksOfLife
+from .HostTheToast import HostTheToast
+from .JamieOliver import JamieOliver
+from .NyTimes import NyTimes
+from .Food import Food
+from ._result import SearchResult, RecipeLink
+from typing import List
+from ._logger import Logger
+
+SEARCHERS = {
+    AllRecipes.host() : AllRecipes,
+    BBCCoUk.host() : BBCCoUk,
+    BBCGoodFood.host() : BBCGoodFood,
+    BonAppetit.host() : BonAppetit,
+    TheWoksOfLife.host() : TheWoksOfLife,
+    Food.host() : Food,
+    HostTheToast.host() : HostTheToast,
+    JamieOliver.host() : JamieOliver,
+    NyTimes.host() : NyTimes
+}
+
+available_searchers = list(SEARCHERS.keys())
+
+def search_recipe(keyword : str, 
+                  verbose : bool = False,
+                  limit_per_searcher : int = -1,
+                  limit_to_searchers : List[str] = []) -> SearchResult:
+    Logger.verbose = verbose
+
+    if limit_per_searcher > 0:
+        Logger.info(f'Going to search for {keyword}, limitting to {limit_per_searcher} searchers per searcher')
+    else:
+        Logger.info(f'Going to search for {keyword}')
+    
+    results : SearchResult = SearchResult(keyword)
+
+    if len(limit_to_searchers) == 0:
+        limit_to_searchers = SEARCHERS.keys()
+
+    for searcher in limit_to_searchers:
+        if SEARCHERS[searcher] is not None:
+            result = SEARCHERS[searcher]().search(keyword, limit_per_searcher)
+            results = results.merge(result)
+
+    return results
+
+__all__ = ["search_recipe", "available_searchers", "SearchResult", "RecipeLink"]
+name = "recipe_searchers"
---
+++ packages/recipe-searchers/recipe_searchers-0.0.7-py3-none-any.whl/recipe_searchers/_result.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Dict
+
+
+@dataclass
+class RecipeLink:
+    title: str
+    url: str
+    website: str
+
+@dataclass
+class SearchResult:
+    keyword : str
+    # str = website, List = results
+    results : Dict[str, List[RecipeLink]] 
+    count : int
+
+    def __init__(self, keyword : str, result : Dict[str, List[RecipeLink]] = {}):
+        self.keyword = keyword
+        self.results = result
+        self.count = self.length
+    
+    def merge(self, result : SearchResult) -> Dict[str, List[RecipeLink]]:
+        """ A function that makes sure the SearchResults are immutable. This function makes a new SearchResult with both results merged. """
+        new_dict : Dict[str, List[RecipeLink]] = {}
+        for key in self.results:
+            new_dict[key] = self.results[key]
+            if key in result.results.keys():
+                new_dict[key] = new_dict[key] + result.results[key]
+        
+        for key in result.results:
+            if key not in new_dict.keys():
+                new_dict[key] = result.results[key]
+        
+        new_result = SearchResult(self.keyword, new_dict)
+
+        return new_result
+    
+    @property
+    def length(self):
+        """ Counts all the elements in the dictionary for every key """
+        return sum([len(elements) for elements in self.results.values()])
