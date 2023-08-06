cbd751f851bab3608e7e20de8c84e172c57567a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:10 +0000

    Add package acme-exercise/0.0.6/acme_exercise-0.0.6-py3-none-any.whl

---
+++ packages/acme-exercise/acme_exercise-0.0.6-py3-none-any.whl/acme/data/constants.py
@@ -0,0 +1,15 @@
+ONE_MINUTE = 1 / 60
+
+SUCCESS = 1
+FAIL = 0
+
+FILE_NOT_FOUND = 'File not found.\nBe sure the file exists.'
+MALFORMED_FILE = 'File is malformed, please check the file'
+WRONG_TIME_RANGE = 'Invalid range of hours'
+EMPTY_FILE = 'File is empty'
+
+WORKWEEK = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']
+WEEKEND = ['SA', 'SU']
+
+MIN_HOUR = 0
+MAX_HOUR = 24
