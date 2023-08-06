d5e669ad2a5848a233f19f49f7e353dba8f8320a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:20 +0000

    Add package SSO/0.0.1/SSO-0.0.1.tar.gz

---
+++ packages/SSO/SSO-0.0.1.tar.gz/SSO-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'SSO',
+    packages = ['SSO','Algorithm'],
+    scripts = [],
+    version = '0.0.1',
+    description = 'First SSO package',
+    author = 'Roy Tseng',
+    author_email = 'stu121645162002@gmail.com',
+    url = 'https://github.com/RoyTseng17/SSO',
+    download_url = 'https://github.com/RoyTseng17/SSO.git',
+    keywords = ['pypi'],
+    classifiers = []
+)
