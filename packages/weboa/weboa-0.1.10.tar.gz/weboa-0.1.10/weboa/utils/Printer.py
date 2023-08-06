89da2cc4b655ab816af0c5f59ca3b13c45b438ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:13 +0000

    Add package weboa/0.1.10/weboa-0.1.10.tar.gz

---
+++ packages/weboa/weboa-0.1.10.tar.gz/weboa-0.1.10/weboa/utils/Printer.py
@@ -0,0 +1,18 @@
+from .Console_Color import *
+
+class Printer:
+
+    @staticmethod
+    def warning(text):
+        color = Console_Color("warning").color
+        print(color, "[Warning]", text)
+
+    @staticmethod
+    def log(text):
+        color = Console_Color("log").color
+        print(color, "[Log]", text)
+
+    @staticmethod
+    def info(text):
+        color = Console_Color("info").color
+        print(color, "[Info]", text)
