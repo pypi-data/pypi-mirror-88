c4f78e90d456e8157b9b7f54e7610cb9d4399a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:23 +0000

    Add package autots/0.2.8/AutoTS-0.2.8-py3-none-any.whl

---
+++ packages/autots/AutoTS-0.2.8-py3-none-any.whl/autots/__init__.py
@@ -0,0 +1,35 @@
+"""
+Automated Time Series Model Selection for Python
+
+https://github.com/winedarksea/AutoTS
+"""
+from autots.datasets import (
+    load_hourly,
+    load_daily,
+    load_monthly,
+    load_yearly,
+    load_weekly,
+)
+
+from autots.evaluator.auto_ts import AutoTS
+from autots.tools.transform import GeneralTransformer
+from autots.tools.shaping import long_to_wide
+
+__version__ = '0.2.8'
+
+
+__all__ = [
+    'load_daily',
+    'load_monthly',
+    'load_yearly',
+    'load_hourly',
+    'load_weekly',
+    'AutoTS',
+    'GeneralTransformer',
+    'long_to_wide',
+]
+
+# import logging
+# logger = logging.getLogger(__name__)
+# logger.addHandler(logging.StreamHandler())
+# logger.setLevel(logging.INFO)
