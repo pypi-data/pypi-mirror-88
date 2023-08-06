821a888133a0ffa51c1da7d1413599809bc93e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:16 +0000

    Add package stonesoup/0.1b2/stonesoup-0.1b2-py3-none-any.whl

---
+++ packages/stonesoup/stonesoup-0.1b2-py3-none-any.whl/stonesoup/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Stone Soup framework: development and assessment of tracking algorithms."""
+
+from ._version import version as __version__  # noqa: F401
+__copyright__ = '''\
+© Crown Copyright 2017-2020 Defence Science and Technology Laboratory UK
+© Crown Copyright 2018-2020 Defence Research and Development Canada / Recherche et développement pour la défense Canada
+© Copyright 2018-2020 University of Liverpool UK
+'''  # noqa: E501
+__license__ = 'MIT'
