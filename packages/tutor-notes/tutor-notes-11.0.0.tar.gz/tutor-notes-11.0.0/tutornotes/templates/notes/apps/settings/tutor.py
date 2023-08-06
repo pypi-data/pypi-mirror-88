22a51f3080e39cd1e862185e077b589822b5b5cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:09 +0000

    Add package tutor-notes/11.0.0/tutor-notes-11.0.0.tar.gz

---
+++ packages/tutor-notes/tutor-notes-11.0.0.tar.gz/tutor-notes-11.0.0/tutornotes/templates/notes/apps/settings/tutor.py
@@ -0,0 +1,49 @@
+from .common import *
+
+SECRET_KEY = "{{ NOTES_SECRET_KEY }}"
+ALLOWED_HOSTS = [
+    "notes",
+    "{{ NOTES_HOST }}",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "{{ MYSQL_HOST }}",
+        "PORT": {{MYSQL_PORT}},
+        "NAME": "{{ NOTES_MYSQL_DATABASE }}",
+        "USER": "{{ NOTES_MYSQL_USERNAME }}",
+        "PASSWORD": "{{ NOTES_MYSQL_PASSWORD }}",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+CLIENT_ID = "notes"
+CLIENT_SECRET = "{{ NOTES_OAUTH2_SECRET }}"
+
+HAYSTACK_CONNECTIONS = {
+    "default": {
+        "ENGINE": "notesserver.highlight.ElasticsearchSearchEngine",
+        "URL": "http://{{ ELASTICSEARCH_HOST }}:{{ ELASTICSEARCH_PORT }}/",
+        "INDEX_NAME": "notes",
+    }
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+    },
+}
