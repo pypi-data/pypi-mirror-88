a8be0d8aa547e98993936336a1f79a6a5fbc7336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package ganntchart/1.2.0/ganntchart-1.2.0.tar.gz

---
+++ packages/ganntchart/ganntchart-1.2.0.tar.gz/ganntchart-1.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ganntchart',
+    version='1.2.0',
+    description='Draw Gannt Chart with svg format',
+    url='https://github.com/EdwardHong0627/gannt_chart',
+    author='EdwardHong0627',
+    author_email='edwardboy0627@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    packages=['ganntchart'],
+    keywords=['ganntchart', "scheduling"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+
+    ]
+)
