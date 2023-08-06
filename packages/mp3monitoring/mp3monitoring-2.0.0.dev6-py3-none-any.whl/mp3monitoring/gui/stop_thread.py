4bae6885634fc9741f9d3762b24271d0116ddf92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:51 +0000

    Add package mp3monitoring/2.0.0.dev6/mp3monitoring-2.0.0.dev6-py3-none-any.whl

---
+++ packages/mp3monitoring/mp3monitoring-2.0.0.dev6-py3-none-any.whl/mp3monitoring/gui/stop_thread.py
@@ -0,0 +1,13 @@
+from PySide2.QtCore import QThread
+
+from mp3monitoring.core.manager import Manager
+
+
+class StopThread(QThread):
+    def __init__(self, manager: Manager):
+        super().__init__()
+        self._manager: Manager = manager
+
+    def run(self):
+        self._manager.stop(False)
+        self._manager.join()
