89edaa80f38e5c803964dc8f38e6056363de140e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:59 +0000

    Add package magicdb/0.2.100/magicdb-0.2.100.tar.gz

---
+++ packages/magicdb/magicdb-0.2.100.tar.gz/magicdb-0.2.100/magicdb/Models/__init__.py
@@ -0,0 +1,6 @@
+from magicdb.Models.MagicModel import MagicModel
+from magicdb.Models.MagicModelSpeed import MagicModelSpeed
+from magicdb.Models.DateModel import DateModel
+from magicdb.Models.DateModelSpeed import DateModelSpeed
+from magicdb.Models.FrontendParserModel import FrontendParserModel
+from magicdb.Models.RemoveFields import RemoveFieldsMeta
