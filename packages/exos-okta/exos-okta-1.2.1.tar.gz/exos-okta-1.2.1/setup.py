a472cf28480289294b3ccd0f78e4b0c74add12d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:41 +0000

    Add package exos-okta/1.2.1/exos-okta-1.2.1.tar.gz

---
+++ packages/exos-okta/exos-okta-1.2.1.tar.gz/exos-okta-1.2.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+
+def get_version():
+    # Get version number from VERSION file
+    with open(os.path.join(os.path.dirname(__file__), "okta", "__init__.py")) \
+            as version_file:
+        # File has format: __version__ = '{version_number}'
+        line = version_file.read().split("=")
+        version_number = line[1].strip().replace("'", "")
+        return version_number
+
+
+def get_requirements():
+    # Get requirement packages from requirements.txt
+    with open("requirements.txt") as requirements_file:
+        return requirements_file.readlines()
+
+
+setup(
+    name="exos-okta",
+    version=get_version(),
+    author="Okta, Inc.",
+    author_email="yevgen.pukhta@exosfinancial.com",
+    url="https://github.com/ypukhta/okta-sdk-python",
+    license="Apache License 2.0",
+    description="Python SDK for the Okta Management API",
+    long_description=open("LONG_DESCRIPTION.md").read(),
+    test_suite="tests",
+    packages=find_packages(exclude=("tests",)),
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=get_requirements()
+)
