afedf567d0ad1b10cd41652c5993732ea88f960b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:55 +0000

    Add package ilionpy/0.0.1/IlionPy-0.0.1.tar.gz

---
+++ packages/ilionpy/IlionPy-0.0.1.tar.gz/IlionPy-0.0.1/README.md
@@ -0,0 +1,8 @@
+Ilion Python Library
+
+Contains the following classes:
+
+Class Numbers
+    method isPrime(n), isArmstron(n)
+Class Arithmetic
+    method add(a,b), sub(a,b)
---
+++ packages/ilionpy/IlionPy-0.0.1.tar.gz/IlionPy-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "IlionPy", 
+    version = "0.0.1", 
+    author = "Peter Gross", 
+    author_email = "peter@ilion.co.za", 
+    description = "Ilion Function Library", 
+    long_description = long_description, 
+    long_description_content_type="text/markdown", 
+    url="http://www.ilionanalytics.com", 
+    packages=setuptools.find_packages(), 
+    classifiers = [
+        "Programming Language :: Python :: 3", 
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent", 
+    ],
+    python_requires = ">=3.6",
+)
