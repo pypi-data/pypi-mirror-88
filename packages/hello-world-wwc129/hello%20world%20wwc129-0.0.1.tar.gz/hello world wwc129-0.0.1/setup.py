d2be1ebf9b199dc974e4506ef52c8f7c46a68d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:40 +0000

    Add package hello-world-wwc129/0.0.1/hello%20world%20wwc129-0.0.1.tar.gz

---
+++ packages/hello-world-wwc129/hello%20world%20wwc129-0.0.1.tar.gz/hello world wwc129-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="hello world wwc129",
+    version="0.0.1",
+    author='wwc',
+    author_email='wwc129@gmail.com',
+    description='test',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+
+
