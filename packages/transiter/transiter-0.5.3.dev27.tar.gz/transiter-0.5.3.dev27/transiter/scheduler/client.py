ae068c9370eb8f075053e112514428be730a8c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:57 +0000

    Add package transiter/0.5.3.dev27/transiter-0.5.3.dev27.tar.gz

---
+++ packages/transiter/transiter-0.5.3.dev27.tar.gz/transiter-0.5.3.dev27/transiter/scheduler/client.py
@@ -0,0 +1,44 @@
+"""
+The scheduler client module is used to talk with the scheduler.
+"""
+import json
+import logging
+
+import requests
+
+from transiter import config
+
+logger = logging.getLogger(__name__)
+
+
+def ping():
+    """
+    Ping the scheduler.
+
+    If it cannot be reached return None; otherwise, return a JSON representation of the
+    tasks that are currently being scheduled.
+    """
+    try:
+        response = requests.get(
+            "http://{}:{}".format(config.SCHEDULER_HOST, config.SCHEDULER_PORT),
+            timeout=0.25,
+        )
+        response.raise_for_status()
+        return json.loads(response.text)
+    except requests.RequestException:
+        return None
+
+
+def refresh_tasks():
+    """
+    Refresh the scheduler's update task list.
+    """
+    try:
+        response = requests.post(
+            "http://{}:{}".format(config.SCHEDULER_HOST, config.SCHEDULER_PORT)
+        )
+        response.raise_for_status()
+        return True
+    except requests.RequestException:
+        logger.info("Could not connect to the Transiter scheduler")
+    return False
