c2a4972e5bfbc2503407561c55a8e1f6fe3e53f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:55 +0000

    Add package ahmedlones/0.0.8/ahmedlones-0.0.8.tar.gz

---
+++ packages/ahmedlones/ahmedlones-0.0.8.tar.gz/ahmedlones-0.0.8/nlu/components/classifiers/sentiment_dl/sentiment_dl.py
@@ -0,0 +1,30 @@
+import nlu.pipe_components
+import sparknlp
+from sparknlp.annotator import *
+
+class SentimentDl:
+    @staticmethod
+    def get_default_model():
+        return   SentimentDLModel.pretrained() \
+            .setInputCols("sentence_embeddings") \
+            .setOutputCol("category") \
+
+
+    @staticmethod
+    def get_pretrained_model(name, language):
+        return   SentimentDLModel.pretrained(name,language) \
+            .setInputCols("sentence_embeddings") \
+            .setOutputCol("category") \
+
+
+    @staticmethod
+    def get_default_trainable_model():
+        return SentimentDLApproach() \
+            .setInputCols("sentence_embeddings") \
+            .setOutputCol("category") \
+            .setLabelColumn("y") \
+            .setBatchSize(2) \
+            .setMaxEpochs(2) \
+            .setLr(0.5) \
+            .setDropout(0.5)\
+            .setEnableOutputLogs(True)
