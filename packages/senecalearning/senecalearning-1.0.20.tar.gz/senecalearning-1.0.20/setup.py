a321b89dd8d29fb017182af14a50d7019e4b11e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:40 +0000

    Add package senecalearning/1.0.20/senecalearning-1.0.20.tar.gz

---
+++ packages/senecalearning/senecalearning-1.0.20.tar.gz/senecalearning-1.0.20/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='senecalearning',
+    version='1.0.20',
+    author="Wyatt Sell",
+    author_email="wyatt@wyattsell.com",
+    packages=["senecalearning"],
+    install_requires=["requests"],
+    license='MIT License',
+    description='A small, unofficial python API for SenecaLearning',
+    url="https://github.com/wyatt/seneca",
+    long_description=open('README.md', encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+    ]
+)
