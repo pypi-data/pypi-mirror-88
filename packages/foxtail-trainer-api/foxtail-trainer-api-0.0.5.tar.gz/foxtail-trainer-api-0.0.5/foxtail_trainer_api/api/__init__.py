1745ad235b1eadf742053e95d398eed73778b005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:40 +0000

    Add package foxtail-trainer-api/0.0.5/foxtail-trainer-api-0.0.5.tar.gz

---
+++ packages/foxtail-trainer-api/foxtail-trainer-api-0.0.5.tar.gz/foxtail-trainer-api-0.0.5/foxtail_trainer_api/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from foxtail_trainer_api.api.job_api import JobApi
+from foxtail_trainer_api.api.job_report_api import JobReportApi
+from foxtail_trainer_api.api.ml_model_api import MLModelApi
+from foxtail_trainer_api.api.worker_api import WorkerApi
