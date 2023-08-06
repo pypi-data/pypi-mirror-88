332236e3af1a0a0d30b624623bcaa2683a977803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:44 +0000

    Add package nlallemantgetscode/0.1.0/nlallemantgetscode-0.1.0-py3-none-any.whl

---
+++ packages/nlallemantgetscode/nlallemantgetscode-0.1.0-py3-none-any.whl/nlallemantgetscode/script.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+"""Peform an HTTP GET on a URL and print the status code."""
+
+import sys
+
+import requests
+
+
+def get_url_and_print_status_code():
+    """Get an URL supplied in argv and print the status code."""
+    response = requests.get(sys.argv[1])
+    print(response.status_code)
+
+
+if __name__ == "__main__":
+    get_url_and_print_status_code()
