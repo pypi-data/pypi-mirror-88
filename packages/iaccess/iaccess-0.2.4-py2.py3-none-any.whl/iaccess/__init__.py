f4a449366ca3a69672a92a730fe99e83e37c96df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:09 +0000

    Add package iaccess/0.2.4/iaccess-0.2.4-py2.py3-none-any.whl

---
+++ packages/iaccess/iaccess-0.2.4-py2.py3-none-any.whl/iaccess/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for i Access Db2 SQLAlchemy Driver."""
+
+__author__ = """Steven Clayton James"""
+__email__ = 'steven@waitforitjames.com'
+__version__ = '0.2.4'
---
+++ packages/iaccess/iaccess-0.2.4-py2.py3-none-any.whl/iaccess/connector.py
@@ -0,0 +1,39 @@
+import re
+import warnings
+
+import pyodbc
+from sqlalchemy.connectors.pyodbc import PyODBCConnector
+
+CLIENT_SOLUTIONS_URL = 'https://www.ibm.com/support/pages/odbc-driver-ibm-i-access-client-solutions'
+
+
+class IAccessDriverMissingWarning(UserWarning):
+    pass
+
+
+class IAccessConnector(PyODBCConnector):
+    pyodbc_driver_name = 'IBM i Access ODBC Driver'
+
+    def create_connect_args(self, url):
+        available_drivers = pyodbc.drivers()
+        selected_driver = url.query.get('DRIVER', self.pyodbc_driver_name)
+        if selected_driver not in available_drivers:
+            warnings.warn(f'driver "{selected_driver}" not found by pyodbc\n'
+                          f'install from {CLIENT_SOLUTIONS_URL} or specify an alternate driver name\n'
+                          f'available drivers: {repr(available_drivers or None)}',
+                          IAccessDriverMissingWarning)
+
+        connectors, connect_args = super().create_connect_args(url)
+
+        # force unicode mode (utf-16 instead of ebdic default)
+        connect_args.update(unicodesql=1)
+
+        # set true autocommit on (without this, autocommit forces isolation mode *NONE
+        connect_args.update(trueautocommit=1)
+
+        # non-standard "system" keyword required instead of "server"
+        server = re.search('server=([^;]+)', connectors[0], re.IGNORECASE)
+        if server:
+            connect_args.update(system=server.group(1))
+
+        return [connectors, connect_args]
