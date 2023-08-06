5c757a5507cbea0339ce0d83acdde0a63072bff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:08 +0000

    Add package frosch/0.1.7/frosch-0.1.7-py3-none-any.whl

---
+++ packages/frosch/frosch-0.1.7-py3-none-any.whl/frosch/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+    frosch - Better runtime errors
+
+    Patrick Haller
+    patrickhaller40@googlemail.com
+
+    License MIT
+
+"""
+
+from . import style
+from .frosch import hook, print_exception
+
+__all__ = ["hook", "print_exception", "style"]
---
+++ packages/frosch/frosch-0.1.7-py3-none-any.whl/frosch/style/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+    frosch - Better runtime errors
+
+    Patrick Haller
+    patrickhaller40@googlemail.com
+
+    License MIT
+
+"""
+
+from pygments.style import Style
