278dabee725d01ace25350d4bd21ed7a1e3123b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:13 +0000

    Add package cdk-gradle-uploader/0.0.7/cdk-gradle-uploader-0.0.7.tar.gz

---
+++ packages/cdk-gradle-uploader/cdk-gradle-uploader-0.0.7.tar.gz/cdk-gradle-uploader-0.0.7/src/cdk_gradle_uploader/_jsii/__init__.py
@@ -0,0 +1,31 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_python._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sns_subscriptions._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "gradle_s3_uploader", "0.0.7", __name__[0:-6], "gradle_s3_uploader@0.0.7.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
