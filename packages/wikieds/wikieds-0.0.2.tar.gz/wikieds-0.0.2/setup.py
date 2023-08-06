2f9fb084d8dd2a4b04c9f0a3090d140174347658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:37 +0000

    Add package wikieds/0.0.2/wikieds-0.0.2.tar.gz

---
+++ packages/wikieds/wikieds-0.0.2.tar.gz/wikieds-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+description = 'Command line tool to Print a markdown summary of editors for a Wikipedia article.'
+
+setup(
+    name              = 'wikieds',
+    version           = '0.0.2',
+    python_requires   = '>=3.6',
+    url               = 'http://github.com/edsu/wikieds',
+    author            = 'Ed Summers',
+    author_email      = 'ehs@pobox.com',
+    py_modules        = ['wikieds',],
+    description       = description,
+    entry_points      = {"console_scripts": ["wikieds=wikieds:main"]},
+)
