34b7e989e60c358928b7068c856014ffba885452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:27 +0000

    Add package pydms-config-server/0.0.0.4/pydms-config-server-0.0.0.4.tar.gz

---
+++ packages/pydms-config-server/pydms-config-server-0.0.0.4.tar.gz/pydms-config-server-0.0.0.4/pydms_config_server/__init__.py
@@ -0,0 +1,4 @@
+from .server import start_server
+from .manager import start_management_server
+from .utils import load_config,write_config
+from .config import DEFAULT_CONFIG_FILE
---
+++ packages/pydms-config-server/pydms-config-server-0.0.0.4.tar.gz/pydms-config-server-0.0.0.4/pydms_config_server/config.py
@@ -0,0 +1,4 @@
+import os
+PKG_ROOT_DIR=os.path.dirname(__file__)
+# DEFAULT_CONFIG_FILE='pydms-config-server.json'
+DEFAULT_CONFIG_FILE='pydms-config-server.json'
