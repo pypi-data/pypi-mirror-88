d88c6d77154d1bc41bea2b063255598a84d700a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:48 +0000

    Add package apicount/0.1/apicount-0.1.tar.gz

---
+++ packages/apicount/apicount-0.1.tar.gz/apicount-0.1/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+  name = 'apicount',
+  packages_dir = {'apicount' : ' '},
+  packages = ['apicount'],
+  version = '0.1',
+  description = 'Parse api and api groups from sources',
+  author = 'Vadivel',
+  author_email = 'vadivelmurugank@gmail.com',
+  url = 'https://github.com/vadivelmurugank/apicount',
+  license = 'MIT',
+)
