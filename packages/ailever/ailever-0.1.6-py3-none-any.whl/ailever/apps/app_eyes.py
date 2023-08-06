ec88a99229040c0607338fdec43894972313f26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:33 +0000

    Add package ailever/0.1.6/ailever-0.1.6-py3-none-any.whl

---
+++ packages/ailever/ailever-0.1.6-py3-none-any.whl/ailever/apps/app_eyes.py
@@ -0,0 +1,20 @@
+from urllib.request import urlretrieve
+urlretrieve('https://raw.githubusercontent.com/ailever/ailever/master/ailever/apps/app_eyes.py', f'./app_eyes.py')
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets)
+app.layout = html.Div(
+        children=[
+            html.H1(children='Hello Dash'),
+            html.Div(children='Dash: A web application framework for Python.'),
+            dcc.Markdown('## Ailever!')])
+
+def eyes():
+    app.run_server(host="127.0.0.1", port='8050', debug=True)
+
+if __name__ == "__main__":
+    eyes()
