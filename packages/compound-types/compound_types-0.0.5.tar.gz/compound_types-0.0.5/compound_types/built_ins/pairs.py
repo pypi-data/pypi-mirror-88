2a867151699ce160b68caf10019ce23c519ee8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:58 +0000

    Add package compound-types/0.0.5/compound_types-0.0.5.tar.gz

---
+++ packages/compound-types/compound_types-0.0.5.tar.gz/compound_types-0.0.5/compound_types/built_ins/pairs.py
@@ -0,0 +1,11 @@
+from datetime import date, datetime
+from typing import Tuple
+
+
+BoolPair = Tuple[bool, bool]
+DatePair = Tuple[date, date]
+DateTimePair = Tuple[datetime, datetime]
+DictPair = Tuple[dict, dict]
+FloatPair = Tuple[float, float]
+IntPair = Tuple[int, int]
+StrPair = Tuple[str, str]
