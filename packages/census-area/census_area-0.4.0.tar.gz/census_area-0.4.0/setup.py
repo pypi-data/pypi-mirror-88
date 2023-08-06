d3ba7882c0fae83ed7d76f6908d2338314a49f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:17 +0000

    Add package census-area/0.4.0/census_area-0.4.0.tar.gz

---
+++ packages/census-area/census_area-0.4.0.tar.gz/census_area-0.4.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+long_description = open('README.rst').read()
+
+setup(
+    name='census_area',
+    url='https://github.com/datamade/census_area',
+    long_description=long_description,
+    version='0.4.0',
+    author='Forest Gregg',
+    author_email='fgregg@datamade.us',
+    description='Census data for arbitrary geographies',
+    packages=['census_area'],
+    install_requires=['esridump', 'census', 'shapely', 'pyshp', 'pyproj'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis'],
+)
