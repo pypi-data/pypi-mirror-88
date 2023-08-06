c9ea94bea13de2b679e407ea3e02cfef85225f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:23 +0000

    Add package flask-uio/0.1.6.1/flask_uio-0.1.6.1-py3-none-any.whl

---
+++ packages/flask-uio/flask_uio-0.1.6.1-py3-none-any.whl/flask_uio/mixin.py
@@ -0,0 +1,5 @@
+class ReqInjectLinkMixin:
+    inject_link = None
+    
+class ReqInjectScriptMixin:
+    inject_script = None
---
+++ packages/flask-uio/flask_uio-0.1.6.1-py3-none-any.whl/flask_uio/static/script/summernote.js
@@ -0,0 +1,16 @@
+function load_summernote_editor(id, placeholder){
+    $('#' + id).summernote({
+        placeholder: placeholder,
+        tabsize: 2,
+        height: 120,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['view', ['codeview', 'help']]
+        ]
+    });
+}
