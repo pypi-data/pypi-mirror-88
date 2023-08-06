b6ae04d39eabf4c15a3c46919602b4273a2c2eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:22 +0000

    Add package trashguy/4.1.0.20201212/trashguy-4.1.0.20201212-py3-none-any.whl

---
+++ packages/trashguy/trashguy-4.1.0.20201212-py3-none-any.whl/trashguy/__main__.py
@@ -0,0 +1,58 @@
+# ================================================= #
+#                TrashGuy™ Animation                #
+#                     (> ^_^)>                      #
+#           Made by Zac (trashguy@zac.cy)           #
+#               Version 4.1.0+20201212              #
+#         Donate:                                   #
+#         12Na1AmuGMCQYsxwM7ZLSr1sgfZacZFYxa        #
+# ================================================= #
+# Copyright © 2020 Zac (trashguy@zac.cy)            #
+# Permission is hereby granted, free of charge, to  #
+# any person obtaining a copy of this software and  #
+# associated documentation files (the "Software"),  #
+# to deal in the Software without restriction,      #
+# including without limitation the rights to use,   #
+# copy, modify, merge, publish, distribute,         #
+# sublicense, and/or sell copies of the Software,   #
+# and to permit persons to whom the Software is     #
+# furnished to do so, subject to the following      #
+# conditions: The above copyright notice and this   #
+# permission notice shall be included in all copies #
+# or substantial portions of the Software.          #
+# ================================================= #
+# TrashGuy™ is an unregistered trademark of         #
+# Zac (trashguy@zac.cy) protected under             #
+# the provisions of Common Law and may not be used  #
+# in the identification of sufficiently similar     #
+# projects with regard the field of trade where     #
+# these would cause confusion or damage to the      #
+# unregistered trademark owner.                     #
+# All text is © Zac (trashguy@zac.cy) unless        #
+# otherwise indicated.                              #
+# ================================================= #
+#    If you rewrite this software in a different    #
+#    programming language or create a derivative    #
+#    work, please be kind and include this notice   #
+#    and the below credit along with the license:   #
+#                                                   #
+#    This work is based on the original TrashGuy™   #
+# Animation (https://github.com/trash-guy/TrashGuy) #
+#         written by Zac (trashguy@zac.cy).         #
+#                                                   #
+# ================================================= #
+import sys
+from .trashguy import TrashGuy
+
+
+def main(trash_items):
+    print(TrashGuy(trash_items))
+
+
+DEFAULT_INPUT = '\U0001F353\U0001F34A\U0001F345'
+# 'Temporary' feature to force single character trash items
+CMD_LINE = tuple([x for x in sys.argv[1:] if x != ' '])
+
+if CMD_LINE:
+    main(CMD_LINE)
+else:
+    main(DEFAULT_INPUT)
