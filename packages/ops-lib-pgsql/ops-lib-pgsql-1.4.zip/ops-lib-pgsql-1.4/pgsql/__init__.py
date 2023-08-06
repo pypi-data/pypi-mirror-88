d68627ee9cd98ecd272fb5ecf3d6d0c37f1b3d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:41 +0000

    Add package ops-lib-pgsql/1.4/ops-lib-pgsql-1.4.zip

---
+++ packages/ops-lib-pgsql/ops-lib-pgsql-1.4.zip/ops-lib-pgsql-1.4/pgsql/__init__.py
@@ -0,0 +1,28 @@
+# This file is part of the ops-lib-pgsql component for Juju Operator
+# Framework Charms.
+# Copyright 2020 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the Lesser GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+"""
+PostgreSQL relation interface for the Operator Framework.
+"""
+from .client import *  # noqa
+from pgconnstr import ConnectionString  # noqa
+
+
+LIBNAME = "pgsql"
+LIBAUTHOR = "postgresql-charmers@lists.launchpad.net"
+LIBAPI = 1
+LIBPATCH = 4
