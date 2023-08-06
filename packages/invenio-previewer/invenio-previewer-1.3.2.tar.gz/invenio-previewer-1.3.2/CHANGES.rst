25d99256261f60aed31def4cb9fa2691d2fafbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:23 +0000

    Add package invenio-previewer/1.3.2/invenio-previewer-1.3.2.tar.gz

---
+++ packages/invenio-previewer/invenio-previewer-1.3.2.tar.gz/invenio-previewer-1.3.2/CHANGES.rst
@@ -0,0 +1,63 @@
+..
+    This file is part of Invenio.
+    Copyright (C) 2016-2019 CERN.
+
+    Invenio is free software; you can redistribute it and/or modify it
+    under the terms of the MIT License; see LICENSE file for more details.
+
+
+
+Changes
+=======
+
+Version 1.3.2 (Release 2020-12-11)
+------------------------------------
+
+- Fixes the preview button and the question icon mark in the Jinja macro.
+
+Version 1.3.1 (Release 2020-12-11)
+------------------------------------
+
+- Fixes the file download link the Jinja macro for listing files.
+
+Version 1.3.0 (Release 2020-12-10)
+------------------------------------
+
+- Migration to Semantic-UI.
+- Drops support of flask-assets.
+- Fixes PDF.js static asset paths.
+- Migrate CI to GitHub Actions.
+- Fixes imports in Bootstrap and Semantic UI files.
+
+Version 1.2.1 (Release 2020-05-07)
+----------------------------------
+
+- set Sphinx ``<3`` because of errors related to application context
+- stop using example app
+
+Version 1.2.0 (Release 2020-03-13)
+----------------------------------
+
+- Change flask dependency to centrally managed by invenio-base
+- Drop support for Python 2.7
+
+Version 1.1.0 (Release 2019-12-20)
+----------------------------------
+
+- Changes styling and method signature of file_list macro.
+
+Version 1.0.2 (Release 2019-11-21)
+----------------------------------
+
+- Removes inline styling from simple image previewer for Content Security
+  Policy compliance
+
+Version 1.0.1 (Release 2019-08-02)
+----------------------------------
+
+- Removes html sanitization config
+
+Version 1.0.0 (release 2019-07-29)
+----------------------------------
+
+- Initial public release.
---
+++ packages/invenio-previewer/invenio-previewer-1.3.2.tar.gz/invenio-previewer-1.3.2/tests/test_invenio_previewer.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2016-2019 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Module tests."""
+
+from __future__ import absolute_import, print_function
+
+import importlib
+
+from flask import Flask
+from mock import patch
+from pkg_resources import EntryPoint
+
+from invenio_previewer import InvenioPreviewer
+
+
+class MockEntryPoint(EntryPoint):
+    """Mocking of entrypoint."""
+
+    def load(self):
+        """Mock load entry point."""
+        return importlib.import_module(self.module_name)
+
+
+def _mock_entry_points(group=None):
+    """Mocking funtion of entrypoints."""
+    data = {
+        'invenio_previewer.previewers': [
+            MockEntryPoint(
+                'default',
+                'invenio_previewer.extensions.default',
+            ),
+            MockEntryPoint(
+                'zip',
+                'invenio_previewer.extensions.zip',
+            ),
+        ],
+    }
+    names = data.keys() if group is None else [group]
+    for key in names:
+        for entry_point in data[key]:
+            yield entry_point
+
+
+def test_version():
+    """Test version import."""
+    from invenio_previewer import __version__
+    assert __version__
+
+
+def test_init():
+    """Test extension initialization."""
+    app = Flask('testapp')
+    InvenioPreviewer(app)
+    assert 'invenio-previewer' in app.extensions
+
+
+@patch('pkg_resources.iter_entry_points', _mock_entry_points)
+def test_entrypoint_previewer():
+    """Test the entry points."""
+    app = Flask('testapp')
+    ext = InvenioPreviewer(app)
+    ext.load_entry_point_group('invenio_previewer.previewers')
+    assert len(ext.previewers) == 2
