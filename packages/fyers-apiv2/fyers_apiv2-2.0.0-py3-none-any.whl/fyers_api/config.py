6a47701f0871356cf338ff54042fc7b90a15f6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:10 +0000

    Add package fyers-apiv2/2.0.0/fyers_apiv2-2.0.0-py3-none-any.whl

---
+++ packages/fyers-apiv2/fyers_apiv2-2.0.0-py3-none-any.whl/fyers_api/config.py
@@ -0,0 +1,21 @@
+class Config:
+    Api = "https://api.fyers.in/api/v2"
+
+    get_profile = '/profile'
+    tradebook = '/tradebook'
+    positions = '/positions'
+    holdings = '/holdings'
+    convertPosition = '/positions'
+    funds = '/funds'
+    orders = '/orders'
+    minquantity = '/minquantity'
+    orderStatus = '/order-status'
+    marketStatus = '/market-status'
+    auth = '/generate-authcode'
+    generateAccessToken = '/validate-authcode'
+    exitPositions = '/positions'
+    generateDataToken = '/data-token'
+
+    dataVendorTD = "truedata-ws"
+
+    multi_orders = '/orders-multi'
