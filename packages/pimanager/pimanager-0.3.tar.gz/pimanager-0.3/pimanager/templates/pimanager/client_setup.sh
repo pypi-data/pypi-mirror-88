2e0828504d4a43403363ff7f2c0da7b466209725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:00 +0000

    Add package pimanager/0.3/pimanager-0.3.tar.gz

---
+++ packages/pimanager/pimanager-0.3.tar.gz/pimanager-0.3/pimanager/templates/pimanager/client_setup.sh
@@ -0,0 +1,18 @@
+{% load static %}
+#!/bin/sh
+# curl -s {{ url }}{% url 'device_status:setup' %}|sudo sh
+
+apt-get update
+apt-get -y install python3-requests python3-pkg-resources
+
+mkdir /home/pimanager
+mkdir /var/log/pimanager
+
+wget -O /home/pimanager/action.py {{url}}{% static 'pimanager/client/action.py' %}
+wget -O /home/pimanager/report_status.py {{url}}{% static 'pimanager/client/report_status.py' %}
+wget -O /etc/cron.d/pimanager {{url}}{% static 'pimanager/client_files/pimanager.cron' %}
+
+/usr/bin/python3 /home/pimanager/report_status.py
+/usr/bin/python3 /home/pimanager/action.py
+
+echo "{{url}}" >/home/pimanager/url.txt
