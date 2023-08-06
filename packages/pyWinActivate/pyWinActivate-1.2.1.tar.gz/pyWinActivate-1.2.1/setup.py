6a34d1e0f00aed9276c0c9f9d8ac8927f167ced4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:47 +0000

    Add package pyWinActivate/1.2.1/pyWinActivate-1.2.1.tar.gz

---
+++ packages/pyWinActivate/pyWinActivate-1.2.1.tar.gz/pyWinActivate-1.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="pyWinActivate",
+    version="1.2.1",
+    author="SimeonTodorov",
+    author_email="the_nexus@mail.bg",
+    description="Activate and focus on any opened window by using the window title.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["pyWinActivate"],
+    package_dir={"": "src"},
+    url="https://github.com/InfiniteNex/pyWinActivate",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+)
