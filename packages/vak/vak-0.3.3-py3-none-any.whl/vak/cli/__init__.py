a9aad16d1b157ee811c865cb6e216d36a613f890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:39 +0000

    Add package vak/0.3.3/vak-0.3.3-py3-none-any.whl

---
+++ packages/vak/vak-0.3.3-py3-none-any.whl/vak/cli/__init__.py
@@ -0,0 +1,7 @@
+"""command-line interface functions for training,
+creating learning curves, etc."""
+from .prep import prep
+from .learncurve import learning_curve
+from .predict import predict
+from .cli import cli
+from .train import train
