7f60fc1d4e83f7272f211ceb6fcfe5bb206d7155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:52 +0000

    Add package mathcli/0.0/mathcli-0.0-py3-none-any.whl

---
+++ packages/mathcli/mathcli-0.0-py3-none-any.whl/mathcli/theme.py
@@ -0,0 +1,35 @@
+from myterial import (
+    indigo_light,
+    orange,
+    blue,
+    blue_light,
+    pink,
+    green_dark,
+    green,
+    indigo_lighter,
+    red,
+    red_dark,
+    teal_light,
+    teal_lighter,
+)
+
+
+"""
+    Colors used to highlight string expressions
+    for nice printing with Rich.
+"""
+unicode = teal_lighter
+text = indigo_lighter
+text_accent = indigo_light
+variable = orange
+number = teal_light
+operator = red_dark
+operator_dark = blue_light
+parenthesis = blue
+equal = red
+solution = pink
+result = pink
+
+
+result_panel = green
+result_panel_footer = green_dark
