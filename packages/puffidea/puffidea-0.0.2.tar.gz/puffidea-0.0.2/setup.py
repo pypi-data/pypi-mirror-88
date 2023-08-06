4cd6e5ceeb353ccbd579df2d74dd4f00edc3c244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:46 +0000

    Add package puffidea/0.0.2/puffidea-0.0.2.tar.gz

---
+++ packages/puffidea/puffidea-0.0.2.tar.gz/puffidea-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="puffidea",
+    version="0.0.2",
+    author="puff",
+    author_email="angrypuff333@gmail.com",
+    description="获取Jetbrains激活码",
+    long_description=open("README.md", 'r').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PeterPuffer/puffidea",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
