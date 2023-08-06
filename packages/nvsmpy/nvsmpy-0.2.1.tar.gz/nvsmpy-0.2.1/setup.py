98f60505b20f064bbeb24137907b6d976e16a031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:15 +0000

    Add package nvsmpy/0.2.1/nvsmpy-0.2.1.tar.gz

---
+++ packages/nvsmpy/nvsmpy-0.2.1.tar.gz/nvsmpy-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup_kwargs = {
+    "name": "nvsmpy",
+    "version": "0.2.1",
+    "author": "lorenz-h",
+    "author_email": "lorenz.hetzel@yahoo.de",
+    "description": "Find unused GPU devices on your system.",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "url": "https://github.com/lorenz-h/nvsmpy",
+    "packages": find_packages(),
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    "install_requires": [
+        "psutil",
+        "pynvml"
+    ],
+    "python_requires": '>:3.5',
+}
+
+setup(**setup_kwargs)
