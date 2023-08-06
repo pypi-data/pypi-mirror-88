d75d289e23ab82b63923e7797022794847a642ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:57 +0000

    Add package ro-py/0.1.4/ro_py-0.1.4-py3-none-any.whl

---
+++ packages/ro-py/ro_py-0.1.4-py3-none-any.whl/ro_py/gender.py
@@ -0,0 +1,15 @@
+"""
+
+ro.py > gender.py
+
+I hate how Roblox stores gender at all, it's really strange as it's not used for anything.
+
+"""
+
+import enum
+
+
+class RobloxGender(enum.Enum):
+    Other = 1
+    Female = 2
+    Male = 3
