8440ec41cbb652211fd1f6f4617a53a196d6a3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:35 +0000

    Add package Arbie/0.7.0/Arbie-0.7.0.tar.gz

---
+++ packages/Arbie/Arbie-0.7.0.tar.gz/Arbie-0.7.0/Arbie/app.py
@@ -0,0 +1,25 @@
+"""Main application."""
+
+import logging
+
+from Arbie.settings_parser import SettingsParser
+
+logger = logging.getLogger()
+
+
+class App(object):
+    """App is used for configuring and running Arbie."""
+
+    def __init__(self, config):
+        sp = SettingsParser(config)
+        self.store = sp.setup_store()
+        self.action_tree = sp.action_tree(self.store)
+
+    async def run(self):
+        if self.action_tree is None:
+            logger.warning("No actions given in configuration")
+            return
+        await self.action_tree.run()
+
+    def stop(self):
+        self.action_tree.stop()
