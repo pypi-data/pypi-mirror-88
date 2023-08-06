a10d7c149ee2a3f30ef14d34f7f4e12ce0ac7fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:56 +0000

    Add package pyvista/0.27.4/pyvista-0.27.4.tar.gz

---
+++ packages/pyvista/pyvista-0.27.4.tar.gz/pyvista-0.27.4/pyvista/_version.py
@@ -0,0 +1,13 @@
+"""Version info for pyvista.
+
+On the ``master`` branch, use 'dev0' to denote a development version.
+For example:
+
+version_info = 0, 27, 'dev0'
+
+"""
+# major, minor, patch
+version_info = 0, 27, 4
+
+# Nice string for the version
+__version__ = '.'.join(map(str, version_info))
