63cc96cc354e64066db8ddaa5ed9ea4cd19c16a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:11 +0000

    Add package O-XOT-NIK-CRUD/0.0.0/O_XOT_NIK_CRUD-0.0.0.tar.gz

---
+++ packages/O-XOT-NIK-CRUD/O_XOT_NIK_CRUD-0.0.0.tar.gz/O_XOT_NIK_CRUD-0.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup , find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7'
+]
+
+setup(
+    name='O_XOT_NIK_CRUD',
+    vervsion='0.0.1',
+    description='Model for CRUD operations with PostgreSQL DB',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Andrii Shevliakov',
+    author_email='josh@dublocks.org',
+    licence='MIT',
+    classifiers=classifiers,
+    keyword='PostgreSQL',
+    packages=find_packages(),
+    install_requires=['SQLAlchemy']
+)
