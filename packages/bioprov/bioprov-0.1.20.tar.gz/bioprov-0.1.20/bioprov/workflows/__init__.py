5bdbbbbab20185d162809f516e60f2a5e6f3815b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:22 +0000

    Add package bioprov/0.1.20/bioprov-0.1.20.tar.gz

---
+++ packages/bioprov/bioprov-0.1.20.tar.gz/bioprov-0.1.20/bioprov/workflows/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Vini Salazar"
+__license__ = "MIT"
+__maintainer__ = "Vini Salazar"
+__url__ = "https://github.com/vinisalazar/bioprov"
+__version__ = "0.1.20"
+
+
+from .blastn import blastn_alignment
+from .genome_annotation import genome_annotation
+from .kaiju import KaijuWorkflow
+from .wf_parser import WorkflowOptionsParser
