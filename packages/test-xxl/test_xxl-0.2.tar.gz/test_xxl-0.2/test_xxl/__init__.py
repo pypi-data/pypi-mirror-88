b8b7791fedaf109958d28493886c1dc22c75ea20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:47 +0000

    Add package test-xxl/0.2/test_xxl-0.2.tar.gz

---
+++ packages/test-xxl/test_xxl-0.2.tar.gz/test_xxl-0.2/test_xxl/__init__.py
@@ -0,0 +1,1 @@
+from test_xxl.test_xxl import twosum
---
+++ packages/test-xxl/test_xxl-0.2.tar.gz/test_xxl-0.2/test_xxl/mytest.py
@@ -0,0 +1,9 @@
+class twosum():
+  def __init__(self,a,b):
+    self.a = a
+    self.b = b
+  def t(self):
+    return self.a+self.b
+
+a=twosum(2,3)
+a.t()
