7c16e2c79031ed7be0f3480f830006a532d27935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:18 +0000

    Add package fishy/0.4.1/fishy-0.4.1-py3-none-any.whl

---
+++ packages/fishy/fishy-0.4.1-py3-none-any.whl/fishy/engine/semifisher/fishing_event.py
@@ -0,0 +1,111 @@
+"""
+fishing_event.py
+Defines different fishing modes (states) which acts as state for state machine
+also implements callbacks which is called when states are changed
+"""
+import logging
+import time
+
+from fishy.engine.semifisher import fishing_mode
+from playsound import playsound
+
+from fishy import web
+from fishy.engine.semifisher.fishing_mode import State, FishingMode
+from fishy.helper import helper
+import keyboard
+
+from fishy.helper.config import config
+
+
+class FishEvent:
+    fishCaught = 0
+    totalFishCaught = 0
+    stickInitTime = 0
+    fish_times = []
+    hole_start_time = 0
+    FishingStarted = False
+    previousState = State.IDLE
+
+    # initialize these
+    action_key = 'e'
+    collect_r = False
+    uid = None
+    sound = False
+
+
+def init():
+    subscribe()
+    FishEvent.action_key = config.get("action_key", 'e')
+    FishEvent.uid = config.get("uid")
+    FishEvent.sound = config.get("sound_notification", False)
+
+
+def unsubscribe():
+    if fisher_callback in fishing_mode.subscribers:
+        fishing_mode.subscribers.remove(fisher_callback)
+
+
+def subscribe():
+    if fisher_callback not in fishing_mode.subscribers:
+        fishing_mode.subscribers.append(fisher_callback)
+
+        if FishingMode.CurrentMode == State.LOOK:
+            fisher_callback(FishingMode.CurrentMode)
+
+
+def fisher_callback(event: State):
+    callbacks_map = {State.HOOK: on_hook, State.LOOK: on_look, State.IDLE: on_idle, State.STICK: on_stick}
+    callbacks_map[event]()
+    FishEvent.previousState = event
+
+
+def on_hook():
+    """
+    called when the fish hook is detected
+    increases the `fishCaught`  and `totalFishCaught`, calculates the time it took to catch
+    presses e to catch the fish
+    """
+    FishEvent.fishCaught += 1
+    FishEvent.totalFishCaught += 1
+    time_to_hook = time.time() - FishEvent.stickInitTime
+    FishEvent.fish_times.append(time_to_hook)
+    logging.info("HOOOOOOOOOOOOOOOOOOOOOOOK....... " + str(FishEvent.fishCaught) + " caught " + "in " + str(
+        round(time_to_hook, 2)) + " secs.  " + "Total: " + str(FishEvent.totalFishCaught))
+
+    keyboard.press_and_release(FishEvent.action_key)
+
+    if FishEvent.collect_r:
+        time.sleep(0.1)
+        keyboard.press_and_release('r')
+        time.sleep(0.1)
+
+
+def on_look():
+    """
+    presses e to throw the fishing rod
+    """
+    keyboard.press_and_release(FishEvent.action_key)
+
+
+def on_idle():
+    if FishEvent.fishCaught > 0:
+        web.send_hole_deplete(FishEvent.uid, FishEvent.fishCaught, time.time() - FishEvent.hole_start_time,
+                              FishEvent.fish_times)
+        FishEvent.fishCaught = 0
+
+    if FishEvent.previousState == State.HOOK:
+        logging.info("HOLE DEPLETED")
+    else:
+        logging.info("FISHING INTERRUPTED")
+
+    if FishEvent.sound:
+        playsound(helper.manifest_file("sound.mp3"), False)
+
+
+def on_stick():
+    FishEvent.stickInitTime = time.time()
+    FishEvent.FishingStarted = True
+
+    if FishEvent.fishCaught == 0:
+        FishEvent.hole_start_time = time.time()
+        FishEvent.fish_times = []
---
+++ packages/fishy/fishy-0.4.1-py3-none-any.whl/fishy/gui/config_top.py
@@ -0,0 +1,94 @@
+import logging
+import os
+import typing
+from tkinter.filedialog import askopenfilename
+
+from fishy.helper import helper
+
+from fishy import web
+
+from tkinter import *
+from tkinter.ttk import *
+
+from fishy.helper.config import config
+from fishy.helper.popup import PopUp
+
+if typing.TYPE_CHECKING:
+    from fishy.gui import GUI
+
+
+def start_fullfisher_config(gui: 'GUI'):
+    top = PopUp(helper.empty_function, gui._root, background=gui._root["background"])
+    controls_frame = Frame(top)
+    top.title("Config")
+
+    def file_name():
+        file = config.get("full_auto_rec_file", None)
+        if file is None:
+            return "Not Selected"
+        return os.path.basename(file)
+
+    def select_file():
+        file = askopenfilename(filetypes=[('Python Files', '*.fishy')])
+        if not file:
+            logging.error("file not selected")
+        else:
+            config.set("full_auto_rec_file", file)
+            logging.info(f"loaded {file}")
+
+        file_name_label.set(file_name())
+
+    file_name_label = StringVar(value=file_name())
+    Label(controls_frame, textvariable=file_name_label).grid(row=0, column=0)
+    Button(controls_frame, text="Select fishy file", command=select_file).grid(row=0, column=1)
+    Label(controls_frame, text="Use semi-fisher config for rest").grid(row=2, column=0, columnspan=2)
+
+    controls_frame.pack(padx=(5, 5), pady=(5, 5))
+    top.start()
+
+
+def start_semifisher_config(gui: 'GUI'):
+    def save():
+        gui.config.set("action_key", action_key_entry.get(), False)
+        gui.config.set("borderless", borderless.instate(['selected']), False)
+        gui.config.set("sound_notification", sound.instate(['selected']), False)
+        gui.config.save_config()
+
+    def toggle_sub():
+        if web.is_subbed(config.get("uid"))[0]:
+            if web.unsub(config.get("uid")):
+                gui._notify.set(0)
+        else:
+            if web.sub(config.get("uid")):
+                gui._notify.set(1)
+
+    top = PopUp(save, gui._root, background=gui._root["background"])
+    controls_frame = Frame(top)
+    top.title("Config")
+
+    Label(controls_frame, text="Notification:").grid(row=0, column=0)
+
+    gui._notify = IntVar(0)
+    gui._notify_check = Checkbutton(controls_frame, command=toggle_sub, variable=gui._notify)
+    gui._notify_check.grid(row=0, column=1)
+    gui._notify_check['state'] = DISABLED
+    is_subbed = web.is_subbed(config.get('uid'))
+    if is_subbed[1]:
+        gui._notify_check['state'] = NORMAL
+        gui._notify.set(is_subbed[0])
+
+    Label(controls_frame, text="Fullscreen: ").grid(row=1, column=0, pady=(5, 5))
+    borderless = Checkbutton(controls_frame, var=BooleanVar(value=config.get("borderless")))
+    borderless.grid(row=1, column=1)
+
+    Label(controls_frame, text="Action Key:").grid(row=2, column=0)
+    action_key_entry = Entry(controls_frame, justify=CENTER)
+    action_key_entry.grid(row=2, column=1)
+    action_key_entry.insert(0, config.get("action_key", "e"))
+
+    Label(controls_frame, text="Sound Notification: ").grid(row=3, column=0, pady=(5, 5))
+    sound = Checkbutton(controls_frame, var=BooleanVar(value=config.get("sound_notification")))
+    sound.grid(row=3, column=1)
+
+    controls_frame.pack(padx=(5, 5), pady=(5, 5))
+    top.start()
