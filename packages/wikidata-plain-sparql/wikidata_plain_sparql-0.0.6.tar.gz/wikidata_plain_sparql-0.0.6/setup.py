2fee88f7a06ec8c939cbf9de2fc7ab9ead8b61af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:28 +0000

    Add package wikidata-plain-sparql/0.0.6/wikidata_plain_sparql-0.0.6.tar.gz

---
+++ packages/wikidata-plain-sparql/wikidata_plain_sparql-0.0.6.tar.gz/wikidata_plain_sparql-0.0.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='wikidata_plain_sparql',
+    version='0.0.6',
+    author='Jelle Schutter',
+    author_email='jelle@schutter.xyz',
+    description='Query WikiData in plain SPARQL and display the result as a map or data frame',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jelleschutter/wikidata-plain-sparql',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'requests',
+        'bokeh'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
