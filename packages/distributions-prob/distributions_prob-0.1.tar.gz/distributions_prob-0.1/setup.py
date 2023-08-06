f2a349705d1b0184a6384f3eead57a9fd7ac792e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:09 +0000

    Add package distributions-prob/0.1/distributions_prob-0.1.tar.gz

---
+++ packages/distributions-prob/distributions_prob-0.1.tar.gz/distributions_prob-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_prob',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Ogundepo Odunayo',
+      author_email = 'ogundepoodunayo@gmail.com',
+      zip_safe=False)
