fa41f15ce93a170a015f030158a6b30eba1e5a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:05 +0000

    Add package equities/4.2.9/equities-4.2.9.tar.gz

---
+++ packages/equities/equities-4.2.9.tar.gz/equities-4.2.9/setup.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import setuptools
+import setuptools.command.build_py
+import distutils.cmd
+import distutils
+from distutils.core import setup, Extension
+import distutils.log
+import subprocess
+import distutils.log
+distutils.log.set_verbosity(distutils.log.INFO) # Set DEBUG level
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+from equities import static 
+
+VERSION = static.__verison__
+
+setup(
+    name="equities", # Replace with your own username
+    version=VERSION,
+    author="Tiger_Shark",
+    author_email="ljwcharles@gmail.com",
+    description="equities aims to democratize access to public company data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ljc-codes/equities.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    keywords="sec stock stockmarket equities equity data financials financial company public companies xbrl",
+    python_requires='>=3.6',
+)
+
