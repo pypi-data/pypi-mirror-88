7d0aac326cbde70a848eac16e4cd100c55cc9283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:14 +0000

    Add package anki-compressor/0.1.0/anki-compressor-0.1.0.tar.gz

---
+++ packages/anki-compressor/anki-compressor-0.1.0.tar.gz/anki-compressor-0.1.0/setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup
+
+from anki_compressor import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="anki-compressor",
+    version=__version__,
+    url="https://github.com/pjsier/anki-compressor",
+    license="MIT",
+    author="Patrick Sier",
+    description="Compress Anki deck .apkg file size",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["Pillow", "pydub", "tqdm"],
+    packages=["anki_compressor"],
+    entry_points={
+        "console_scripts": ["anki-compressor = anki_compressor.__main__:main"]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    project_urls={
+        "Bug Reports": "https://github.com/pjsier/anki-compressor/issues",
+        "Source": "https://github.com/pjsier/anki-compressor",
+    },
+)
