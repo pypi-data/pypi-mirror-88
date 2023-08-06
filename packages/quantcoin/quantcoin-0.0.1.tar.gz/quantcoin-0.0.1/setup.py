d81d6d93bd35d2aa88aa4dab9790741fca2e0380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:34 +0000

    Add package quantcoin/0.0.1/quantcoin-0.0.1.tar.gz

---
+++ packages/quantcoin/quantcoin-0.0.1.tar.gz/quantcoin-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.rst") as f:
+    readme = f.read()
+
+setup(
+    name="quantcoin",
+    version="0.0.1",
+    description="Package for backtesting and paper trading cryptocurrency.",
+    long_description=readme,
+    author="GetQuantCoin",
+    author_email="tuan@quantcoin.co",
+    url="https://github.com/GetQuantCoin/quantcoin",
+    packages=find_packages(exclude=("tests", "docs")),
+    install_requires=[
+        "pandas",
+        "backtesting",
+        "ujson",
+        "requests"
+    ],
+)
