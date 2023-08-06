3239c46d981283ae310110f318e0fc049a01ddff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:59 +0000

    Add package sotrace/1.1.2/sotrace-1.1.2.tar.gz

---
+++ packages/sotrace/sotrace-1.1.2.tar.gz/sotrace-1.1.2/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+from sotrace import version
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+REQUIREMENTS = (HERE / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="sotrace",
+    version=version,
+    description="Get and open StackOverflow posts for your tracebacks.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SuperMaZingCoder/sotrace",
+    author="SuperMaZingCoder",
+    author_email="supermazingcoder@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["sotrace"],
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    python_requires=">=3.5.0"
+)
