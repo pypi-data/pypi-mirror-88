389ed3ff7a9066e8eeaf3452c8ead2c97fe98e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:26 +0000

    Add package zha-quirks/0.0.49/zha-quirks-0.0.49.tar.gz

---
+++ packages/zha-quirks/zha-quirks-0.0.49.tar.gz/zha-quirks-0.0.49/zhaquirks/xiaomi/aqara/wleak_aq1.py
@@ -0,0 +1,75 @@
+"""Xiaomi aqara leak sensor device."""
+from zigpy.profiles import zha
+from zigpy.quirks import CustomCluster
+from zigpy.zcl.clusters.general import Identify, Ota
+from zigpy.zcl.clusters.security import IasZone
+
+from .. import (
+    LUMI,
+    XIAOMI_NODE_DESC,
+    BasicCluster,
+    PowerConfigurationCluster,
+    XiaomiQuickInitDevice,
+)
+from ...const import (
+    DEVICE_TYPE,
+    ENDPOINTS,
+    INPUT_CLUSTERS,
+    MODELS_INFO,
+    NODE_DESCRIPTOR,
+    OUTPUT_CLUSTERS,
+    PROFILE_ID,
+    SKIP_CONFIGURATION,
+)
+
+
+class CustomIasZone(CustomCluster, IasZone):
+    """Custom IasZone cluster."""
+
+    MOISTURE_TYPE = 0x002A
+    ZONE_TYPE = 0x0001
+
+    def _update_attribute(self, attrid, value):
+        if attrid == self.ZONE_TYPE:
+            super()._update_attribute(attrid, self.MOISTURE_TYPE)
+        else:
+            super()._update_attribute(attrid, value)
+
+
+class LeakAQ1(XiaomiQuickInitDevice):
+    """Xiaomi aqara leak sensor device."""
+
+    signature = {
+        #  <SimpleDescriptor endpoint=1 profile=260 device_type=1026
+        #  device_version=1
+        #  input_clusters=[0, 3, 1]
+        #  output_clusters=[25]>
+        MODELS_INFO: [(LUMI, "lumi.sensor_wleak.aq1")],
+        NODE_DESCRIPTOR: XIAOMI_NODE_DESC,
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.IAS_ZONE,
+                INPUT_CLUSTERS: [
+                    BasicCluster.cluster_id,
+                    Identify.cluster_id,
+                    PowerConfigurationCluster.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            }
+        },
+    }
+    replacement = {
+        SKIP_CONFIGURATION: True,
+        ENDPOINTS: {
+            1: {
+                INPUT_CLUSTERS: [
+                    BasicCluster,
+                    Identify.cluster_id,
+                    PowerConfigurationCluster,
+                    CustomIasZone,
+                ],
+                OUTPUT_CLUSTERS: [Ota.cluster_id],
+            }
+        },
+    }
