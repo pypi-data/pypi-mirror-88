7b848fd0a0e06930a99530f16e3e999b7b60bdf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:28 +0000

    Add package py-ds-serial/0.1.0/py_ds_serial-0.1.0.tar.gz

---
+++ packages/py-ds-serial/py_ds_serial-0.1.0.tar.gz/py_ds_serial-0.1.0/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for py_ds_serial."""
---
+++ packages/py-ds-serial/py_ds_serial-0.1.0.tar.gz/py_ds_serial-0.1.0/tests/test_py_ds_serial.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `py_ds_serial` package."""
+
+
+import unittest
+
+from tango_serial import core
+
+
+class TestSerial(unittest.TestCase):
+    """Tests for `py_ds_serial` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
