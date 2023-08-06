a0cee5baa6ede31e509f61eeefd8f10806e46abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:21 +0000

    Add package eksipy/1.0.3/eksipy-1.0.3.tar.gz

---
+++ packages/eksipy/eksipy-1.0.3.tar.gz/eksipy-1.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import sys
+
+required = ['requests', 'markdownify', 'bs4', 'unidecode', 'fake_useragent', 'lxml']
+long_description = ""
+with open('README.md') as f:
+    long_description += f.read()
+
+setup(
+    name='eksipy',
+    version='1.0.3',
+    description='Unofficial API for Eksi Sozluk.',
+    long_description=long_description,
+    author='Yusuf Usta',
+    author_email='yusuf@fusuf.codes',
+    maintainer='Yusuf Usta',
+    maintainer_email='yusuf@fusuf.codes',
+    url='https://github.com/quiec/EksiPy',
+    license='GPL3',
+    packages=['eksipy'],
+    install_requires=required,
+    keywords=['eksi', 'eksisozluk', 'api', 'eksi-sozluk'],
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
