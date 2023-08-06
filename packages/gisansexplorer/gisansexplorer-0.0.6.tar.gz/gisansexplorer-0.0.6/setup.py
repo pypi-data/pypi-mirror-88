dc87c305c1b02e036c10e7a1087e5e4902802e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:07 +0000

    Add package gisansexplorer/0.0.6/gisansexplorer-0.0.6.tar.gz

---
+++ packages/gisansexplorer/gisansexplorer-0.0.6.tar.gz/gisansexplorer-0.0.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gisansexplorer", # Replace with your own username
+    version="0.0.6",
+    #scripts=['pippo.bat', 'pippo.py', 'pippo'],
+    #scripts=['pippo.py', 'pippo'],
+    #console=['pippo'],
+    author="Juan M. Carmona Loaiza",
+    author_email="j.carmona.loaiza@fz-juelich.de",
+    description="Nicos data reduction and visualisation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/juanmcloaiza/gisansexplorer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+    'console_scripts':
+        ['pippo=GisansExplorer:entry_point']
+                },
+    python_requires='>=3.6',
+    data_files=[('resources', ['resources/Icon.png', 'resources/show_test_map.npy'])]#,
+                #('test_data', ['notToVersion/Archive/*.dat'])],
+)
