5927687923b714a23373e9f3c3da86ba55a4d0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:08 +0000

    Add package RAKEkeywords/0.2.0/RAKEkeywords-0.2.0.tar.gz

---
+++ packages/RAKEkeywords/RAKEkeywords-0.2.0.tar.gz/RAKEkeywords-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='RAKEkeywords',
+    version='0.2.0',
+    packages=['RAKEkeywords'],
+    url='https://github.com/OpenJarbas/RAKEkeywords',
+    license='Apache2',
+    author='jarbasAI',
+    install_requires=["stopwordsiso"],
+    author_email='jarbasai@mailfence.com',
+    description='Implementation of RAKE - Rapid Automatic Keyword Extraction'
+)
