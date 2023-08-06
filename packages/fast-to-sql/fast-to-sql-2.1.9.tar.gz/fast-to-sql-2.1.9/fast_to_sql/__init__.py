d6932fb55286ff2a40d36ccb975913c638c35d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:40 +0000

    Add package fast-to-sql/2.1.9/fast-to-sql-2.1.9.tar.gz

---
+++ packages/fast-to-sql/fast-to-sql-2.1.9.tar.gz/fast-to-sql-2.1.9/fast_to_sql/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from .fast_to_sql import fast_to_sql
+
+__project__ = "fast-to-sql"
+__version__ = "2.1.9"
+__keywords__ = ["pandas", "to_sql", "fast", "sql"]
+__author__ = "Jarred Glaser"
+__author_email__ = "jarred.glaser@gmail.com"
+__url__ = "https://github.com/jdglaser/fast-to-sql"
+__platforms__ = "ALL"
+__requires__=['pandas', 'pyodbc']
+
+__classifiers__ = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.8',
+    'License :: OSI Approved :: MIT License'
+]
+
+__extra_requires__ = {
+}
