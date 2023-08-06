f02d8e9b9e9100c67746b1d74058d46ab188af6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:55 +0000

    Add package django-admin-sso/3.1.0/django_admin_sso-3.1.0-py2.py3-none-any.whl

---
+++ packages/django-admin-sso/django_admin_sso-3.1.0-py2.py3-none-any.whl/admin_sso/auth.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+
+from admin_sso.models import Assignment
+
+
+class DjangoSSOAuthBackend(object):
+    def get_user(self, user_id):
+        cls = get_user_model()
+        try:
+            return cls.objects.get(pk=user_id)
+        except cls.DoesNotExist:
+            return None
+
+    def authenticate(self, request, **kwargs):
+        sso_email = kwargs.pop("sso_email", None)
+
+        assignment = Assignment.objects.for_email(sso_email)
+        if assignment is None:
+            return None
+
+        return assignment.user
---
+++ packages/django-admin-sso/django_admin_sso-3.1.0-py2.py3-none-any.whl/admin_sso/models.py
@@ -0,0 +1,58 @@
+from __future__ import unicode_literals
+
+import fnmatch
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from admin_sso import settings
+
+
+class AssignmentManager(models.Manager):
+    def for_email(self, email):
+        try:
+            username, domain = email.split("@")
+        except (AttributeError, TypeError, ValueError):
+            return None
+        possible_assignments = self.filter(domain=domain)
+        used_assignment = None
+        for assignment in possible_assignments:
+            if assignment.username_mode == settings.ASSIGNMENT_ANY:
+                used_assignment = assignment
+                break
+            elif assignment.username_mode == settings.ASSIGNMENT_MATCH:
+                if fnmatch.fnmatch(username, assignment.username):
+                    used_assignment = assignment
+                    break
+            elif assignment.username_mode == settings.ASSIGNMENT_EXCEPT:
+                if not fnmatch.fnmatch(username, assignment.username):
+                    used_assignment = assignment
+                    break
+        if used_assignment is None:
+            return None
+        return used_assignment
+
+
+class Assignment(models.Model):
+    username_mode = models.IntegerField(choices=settings.ASSIGNMENT_CHOICES)
+    username = models.CharField(max_length=255, blank=True)
+    domain = models.CharField(max_length=255)
+    copy = models.BooleanField(default=False)
+    weight = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ("-weight",)
+        verbose_name = _("Assignment")
+        verbose_name_plural = _("Assignments")
+
+    def __str__(self):
+        return "%s(%s) @%s" % (
+            dict(settings.ASSIGNMENT_CHOICES)[self.username_mode],
+            self.username,
+            self.domain,
+        )
+
+    objects = AssignmentManager()
