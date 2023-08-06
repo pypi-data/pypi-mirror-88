e31c905c17a00efb338ef410cd1aea4bfe21c51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:12 +0000

    Add package mitosheet/0.1.62/mitosheet-0.1.62.tar.gz

---
+++ packages/mitosheet/mitosheet-0.1.62.tar.gz/mitosheet-0.1.62/mitosheet/_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Mito.
+# Distributed under the terms of the Modified BSD License.
+
+version_info = (0, 1, 62)
+__version__ = ".".join(map(str, version_info))
