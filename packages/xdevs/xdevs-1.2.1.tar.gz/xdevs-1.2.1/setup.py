046928855ab4f9c9b1175f216b7ac739f51fe4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:00 +0000

    Add package xdevs/1.2.1/xdevs-1.2.1.tar.gz

---
+++ packages/xdevs/xdevs-1.2.1.tar.gz/xdevs-1.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='xdevs',
+      version='1.2.1',
+      description='xDEVS M&S framework',
+      url='https://github.com/dacya/xdevs',
+      author='Kevin Henares, Román Cárdenas',
+      author_email='khenares@ucm.es, r.cardenas@upm.es',
+      packages=['xdevs'],
+      zip_safe=False)
