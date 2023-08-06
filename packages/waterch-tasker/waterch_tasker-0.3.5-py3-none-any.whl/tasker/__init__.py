90fe8a187ddce320cdbd9f74423d3e0485e7d29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:14 +0000

    Add package waterch-tasker/0.3.5/waterch_tasker-0.3.5-py3-none-any.whl

---
+++ packages/waterch-tasker/waterch_tasker-0.3.5-py3-none-any.whl/tasker/__init__.py
@@ -0,0 +1,21 @@
+__all__ = [
+    'Profile',
+    'Definition',
+    'Return',
+    'storage',
+    'storages',
+    'mixin',
+    'value',
+    'include',
+    'launch',
+    'utils',
+    'tasks',
+    'version'
+]
+
+from tasker import storages, storage, mixin, utils, tasks
+from tasker.launcher import launch
+from tasker.mixin import value, include
+from tasker.typedef import Profile, Definition, Return
+from tasker.decorator import def_task
+from tasker._version import version
---
+++ packages/waterch-tasker/waterch_tasker-0.3.5-py3-none-any.whl/tasker/tasks/utils.py
@@ -0,0 +1,177 @@
+from datetime import datetime
+from logging import Logger
+from os import environ
+from typing import List, Text
+from uuid import uuid4
+
+from tasker.typedef import Definition, Profile, Return
+from tasker.mixin import value
+from tasker.storage import Storage
+from tasker.tasks.containers import Task
+
+
+class GenerateIdTask(Task):
+    """
+    <b>waterch.tasker.example_tasks.utils.GenerateIdTask</b>
+
+    Task to generate ID.
+    The task will declare a field named `id` in shared storage.
+
+    Currently, `GenerateIdTask` supports 3 type of items to generate ID,
+    including "datetime", "uuid", "label". If you need to label something,
+    considerate using "label" into "join" fields. Items will be joined by
+    string defined in field "by".
+    """
+
+    def generate_datetime(self, profile) -> Text:
+        return datetime.now().strftime('%Y%m%d%H%M%S')
+
+    def generate_uuid(self, profile) -> Text:
+        return uuid4().hex.replace('-', '')
+
+    def generate_label(self, profile) -> Text:
+        try:
+            return profile.label
+        except Exception:
+            raise RuntimeError('Please define "label" attribute in profile when including label item.')
+
+    def invoke(self, profile: Profile, shared: Storage, logger: Logger) -> Return:
+        """
+        Generate ID.
+        Args:
+            profile: Runtime profile defined in TOML file.
+            shared: Shared storage in the whole lifecycle.
+            logger: The logger named with this Task.
+
+        Returns:
+            Always return [SUCCESS][Return.SUCCESS].
+        """
+
+        def generate(name: Text) -> Text:
+            generator = getattr(self, f'generate_{name}', None)
+            if generator is None:
+                raise RuntimeError(f'ID item {name} is invalid, please check and fix.')
+            return generator(profile)
+
+        shared['id'] = profile.by.join(filter(lambda t: t != '', map(generate, profile.join)))
+        logger.debug(f'ID: {shared["id"]}')
+        return Return.SUCCESS.value
+
+    def require(self) -> List[Text]:
+        """
+        This task requires nothing.
+        Returns:
+            Nothing
+        """
+        return ['id']
+
+    def provide(self) -> List[Text]:
+        """
+        This task provides:
+
+        | Key  | Value                                 |
+        | ---- | ------------------------------------- |
+        | id   | Generated ID provided to other example_tasks. |
+
+        Returns:
+            Only contains "id"
+        """
+        return ['id']
+
+    def remove(self) -> List[Text]:
+        """
+        This task removes nothing.
+        Returns:
+            Nothing
+        """
+
+    @classmethod
+    def define(cls) -> List[Definition]:
+        """
+        Examples:
+            ```toml
+            __schema__ = "waterch.tasker.example_tasks.utils.GenerateIdTask"
+            by = ""
+            label = ""
+            ```
+        Returns:
+            Schema of profile
+        """
+        return [
+            value('by', str),
+            value('join', list, [str]),
+            value('label', str)
+        ]
+
+
+class SetEnvironmentTask(Task):
+    """
+    <b>waterch.tasker.example_tasks.utils.SetEnvironmentTask</b>
+
+    Task to set environment values.
+    """
+
+    def invoke(self, profile: Profile, shared: Storage, logger: Logger) -> int:
+        """
+        Set environment values.
+        Args:
+            profile: Runtime profile defined in TOML file.
+            shared: Shared storage in the whole lifecycle.
+            logger: The logger named with this Task.
+
+        Returns:
+            Always return [SUCCESS][Return.SUCCESS].
+        """
+        for item in profile.env:
+            assert len(item) == 2
+            environ[item[0]] = item[1]
+        logger.debug(f'Environment:')
+        for item in environ.items():
+            logger.debug(f'  {"=".join(item)}')
+        return Return.SUCCESS.value
+
+    def require(self) -> List[Text]:
+        """
+        This task requires nothing.
+
+        Returns:
+            Nothing
+        """
+        return []
+
+    def provide(self) -> List[Text]:
+        """
+        This task provides nothing.
+
+        Returns:
+            Nothing
+        """
+        return []
+
+    def remove(self) -> List[Text]:
+        """
+        This task removes nothing.
+
+        Returns:
+            Nothing
+        """
+        return []
+
+    @classmethod
+    def define(cls) -> List[Definition]:
+        """
+        ```toml
+        __schema__ = "waterch.tasker.example_tasks.utils.SetEnvironmentTask"
+        env = [
+            ["CUDA_VISIBLE_DEVICES", "0"],
+        ]
+        ```
+
+        Returns:
+            Schema of profile.
+        """
+        return [
+            value('env', list, [[str]]),
+        ]
+
+
