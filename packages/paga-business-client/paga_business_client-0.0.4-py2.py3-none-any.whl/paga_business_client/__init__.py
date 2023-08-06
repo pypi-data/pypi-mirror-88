9e3638d9e78f4f6392d9983520450e2663044298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:15 +0000

    Add package paga-business-client/0.0.4/paga_business_client-0.0.4-py2.py3-none-any.whl

---
+++ packages/paga-business-client/paga_business_client-0.0.4-py2.py3-none-any.whl/paga_business_client/__init__.py
@@ -0,0 +1,1 @@
+from .business_client_core import BusinessClientCore
---
+++ packages/paga-business-client/paga_business_client-0.0.4-py2.py3-none-any.whl/paga_business_client/version.py
@@ -0,0 +1,4 @@
+__title__ = 'paga_business_library'
+__version__ = '0.0.4'
+__author__ = 'Paga Developer Community'
+__copyright__ = 'Copyright (c) 2018-2020, Paga Developer Community'
