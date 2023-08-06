eb118c2c19cce2ea6e8951fc25495c6ec62319c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:09 +0000

    Add package djangoldp/1.5.3/djangoldp-1.5.3-py3-none-any.whl

---
+++ packages/djangoldp/djangoldp-1.5.3-py3-none-any.whl/djangoldp/tests/runner.py
@@ -0,0 +1,32 @@
+import sys
+
+import django
+from djangoldp.tests import settings_default
+from django.conf import settings
+
+settings.configure(default_settings=settings_default)
+
+django.setup()
+from django.test.runner import DiscoverRunner
+
+test_runner = DiscoverRunner(verbosity=1)
+
+failures = test_runner.run_tests([
+    'djangoldp.tests.tests_ldp_model',
+    'djangoldp.tests.tests_ldp_viewset',
+    'djangoldp.tests.tests_save',
+    'djangoldp.tests.tests_user_permissions',
+    'djangoldp.tests.tests_guardian',
+    'djangoldp.tests.tests_anonymous_permissions',
+    'djangoldp.tests.tests_update',
+    'djangoldp.tests.tests_auto_author',
+    'djangoldp.tests.tests_get',
+    'djangoldp.tests.tests_delete',
+    'djangoldp.tests.tests_sources',
+    'djangoldp.tests.tests_pagination',
+    'djangoldp.tests.tests_inbox',
+    'djangoldp.tests.tests_backlinks_service',
+    'djangoldp.tests.tests_cache'
+])
+if failures:
+    sys.exit(failures)
