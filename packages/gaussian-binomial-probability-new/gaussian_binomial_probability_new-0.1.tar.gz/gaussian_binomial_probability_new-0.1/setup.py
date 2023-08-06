d256db21806d5b86696a986a5c2e371feb9bb07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:50 +0000

    Add package gaussian-binomial-probability-new/0.1/gaussian_binomial_probability_new-0.1.tar.gz

---
+++ packages/gaussian-binomial-probability-new/gaussian_binomial_probability_new-0.1.tar.gz/gaussian_binomial_probability_new-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_probability_new',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Adarsh Vinayak',
+      zip_safe=False)
