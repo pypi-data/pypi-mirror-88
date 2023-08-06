6610f3626a43b7822c3181d22676ed562f9efea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:46 +0000

    Add package oebl-irs-workflow/0.1.4/oebl-irs-workflow-0.1.4.tar.gz

---
+++ packages/oebl-irs-workflow/oebl-irs-workflow-0.1.4.tar.gz/oebl-irs-workflow-0.1.4/oebl_irs_workflow/api_views.py
@@ -0,0 +1,70 @@
+from rest_framework import filters, viewsets, renderers
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Author, Issue, IssueLemma, LemmaStatus, LemmaNote, Lemma, LemmaLabel
+from .serializers import (
+    AuthorSerializer,
+    EditorSerializer,
+    IssueSerializer,
+    IssueLemmaSerializer,
+    LemmaStatusSerializer,
+    LemmaNoteSerializer,
+    LemmaSerializer,
+    LemmaLabelSerializer,
+)
+
+
+class AuthorViewset(viewsets.ReadOnlyModelViewSet):
+    """Viewset to retrieve Author objects"""
+
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    filterset_fields = ["username"]
+    permission_classes = [IsAuthenticated]
+
+
+class IssueViewset(viewsets.ModelViewSet):
+
+    queryset = Issue.objects.all()
+    serializer_class = IssueSerializer
+    filter_fields = ["name", "pubDate"]
+    permission_classes = [IsAuthenticated]
+
+
+class IssueLemmaViewset(viewsets.ModelViewSet):
+
+    queryset = IssueLemma.objects.all()
+    serializer_class = IssueLemmaSerializer
+    filter_fields = ["lemma", "issue", "author", "editor"]
+    permission_classes = [IsAuthenticated]
+
+
+class LemmaViewset(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Lemma.objects.all()
+    serializer_class = LemmaSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class LemmaNoteViewset(viewsets.ModelViewSet):
+
+    queryset = LemmaNote.objects.all()
+    serializer_class = LemmaNoteSerializer
+    filter_fields = ["lemma"]
+    permission_classes = [IsAuthenticated]
+
+
+class LemmaStatusViewset(viewsets.ModelViewSet):
+
+    queryset = LemmaStatus.objects.all()
+    serializer_class = LemmaStatusSerializer
+    filter_fields = ["issuelemma", "issue"]
+    permission_classes = [IsAuthenticated]
+
+
+class LemmaLabelViewset(viewsets.ModelViewSet):
+
+    queryset = LemmaLabel.objects.all()
+    serializer_class = LemmaLabelSerializer
+    filter_fields = ["issuelemma"]
+    permission_classes = [IsAuthenticated]
