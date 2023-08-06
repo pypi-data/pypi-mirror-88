57cc8bd608233d869df83eefdb0596c69164d488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:29 +0000

    Add package python-ioc/1.4.2/python-ioc-1.4.2.tar.gz

---
+++ packages/python-ioc/python-ioc-1.4.2.tar.gz/python-ioc-1.4.2/ioc/exc.py
@@ -0,0 +1,27 @@
+
+
+class ConfigurationError(Exception):
+    """An :exc:`ConfigurationError` is raised when a malformed or invalid
+    configuration is parsed by the framework.
+    """
+    pass
+
+
+class RequirementAlreadyResolved(Exception):
+    pass
+
+
+class UnsatisfiedDependency(LookupError):
+    """The :exc:`UnsatisfiedDependency` exception is raised when an unknown
+    dependency is requested from the provider.
+    """
+    pass
+
+
+class EnvironmentVariableNotDefined(EnvironmentError):
+    """Raises when an environment variable does not exist."""
+    pass
+
+
+class DependencySatisfied(Exception):
+    """Raised when a duplicate dependency is provided."""
