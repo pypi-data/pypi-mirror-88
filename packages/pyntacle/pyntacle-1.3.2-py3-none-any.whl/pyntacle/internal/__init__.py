fb2a91ef897fbe49ef9344659481013a5aa408df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:55 +0000

    Add package pyntacle/1.3.2/pyntacle-1.3.2-py3-none-any.whl

---
+++ packages/pyntacle/pyntacle-1.3.2-py3-none-any.whl/pyntacle/internal/__init__.py
@@ -0,0 +1,6 @@
+"""Decorators and functions used by internally by pyntacle to standardize its methods"""
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
