69bebab68534d137d152ad7b19b08dbd6ba0b0dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:07 +0000

    Add package mmpm/2.08/mmpm-2.08.tar.gz

---
+++ packages/mmpm/mmpm-2.08.tar.gz/mmpm-2.08/mmpm/wsgi.py
@@ -0,0 +1,23 @@
+#!/usr/bin python3
+# pylint: disable=unused-import
+import eventlet
+eventlet.monkey_patch() # do not move these
+
+from mmpm.api import app
+
+import mmpm.utils
+import mmpm.consts
+import mmpm.core
+
+if __name__ == '__main__':
+    app.run(
+        threaded=True,
+        keepalive=True,
+        log=mmpm.utils.log,
+        extra_files=[
+            mmpm.consts.MAGICMIRROR_3RD_PARTY_PACKAGES_DB_FILE,
+            mmpm.consts.MMPM_EXTERNAL_PACKAGES_FILE,
+            mmpm.consts.MMPM_AVAILABLE_UPGRADES_FILE,
+            mmpm.consts.MMPM_ENV_FILE,
+        ]
+    )
