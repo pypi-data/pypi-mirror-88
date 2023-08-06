6dcb24ff8e2da1f3505833595f73a2dd54be89a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:18 +0000

    Add package pybites-pysource/1.1.0/pybites-pysource-1.1.0.tar.gz

---
+++ packages/pybites-pysource/pybites-pysource-1.1.0.tar.gz/pybites-pysource-1.1.0/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="pybites-pysource",
+    version="1.1.0",
+    description="Read Python source code from the command line",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PyBites-Open-Source/pysource",
+    author="PyBites",
+    author_email="support@pybit.es",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["pysource"],
+    include_package_data=True,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points={
+        "console_scripts": [
+            "pysource=pysource.__main__:main",
+        ]
+    },
+)
