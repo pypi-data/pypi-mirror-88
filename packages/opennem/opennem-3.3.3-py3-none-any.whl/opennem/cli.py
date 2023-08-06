df054deb961792b4f7006419c52aeda92820c494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:33 +0000

    Add package opennem/3.3.3/opennem-3.3.3-py3-none-any.whl

---
+++ packages/opennem/opennem-3.3.3-py3-none-any.whl/opennem/cli.py
@@ -0,0 +1,125 @@
+import logging
+
+import click
+from scrapy.utils.python import garbage_collect
+
+from opennem.db.load_fixtures import load_bom_stations_json, load_fixtures
+from opennem.diff.facility_diff import run_diff
+from opennem.importer.all import run_all
+from opennem.importer.db import init as db_init
+from opennem.importer.mms import mms_export
+from opennem.importer.opennem import opennem_export, opennem_import
+from opennem.settings import settings
+
+logger = logging.getLogger("opennem.cli")
+
+
+@click.group()
+def main():
+    pass
+
+
+@click.command()
+def crawl():
+    logger.info("crawl @TODO")
+
+
+@click.command()
+def diff():
+    run_diff()
+
+
+@click.group()
+def cmd_db():
+    pass
+
+
+@click.command()
+def cmd_db_init():
+    db_init()
+
+
+@click.command()
+def cmd_db_fixturer():
+    load_fixtures()
+
+
+@click.group()
+def cmd_import():
+    pass
+
+
+@click.group()
+def cmd_export():
+    pass
+
+
+@click.command()
+def cmd_import_opennem():
+    opennem_import()
+
+
+@click.command()
+def cmd_import_mms():
+    mms_export()
+
+
+@click.command()
+def cmd_import_all():
+    run_all()
+
+
+@click.command()
+def cmd_export_opennem():
+    opennem_export()
+
+
+@click.command()
+def cmd_export_all():
+    run_all()
+
+
+@click.group()
+def cmd_weather():
+    pass
+
+
+@click.command()
+def cmd_weather_init():
+    load_bom_stations_json()
+
+
+main.add_command(crawl)
+main.add_command(diff)
+main.add_command(cmd_db, name="db")
+main.add_command(cmd_import, name="import")
+main.add_command(cmd_export, name="export")
+main.add_command(cmd_weather, name="weather")
+
+cmd_import.add_command(cmd_import_opennem, name="opennem")
+cmd_import.add_command(cmd_import_mms, name="mms")
+cmd_import.add_command(cmd_import_all, name="all")
+
+cmd_export.add_command(cmd_export_opennem, name="opennem")
+cmd_export.add_command(cmd_export_all, name="all")
+
+cmd_db.add_command(cmd_db_init, name="init")
+cmd_db.add_command(cmd_db_fixturer, name="load_fixtures")
+
+cmd_weather.add_command(cmd_weather_init, name="init")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.error("User interrupted")
+    except Exception as e:
+        logger.error(e)
+
+        if settings.debug:
+            import traceback
+
+            traceback.print_exc()
+
+    finally:
+        garbage_collect()
---
+++ packages/opennem/opennem-3.3.3-py3-none-any.whl/opennem/spiders/apvi/data.py
@@ -0,0 +1,71 @@
+from datetime import datetime, timedelta
+from typing import Any, Dict, Generator
+
+import scrapy
+
+from opennem.pipelines.apvi.data import APVIStoreData
+from opennem.utils.dates import date_series, get_date_component
+
+APVI_DATA_URI = "https://pv-map.apvi.org.au/data"
+
+TODAY = datetime.now().date()
+
+YESTERDAY = TODAY - timedelta(days=1)
+
+MONTH_AGO = TODAY - timedelta(days=29)
+
+APVI_DATE_QUERY_FORMAT = "%Y-%m-%d"
+
+APVI_EARLIEST_DATE = "2013-05-07"
+
+
+class APVIDataSpiderBase(scrapy.Spider):
+
+    name = "au.apvi.latest.data"
+
+    pipelines = set([APVIStoreData])
+
+    def start_requests(self) -> Generator[scrapy.FormRequest, None, None]:
+        yield scrapy.FormRequest(
+            APVI_DATA_URI,
+            formdata={"day": get_date_component(APVI_DATE_QUERY_FORMAT)},
+            meta={
+                "is_latest": True,
+                "record_date": get_date_component(APVI_DATE_QUERY_FORMAT),
+            },
+        )
+
+    def parse(self, response: Any) -> Generator[Dict, None, None]:
+        yield {"records": response.json(), "meta": response.meta}
+
+
+class APVIDataCurrentSpider(APVIDataSpiderBase):
+    name = "au.apvi.current.data"
+
+    def start_requests(self) -> Generator[scrapy.FormRequest, None, None]:
+        for date in date_series(TODAY, length=31, reverse=True):
+            yield scrapy.FormRequest(
+                APVI_DATA_URI,
+                formdata={"day": date.strftime(APVI_DATE_QUERY_FORMAT)},
+                meta={"is_latest": True if date == TODAY else False},
+            )
+
+
+class APVIDataArchiveSpider(APVIDataSpiderBase):
+    name = "au.apvi.archive.data"
+
+    def start_requests(self) -> Generator[scrapy.FormRequest, None, None]:
+        end_date = datetime.strptime(
+            APVI_EARLIEST_DATE, APVI_DATE_QUERY_FORMAT
+        ).date()
+
+        for date in date_series(start=MONTH_AGO, end=end_date, reverse=True):
+            yield scrapy.FormRequest(
+                APVI_DATA_URI,
+                formdata={"day": date.strftime(APVI_DATE_QUERY_FORMAT)},
+            )
+
+    custom_settings = {
+        "CONCURRENT_REQUESTS": 1,
+        "DOWNLOAD_DELAY": 1,
+    }
