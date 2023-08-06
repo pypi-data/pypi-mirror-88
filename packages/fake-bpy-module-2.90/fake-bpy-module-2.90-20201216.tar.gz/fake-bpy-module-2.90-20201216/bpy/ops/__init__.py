a483694085cd75434089ba977bc8bca2dee5b74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:50 +0000

    Add package fake-bpy-module-2.90/20201216/fake-bpy-module-2.90-20201216.tar.gz

---
+++ packages/fake-bpy-module-2.90/fake-bpy-module-2.90-20201216.tar.gz/fake-bpy-module-2.90-20201216/bpy/ops/__init__.py
@@ -0,0 +1,107 @@
+import sys
+import typing
+from . import import_mesh
+from . import graph
+from . import brush
+from . import font
+from . import mask
+from . import material
+from . import marker
+from . import ui
+from . import poselib
+from . import file
+from . import export_anim
+from . import workspace
+from . import import_curve
+from . import cachefile
+from . import anim
+from . import ptcache
+from . import scene
+from . import image
+from . import particle
+from . import buttons
+from . import paint
+from . import sculpt
+from . import mball
+from . import fluid
+from . import world
+from . import armature
+from . import console
+from . import camera
+from . import dpaint
+from . import nla
+from . import render
+from . import gpencil
+from . import constraint
+from . import import_anim
+from . import surface
+from . import export_mesh
+from . import screen
+from . import preferences
+from . import rigidbody
+from . import boid
+from . import cloth
+from . import view3d
+from . import curve
+from . import sound
+from . import paintcurve
+from . import pose
+from . import collection
+from . import node
+from . import lattice
+from . import uv
+from . import cycles
+from . import outliner
+from . import view2d
+from . import safe_areas
+from . import ed
+from . import text
+from . import import_scene
+from . import palette
+from . import script
+from . import export_scene
+from . import clip
+from . import action
+from . import simulation
+from . import texture
+from . import transform
+from . import wm
+from . import sequencer
+from . import info
+from . import mesh
+from . import gizmogroup
+from . import object
+
+
+class BPyOps:
+    pass
+
+
+class BPyOpsSubMod:
+    pass
+
+
+class BPyOpsSubModOp:
+    def get_rna_type(self):
+        ''' 
+
+        '''
+        pass
+
+    def idname(self):
+        ''' 
+
+        '''
+        pass
+
+    def idname_py(self):
+        ''' 
+
+        '''
+        pass
+
+    def poll(self, args):
+        ''' 
+
+        '''
+        pass
