58c78eb48afb6a26e6e5ffbb6ae916947ff6d0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:53 +0000

    Add package backvm/0.1.2/backvm-0.1.2.tar.gz

---
+++ packages/backvm/backvm-0.1.2.tar.gz/backvm-0.1.2/setup.py
@@ -0,0 +1,43 @@
+
+# Author: Pietro Marini <backvm@fastmail.com>
+# License: BSD 3 clause
+
+from setuptools import setup, find_packages
+
+README = open("./Readme.md","r").read()
+
+setup(
+    name='backvm',
+
+    version='0.1.2',
+    
+    description="KVM domains backup utility",
+    
+    long_description=README,
+    
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(),
+    
+    include_package_data=True,
+    
+    license="new BSD",
+    
+    author="Pietro Marini",
+    
+    author_email="backvm@fastmail.com",
+    
+    install_requires=[
+        'click',
+        'libvirt-python',
+        'pandas',
+        'paramiko',
+        'scp'
+        
+    ],
+    
+    entry_points='''
+        [console_scripts]
+        backvm=backvm.scripts.backvm:perform_action
+    ''',
+)
