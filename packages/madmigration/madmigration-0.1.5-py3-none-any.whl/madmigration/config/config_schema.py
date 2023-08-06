cfffa2cf2bca28bbce9f70bd80b8e4de93e12cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:40 +0000

    Add package madmigration/0.1.5/madmigration-0.1.5-py3-none-any.whl

---
+++ packages/madmigration/madmigration-0.1.5-py3-none-any.whl/madmigration/config/config_schema.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel
+from typing import Union, List, Any, AnyStr, Dict
+
+
+class ForeignKeySchema(BaseModel):
+    use_alter: bool = True
+    table_name: str
+    column_name: str
+    ondelete: str = "CASCADE"
+
+class OptionsSchema(BaseModel):
+    primary_key: bool = False
+    nullable: bool = False
+    default: Union[bool,None] = None
+    index: Union[bool,None] = None
+    unique: Union[bool,None] = None
+    autoincrement: bool = False
+    foreign_key: Union[ForeignKeySchema,None] = None
+    length: Union[int,None] = None
+    type_cast: Union[str,None] = None
+
+
+class SourceConfigSchema(BaseModel):
+    SourceConfig: Dict[str, str]
+
+
+class DestinationConfigSchema(BaseModel):
+    DestinationConfig: Dict[str, str]
+
+
+class SourceTableSchema(BaseModel):
+    name: Dict[str, str]
+
+class DestinationColumn(BaseModel):
+    name: str
+    options: OptionsSchema
+
+
+class ColumnParametersSchema(BaseModel):
+    destinationColumn: DestinationColumn
+    sourceColumn: Any
+
+
+class TablesInfo(BaseModel):
+    SourceTable: Dict[str, str]
+    DestinationTable: Dict[str, str]
+    MigrationColumns: List[ColumnParametersSchema]
+
+
+class MigrationTablesSchema(BaseModel):
+    migrationTable: TablesInfo
+
+
+class ConfigSchema(BaseModel):
+    Configs: List[Union[SourceConfigSchema, DestinationConfigSchema]]
+    migrationTables: List[MigrationTablesSchema]
+    version: float
---
+++ packages/madmigration/madmigration-0.1.5-py3-none-any.whl/madmigration/utils/helpers.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+from sqlalchemy import (
+    Integer,
+    String,
+    DateTime,
+    Date,
+    BigInteger,
+    VARCHAR,
+    Float,
+    TIMESTAMP
+)
+
+from madmigration.mysqldb.migration import Migrate as mysql_migrate
+from madmigration.postgresqldb.migration import Migrate as postgres_migrate
+
+
+###########################
+# Get class of cast #
+###########################
+def get_cast_type(type_name: str) -> object:
+    """
+    :param type_name: str
+    :return: object class
+    """
+    return {
+        'str': str,
+        'string': str,
+        'int': int,
+        'integer': int,
+        'float': float,
+        'datetime': datetime,
+        'varchar': VARCHAR
+    }.get(type_name.lower())
+
+
+###########################
+# Get class of db type #
+###########################
+def get_column_type(type_name: str) -> object:
+    """
+    :param type_name: str
+    :return: object class
+    """
+    return {
+        'string': String,
+        'integer': Integer,
+        'biginteger': BigInteger,
+        'float': Float,
+        'datetime': DateTime,
+        'date' : Date,
+        'timestamp' : TIMESTAMP,
+        'varchar': VARCHAR
+    }.get(type_name.lower())
+
+
+###########################
+# Detect db driver fro migration #
+###########################
+def detect_driver(driver: str) -> object:
+    """
+    :param driver: str
+    :return: object class
+    """
+    return {
+        "mysqldb" : mysql_migrate,
+        "pymysql": mysql_migrate,
+        "mysql+pymysql" : mysql_migrate,
+        "psycopg2": postgres_migrate  # heleki ozum verdim ki mende error vermesin
+
+    }.get(driver)
