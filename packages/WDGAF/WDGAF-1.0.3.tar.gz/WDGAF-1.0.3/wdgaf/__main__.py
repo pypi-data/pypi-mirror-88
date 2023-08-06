2e77799d79d124540a1dd57353a7895b3cc2f458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:59 +0000

    Add package WDGAF/1.0.3/WDGAF-1.0.3.tar.gz

---
+++ packages/WDGAF/WDGAF-1.0.3.tar.gz/WDGAF-1.0.3/wdgaf/__main__.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import logging
+import warnings
+from pathlib import Path
+from lark import Lark
+import lark
+
+
+sys.path.append(str(Path(__file__).parent))
+print('test0')
+
+from modules.process_tree import ProcessTree
+from modules.fu_config import config
+
+import wdgaf.gaf as gaf
+
+import argparse
+
+
+def main():
+    # setup mechanism for any meta-requirements in the close future
+    gaf.driver()
+
+if __name__ == '__main__':
+    main()
