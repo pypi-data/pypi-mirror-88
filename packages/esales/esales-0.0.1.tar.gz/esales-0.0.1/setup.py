69fb4663c762e022852b6c94eaad7a3c768d4294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:34 +0000

    Add package esales/0.0.1/esales-0.0.1.tar.gz

---
+++ packages/esales/esales-0.0.1.tar.gz/esales-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='esales',
+    version='0.0.1',
+    description='This is a very simple eslaes module :)',
+    long_description='there is no long description :)',
+    long_description_content_type='text/markdown' ,
+    url='',
+    author='Rients Hoeksma',
+    author_email='rientshoeksma@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='esales',
+    packages=find_packages(),
+    install_requires=['random']
+)
+
