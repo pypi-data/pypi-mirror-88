9bd32646740b58424e49c5b36c50975d44011671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:56 +0000

    Add package xrayconfigurator/0.0.28/xrayconfigurator-0.0.28.tar.gz

---
+++ packages/xrayconfigurator/xrayconfigurator-0.0.28.tar.gz/xrayconfigurator-0.0.28/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xrayconfigurator", # Replace with your own username
+    version="0.0.28",
+    author="Ondrej Jurcak",
+    author_email="ondrej@surglogs.com",
+    description="Configuration helper for xray",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=['aws-xray-sdk>=2.6.0','celery>=4.4.7', 'sentry-sdk[flask]>=0.14.3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
