a6cd2550b51f6dbea385f62cc34d8837c2400d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:58 +0000

    Add package little-mallet-wrapper/0.2.0/little-mallet-wrapper-0.2.0.tar.gz

---
+++ packages/little-mallet-wrapper/little-mallet-wrapper-0.2.0.tar.gz/little-mallet-wrapper-0.2.0/little_mallet_wrapper/__init__.py
@@ -0,0 +1,17 @@
+from .little_mallet_wrapper import (
+  print_dataset_stats,
+  process_string,
+  import_data,
+  train_topic_model,
+  load_topic_keys,
+  load_topic_distributions,
+  load_training_ids,
+  get_top_docs,
+  plot_categories_by_topics_heatmap,
+  plot_categories_by_topic_boxplots,
+  divide_training_data,
+  infer_topics,
+  plot_topics_over_time,
+  quick_train_topic_model,
+  get_js_divergence
+)
---
+++ packages/little-mallet-wrapper/little-mallet-wrapper-0.2.0.tar.gz/little-mallet-wrapper-0.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="little-mallet-wrapper",
+    version="0.02.00",
+    author="Maria Antoniak",
+    author_email="maa343@cornell.edu",
+    description="A little wrapper for the topic modeling functions of MALLET",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maria-antoniak/little-mallet-wrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
