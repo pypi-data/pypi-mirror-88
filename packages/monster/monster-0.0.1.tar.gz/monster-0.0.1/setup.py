fbe8a6ba1a13360ab1bdd36e76f57e99c03486a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:23 +0000

    Add package monster/0.0.1/monster-0.0.1.tar.gz

---
+++ packages/monster/monster-0.0.1.tar.gz/monster-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="monster",
+    version="0.0.1",
+    description="include some useful tools, alg, sys",
+    author="mazewu",
+    author_email="mazewu1102@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
