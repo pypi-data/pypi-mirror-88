37ea4f4b16ab878b90ac182a92b102469da693c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:42 +0000

    Add package django-history-triggers/1.1.2/django-history-triggers-1.1.2.tar.gz

---
+++ packages/django-history-triggers/django-history-triggers-1.1.2.tar.gz/django-history-triggers-1.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+import history
+
+
+setup(
+    name='django-history-triggers',
+    version=history.__version__,
+    description='Management command and middleware for Django history triggers.',
+    author='Dan Watson',
+    author_email='watsond@imsweb.com',
+    url='https://github.com/imsweb/django-history-triggers',
+    license='BSD',
+    packages=find_packages(),
+    install_requires=[
+        'six',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
