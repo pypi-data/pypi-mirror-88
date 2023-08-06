76c5124637456e30b86287fdf126a17fb2a06ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:54 +0000

    Add package express-py/2020.10.19.3/express_py-2020.10.19.3-py3-none-any.whl

---
+++ packages/express-py/express_py-2020.10.19.3-py3-none-any.whl/express/properties/workflow.py
@@ -0,0 +1,198 @@
+from express.properties import BaseProperty
+
+
+class ExabyteMLPredictWorkflow(BaseProperty):
+    """
+    Exabyte ML predict Workflow property class.
+    """
+
+    def __init__(self, name, parser, *args, **kwargs):
+        super(ExabyteMLPredictWorkflow, self).__init__(name, parser, *args, **kwargs)
+        self.name = name
+        self.model = self.parser.model
+        self.targets = self.parser.targets
+        self.features = self.parser.features
+        self.scaling_params_per_feature = self.parser.scaling_params_per_feature
+
+    def _serialize(self):
+        """
+        Serialize a ML predict workflow.
+
+        Returns:
+             dict
+        """
+        return {
+            "units": [
+                {
+                    "_id": "LCthJ6E2QabYCZqf4",
+                    "name": "ml_predict_subworkflow",
+                    "type": "subworkflow",
+                    "flowchartId": "subworkflow",
+                    "head": True
+                }
+            ],
+            "subworkflows": [
+                {
+                    "name": "ml_predict_subworkflow",
+                    "isDraft": True,
+                    "application": {
+                        "version": "0.2.0",
+                        "summary": "Exabyte Machine Learning Engine",
+                        "name": "exabyteml",
+                        "shortName": "ml",
+                        "build": "Default"
+                    },
+                    "units": [
+                        {
+                            "status": "idle",
+                            "statusTrack": [],
+                            "head": True,
+                            "flowchartId": "io",
+                            "name": "input",
+                            "application": {
+                                "version": "0.2.0",
+                                "summary": "Exabyte Machine Learning Engine",
+                                "name": "exabyteml",
+                                "shortName": "ml",
+                                "build": "Default"
+                            },
+                            "results": [],
+                            "next": "data_transformation_manipulation",
+                            "source": "api",
+                            "postProcessors": [],
+                            "preProcessors": [],
+                            "subtype": "dataFrame",
+                            "input": [
+                                {
+                                    "endpoint": "dataframe",
+                                    "endpoint_options": {
+                                        "headers": {},
+                                        "data": {
+                                            "features": self.features,
+                                            "ids": [],
+                                            "targets": self.targets
+                                        },
+                                        "method": "POST",
+                                        "params": {},
+                                        "jobId": ""
+                                    }
+                                }
+                            ],
+                            "type": "io",
+                            "monitors": []
+                        },
+                        {
+                            "status": "idle",
+                            "statusTrack": [],
+                            "head": False,
+                            "flowchartId": "data_transformation_manipulation",
+                            "name": "clean data",
+                            "monitors": [],
+                            "results": [],
+                            "next": "data_transformation_scale_and_reduce",
+                            "application": {
+                                "version": "0.2.0",
+                                "summary": "Exabyte Machine Learning Engine",
+                                "name": "exabyteml",
+                                "shortName": "ml",
+                                "build": "Default"
+                            },
+                            "postProcessors": [],
+                            "preProcessors": [],
+                            "operationType": "manipulation",
+                            "operation": "data_transformation",
+                            "type": "processing",
+                            "inputData": {
+                                "cleanMissingData": True,
+                                "replaceNoneValuesWith": 0,
+                                "removeDuplicateRows": True
+                            }
+                        },
+                        {
+                            "status": "idle",
+                            "statusTrack": [],
+                            "head": False,
+                            "flowchartId": "data_transformation_scale_and_reduce",
+                            "name": "scale and reduce",
+                            "monitors": [],
+                            "results": [],
+                            "next": "score",
+                            "application": {
+                                "version": "0.2.0",
+                                "build": "Default",
+                                "name": "exabyteml",
+                                "shortName": "ml",
+                                "summary": "Exabyte Machine Learning Engine"
+                            },
+                            "postProcessors": [],
+                            "preProcessors": [],
+                            "operationType": "scale_and_reduce",
+                            "operation": "data_transformation",
+                            "type": "processing",
+                            "inputData": {
+                                "scaler": "standard_scaler",
+                                "perFeature": self.scaling_params_per_feature,
+                            }
+                        },
+                        {
+                            "status": "idle",
+                            "statusTrack": [],
+                            "executable": {
+                                "name": "score"
+                            },
+                            "flowchartId": "score",
+                            "name": "score",
+                            "head": False,
+                            "results": [
+                                {
+                                    "name": "predicted_properties"
+                                }
+                            ],
+                            "application": {
+                                "version": "0.2.0",
+                                "build": "Default",
+                                "name": "exabyteml",
+                                "shortName": "ml",
+                                "summary": "Exabyte Machine Learning Engine"
+                            },
+                            "postProcessors": [],
+                            "preProcessors": [],
+                            "context": {},
+                            "input": [],
+                            "flavor": {
+                                "name": "score"
+                            },
+                            "type": "execution",
+                            "monitors": [
+                                {
+                                    "name": "standard_output"
+                                }
+                            ]
+                        }
+                    ],
+                    "model": self.model,
+                    "_id": "LCthJ6E2QabYCZqf4",
+                    "properties": self.targets
+                }
+            ],
+            "name": self.name,
+            "properties": self.targets,
+            "creator": {
+                "_id": "",
+                "cls": "User",
+                "slug": ""
+            },
+            "owner": {
+                "_id": "",
+                "cls": "Account",
+                "slug": ""
+            },
+            "schemaVersion": "0.2.0",
+            "exabyteId": "",
+            "hash": "",
+            "_id": "",
+        }
+
+    @property
+    def schema(self):
+        return self.esse.get_schema_by_id("workflow")
