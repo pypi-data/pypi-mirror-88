6e683e762eab239049ccefcff56fc9e9d9a509ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:48 +0000

    Add package ailever/0.1.1/ailever-0.1.1-py3-none-any.whl

---
+++ packages/ailever/ailever-0.1.1-py3-none-any.whl/ailever/utils/openapi.py
@@ -0,0 +1,6 @@
+import os
+from urllib.request import urlretrieve
+
+def source(name):
+    urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/source/'+name, f'./{name}.py')
+    print(f'[AILEVER] The file "{name}.py" is downloaded!')
