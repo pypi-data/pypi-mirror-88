71bb392a289e36bcbd995043a978010698cc8e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:16 +0000

    Add package XLMMacroDeobfuscator/0.1.6/XLMMacroDeobfuscator-0.1.6-py3-none-any.whl

---
+++ packages/XLMMacroDeobfuscator/XLMMacroDeobfuscator-0.1.6-py3-none-any.whl/XLMMacroDeobfuscator/boundsheet.py
@@ -0,0 +1,132 @@
+import re
+
+
+class Cell:
+    _cell_addr_regex_str = r"((?P<sheetname>[^\s]+?|'.+?')!)?\$?(?P<column>[a-zA-Z]+)\$?(?P<row>\d+)"
+    _cell_addr_regex = re.compile(_cell_addr_regex_str)
+
+    _alternate_cell_addr_regex_str = "((?P<sheetname>[^\s]+?|'.+?')!)?\$?R(?P<row>\d+)\$?C(?P<column>\d+)"
+    _alternate_cell_addr_regex = re.compile(_alternate_cell_addr_regex_str)
+
+    _range_addr_regex_str = r"((?P<sheetname>[^\s]+?|'.+?')[!|$])?\$?(?P<column1>[a-zA-Z]+)\$?(?P<row1>\d+)\:?\$?(?P<column2>[a-zA-Z]+)\$?(?P<row2>\d+)"
+    _range_addr_regex = re.compile(_range_addr_regex_str)
+
+    def __init__(self):
+        self.sheet = None
+        self.column = ''
+        self.row = 0
+        self.formula = None
+        self.value = None
+        self.attributes = {}
+
+    def get_attribute(self, attribute_name):
+        # return default value if attributes doesn't cointain the attribute_name
+        pass
+
+    def __deepcopy__(self, memodict={}):
+        copy = type(self)()
+        memodict[id(self)] = copy
+        copy.sheet = self.sheet
+        copy.column = self.column
+        copy.row = self.row
+        copy.formula = self.formula
+        copy.value = self.value
+        copy.attributes = self.attributes
+        return copy
+
+    def get_local_address(self):
+        return self.column + str(self.row)
+
+    def __str__(self):
+        return "'{}'!{}".format(self.sheet.name,self.get_local_address())
+
+    @staticmethod
+    def convert_to_column_index(s):
+        number = 0
+        power = 1
+        for character in reversed(s):
+            character = character.upper()
+            digit = ((ord(character) - ord('A'))+1) * power
+            number = number + digit
+            power = power * 26
+
+        return number
+
+    @staticmethod
+    def convert_to_column_name(n):
+        string = ""
+        while n > 0:
+            n, remainder = divmod(n - 1, 26)
+            string = chr(ord('A') + remainder) + string
+        return string
+
+    @staticmethod
+    def parse_cell_addr(cell_addr_str):
+        cell_addr_str = cell_addr_str.strip('\"')
+        alternate_res = Cell._alternate_cell_addr_regex.match(cell_addr_str)
+        if alternate_res is not None:
+            sheet_name = alternate_res.group('sheetname')
+            sheet_name = sheet_name.strip('\'') if sheet_name is not None else sheet_name
+            column = Cell.convert_to_column_name(int(alternate_res.group('column')))
+            row = alternate_res.group('row')
+            return sheet_name, column, row
+        else:
+            res = Cell._cell_addr_regex.match(cell_addr_str)
+            if res is not None:
+                sheet_name = res.group('sheetname')
+                sheet_name = sheet_name.strip('\'') if sheet_name is not None else sheet_name
+                column = res.group('column')
+                row = res.group('row')
+                return sheet_name, column, row
+            else:
+                return None, None, None
+
+    @staticmethod
+    def parse_range_addr(range_addr_str):
+        res = Cell._range_addr_regex.match(range_addr_str)
+        if res is not None:
+            sheet_name = res.group('sheetname')
+            sheet_name = sheet_name.strip('\'') if sheet_name is not None else sheet_name
+            startcolumn = res.group('column1')
+            startrow = res.group('row1')
+            endcolumn = res.group('column2')
+            endrow = res.group('row2')
+            return sheet_name, startcolumn, startrow, endcolumn, endrow
+        else:
+            return None, None, None
+
+    @staticmethod
+    def convert_twip_to_point(twips):
+        # A twip is 1/20 of a point
+        point = int(twips) * 0.05
+        return point
+
+
+class Boundsheet:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        self.cells = {}
+        self.row_attributes = {}
+        self.col_attributes = {}
+        self.default_height = None
+
+    def get_row_attribute(self, row, attrib_name):
+        # default values if row doesn't exist in row_attributes
+        pass
+
+    def get_col_attribute(self, col, attrib_name):
+        # default value if row doesn't exist in row_attributes
+
+        pass
+
+
+    def add_cell(self, cell):
+        cell.sheet = self
+        self.cells[cell.get_local_address()] = cell
+
+    def get_cell(self, local_address):
+        result = None
+        if local_address in self.cells:
+            result = self.cells[local_address]
+        return
---
+++ packages/XLMMacroDeobfuscator/XLMMacroDeobfuscator-0.1.6-py3-none-any.whl/XLMMacroDeobfuscator/xls_wrapper.py
@@ -0,0 +1,196 @@
+from XLMMacroDeobfuscator.excel_wrapper import ExcelWrapper
+from XLMMacroDeobfuscator.boundsheet import Boundsheet
+from XLMMacroDeobfuscator.boundsheet import Cell
+from win32com.client import Dispatch
+import pywintypes
+from enum import Enum
+import os
+
+
+class XlCellType(Enum):
+    xlCellTypeFormulas = -4123
+    xlCellTypeConstants = 2
+
+
+class XLSWrapper(ExcelWrapper):
+    XLEXCEL4MACROSHEET = 3
+
+    def __init__(self, xls_doc_path):
+        self._excel = Dispatch("Excel.Application")
+        self.xls_workbook = self._excel.Workbooks.Open(xls_doc_path)
+        self.xls_workbook_name = re.search(".*\/([^\/]+)\.",xls_doc_path).group(1)
+        self._macrosheets = None
+        self._defined_names = None
+        self.xl_international_flags = {}
+        self._international_flags = None
+
+    def get_xl_international_char(self, flag_name):
+        if flag_name not in self.xl_international_flags:
+            if self._international_flags is None:
+                self._international_flags = self._excel.Application.International
+            # flag value starts at 1, list index starts at 0
+            self.xl_international_flags[flag_name] = self._international_flags[flag_name.value - 1]
+
+        result = self.xl_international_flags[flag_name]
+        return result
+
+    def get_defined_names(self):
+        result = {}
+
+        name_objects = self.xls_workbook.Excel4MacroSheets.Application.Names
+
+        for name_obj in name_objects:
+            result[name_obj.NameLocal.lower()] = str(name_obj.RefersToLocal).strip('=')
+
+        return result
+
+    def get_defined_name(self, name, full_match=True):
+        result = []
+        name = name.lower()
+        if self._defined_names is None:
+            self._defined_names = self.get_defined_names()
+
+        if full_match:
+            if name in self._defined_names:
+                result = self._defined_names[name]
+        else:
+            for defined_name, cell_address in self._defined_names.items():
+                if defined_name.startswith(name):
+                    result.append((defined_name, cell_address))
+
+        return result
+
+    def load_cells(self, macrosheet, xls_sheet):
+        cells = {}
+        try:
+            self._excel.Application.ScreenUpdating = False
+            col_offset = xls_sheet.UsedRange.Column
+            row_offset = xls_sheet.UsedRange.Row
+            formulas = xls_sheet.UsedRange.Formula
+            if formulas is not None:
+                for row_no, row in enumerate(formulas):
+                    for col_no, col in enumerate(row):
+                        if col:
+                            cell = Cell()
+                            cell.sheet = macrosheet
+                            if len(col)>1 and col.startswith('='):
+                                cell.formula = col
+                            else:
+                                cell.value = col
+                            row_addr = row_offset + row_no
+                            col_addr = col_offset + col_no
+                            cell.row = row_addr
+                            cell.column = Cell.convert_to_column_name(col_addr)
+
+                            cells[(col_addr, row_addr)] = cell
+
+            self._excel.Application.ScreenUpdating = True
+
+        except pywintypes.com_error as error:
+            print('CELL(Formula): ' + str(error.args[2]))
+
+        try:
+            values= xls_sheet.UsedRange.Value
+            if values is not None:
+                for row_no, row in enumerate(values):
+                    for col_no, col in enumerate(row):
+                        if col:
+                            row_addr = row_offset + row_no
+                            col_addr = col_offset + col_no
+                            if (col_addr, row_addr) in cells:
+                                cell = cells[(col_addr, row_addr)]
+                                cell.value = col
+                            else:
+                                cell = Cell()
+                                cell.sheet = macrosheet
+                                cell.value = col
+                                cell.row = row_addr
+                                cell.column = Cell.convert_to_column_name(col_addr)
+                                cells[(col_addr, row_addr)] = cell
+        except pywintypes.com_error as error:
+            print('CELL(Constant): ' + str(error.args[2]))
+
+        for cell in cells:
+            macrosheet.add_cell(cells[cell])
+
+    def get_macrosheets(self):
+        if self._macrosheets is None:
+            self._macrosheets = {}
+            for sheet in self.xls_workbook.Excel4MacroSheets:
+                macrosheet = Boundsheet(sheet.name, 'Macrosheet')
+                self.load_cells(macrosheet, sheet)
+                self._macrosheets[sheet.name] = macrosheet
+
+        return self._macrosheets
+
+    def get_workbook_name(self):
+        return self.xls_workbook_name
+
+    def get_cell_info(self, sheet_name, col, row, type_ID):
+        sheet = self._excel.Excel4MacroSheets(sheet_name)
+        cell = col + row
+        data = None
+
+        if int(type_ID) == 2:
+            data = sheet.Range(col + row).Row
+            print(data)
+
+        elif int(type_ID) == 3:
+            data = sheet.Range(cell).Column
+            print(data)
+
+        elif int(type_ID) == 8:
+            data = sheet.Range(cell).HorizontalAlignment
+
+        elif int(type_ID) == 17:
+            data = sheet.Range(cell).Height
+
+        elif int(type_ID) == 19:
+            data = sheet.Range(cell).Font.Size
+
+        elif int(type_ID) == 20:
+            data = sheet.Range(cell).Font.Bold
+
+        elif int(type_ID) == 21:
+            data = sheet.Range(cell).Font.Italic
+
+        elif int(type_ID) == 23:
+            data = sheet.Range(cell).Font.Strikethrough
+
+        elif int(type_ID) == 24:
+            data = sheet.Range(cell).Font.ColorIndex
+
+        elif int(type_ID) == 50:
+            data = sheet.Range(cell).VerticalAlignment
+        else:
+            print("Unknown info_type (%d) at cell %s" % (type_ID, cell))
+
+        return data, False, False
+
+
+if __name__ == '__main__':
+
+    path = r"tmp\xls\edd554502033d78ac18e4bd917d023da2fd64843c823c1be8bc273f48a5f3f5f.xls"
+
+    path = os.path.abspath(path)
+    excel_doc = XLSWrapper(path)
+    try:
+        macrosheets = excel_doc.get_macrosheets()
+
+        auto_open_labels = excel_doc.get_defined_name('auto_open', full_match=False)
+        for label in auto_open_labels:
+            print('auto_open: {}->{}'.format(label[0], label[1]))
+
+        for macrosheet_name in macrosheets:
+            print('SHEET: {}\t{}'.format(macrosheets[macrosheet_name].name,
+                                         macrosheets[macrosheet_name].type))
+            for formula_loc, info in macrosheets[macrosheet_name].cells.items():
+                if info.formula is not None:
+                    print('{}\t{}\t{}'.format(formula_loc, info.formula, info.value))
+
+            for formula_loc, info in macrosheets[macrosheet_name].cells.items():
+                if info.formula is None:
+                    print('{}\t{}\t{}'.format(formula_loc, info.formula, info.value))
+    finally:
+        excel_doc._excel.Application.DisplayAlerts = False
+        excel_doc._excel.Application.Quit()
