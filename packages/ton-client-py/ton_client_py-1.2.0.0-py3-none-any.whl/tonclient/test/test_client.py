227e87989eb5fa23dabb1c49bf77b2d5d5da3187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:29 +0000

    Add package ton-client-py/1.2.0.0/ton_client_py-1.2.0.0-py3-none-any.whl

---
+++ packages/ton-client-py/ton_client_py-1.2.0.0-py3-none-any.whl/tonclient/test/test_client.py
@@ -0,0 +1,45 @@
+import unittest
+
+from tonclient.bindings.lib import LIB_VERSION
+from tonclient.client import TonClient, DEVNET_BASE_URL
+
+
+class TestTonClientAsyncCore(unittest.TestCase):
+    def setUp(self) -> None:
+        self.client = TonClient(network={'server_address': DEVNET_BASE_URL})
+
+    def test_version(self):
+        self.assertEqual(LIB_VERSION, self.client.version())
+
+    def test_get_api_reference(self):
+        reference = self.client.get_api_reference()
+        self.assertGreater(len(reference['modules']), 0)
+        self.assertEqual(LIB_VERSION, reference['version'])
+
+    def test_build_info(self):
+        info = self.client.build_info()
+        self.assertNotEqual(None, info.get('build_number'))
+
+    def test_destroy_context(self):
+        self.client.destroy_context()
+
+
+class TestTonClientSyncCore(unittest.TestCase):
+    """ Sync core is not recommended to use """
+    def setUp(self) -> None:
+        self.client = TonClient(
+            network={'server_address': DEVNET_BASE_URL}, is_core_async=False)
+
+    def test_version(self):
+        self.assertEqual(LIB_VERSION, self.client.version())
+
+    def test_get_api_reference(self):
+        reference = self.client.get_api_reference()
+        self.assertGreater(len(reference['modules']), 0)
+
+    def test_build_info(self):
+        info = self.client.build_info()
+        self.assertNotEqual(None, info.get('build_number'))
+
+    def test_destroy_context(self):
+        self.client.destroy_context()
