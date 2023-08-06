4bc1dbab4ced79b6fe1ecf0bf5dfba5f43113ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:23 +0000

    Add package transformer-discord-notifier/0.2.0/transformer_discord_notifier-0.2.0-py2.py3-none-any.whl

---
+++ packages/transformer-discord-notifier/transformer_discord_notifier-0.2.0-py2.py3-none-any.whl/transformer_discord_notifier/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.0"
+
+
+# ----------------------------------------------------------------------------
+
+
+from .discord import DiscordClient  # noqa: F401
+from .transformers import DiscordProgressCallback  # noqa: F401
+
+
+# ----------------------------------------------------------------------------
