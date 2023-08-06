c717e072c3d358b0d52f311db213bbd3d5edcb81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:33 +0000

    Add package RJcalculator/0.1.0/RJcalculator-0.1.0-py3-none-any.whl

---
+++ packages/RJcalculator/RJcalculator-0.1.0-py3-none-any.whl/RJcalculator/__init__.py
@@ -0,0 +1,20 @@
+# class Operations:
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def power(x, y):
+    return x ** y
+
+def remainder(x, y):
+    return x // y
+
