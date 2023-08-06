fba20fd392d37ddffcefae12d0bb45b67b0bd05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:18 +0000

    Add package sphinx-a4doc/1.2.2/sphinx-a4doc-1.2.2.tar.gz

---
+++ packages/sphinx-a4doc/sphinx-a4doc-1.2.2.tar.gz/sphinx-a4doc-1.2.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(project_urls={
+        'Documentation': 'https://amatanhead.github.io/sphinx-a4doc/',
+        'Source': 'https://github.com/AmatanHead/sphinx-a4doc',
+        'Tracker': 'https://github.com/AmatanHead/sphinx-a4doc/issues',
+    })
+
