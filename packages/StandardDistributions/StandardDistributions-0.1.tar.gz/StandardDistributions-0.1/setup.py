392adf3a0455a930206bce0cee89186366a0a294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:32 +0000

    Add package StandardDistributions/0.1/StandardDistributions-0.1.tar.gz

---
+++ packages/StandardDistributions/StandardDistributions-0.1.tar.gz/StandardDistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='StandardDistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['StandardDistributions'],
+      zip_safe=False)
