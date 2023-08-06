6be58de44929b99e052c6d27ad0a940aa00d1180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:44 +0000

    Add package BootcampDSC/1.1/BootcampDSC-1.1.tar.gz

---
+++ packages/BootcampDSC/BootcampDSC-1.1.tar.gz/BootcampDSC-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'BootcampDSC',
+        version = '1.1',
+        description = 'Data Literate Bootcamp DSC UMM', 
+        packages = ['BootcampDSC'],
+        author = 'shandytp',
+        author_email = 'shandytsalasa@gmail.com',
+        zip_safe = False)
