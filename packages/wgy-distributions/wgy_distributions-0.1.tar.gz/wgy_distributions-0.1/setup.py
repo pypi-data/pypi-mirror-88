b30fe154e1ad1b3f68852671a81eed1b4f78d533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:27 +0000

    Add package wgy-distributions/0.1/wgy_distributions-0.1.tar.gz

---
+++ packages/wgy-distributions/wgy_distributions-0.1.tar.gz/wgy_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='wgy_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['wgy_distributions'],
+      zip_safe=False)
