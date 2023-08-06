50f64c2bbeb0e7262fec66e43fdba8da18da105a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:14 +0000

    Add package cioc4d/0.2.0/cioc4d-0.2.0.tar.gz

---
+++ packages/cioc4d/cioc4d-0.2.0.tar.gz/cioc4d-0.2.0/README.md
@@ -0,0 +1,33 @@
+# Conductor for Cinema 4d
+
+Cinema 4d plugin submitter for the Conductor Cloud rendering service.
+
+## Install
+
+**To install the latest version.**
+```bash
+pip install --upgrade cioc4d --target=$HOME/Conductor
+```
+
+**To install a specific version, for example 0.1.0.**
+```bash
+pip install --upgrade --force-reinstall cioc4d==0.1.0 --target=$HOME/Conductor
+```
+
+## Usage
+
+Go to **Edit->Preferences->Plugins** and add the path to the cioc4d installation directory. 
+
+```
+~/Conductor/cioc4d
+```
+
+restart Cinema 4d.
+
+To set up a render, choose **Conductor->ConductorRender**. 
+
+For detailed help, checkout the [tutorial](https://docs.conductortech.com/tutorials/c4d) and [reference](https://docs.conductortech.com/reference/c4d) documentation.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit)
---
+++ packages/cioc4d/cioc4d-0.2.0.tar.gz/cioc4d-0.2.0/setup.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+import os
+import setuptools
+from PIL import Image
+
+from setuptools.command.build_py import build_py
+
+
+NAME = "cioc4d"
+DESCRIPTION = "C4D plugin for Conductor Cloud Rendering Platform."
+URL = "https://github.com/AtomicConductor/conductor-c4d"
+EMAIL = "info@conductortech.com"
+AUTHOR = "conductor"
+REQUIRES_PYTHON = "~=2.7"
+REQUIRED = ["ciocore>=0.2.15,<1.0.0"]
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, 'VERSION')) as version_file:
+    VERSION = version_file.read().strip()
+
+long_description = ""
+with open(os.path.join(HERE, 'README.md')) as readme:
+    long_description = readme.read().strip()
+long_description += "\n\n## Changelog\n\n"
+with open(os.path.join(HERE, 'CHANGELOG.md')) as changelog:
+    long_description += changelog.read().strip()   
+ 
+
+
+
+class BuildCommand(build_py):
+    def run(self):
+        build_py.run(self)
+
+        if not self.dry_run:
+            target_dir = os.path.join(self.build_lib, NAME)
+            self.write_version_info(target_dir)
+            self.convert_icons(target_dir)
+
+    def write_version_info(self, target_dir):
+        with open(os.path.join(target_dir, "VERSION"), "w") as f:
+            f.write(VERSION)
+
+    def convert_icons(self, target_dir):
+        """Convert icons using PIL."""
+        out_icon_dir = os.path.join(target_dir, "res")
+        icons_dir = os.path.join("images", "icons")
+        icon_files = os.listdir(icons_dir)
+        for icon_file in icon_files:
+            base, ext = os.path.splitext(icon_file)
+            icon_path = os.path.join(icons_dir, icon_file)
+            size = 30
+            icon = Image.open(icon_path)
+            icon.thumbnail((size, size), Image.BICUBIC)
+            out_icon_file = os.path.join(
+                out_icon_dir, "{}_{}x{}{}".format(base, size, size, ext))
+            icon.save(out_icon_file)
+ 
+setuptools.setup(
+    author=AUTHOR,
+    author_email=EMAIL,
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python",
+        "Topic :: Multimedia :: Graphics :: 3D Rendering",
+    ],
+    cmdclass={"build_py": BuildCommand},
+    description=DESCRIPTION,
+    install_requires=REQUIRED,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name=NAME,
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    include_package_data=True,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    version=VERSION,
+    zip_safe=False,
+    package_data={NAME: ["res/*.*", "res/*/*.*", "res/*/*/*.*", "Conductor.pyp", ]}
+)
