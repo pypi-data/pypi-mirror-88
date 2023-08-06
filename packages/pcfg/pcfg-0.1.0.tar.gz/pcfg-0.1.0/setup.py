7a665bce1ad8d4e813038f0b4404493e9dff4846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:23 +0000

    Add package pcfg/0.1.0/pcfg-0.1.0.tar.gz

---
+++ packages/pcfg/pcfg-0.1.0.tar.gz/pcfg-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pcfg']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nltk>=3.5,<4.0']
+
+setup_kwargs = {
+    'name': 'pcfg',
+    'version': '0.1.0',
+    'description': 'Generate sentences from a probabilistic context-free grammar.',
+    'long_description': None,
+    'author': 'Thomas Breydo',
+    'author_email': 'tbreydo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/thomasbreydo/pcfg',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
