16c8f122be62bd841f83d4f077abb8b6d30a080e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:47 +0000

    Add package vk-spammer/1.2.2.4/vk-spammer-1.2.2.4.tar.gz

---
+++ packages/vk-spammer/vk-spammer-1.2.2.4.tar.gz/vk-spammer-1.2.2.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+DESCRIPTION = 'Спаммер для ВК'
+LONG_DESC = DESCRIPTION
+
+setup(
+    name='vk-spammer',
+    version='1.2.2.4',
+    author='fgRuslan',
+    author_email='ubijca16@gmail.com',
+    url='https://github.com/fgRuslan/vk-spammer',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    long_description=LONG_DESC,
+    entry_points={
+        'console_scripts':
+            ['vk-spammer=core.spam:main']
+    },
+    install_requires=[
+        'vk'
+    ]
+)
