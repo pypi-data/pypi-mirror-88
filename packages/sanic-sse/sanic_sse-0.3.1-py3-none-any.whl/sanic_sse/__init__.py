d26d163be9fe2ef9803c139d36bbff2984198fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:58 +0000

    Add package sanic-sse/0.3.1/sanic_sse-0.3.1-py3-none-any.whl

---
+++ packages/sanic-sse/sanic_sse-0.3.1-py3-none-any.whl/sanic_sse/__init__.py
@@ -0,0 +1,11 @@
+"""
+Sanic sse package that adds Server-Sent Events compability to sanic application.
+
+Author: Innokenty Lebedev
+"""
+
+from .sse import Sse
+
+__version__ = "0.3.1"
+
+__all__ = ["Sse"]
