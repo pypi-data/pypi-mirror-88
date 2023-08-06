233da4fcde2b729134f9938631b74049fb388261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:19 +0000

    Add package streamlit-discourse/0.1.3/streamlit-discourse-0.1.3.tar.gz

---
+++ packages/streamlit-discourse/streamlit-discourse-0.1.3.tar.gz/streamlit-discourse-0.1.3/streamlit_discourse/frontend/build/precache-manifest.83b0bd8a8ce386e3bf0fe7e27447b4e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a28377b10af44e1b73388cc9d2e564",
+    "url": "./index.html"
+  },
+  {
+    "revision": "759fee200d4789df1a8a",
+    "url": "./static/js/2.8bf4e17c.chunk.js"
+  },
+  {
+    "revision": "9bf7777849ff7c0e7b1f4ddb70e6eb95",
+    "url": "./static/js/2.8bf4e17c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a51a65de718939b963c",
+    "url": "./static/js/main.3f9c07f9.chunk.js"
+  },
+  {
+    "revision": "5d93b832dda06546d95d",
+    "url": "./static/js/runtime-main.7e8c6cf9.js"
+  }
+]);
