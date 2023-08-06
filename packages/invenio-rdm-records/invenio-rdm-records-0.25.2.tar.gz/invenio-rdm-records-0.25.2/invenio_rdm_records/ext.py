2af6a0f441c416e6dd6c9e281aaf844684a7cd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:57 +0000

    Add package invenio-rdm-records/0.25.2/invenio-rdm-records-0.25.2.tar.gz

---
+++ packages/invenio-rdm-records/invenio-rdm-records-0.25.2.tar.gz/invenio-rdm-records-0.25.2/invenio_rdm_records/ext.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+# Copyright (C) 2019 Northwestern University.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""DataCite-based data model for Invenio."""
+from celery import shared_task
+from invenio_db import db
+from invenio_files_rest.signals import file_deleted, file_uploaded
+from invenio_indexer.signals import before_record_index
+
+from . import config
+from .services.schemas.metadata_extensions import MetadataExtensions, \
+    add_es_metadata_extensions
+
+
+class InvenioRDMRecords(object):
+    """Invenio-RDM-Records extension."""
+
+    def __init__(self, app=None):
+        """Extension initialization."""
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization."""
+        self.init_config(app)
+        self.metadata_extensions = MetadataExtensions(
+            app.config['RDM_RECORDS_METADATA_NAMESPACES'],
+            app.config['RDM_RECORDS_METADATA_EXTENSIONS']
+        )
+
+        app.extensions['invenio-rdm-records'] = self
+
+    def init_config(self, app):
+        """Initialize configuration."""
+        supported_configurations = [
+            'FILES_REST_PERMISSION_FACTORY',
+            'RECORDS_UI_ENDPOINTS',
+            'THEME_SITEURL',
+        ]
+        overriding_configurations = [
+            'PREVIEWER_RECORD_FILE_FACOTRY',
+        ]
+
+        for k in dir(config):
+            if k in supported_configurations or k.startswith('RDM_RECORDS_'):
+                app.config.setdefault(k, getattr(config, k))
+            if k in overriding_configurations and not app.config.get(k):
+                app.config[k] = getattr(config, k)
