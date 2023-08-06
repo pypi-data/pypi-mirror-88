91d91bade9336a10708841965d70918d9de2e903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:22 +0000

    Add package software-package-RK-BE103-Caltech/0.0.2/software_package_RK_BE103_Caltech-0.0.2.tar.gz

---
+++ packages/software-package-RK-BE103-Caltech/software_package_RK_BE103_Caltech-0.0.2.tar.gz/software_package_RK_BE103_Caltech-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='software_package_RK_BE103_Caltech',
+    version='0.0.2',
+    author='Rohit Kantipudi',
+    author_email='rohitk@caltech.edu',
+    description='Package created for extra-credit assignment be103a team 18 to analyze microtubule catastrophe.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=["numpy","pandas", "bokeh>=1.4.0"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
