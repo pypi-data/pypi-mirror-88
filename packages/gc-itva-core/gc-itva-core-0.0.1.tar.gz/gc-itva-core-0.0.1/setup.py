35fa8a412e429c28ab77e0738ef4fa4409a9f1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:09 +0000

    Add package gc-itva-core/0.0.1/gc-itva-core-0.0.1.tar.gz

---
+++ packages/gc-itva-core/gc-itva-core-0.0.1.tar.gz/gc-itva-core-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='gc-itva-core',
+    version='0.0.1',
+    description='Python REST API client for Guacamole 0.9.13 version',
+    author='author',
+    author_email='xxxxxxxx@163.com',
+    url='https://github.com/xxxxx',
+    # packages=find_packages(),
+    packages=['gc-itva-core'],
+    install_requires=['requests']
+)
