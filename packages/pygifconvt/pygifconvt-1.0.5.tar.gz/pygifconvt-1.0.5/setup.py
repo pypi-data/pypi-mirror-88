119bef24221ce06c5016faefb2f1218f6b22317c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:22 +0000

    Add package pygifconvt/1.0.5/pygifconvt-1.0.5.tar.gz

---
+++ packages/pygifconvt/pygifconvt-1.0.5.tar.gz/pygifconvt-1.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name             = 'pygifconvt',
+    version          = '1.0.5',
+    description      = 'Test package for distribution',
+    author           = 'Eunki7',
+    author_email     = 'outsider7224@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['pillow'],
+    keywords         = ['GIFCONVERTER', 'gifconverter'],
+    python_requires  = '>=3',
+    zip_safe=True,
+    classifiers      = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+) 
