d8389f3a16741e4c39229d77fe2cb9be75cae793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:13 +0000

    Add package graphical-anim/0.0.3/graphical-anim-0.0.3.tar.gz

---
+++ packages/graphical-anim/graphical-anim-0.0.3.tar.gz/graphical-anim-0.0.3/graphical_anim/utils.py
@@ -0,0 +1,31 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import numpy
+from PIL import Image
+
+def surf_to_image(surface):
+    pixels = []
+    for y in range(surface.get_height()):
+        row = []
+        for x in range(surface.get_width()):
+            row.append(surface.get_at((x, y)))
+        pixels.append(row)
+
+    pixels = numpy.array(pixels, dtype=numpy.uint8)
+    image = Image.fromarray(pixels)
+    return image
