bc32e49199040f8d7b2d1717005483d6338abf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:19 +0000

    Add package gisansexplorer/0.0.8/gisansexplorer-0.0.8-py3-none-any.whl

---
+++ packages/gisansexplorer/gisansexplorer-0.0.8-py3-none-any.whl/GisansExplorer/main_app.py
@@ -0,0 +1,34 @@
+#Qt stuff:
+from GisansExplorer.utils import Frozen
+from GisansExplorer.GUI import MyTabs
+import PyQt5.QtWidgets as qtw
+from PyQt5.QtGui import QIcon
+
+import traceback
+
+class App(qtw.QMainWindow,Frozen):
+    def __init__(self):
+        super().__init__()
+        self.title = 'GisansExplorer'
+        self.myTabs = MyTabs()
+        self.setCentralWidget(self.myTabs)
+        self._freeze()
+        self.setWindowTitle(self.title)
+        icon = QIcon('gisansexplorer/resources/Icon.png')
+        self.setWindowIcon(icon)
+        self.show()
+
+    def closeEvent(self, event):
+
+        quit_msg = "Are you sure you want to exit?"
+        reply = qtw.QMessageBox.question(self, 'Message',
+                     quit_msg, qtw.QMessageBox.Yes, qtw.QMessageBox.No)
+
+        if reply == qtw.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+    def addTab(self):
+        self.myTabs.addTab()
