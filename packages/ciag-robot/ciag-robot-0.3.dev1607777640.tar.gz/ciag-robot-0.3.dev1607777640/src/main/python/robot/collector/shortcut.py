dd333cb76fce78f750054a65a2f859efba9fe795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:37 +0000

    Add package ciag-robot/0.3.dev1607777640/ciag-robot-0.3.dev1607777640.tar.gz

---
+++ packages/ciag-robot/ciag-robot-0.3.dev1607777640.tar.gz/ciag-robot-0.3.dev1607777640/src/main/python/robot/collector/shortcut.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, Callable
+
+from robot.api import Collector, X, Y
+from robot.collector import core
+
+const = core.ConstCollector
+fn = core.FnCollector
+noop = lambda: core.NOOP_COLLECTOR
+afn = core.AsyncFnCollector
+as_text = core.AsTextCollector
+text = core.TextCollector
+attr = core.AttrCollector
+css = core.CssCollector
+xpath = core.XPathCollector
+pipe = core.PipeCollector
+array = core.ArrayCollector
+tuple = core.TupleCollector
+any = core.AnyCollector
+default = core.DefaultCollector
+delay = core.DelayCollector
+
+
+def dict(*args: Collector[X, Dict[str, Any]], **kwargs: Collector[X, Any]) -> Collector[X, Dict[str, Any]]:
+    return core.DictCollector(
+        args,
+        kwargs
+    )
+
+
+def obj(*args: Collector[X, Dict[str, Any]], **kwargs: Collector[X, Any]) -> Collector[X, core.Object]:
+    dict_collector = core.DictCollector(
+        args,
+        kwargs
+    )
+    return core.ObjectCollector(dict_collector)
