64e488f46a203145ac5e8241a800e39085e13a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:29 +0000

    Add package sczr-tcp/0.1.3/sczr_tcp-0.1.3.tar.gz

---
+++ packages/sczr-tcp/sczr_tcp-0.1.3.tar.gz/sczr_tcp-0.1.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sczr_tcp']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['loguru>=0.5.3,<0.6.0',
+ 'orjson>=3.4.6,<4.0.0',
+ 'pydantic>=1.7.3,<2.0.0',
+ 'pygame>=2.0.0,<3.0.0',
+ 'typer>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['sczr-tcp = sczr_tcp.cli:main']}
+
+setup_kwargs = {
+    'name': 'sczr-tcp',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Michał Rokita',
+    'author_email': 'mrokita@mrokita.pl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
