33240a3779e352033db12780262a46784eb9a426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:00 +0000

    Add package xyz-tools/1.0.0/xyz_tools-1.0.0.tar.gz

---
+++ packages/xyz-tools/xyz_tools-1.0.0.tar.gz/xyz_tools-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         ='xyz_tools',
+    version      ='1.0.0',
+    py_modules   =['xyz_tools'],
+    author       ='bowen xu',
+    author_email ='575650581@qq.com',
+    description  ='frequently-used tools',
+    )
