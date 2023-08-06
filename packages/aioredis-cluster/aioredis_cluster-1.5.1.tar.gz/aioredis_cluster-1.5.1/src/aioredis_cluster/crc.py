cdf61cb773cd627f6806b1a32d6a7504c83b25d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:16 +0000

    Add package aioredis-cluster/1.5.1/aioredis_cluster-1.5.1.tar.gz

---
+++ packages/aioredis-cluster/aioredis_cluster-1.5.1.tar.gz/aioredis_cluster-1.5.1/src/aioredis_cluster/crc.py
@@ -0,0 +1,28 @@
+from binascii import crc_hqx
+
+
+__all__ = [
+    "crc16",
+    "key_slot",
+]
+
+REDIS_CLUSTER_HASH_SLOTS = 16384
+
+
+def crc16(data: bytes) -> int:
+    return crc_hqx(data, 0)
+
+
+def key_slot(k: bytes, bucket: int = REDIS_CLUSTER_HASH_SLOTS) -> int:
+    """Calculate key slot for a given key.
+
+    :param key - bytes
+    :param bucket - int
+    """
+
+    start = k.find(b"{")
+    if start > -1:
+        end = k.find(b"}", start + 1)
+        if end > -1 and end != start + 1:
+            k = k[start + 1 : end]
+    return crc16(k) % bucket
