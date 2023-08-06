870aa7fb99dff4f0d92ddeb985f5571ee3abc725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:50 +0000

    Add package automlsa2/0.7.0/automlsa2-0.7.0.tar.gz

---
+++ packages/automlsa2/automlsa2-0.7.0.tar.gz/automlsa2-0.7.0/automlsa2/mafft.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import logging
+import shlex
+import subprocess
+from .helper_functions import checkpoint_reached, checkpoint_tracker
+from rich.progress import track
+from typing import List
+
+
+def run_mafft(
+    threads: int,
+    mafft: str,
+    unaligned: List[str],
+    checkpoint: bool,
+    opts: List[str],
+) -> List[str]:
+    """
+    input  - unaligned fasta files per query
+    return - list of aligned files per query
+    """
+    logger = logging.getLogger(__name__)
+    base_cmd: List[str] = [mafft, '--thread', str(threads)]
+    base_cmd.extend(opts)
+    aligneddir: str = 'aligned'
+    aligned: List[str] = []
+    cmdstrs: List[str] = []
+    logger.info('Aligning FASTA sequences, if necessary.')
+    if not os.path.exists(aligneddir):
+        os.mkdir(aligneddir)
+    for unalign in track(unaligned, 'Running mafft...'.rjust(19, ' ')):
+        outname: str = '{}/{}.aln'.format(
+            aligneddir, os.path.basename(os.path.splitext(unalign)[0])
+        )
+        logname: str = '{}.log'.format(outname)
+        logger.debug('Aligning and writing to {}'.format(outname))
+        aligned.append(outname)
+        if os.path.exists(outname):
+            msg = 'Aligned file {} already found, skipping.'
+            logger.debug(msg.format(outname))
+        else:
+            cmd: List[str] = base_cmd + [unalign]
+            cmdstr = ' '.join([shlex.quote(x) for x in cmd]) + ' > {}'.format(
+                shlex.quote(outname)
+            )
+            logger.debug(cmdstr)
+            if checkpoint == 'prealign':
+                cmdstrs.append(cmdstr)
+            else:
+                with open(outname, 'w') as fh, open(logname, 'w') as logfh:
+                    subprocess.run(cmd, stdout=fh, stderr=logfh, text=True)
+    if cmdstrs:
+        with open('mafftcmds.txt', 'w') as fh:
+            fh.write('\n'.join(cmdstrs))
+        logger.info('MAFFT alignment commands written to mafftcmds.txt.')
+        logger.info('Run these commands and resubmit to continue.')
+        checkpoint_reached('prior to MAFFT alignments')
+    elif checkpoint == 'postalign':
+        checkpoint_reached('after MAFFT alignments')
+
+    checkpoint_tracker('run_mafft')
+
+    return aligned
---
+++ packages/automlsa2/automlsa2-0.7.0.tar.gz/automlsa2-0.7.0/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup  # type: ignore
+
+with open('automlsa2/__version__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+REQUIRES = ['biopython', 'pandas>=1.0.0', 'numpy', 'rich', 'packaging']
+
+kwargs = {
+    'name': 'automlsa2',
+    'version': version,
+    'description': 'Automated Multi-Locus Sequence Analysis tool',
+    'long_description_content_type': 'text/x-rst',
+    'long_description': readme,
+    'author': 'Edward Davis',
+    'author_email': 'ed@cgrb.oregonstate.edu',
+    'maintainer': 'Edward Davis',
+    'maintainer_email': 'ed@cgrb.oregonstate.edu',
+    'url': 'https://github.com/davised/automlsa2',
+    'license': 'Custom',
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: Free for non-commercial use',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    'install_requires': REQUIRES,
+    'tests_require': ['coverage', 'pytest'],
+    'packages': find_packages(exclude=('tests', 'tests.*')),
+}
+
+setup(
+    **kwargs, entry_points={"console_scripts": ['automlsa2 = automlsa2.__main__:main']}
+)
