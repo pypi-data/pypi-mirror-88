a674f604614b8f6feacbaf0bd2c78d22cd58ac9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:58 +0000

    Add package dagster-slack/0.9.22rc1/dagster-slack-0.9.22rc1.tar.gz

---
+++ packages/dagster-slack/dagster-slack-0.9.22rc1.tar.gz/dagster-slack-0.9.22rc1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+
+def get_version():
+    version = {}
+    with open("dagster_slack/version.py") as fp:
+        exec(fp.read(), version)  # pylint: disable=W0122
+
+    return version["__version__"]
+
+
+if __name__ == "__main__":
+    setup(
+        name="dagster-slack",
+        version=get_version(),
+        author="Elementl",
+        author_email="hello@elementl.com",
+        license="Apache-2.0",
+        description="A Slack client resource for posting to Slack",
+        url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-slack",
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        packages=find_packages(exclude=["test"]),
+        install_requires=["dagster", "slackclient>=2,<3"],
+        zip_safe=False,
+    )
