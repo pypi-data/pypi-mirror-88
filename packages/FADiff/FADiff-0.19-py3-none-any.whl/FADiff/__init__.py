cd8cc539547c0a81138d75f019d764f6df5c7113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:53 +0000

    Add package FADiff/0.19/FADiff-0.19-py3-none-any.whl

---
+++ packages/FADiff/FADiff-0.19-py3-none-any.whl/FADiff/__init__.py
@@ -0,0 +1,7 @@
+from FADiff.FADiff import FADiff
+from FADiff.FuncVect import FuncVect
+# from FADiff.fad.Gradients import Scal
+# from FADiff.fad.Matrices import Vect
+# from FADiff.rev.Gradients import Scal
+# from FADiff.rev.Matrices import Vect
+
