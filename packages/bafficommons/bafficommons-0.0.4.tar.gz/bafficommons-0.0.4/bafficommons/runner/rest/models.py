c256a4785abc1f044cdeb772b15e5dda51d16842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:52 +0000

    Add package bafficommons/0.0.4/bafficommons-0.0.4.tar.gz

---
+++ packages/bafficommons/bafficommons-0.0.4.tar.gz/bafficommons-0.0.4/bafficommons/runner/rest/models.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+from enum import Enum
+from Typing import List, Tuple, Optional
+
+# Algorithms
+
+class AlgorithmName(str, Enum):
+    winnowing = "winnowing"
+
+class AlgorithmRestModel(BaseModel):
+    algorithm_name: AlgorithmName
+
+# Checks
+
+class OptionsRestModel(BaseModel):
+    sensitivity: float
+
+
+class CreateCheckRestModel(BaseModel):
+    check_name: str
+    algorithm_name: AlgorithmName
+    source_file_ids: List[str]
+    target_file_ids: List[str]
+    options: Optional[OptionsRestModel]
+
+
+class FileResultRestModel(BaseModel):
+    id: str
+    name: str
+    match_percentage: float
+    lines_matched: List[Tuple[int, int]]
+
+
+class CheckResultRestModel(BaseModel):
+    source_file: FileResultRestModel
+    target_file: FileResultRestModel
+
+
+class CheckRestModel(BaseModel):
+    name: str
+    matches: List[CheckResultRestModel]
+
