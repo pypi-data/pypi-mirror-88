7d83f569eba812d2fef7ee7f7c1351163901ad29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:02 +0000

    Add package redshift-query/0.1.4/redshift_query-0.1.4-py2.py3-none-any.whl

---
+++ packages/redshift-query/redshift_query-0.1.4-py2.py3-none-any.whl/redshift_query/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for redshift-query."""
+
+__author__ = """Farid Nouri Neshat"""
+__email__ = 'faridn@helecloud.com'
+__version__ = '0.1.4'
+
+from .redshift_query import set_config, query
