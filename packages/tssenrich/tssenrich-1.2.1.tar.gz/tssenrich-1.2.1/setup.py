5d302326c32c1557691db3a5711a81b4a3a045e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:39 +0000

    Add package tssenrich/1.2.1/tssenrich-1.2.1.tar.gz

---
+++ packages/tssenrich/tssenrich-1.2.1.tar.gz/tssenrich-1.2.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tssenrich',
+    version='1.2.1',
+    author='Anthony Aylward',
+    author_email='aaylward@eng.ucsd.edu',
+    description='Calculate TSS enrichment for ATAC-seq data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/anthony-aylward/tssenrich.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=['pybedtools'],
+    entry_points={
+        'console_scripts': ['tssenrich=tssenrich.tssenrich:main',]
+    },
+    include_package_data=True
+)
