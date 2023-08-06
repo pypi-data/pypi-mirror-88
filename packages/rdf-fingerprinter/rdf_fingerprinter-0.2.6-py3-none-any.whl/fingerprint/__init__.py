32cd4c49931a07f5f6bfae413d86f94c3e2d7d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:08 +0000

    Add package rdf-fingerprinter/0.2.6/rdf_fingerprinter-0.2.6-py3-none-any.whl

---
+++ packages/rdf-fingerprinter/rdf_fingerprinter-0.2.6-py3-none-any.whl/fingerprint/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# __init__.py
+# Date:  15/08/2019
+# Author: Eugeniu Costetchi
+# Email: costezki.eugen@gmail.com
+
+import logging
+
+__docformat__ = "restructuredtext en"
+
+__version__ = "0.2.6"
+__date__ = "2020-12-11"
+
+# hard coding the log level and format
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
