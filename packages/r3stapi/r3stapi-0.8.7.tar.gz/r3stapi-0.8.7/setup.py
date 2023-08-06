f37241ab5f9148fd5b56b0727caf1e2c398f62ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:19 +0000

    Add package r3stapi/0.8.7/r3stapi-0.8.7.tar.gz

---
+++ packages/r3stapi/r3stapi-0.8.7.tar.gz/r3stapi-0.8.7/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any ".
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='r3stapi',
+	version='0.8.7',
+	description='Some description.',
+	url="http://github.com/vandenberghinc/r3stapi",
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	install_requires=[
+		"fil3s",
+		"r3sponse",
+		"fir3base",
+	])
