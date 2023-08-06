ed81dbccf5bd716b35c12734c8a21f3a4ad59aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:56 +0000

    Add package tangUtils/0.0.17/tangUtils-0.0.17.tar.gz

---
+++ packages/tangUtils/tangUtils-0.0.17.tar.gz/tangUtils-0.0.17/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+see
+  https://zhuanlan.zhihu.com/p/26159930
+  https://pypi.org/project/twine/
+
+1. python setup.py sdist
+2. twine upload dist/tangUtils-0.0.17.tar.gz
+"""
+
+from setuptools import setup
+
+setup(
+    name='tangUtils',
+    version='0.0.17',
+    author='xiaomingtang',
+    author_email='1038761793@qq.com',
+    url='https://github.com/xiaomingTang/python-utils',
+    description=u'文件/目录相关utils',
+    packages=['tangUtils'],
+    install_requires=[
+      "pillow"
+    ],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
