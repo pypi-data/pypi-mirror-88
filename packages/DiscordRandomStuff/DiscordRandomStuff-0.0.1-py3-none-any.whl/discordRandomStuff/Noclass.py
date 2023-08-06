51835e032a36361333b35e35b901c23773cfb5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:15 +0000

    Add package DiscordRandomStuff/0.0.1/DiscordRandomStuff-0.0.1-py3-none-any.whl

---
+++ packages/DiscordRandomStuff/DiscordRandomStuff-0.0.1-py3-none-any.whl/discordRandomStuff/Noclass.py
@@ -0,0 +1,2 @@
+async def loaded(self):
+    print("DRS is loaded")
---
+++ packages/DiscordRandomStuff/DiscordRandomStuff-0.0.1-py3-none-any.whl/discordRandomStuff/__init__.py
@@ -0,0 +1,6 @@
+from .welcome import Welcome
+from .Noclass import loaded
+__title__ = "discordRandomStuff"
+__version__ = "0.0.3"
+__author__ = "sldless"
+__license__ = "MIT"
