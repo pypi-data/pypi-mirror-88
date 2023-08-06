b3220c9b7cc42ba7d61b83874d697d78903b76a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:25 +0000

    Add package niaaml/1.0.0rc3/NiaAML-1.0.0rc3.tar.gz

---
+++ packages/niaaml/NiaAML-1.0.0rc3.tar.gz/NiaAML-1.0.0rc3/niaaml/classifiers/utility.py
@@ -0,0 +1,37 @@
+from niaaml.utilities import Factory
+from niaaml import classifiers
+
+__all__ = [
+    'ClassifierFactory'
+]
+
+class ClassifierFactory(Factory):
+    r"""Class with string mappings to classifiers.
+    
+    Date:
+        2020
+
+    Author:
+        Luka Pečnik
+
+    License:
+        MIT
+
+    Attributes:
+        _entities (Dict[str, Classifier]): Mapping from strings to classifiers.
+
+    See Also:
+        * :class:`niaaml.utilities.Factory`
+    """
+
+    def _set_parameters(self, **kwargs):
+        r"""Set the parameters/arguments of the factory.
+        """
+        self._entities = {
+            'AdaBoost': classifiers.AdaBoost,
+            'Bagging': classifiers.Bagging,
+            'ExtremelyRandomizedTrees': classifiers.ExtremelyRandomizedTrees,
+            'LinearSVC': classifiers.LinearSVC,
+            'MultiLayerPerceptron': classifiers.MultiLayerPerceptron,
+            'RandomForest': classifiers.RandomForest
+        }
