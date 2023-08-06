81a2f08b6c758370981ddb7e69aeabbebdc61334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:19 +0000

    Add package odoo13-addon-sale-by-packaging/13.0.1.4.0/odoo13_addon_sale_by_packaging-13.0.1.4.0-py3-none-any.whl

---
+++ packages/odoo13-addon-sale-by-packaging/odoo13_addon_sale_by_packaging-13.0.1.4.0-py3-none-any.whl/odoo/addons/sale_by_packaging/models/product_packaging.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from odoo import fields, models
+
+
+class ProductPackaging(models.Model):
+    _inherit = "product.packaging"
+
+    can_be_sold = fields.Boolean(
+        related="packaging_type_id.can_be_sold", readonly=True,
+    )
+    force_sale_qty = fields.Boolean(
+        string="Force sale quantity",
+        help="Determine if during the creation of a sale order line, the "
+        "quantity should be forced with a multiple of the packaging.\n"
+        "Example:\n"
+        "You sell a product by packaging of 5 products.\n"
+        "When the user will put 3 as quantity, the system can force the "
+        "quantity to the superior unit (5 for this example).",
+    )
