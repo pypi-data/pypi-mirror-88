a85f602496ccf557aed6f2489f52f5830fcd2650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:48 +0000

    Add package miningpy/0.4.0/miningpy-0.4.0-py3-none-any.whl

---
+++ packages/miningpy/miningpy-0.4.0-py3-none-any.whl/miningpy/__init__.py
@@ -0,0 +1,14 @@
+from miningpy.core import *
+from miningpy.visualisation import *
+from miningpy.vulcan import *
+from miningpy.whittle import *
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# versioning
+with open(os.path.join(__location__, 'VERSION')) as version_file:
+    ver = version_file.read().strip()
+
+__version__ = ver
