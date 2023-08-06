1115a8e4b167334826946455c3e4db5391ad3b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:22 +0000

    Add package survival-evaluation/0.1.0/survival_evaluation-0.1.0.tar.gz

---
+++ packages/survival-evaluation/survival_evaluation-0.1.0.tar.gz/survival_evaluation-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['survival_evaluation']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.19.4,<2.0.0', 'scipy>=1.5.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'survival-evaluation',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Humza Haider',
+    'author_email': 'humza@haiderstats.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
