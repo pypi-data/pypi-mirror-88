8265d3a7e8f43f82e88a656bce77809e8d215e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:00 +0000

    Add package vpype/1.1.0/vpype-1.1.0.tar.gz

---
+++ packages/vpype/vpype-1.1.0.tar.gz/vpype-1.1.0/vpype/__init__.py
@@ -0,0 +1,11 @@
+"""This module contains vpype core and its API."""
+
+from .config import *
+from .decorators import *
+from .geometry import *
+from .io import *
+from .layers import *
+from .line_index import *
+from .model import *
+from .primitives import *
+from .utils import *
