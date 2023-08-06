392086f92e8fddd70b9bbad7e9c4416e024aea3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:01 +0000

    Add package snapflow/0.1.4/snapflow-0.1.4.tar.gz

---
+++ packages/snapflow/snapflow-0.1.4.tar.gz/snapflow-0.1.4/snapflow/project/default_project_tmpl.py
@@ -0,0 +1,13 @@
+metadata_storage = "sqlite:///.snapflow_metadata.db"  # Accepts any database url
+storages = [
+    # Add at least one storage
+    # "postgres://localhost:5432/db",
+    # "mysql://localhost:3619/db",
+    # "redshift://....",
+    # "bigquery://....",
+    # "file:///usr/....",
+    # "s3:///bucket/path",
+]
+modules = []
+# add_default_python_runtime = True
+# runtimes = []
