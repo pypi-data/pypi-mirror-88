a3e7efa58cb5e8f22d10e8a5c75ad58ae37d97fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:09 +0000

    Add package fake-bpy/0.1.0/fake_bpy-0.1.0-py3-none-any.whl

---
+++ packages/fake-bpy/fake_bpy-0.1.0-py3-none-any.whl/fake_bpy/__init__.py
@@ -0,0 +1,17 @@
+from . import context
+# from . import data
+# from . import msgbus
+# from . import ops
+from . import types
+# from . import utils
+# from . import path
+# from . import app
+# from . import props
+
+print(
+"""
+Note:
+It is not necessary that your code will not work in bpy if it doesn't work in fake-bpy.
+fake-bpy is just a module that will help check your code for missing params or incorrect method usage. 
+It does not contain the data that Blender does.
+""")
---
+++ packages/fake-bpy/fake_bpy-0.1.0-py3-none-any.whl/fake_bpy/utils.py
@@ -0,0 +1,141 @@
+class NoneType:
+    def __call__(self, *args, **kwargs):
+        return NoneType()
+    def __getattr__(self, *args, **kwargs):
+        return NoneType()
+    def __lt__(self, *args, **kwargs):
+        return NoneType()
+    def __le__(self, *args, **kwargs):
+        return NoneType()
+    def __eq__(self, *args, **kwargs):
+        return NoneType()
+    def __ne__(self, *args, **kwargs):
+        return NoneType()
+    def __gt__(self, *args, **kwargs):
+        return NoneType()
+    def __ge__(self, *args, **kwargs):
+        return NoneType()
+    def __repr__(self, *args, **kwargs):
+        return "This is a fake object. Actual bpy cannot be used outside of blender"
+    def __str__(self, *args, **kwargs):
+        return "This is a fake object. Actual bpy cannot be used outside of blender"
+    def __bytes__(self, *args, **kwargs):
+        return b"This is a fake object. Actual bpy cannot be used outside of blender"
+    def __format__(self, *args, **kwargs):
+        return NoneType()
+    def __hash__(self, *args, **kwargs):
+        return hash("This is a fake object. Actual bpy cannot be used outside of blender")
+    def __bool__(self, *args, **kwargs):
+        return True
+    def __len__(self, *args, **kwargs):
+        return 0
+    def __iter__(self, *args, **kwargs):
+        return NoneType()
+    def __reversed__(self, *args, **kwargs):
+        return NoneType()
+    def __add__(self, *args, **kwargs):
+        return NoneType()
+    def __sub__(self, *args, **kwargs):
+        return NoneType()
+    def __mul__(self, *args, **kwargs):
+        return NoneType()
+    def __matmul__(self, *args, **kwargs):
+        return NoneType()
+    def __truediv__(self, *args, **kwargs):
+        return NoneType()
+    def __floordiv__(self, *args, **kwargs):
+        return NoneType()
+    def __mod__(self, *args, **kwargs):
+        return NoneType()
+    def __divmod__(self, *args, **kwargs):
+        return NoneType()
+    def __pow__(self, *args, **kwargs):
+        return NoneType()
+    def __lshift__(self, *args, **kwargs):
+        return NoneType()
+    def __rshift__(self, *args, **kwargs):
+        return NoneType()
+    def __and__(self, *args, **kwargs):
+        return NoneType()
+    def __xor__(self, *args, **kwargs):
+        return NoneType()
+    def __or__(self, *args, **kwargs):
+        return NoneType()
+    def __radd__(self, *args, **kwargs):
+        return NoneType()
+    def __rsub__(self, *args, **kwargs):
+        return NoneType()
+    def __rmul__(self, *args, **kwargs):
+        return NoneType()
+    def __rmatmul__(self, *args, **kwargs):
+        return NoneType()
+    def __rtruediv__(self, *args, **kwargs):
+        return NoneType()
+    def __rfloordiv__(self, *args, **kwargs):
+        return NoneType()
+    def __rmod__(self, *args, **kwargs):
+        return NoneType()
+    def __rdivmod__(self, *args, **kwargs):
+        return NoneType()
+    def __rpow__(self, *args, **kwargs):
+        return NoneType()
+    def __rlshift__(self, *args, **kwargs):
+        return NoneType()
+    def __rrshift__(self, *args, **kwargs):
+        return NoneType()
+    def __rand__(self, *args, **kwargs):
+        return NoneType()
+    def __rxor__(self, *args, **kwargs):
+        return NoneType()
+    def __ror__(self, *args, **kwargs):
+        return NoneType()
+    def __iadd__(self, *args, **kwargs):
+        return NoneType()
+    def __isub__(self, *args, **kwargs):
+        return NoneType()
+    def __imul__(self, *args, **kwargs):
+        return NoneType()
+    def __imatmul__(self, *args, **kwargs):
+        return NoneType()
+    def __itruediv__(self, *args, **kwargs):
+        return NoneType()
+    def __ifloordiv__(self, *args, **kwargs):
+        return NoneType()
+    def __imod__(self, *args, **kwargs):
+        return NoneType()
+    def __ipow__(self, *args, **kwargs):
+        return NoneType()
+    def __ilshift__(self, *args, **kwargs):
+        return NoneType()
+    def __irshift__(self, *args, **kwargs):
+        return NoneType()
+    def __iand__(self, *args, **kwargs):
+        return NoneType()
+    def __ixor__(self, *args, **kwargs):
+        return NoneType()
+    def __ior__(self, *args, **kwargs):
+        return NoneType()
+    def __neg__(self, *args, **kwargs):
+        return NoneType()
+    def __pos__(self, *args, **kwargs):
+        return NoneType()
+    def __abs__(self, *args, **kwargs):
+        return NoneType()
+    def __invert__(self, *args, **kwargs):
+        return NoneType()
+    def __complex__(self, *args, **kwargs):
+        return NoneType()
+    def __int__(self, *args, **kwargs):
+        return NoneType()
+    def __float__(self, *args, **kwargs):
+        return NoneType()
+    def __index__(self, *args, **kwargs):
+        return NoneType()
+    def __round__(self, *args, **kwargs):
+        return NoneType()
+    def __trunc__(self, *args, **kwargs):
+        return NoneType()
+    def __floor__(self, *args, **kwargs):
+        return NoneType()
+    def __ceil__(self, *args, **kwargs):
+        return NoneType()
