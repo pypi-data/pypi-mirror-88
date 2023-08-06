41ab98297312ebd011c7329387f42a3d0c2666e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:01 +0000

    Add package dkpro-cassis-tools/0.0.5/dkpro_cassis_tools-0.0.5.tar.gz

---
+++ packages/dkpro-cassis-tools/dkpro_cassis_tools-0.0.5.tar.gz/dkpro_cassis_tools-0.0.5/dkpro_cassis_tools/__init__.py
@@ -0,0 +1,9 @@
+from dkpro_cassis_tools.load_cas_from_zip_file import load_cas_from_zip_file
+from dkpro_cassis_tools.dump_cas_to_zip_file import dump_cas_to_zip_file
+from dkpro_cassis_tools.create_cas_from_sentences import create_cas_from_sentences
+from dkpro_cassis_tools.restore_cas_segmentation_by_newline import restore_cas_segmentation_by_newline
+from dkpro_cassis_tools.tokenize_cas import tokenize_cas
+from dkpro_cassis_tools.ns import SENTENCE_NS, TOKEN_NS, NAMED_ENTITY_NS, DOCUMENT_METADATA
+
+
+
---
+++ packages/dkpro-cassis-tools/dkpro_cassis_tools-0.0.5.tar.gz/dkpro_cassis_tools-0.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dkpro_cassis_tools",
+    version="0.0.5",
+    author="Laurent Bié",
+    author_email="l.bie@pangeanic.com  ",
+    description="Tools for dkpro cassis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pangeamt/dkpro_cassis_tools",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'dkpro-cassis>=0.5.0',
+        'sequence_transfer>=0.1.0'
+    ],
+    tests_require=[
+        'mecab-python3>=1.0.2',
+        'unidic-lite>=1.0.7'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires='>=3.7',
+)
