b21ca7a033fb8e8ac380c62f8608bce29f8f56f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:44 +0000

    Add package simple-loggers/1.0.2/simple-loggers-1.0.2.tar.gz

---
+++ packages/simple-loggers/simple-loggers-1.0.2.tar.gz/simple-loggers-1.0.2/setup.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf8 -*-
+import os
+import json
+from setuptools import setup, find_packages
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+version_info = json.load(open(os.path.join(BASE_DIR, 'simple_loggers', 'version', 'version.json')))
+
+
+setup(
+    name='simple-loggers',
+    version=version_info['version'],
+    author=version_info['author'],
+    author_email=version_info['author_email'],
+    description=version_info['desc'],
+    long_description=open(os.path.join(BASE_DIR, 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/suqingdong/simple-loggers',
+    project_urls={
+        'Tracker': 'https://github.com/suqingdong/simple-loggers/issues',
+    },
+    license='BSD License',
+    install_requires=['coloredlogs'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
