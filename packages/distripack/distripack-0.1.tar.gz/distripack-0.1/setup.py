f1006ec0003b476e0dc2c779d9b779e5dd011e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:48 +0000

    Add package distripack/0.1/distripack-0.1.tar.gz

---
+++ packages/distripack/distripack-0.1.tar.gz/distripack-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distripack',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distripack'],
+      author='himanshu_raw',
+      author_email = 'hr8077677@gmail.com',
+      zip_safe=False)
