fba5624a158eb6666f8db07c3c04d1424c95cf06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:14 +0000

    Add package test-distributions-km/1.0/test_distributions_km-1.0.tar.gz

---
+++ packages/test-distributions-km/test_distributions_km-1.0.tar.gz/test_distributions_km-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='test_distributions_km',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['test_distributions_km'],
+      author='Kengo Matsumura',
+      zip_safe=False)
