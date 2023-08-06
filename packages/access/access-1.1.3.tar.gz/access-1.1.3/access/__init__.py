4ee8fe8354283b2afbf6fc9a39396adbddc56186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package access/1.1.3/access-1.1.3.tar.gz

---
+++ packages/access/access-1.1.3.tar.gz/access-1.1.3/access/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.1.3"
+"""
+:mod:`access` --- Accessibility Metrics
+=================================================
+"""
+
+from .access import Access
+from .datasets import Datasets
