c297dbf26803252f40c52b881b07ee4f581e8a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:03 +0000

    Add package aspaceai/1.0.12/aspaceai-1.0.12.tar.gz

---
+++ packages/aspaceai/aspaceai-1.0.12.tar.gz/aspaceai-1.0.12/aspaceai/aithemes/__init__.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: __init__.py
+Description: Python SDK of the aithemes API.
+"""
+from aspaceai.aithemes.sense import *
+from aspaceai.aithemes.conversation import *
+from aspaceai.aithemes.vision import *
---
+++ packages/aspaceai/aspaceai-1.0.12.tar.gz/aspaceai-1.0.12/aspaceai/aithemes/conversation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: conversation.py
+Description: ai solutions for each Sense.
+"""
+import aspaceai
+
+#         Theme Conversation
+class conversation:
+    def translate(text, from_lang, to_lang):
+
+        """Perform translation for a given text.
+                Args:
+                    text: Text to be translated
+
+                Returns:
+                    An array of translated text , from_lang language, to_lang language
+        """
+        return "translate"
