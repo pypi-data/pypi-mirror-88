b9f3de1c72e19e1a570f0aceb439108cc3e6d810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:25 +0000

    Add package sourceRank/1.0.9/SourceRank-1.0.9-py3-none-any.whl

---
+++ packages/sourceRank/SourceRank-1.0.9-py3-none-any.whl/sourceRank/helper/settings.py
@@ -0,0 +1,11 @@
+import coloredlogs, logging
+import warnings, os
+warnings.filterwarnings('ignore')
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger)
+
+parent_dir: str = os.getenv("PARENT_DIR") if "PARENT_DIR" in os.environ else ""
+default_field: str = "N/A"
+resources_directory: str = os.path.join(parent_dir, "sourceRank", "resources")
+resources_domains_filename: str = "resources_domains.csv"
