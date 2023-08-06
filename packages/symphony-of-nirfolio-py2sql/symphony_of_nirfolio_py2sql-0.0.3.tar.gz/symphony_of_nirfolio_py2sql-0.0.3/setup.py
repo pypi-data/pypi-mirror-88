bbd3853167027b6e36485574a7093eacc09deb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:04 +0000

    Add package symphony-of-nirfolio-py2sql/0.0.3/symphony_of_nirfolio_py2sql-0.0.3.tar.gz

---
+++ packages/symphony-of-nirfolio-py2sql/symphony_of_nirfolio_py2sql-0.0.3.tar.gz/symphony_of_nirfolio_py2sql-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name="symphony_of_nirfolio_py2sql",
+    version="0.0.3",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    url="",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
