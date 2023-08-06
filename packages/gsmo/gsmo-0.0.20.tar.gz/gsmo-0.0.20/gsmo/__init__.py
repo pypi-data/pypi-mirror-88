d701d6a0322128f919e7d703c910aea0783380e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:57 +0000

    Add package gsmo/0.0.20/gsmo-0.0.20.tar.gz

---
+++ packages/gsmo/gsmo-0.0.20.tar.gz/gsmo-0.0.20/gsmo/__init__.py
@@ -0,0 +1,10 @@
+from gsmo import control
+from .modules import Modules
+from .papermill import execute
+
+def OK(msg, throw=True):
+    exc = control.OK(msg)
+    if throw:
+        raise exc
+    else:
+        return exc
