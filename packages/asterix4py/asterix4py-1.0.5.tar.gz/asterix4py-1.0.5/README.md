ab779d54aa70c4f08f3a1164b226f0bf06e9f728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:40 +0000

    Add package asterix4py/1.0.5/asterix4py-1.0.5.tar.gz

---
+++ packages/asterix4py/asterix4py-1.0.5.tar.gz/asterix4py-1.0.5/README.md
@@ -0,0 +1,23 @@
+# asterix4py
+Pure python library for parsing/decoding EUROCONTROL ASTERIX protocol binary data.
+
+This library tries to be an easy way to use Python as Asterix decoder without the need of a big library. Focus is currently not yet on speed but to be simple and userfriendly.
+The code is based on the discontinued code from Wontor, adding missing functionalities and several bugfixes.  Some simple code examples and Asterix sample files are included for testing purposes only.
+
+This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to contribute and/or redistribute it under certain conditions.
+
+
+## Asterix
+
+ASTERIX stands for All Purpose STructured EUROCONTROL SuRveillance Information EXchange. It is an ATM Surveillance Data Binary Messaging Format which allows transmission of harmonised information between any surveillance and automation system. ASTERIX defines the structure of the data to be exchanged over a communication medium, from the encoding of every bit of information up to the organisation of the data within a block of data - without any loss of information during the whole process. More about ASTERIX protocol you can find here: http://www.eurocontrol.int/services/asterix
+
+
+## References
+
+Based on the work of:
+- Wontor - https://github.com/wontor/pyasterix
+- Damir Salantic - https://github.com/CroatiaControlLtd/asterix
+
+
+## Contributors
+- Jake Hawkins - https://github.com/drumlight
---
+++ packages/asterix4py/asterix4py-1.0.5.tar.gz/asterix4py-1.0.5/setup.py
@@ -0,0 +1,70 @@
+import pathlib
+import shutil
+
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.0.5'
+PYTHON_REQUIRES = '>=3.7'
+PACKAGE_NAME = 'asterix4py'
+AUTHOR = 'Filip Jonckers'
+AUTHOR_EMAIL = ''
+URL = 'https://github.com/filipjonckers/asterix4py'
+LICENSE = 'MIT License'
+DESCRIPTION = 'Pure python library for decoding Eurocontrol Asterix binary data'
+KEYWORDS = 'asterix radar artas eurocontrol mode-s'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+CLASSIFIERS = [
+      'Development Status :: 3 - Alpha',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Information Technology',
+      'Intended Audience :: Science/Research',
+      'Operating System :: MacOS',
+      'Operating System :: Microsoft :: Windows',
+      'Operating System :: POSIX :: Linux',
+      'License :: OSI Approved :: MIT License',
+      'Topic :: Software Development',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Scientific/Engineering',
+      'Topic :: Scientific/Engineering :: Information Analysis',
+      'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator'
+]
+
+PROJECT_URLS = {
+      'Bug Reports': 'https://github.com/filipjonckers/asterix4py/issues',
+      'Source': 'https://github.com/filipjonckers/asterix4py'
+}
+
+INSTALL_REQUIRES = [
+]
+
+try:
+    shutil.rmtree("./build")
+except(OSError):
+    pass
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      keywords=KEYWORDS,
+      classifiers=CLASSIFIERS,
+      install_requires=INSTALL_REQUIRES,
+      python_requires=PYTHON_REQUIRES,
+      project_urls=PROJECT_URLS,
+      platforms=['any'],
+      include_package_data=True,
+      zip_safe=False,
+      packages=find_packages()
+      )
