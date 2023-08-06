531077b406467bd42eca8bc254d3d0c0abde9698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:02 +0000

    Add package labtool/0.0.9/labtool-0.0.9.tar.gz

---
+++ packages/labtool/labtool-0.0.9.tar.gz/labtool-0.0.9/bin/labtool.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+import labtool.__main__
---
+++ packages/labtool/labtool-0.0.9.tar.gz/labtool-0.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="labtool",
+	version="0.0.9",
+	author="Alejandro Caravaca Puchades",
+	author_email="acaravacapuchades@gmail.com",
+	description="A tool to analyze lab report files",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/acpuchades/libtool",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Operating System :: OS Independent",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+	],
+	python_requires='>=3.6',
+	scripts=['bin/labtool.py'],
+)
