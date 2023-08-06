6f4e47c86f90e1af6c19cfe096562327517b6ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:51 +0000

    Add package actk/0.2.1/actk-0.2.1.tar.gz

---
+++ packages/actk/actk-0.2.1.tar.gz/actk-0.2.1/actk/constants.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Channels:
+    NucleusSegmentation = "nucleus_segmentation"
+    MembraneSegmentation = "membrane_segmentation"
+    DNA = "dna"
+    Membrane = "membrane"
+    Structure = "structure"
+    Brightfield = "brightfield"
+    DefaultOrderList = [
+        NucleusSegmentation,
+        MembraneSegmentation,
+        DNA,
+        Membrane,
+        Structure,
+        Brightfield,
+    ]
+
+
+class DatasetFields:
+    CellId = "CellId"
+    CellIndex = "CellIndex"
+    FOVId = "FOVId"
+    SourceReadPath = "SourceReadPath"
+    NucleusSegmentationReadPath = "NucleusSegmentationReadPath"
+    MembraneSegmentationReadPath = "MembraneSegmentationReadPath"
+    ChannelIndexDNA = "ChannelIndexDNA"
+    ChannelIndexMembrane = "ChannelIndexMembrane"
+    ChannelIndexStructure = "ChannelIndexStructure"
+    ChannelIndexBrightfield = "ChannelIndexBrightfield"
+    ChannelIndexNucleusSegmentation = "ChannelIndexNucleusSegmentation"
+    ChannelIndexMembraneSegmentation = "ChannelIndexMembraneSegmentation"
+    StandardizedFOVPath = "StandardizedFOVPath"
+    CellFeaturesPath = "CellFeaturesPath"
+    CellImage3DPath = "CellImage3DPath"
+    CellImage2DAllProjectionsPath = "CellImage2DAllProjectionsPath"
+    CellImage2DYXProjectionPath = "CellImage2DYXProjectionPath"
+    DiagnosticSheetPath = "DiagnosticSheetPath"
+    AllExpectedInputs = [
+        CellId,
+        CellIndex,
+        FOVId,
+        SourceReadPath,
+        NucleusSegmentationReadPath,
+        MembraneSegmentationReadPath,
+        ChannelIndexDNA,
+        ChannelIndexMembrane,
+        ChannelIndexStructure,
+        ChannelIndexBrightfield,
+        ChannelIndexNucleusSegmentation,
+        ChannelIndexMembraneSegmentation,
+    ]
