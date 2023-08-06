4c45d986e1a8b867d1c98302e2b4292d00ab6eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:21 +0000

    Add package django-juno/0.0.5/django-juno-0.0.5.tar.gz

---
+++ packages/django-juno/django-juno-0.0.5.tar.gz/django-juno-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django-juno',
+    version='0.0.5',
+    author='Alessandra Carneiro',
+    author_email='alessandra@rd2.ventures',
+    description='Integração da Juno com Django',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alessandrak/django-juno',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Django :: 3.1',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
