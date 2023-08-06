a57d055963a806f73a44927e9df3f9dc9c86bc7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:21 +0000

    Add package stripe3/0.0.0/stripe3-0.0.0.tar.gz

---
+++ packages/stripe3/stripe3-0.0.0.tar.gz/stripe3-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="stripe3",
+    version="0.0.0",
+    packages=[
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+    zip_safe=False
+)
