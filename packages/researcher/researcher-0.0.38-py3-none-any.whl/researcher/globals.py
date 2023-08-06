bdcd345f8cd0f09108d88a985453ed32b8702676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:04 +0000

    Add package researcher/0.0.38/researcher-0.0.38-py3-none-any.whl

---
+++ packages/researcher/researcher-0.0.38-py3-none-any.whl/researcher/globals.py
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+OBSERVATIONS_NAME = "observations"
+=======
+"""Contains global constants and nothing else.
+"""
+
+RESULTS_NAME = "results"
+FOLD_RESULTS_NAME = "fold_results"
+GENERAL_RESULTS_NAME = "general_results"
+>>>>>>> 2b089458a9757466dd0d4fe19e74be41a7b045eb
+DATE_FORMAT = "%Y-%m-%d_%H:%M:%S"
