0bfaa5fdaf35c5b2876f4775bf0946282b1755cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:52 +0000

    Add package wwc129/0.0.5/wwc129-0.0.5.tar.gz

---
+++ packages/wwc129/wwc129-0.0.5.tar.gz/wwc129-0.0.5/myproject/main.py
@@ -0,0 +1,7 @@
+import sys 
+from .utils.utils import add
+
+
+def main():
+    res = add(1, 2)
+    print("result is" , res)
---
+++ packages/wwc129/wwc129-0.0.5.tar.gz/wwc129-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="wwc129",
+    version="0.0.5",
+    author='wwc129',
+    author_email='wwc129@gmail.com',
+    description='test',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'wwc129=myproject.main:main'
+        ]
+    }
+)
+
