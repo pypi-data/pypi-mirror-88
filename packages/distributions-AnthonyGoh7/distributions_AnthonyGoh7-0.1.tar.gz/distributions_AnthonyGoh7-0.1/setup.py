11dab5f41a631bd0be5312c05de45ec4bc435513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:59 +0000

    Add package distributions-AnthonyGoh7/0.1/distributions_AnthonyGoh7-0.1.tar.gz

---
+++ packages/distributions-AnthonyGoh7/distributions_AnthonyGoh7-0.1.tar.gz/distributions_AnthonyGoh7-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_AnthonyGoh7',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_AnthonyGoh7'],
+      zip_safe=False)
