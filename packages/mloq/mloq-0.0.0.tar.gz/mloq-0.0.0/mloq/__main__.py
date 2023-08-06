c9580029e34e5127f4d533d191d51d0102fca8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:46 +0000

    Add package mloq/0.0.0/mloq-0.0.0.tar.gz

---
+++ packages/mloq/mloq-0.0.0.tar.gz/mloq-0.0.0/mloq/__main__.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+import sys
+
+from mloq.api import init_repository
+
+
+def main():
+    """Initialize the target repo."""
+    root_path = Path(os.getcwd())
+    init_repository(root_path, override=False)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
