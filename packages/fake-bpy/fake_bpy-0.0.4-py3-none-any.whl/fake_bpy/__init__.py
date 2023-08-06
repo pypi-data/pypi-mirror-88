cf4c8b48c52874928020fbe5ac88f842b5cbf932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:45 +0000

    Add package fake-bpy/0.0.4/fake_bpy-0.0.4-py3-none-any.whl

---
+++ packages/fake-bpy/fake_bpy-0.0.4-py3-none-any.whl/fake_bpy/__init__.py
@@ -0,0 +1,9 @@
+from . import context
+# from . import data
+# from . import msgbus
+# from . import ops
+# from . import types
+# from . import utils
+# from . import path
+# from . import app
+# from . import props
