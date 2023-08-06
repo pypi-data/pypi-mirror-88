73ed50437a6e39578751df8944f35a8625807988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:30 +0000

    Add package invenio-vocabularies/0.1.2/invenio_vocabularies-0.1.2-py2.py3-none-any.whl

---
+++ packages/invenio-vocabularies/invenio_vocabularies-0.1.2-py2.py3-none-any.whl/invenio_vocabularies/records/dumper_extensions.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2020 CERN.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Vocabulary dumper extension."""
+
+from invenio_records.dumpers import ElasticsearchDumperExt
+
+from .models import VocabularyType
+
+
+class VocabularyTypeElasticsearchDumperExt(ElasticsearchDumperExt):
+    """Elasticsearch vocabulary dumper."""
+
+    def dump(self, record, data):
+        """Dump the data from a record."""
+        vocabulary_type_id = data.get("vocabulary_type_id")
+        if vocabulary_type_id:
+            data["vocabulary_type"] = VocabularyType.query.get(
+                vocabulary_type_id
+            ).name
+
+    def load(self, data, record_cls):
+        """Load the data into a record."""
+        data.pop("vocabulary_type", None)
