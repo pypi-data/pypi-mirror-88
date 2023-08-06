95b5d81b3f1cc66168bce7a92b9b4ab081c90035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:44 +0000

    Add package instagrapi/1.2.9/instagrapi-1.2.9.tar.gz

---
+++ packages/instagrapi/instagrapi-1.2.9.tar.gz/instagrapi-1.2.9/instagrapi/__init__.py
@@ -0,0 +1,74 @@
+import logging
+from urllib.parse import urlparse
+
+from .auth import Login
+from .public import (
+    PublicRequest,
+    TopSearchesPublic,
+    ProfilePublic,
+)
+from .private import PrivateRequest
+from .challenge import ChallengeResolve
+from .photo import DownloadPhoto, UploadPhoto
+from .video import DownloadVideo, UploadVideo
+from .album import DownloadAlbum, UploadAlbum
+from .igtv import DownloadIGTV, UploadIGTV
+from .media import Media
+from .user import User
+from .insights import Insights
+from .collection import Collection
+from .account import Account
+from .direct import Direct
+from .location import LocationMixin
+from .hashtag import HashtagMixin
+
+
+class Client(
+    PublicRequest,
+    ChallengeResolve,
+    PrivateRequest,
+    TopSearchesPublic,
+    ProfilePublic,
+    Login,
+    DownloadPhoto,
+    UploadPhoto,
+    DownloadVideo,
+    UploadVideo,
+    DownloadAlbum,
+    UploadAlbum,
+    DownloadIGTV,
+    UploadIGTV,
+    Media,
+    User,
+    Insights,
+    Collection,
+    Account,
+    Direct,
+    LocationMixin,
+    HashtagMixin
+):
+    proxy = None
+    logger = logging.getLogger("instagrapi")
+
+    def __init__(self, settings: dict = {}, proxy: str = None, **kwargs):
+        super().__init__(**kwargs)
+        self.settings = settings
+        self.set_proxy(proxy)
+        self.init()
+
+    def set_proxy(self, dsn: str):
+        if dsn:
+            assert isinstance(dsn, str),\
+                f'Proxy must been string (URL), but now "{dsn}" ({type(dsn)})'
+            self.proxy = dsn
+            proxy_href = "{scheme}{href}".format(
+                scheme="http://" if not urlparse(self.proxy).scheme else "",
+                href=self.proxy,
+            )
+            self.public.proxies = self.private.proxies = {
+                "http": proxy_href,
+                "https": proxy_href,
+            }
+            return True
+        self.public.proxies = self.private.proxies = {}
+        return False
---
+++ packages/instagrapi/instagrapi-1.2.9.tar.gz/instagrapi-1.2.9/instagrapi/types.py
@@ -0,0 +1,195 @@
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel, HttpUrl, FilePath
+
+
+class Resource(BaseModel):
+    pk: int
+    video_url: Optional[HttpUrl]  # for Video and IGTV
+    thumbnail_url: HttpUrl
+    media_type: int
+
+
+class User(BaseModel):
+    pk: int
+    username: str
+    full_name: str
+    is_private: bool
+    profile_pic_url: HttpUrl
+    is_verified: bool
+    media_count: int
+    follower_count: int
+    following_count: int
+    biography: Optional[str] = ''
+    external_url: Optional[HttpUrl]
+    is_business: bool
+
+
+class Account(BaseModel):
+    pk: int
+    username: str
+    full_name: str
+    is_private: bool
+    profile_pic_url: HttpUrl
+    is_verified: bool
+    biography: Optional[str] = ''
+    external_url: Optional[HttpUrl]
+    is_business: bool
+    birthday: Optional[str]
+    phone_number: Optional[str]
+    gender: Optional[int]
+    email: Optional[str]
+
+
+class UserShort(BaseModel):
+    pk: int
+    username: Optional[str]
+    full_name: Optional[str] = ''
+    profile_pic_url: Optional[HttpUrl]
+    # is_private: bool
+    # is_verified: bool
+
+
+class Usertag(BaseModel):
+    user: UserShort
+    x: float
+    y: float
+
+
+class Location(BaseModel):
+    pk: Optional[int]
+    name: str
+    address: Optional[str] = ''
+    lng: Optional[float]
+    lat: Optional[float]
+    external_id: Optional[int]
+    external_id_source: Optional[str]
+    # address_json: Optional[dict] = {}
+    # profile_pic_url: Optional[HttpUrl]
+    # directory: Optional[dict] = {}
+
+
+class Media(BaseModel):
+    pk: int
+    id: str
+    code: str
+    taken_at: datetime
+    media_type: int
+    product_type: Optional[str] = ''  # only for IGTV
+    thumbnail_url: Optional[HttpUrl]
+    location: Optional[Location] = None
+    user: UserShort
+    comment_count: int
+    like_count: int
+    caption_text: str
+    usertags: List[Usertag]
+    video_url: Optional[HttpUrl]  # for Video and IGTV
+    view_count: Optional[int] = 0  # for Video and IGTV
+    video_duration: Optional[float] = 0.0  # for Video and IGTV
+    title: Optional[str] = ''
+    resources: List[Resource] = []
+
+
+class MediaOembed(BaseModel):
+    title: str
+    author_name: str
+    author_url: str
+    author_id: int
+    media_id: str
+    provider_name: str
+    provider_url: HttpUrl
+    type: str
+    width: Optional[int] = None
+    height: Optional[int] = None
+    html: str
+    thumbnail_url: HttpUrl
+    thumbnail_width: int
+    thumbnail_height: int
+    can_view: bool
+
+
+class Collection(BaseModel):
+    id: str
+    name: str
+    type: str
+    media_count: int
+
+
+class Comment(BaseModel):
+    pk: int
+    text: str
+    user: UserShort
+    created_at_utc: datetime
+    content_type: str
+    status: str
+
+
+class StoryMention(BaseModel):
+    user: UserShort
+    x: Optional[float]
+    y: Optional[float]
+    width: Optional[float]
+    height: Optional[float]
+
+
+class StoryBuild(BaseModel):
+    mentions: List[StoryMention]
+    path: FilePath
+
+
+class StoryLink(BaseModel):
+    webUri: HttpUrl
+
+
+class DirectMessage(BaseModel):
+    id: int  # e.g. 28597946203914980615241927545176064
+    user_id: Optional[int]
+    thread_id: Optional[int]
+    timestamp: datetime
+    item_type: Optional[str]
+    is_shh_mode: Optional[bool]
+    reactions: Optional[dict]
+    text: Optional[str]
+    media_share: Optional[Media]
+    reel_share: Optional[dict]
+    story_share: Optional[dict]
+    felix_share: Optional[dict]
+    placeholder: Optional[dict]
+
+
+class DirectThread(BaseModel):
+    pk: int  # thread_v2_id, e.g. 17898572618026348
+    id: int  # thread_id, e.g. 340282366841510300949128268610842297468
+    messages: List[DirectMessage]
+    users: List[UserShort]
+    inviter: UserShort
+    left_users: List[UserShort]
+    admin_user_ids: list
+    last_activity_at: datetime
+    muted: bool
+    is_pin: bool
+    named: bool
+    canonical: bool
+    pending: bool
+    archived: bool
+    thread_type: str
+    thread_title: str
+    folder: int
+    vc_muted: bool
+    is_group: bool
+    mentions_muted: bool
+    approval_required_for_new_members: bool
+    input_mode: int
+    business_thread_folder: int
+    read_state: int
+    is_close_friend_thread: bool
+    assigned_admin_id: int
+    shh_mode_enabled: bool
+    last_seen_at: dict
+
+
+class Hashtag(BaseModel):
+    id: int
+    name: str
+    media_count: Optional[int]
+    profile_pic_url: Optional[HttpUrl]
