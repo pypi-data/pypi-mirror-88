13e8170536a79fc6b0b6eaa6c4fe6a59665d4d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:17 +0000

    Add package falcano/0.0.7/falcano-0.0.7-py3-none-any.whl

---
+++ packages/falcano/falcano-0.0.7-py3-none-any.whl/falcano/exceptions.py
@@ -0,0 +1,49 @@
+from typing import Any, Optional
+
+import botocore.exceptions
+
+
+class FalcanoException(Exception):
+    '''
+    A common exception class
+    '''
+
+    def __init__(self, msg: Optional[str] = None, cause: Optional[Exception] = None) -> None:
+        self.msg = msg
+        self.cause = cause
+        super(FalcanoException, self).__init__(self.msg)
+
+    @property
+    def cause_response_code(self) -> Optional[str]:
+        return getattr(self.cause, 'response', {}).get('Error', {}).get('Code')
+
+    @property
+    def cause_response_message(self) -> Optional[str]:
+        return getattr(self.cause, 'response', {}).get('Error', {}).get('Message')
+
+
+class TableDoesNotExist(FalcanoException):
+    '''
+    Raised when an operation is attempted on a table that doesn't exist
+    '''
+
+    def __init__(self, table_name: str) -> None:
+        msg = f"Table does not exist: `{table_name}`"
+        super(TableDoesNotExist, self).__init__(msg)
+
+
+class DoesNotExist(FalcanoException):
+    '''
+    Raised when an item queried does not exist
+    '''
+
+    def __init__(self) -> None:
+        msg = 'Item does not exist'
+        super(DoesNotExist, self).__init__(msg)
+
+
+class InvalidStateError(FalcanoException):
+    '''
+    Raises when the internal state of an operation context is invalid
+    '''
+    msg = 'Operation in invalid state'
