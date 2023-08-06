1855c7492768c1b8620e66fe27a1107aff24593a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:54 +0000

    Add package infrastructure/2.7.1/infrastructure-2.7.1-py3.7.egg

---
+++ packages/infrastructure/infrastructure-2.7.1-py3.7.egg/infrastructure/base/__init__.py
@@ -0,0 +1,6 @@
+_# -*- coding: utf-8 -*-
+# @Author: yongfanmao
+# @Date:   2020-06-19 14:12:22
+# @E-mail: maoyongfan@163.com
+# @Last Modified by:   yongfanmao
+# @Last Modified time: 2020-12-11 16:43:22
