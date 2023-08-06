c8325df36fee3f2a76a8f089e90023fc7a45a3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:52 +0000

    Add package cgkit1/1.2.6/cgkit1-1.2.6-cp27-cp27m-win_amd64.whl

---
+++ packages/cgkit1/cgkit1-1.2.6-cp27-cp27m-win_amd64.whl/cgkitinfo.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+######################################################################
+# cgkitinfo
+#
+# Copyright (C) 2004, Matthias Baas (baas@ira.uka.de)
+#
+# http://cgkit.sourceforge.net
+#
+# You may distribute under the terms of the BSD license, as
+# specified in the file license.txt.
+####################################################################
+
+# also update setup.py
+version_info = (1, 2, 6, "final")
+version = "1.2.6"
---
+++ packages/cgkit1/cgkit1-1.2.6-cp27-cp27m-win_amd64.whl/pycgtypes/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+# Initialization
+
+# Import types into the cgtypes namespace
+from .vec3 import vec3
+from .vec4 import vec4
+from .mat3 import mat3
+from .mat4 import mat4
+from .quat import quat
+
+# CGKit version
+# Applies to the whole kit and is a cheap way for applications to check
+# which version is installed
+_CGKit_version = "1.2.0"
