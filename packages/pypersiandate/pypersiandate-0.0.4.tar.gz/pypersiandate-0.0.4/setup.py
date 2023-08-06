2b95c9533d1357cda30235ed79063811fc59cd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:24 +0000

    Add package pypersiandate/0.0.4/pypersiandate-0.0.4.tar.gz

---
+++ packages/pypersiandate/pypersiandate-0.0.4.tar.gz/pypersiandate-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pypersiandate',
+    version='0.0.4',
+    description='Python library for converting gregorian date to persian date.',
+    author='Hamed Ghasemi',
+    author_email='hamed.ghasemi.gl@gmail.com',
+    url='https://github.com/Hamed-Ghasemi/pypersiandate',
+    license='MIT',
+    packages=['pypersiandate'],
+)
