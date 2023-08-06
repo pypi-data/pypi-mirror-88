6f965b513c4742bdbff3da5a71036d1275838d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:45 +0000

    Add package py-shotgun/0.0.1/py_shotgun-0.0.1.tar.gz

---
+++ packages/py-shotgun/py_shotgun-0.0.1.tar.gz/py_shotgun-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_shotgun", # Replace with your own username
+    version="0.0.1",
+    author="Noflame Lin",
+    author_email="linjuang@gmail.com",
+    description="simple shotgun api wrap",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/MoonShineVFX/py_shotgun",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
