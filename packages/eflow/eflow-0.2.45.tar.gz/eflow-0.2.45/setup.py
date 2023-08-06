d14b7eb5d159db8f0d28d356d7f669990f8c48c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:56 +0000

    Add package eflow/0.2.45/eflow-0.2.45.tar.gz

---
+++ packages/eflow/eflow-0.2.45.tar.gz/eflow-0.2.45/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='eflow',
+     version='0.2.45',
+     install_requires=['numpy',
+                       'matplotlib',
+                       'scipy',
+                       'seaborn',
+                       'jupyterlab',
+                       'pandas',
+                       'missingno',
+                       'sklearn',
+                       'kneed',
+                       'tqdm',
+                       'Pillow',
+                       'python-dateutil',
+                       'scikit-plot',
+                       'ipywidgets',
+                       'nltk',
+                       'pyclustering',
+                       'plotly'],
+     scripts=['init_env'],
+     author="Eric Cacciavillani",
+     author_email="eric.cacciavillani@gmail.com",
+     description="",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/EricCacciavillani/eFlow",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3.7",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
