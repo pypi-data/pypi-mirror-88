f2d1a85c8700fb02c577f75a77f1991d08947fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:18 +0000

    Add package qobuz-dl/0.5.4.2/qobuz_dl-0.5.4.2-py3-none-any.whl

---
+++ packages/qobuz-dl/qobuz_dl-0.5.4.2-py3-none-any.whl/qobuz_dl/exceptions.py
@@ -0,0 +1,14 @@
+class AuthenticationError(Exception):
+    pass
+
+
+class IneligibleError(Exception):
+    pass
+
+
+class InvalidAppIdError(Exception):
+    pass
+
+
+class InvalidAppSecretError(Exception):
+    pass
