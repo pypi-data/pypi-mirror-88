8db3abb5857a8b50933af2f7d56da9db4c9bf509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:24 +0000

    Add package crawlertool/0.1.2/crawlertool-0.1.2.tar.gz

---
+++ packages/crawlertool/crawlertool-0.1.2.tar.gz/crawlertool-0.1.2/crawlertool/_abc/__init__.py
@@ -0,0 +1,7 @@
+"""
+BaseSpider(object) 基础爬虫抽象类
+
+SingleSpider(BaseSpider) 简单单线程爬虫抽象类
+"""
+
+from .single_spider import SingleSpider
---
+++ packages/crawlertool/crawlertool-0.1.2.tar.gz/crawlertool-0.1.2/crawlertool/_abc/single_spider.py
@@ -0,0 +1,12 @@
+from abc import ABCMeta
+from abc import abstractmethod
+
+from .base_spider import BaseSpider
+
+
+class SingleSpider(BaseSpider, metaclass=ABCMeta):
+    """简单单线程爬虫的抽象基类"""
+
+    @abstractmethod
+    def run(self):
+        """执行爬虫"""
