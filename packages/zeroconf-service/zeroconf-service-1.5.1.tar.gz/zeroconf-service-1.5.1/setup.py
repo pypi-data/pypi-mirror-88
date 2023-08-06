2b45fe2315ab98afea9af34e8071bf49602490a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:07 +0000

    Add package zeroconf-service/1.5.1/zeroconf-service-1.5.1.tar.gz

---
+++ packages/zeroconf-service/zeroconf-service-1.5.1.tar.gz/zeroconf-service-1.5.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zeroconf_service']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['compress-pickle[lz4]>=1.2.0,<2.0.0',
+ 'logzero>=1.6.3,<2.0.0',
+ 'websockets>=8.1,<9.0',
+ 'zeroconf>=0.28.6,<0.29.0']
+
+setup_kwargs = {
+    'name': 'zeroconf-service',
+    'version': '1.5.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Tim Wedde',
+    'author_email': 'timwedde@icloud.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
