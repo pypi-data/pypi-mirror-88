8188690fe94392977fda84feb3c18da3f981b9a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:43 +0000

    Add package deepnog/1.2.2/deepnog-1.2.2-py3-none-win_amd64.whl

---
+++ packages/deepnog/deepnog-1.2.2-py3-none-win_amd64.whl/deepnog/models/__init__.py
@@ -0,0 +1,13 @@
+"""
+Description:
+
+    Network definitions (PyTorch modules)
+"""
+from ..utils.imports import try_import_pytorch
+
+__all__ = ['deepencoding',
+           'deepfam',
+           'deepnog',
+           ]
+
+try_import_pytorch()
