2ba6c548d695cc457ddcd102f5c90326dac76b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package sdata/0.10.2/sdata-0.10.2.tar.gz

---
+++ packages/sdata/sdata-0.10.2.tar.gz/sdata-0.10.2/sdata/experiments/__init__.py
@@ -0,0 +1,10 @@
+import sdata.experiments.ks2
+import sdata.experiments.lapshear
+import sdata.experiments.material
+
+# ["name", "default_value", "dtype", "unit", "description", "required"]
+part_default_attributes = [
+    ["part_id", None, "str", "-", "Part ID", True],
+]
+
+__all__ = []
