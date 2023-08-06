4859481e0117aa3bf27a8aaf59b6a514f026fd20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:46 +0000

    Add package varg/1.6.11/varg-1.6.11.tar.gz

---
+++ packages/varg/varg-1.6.11.tar.gz/varg-1.6.11/varg/cli/root.py
@@ -0,0 +1,25 @@
+""" varg root command """
+import logging
+
+import click
+import coloredlogs
+
+from varg import __version__
+from .compare import compare as compare_command
+
+
+LOG_LEVELS = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+LOG = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option("--loglevel", default="INFO", type=click.Choice(LOG_LEVELS))
+@click.version_option(__version__)
+@click.pass_context
+def root_command(context, loglevel):
+    """Variants-Validation-Report-Generator base command"""
+    coloredlogs.install(level=loglevel)
+    LOG.info("Running varg")
+
+
+root_command.add_command(compare_command)
