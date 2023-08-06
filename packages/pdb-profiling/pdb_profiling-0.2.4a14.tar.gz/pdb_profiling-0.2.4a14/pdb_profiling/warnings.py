1be3b6545fd327978135f64805471c3fb840d398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:33 +0000

    Add package pdb-profiling/0.2.4a14/pdb_profiling-0.2.4a14.tar.gz

---
+++ packages/pdb-profiling/pdb_profiling-0.2.4a14.tar.gz/pdb_profiling-0.2.4a14/pdb_profiling/warnings.py
@@ -0,0 +1,41 @@
+# @Created Date: 2020-10-26 01:00:07 pm
+# @Filename: warnings.py
+# @Email:  1730416009@stu.suda.edu.cn
+# @Author: ZeFeng Zhu
+# @Last Modified: 2020-10-26 01:00:42 pm
+# @Copyright (c) 2020 MinghuiGroup, Soochow University
+
+
+class MultiWrittenWarning(UserWarning):
+    pass
+
+
+class WithoutCifKeyWarning(UserWarning):
+    pass
+
+class PISAErrorWarning(UserWarning):
+    pass
+
+
+class MultipleConformersWarning(UserWarning):
+    pass
+
+
+class ConflictChainIDWarning(UserWarning):
+    pass
+
+
+class PossibleObsoletedUniProtWarning(UserWarning):
+    pass
+
+
+class PossibleObsoletedPDBEntryWarning(UserWarning):
+    pass
+
+
+class SkipAssemblyWarning(UserWarning):
+    pass
+
+
+class PeptideLinkingWarning(UserWarning):
+    pass
