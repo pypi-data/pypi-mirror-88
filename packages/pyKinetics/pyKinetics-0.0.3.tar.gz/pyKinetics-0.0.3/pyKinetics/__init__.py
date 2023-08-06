1ea73f6104138735f6a7e284a92f09f2560b8afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:27 +0000

    Add package pyKinetics/0.0.3/pyKinetics-0.0.3.tar.gz

---
+++ packages/pyKinetics/pyKinetics-0.0.3.tar.gz/pyKinetics-0.0.3/pyKinetics/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 15:28:57 2020
+
+@author: Rui Campos
+"""
+
+from pyKinetics.pyKinetics import Compartimento
+from pyKinetics.pyKinetics import Model
