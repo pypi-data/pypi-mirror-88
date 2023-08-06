a1d5f76ec2a1acbb4d63408e9d3cddf79e994798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:59 +0000

    Add package envoy-reader/0.17.0rc0/envoy_reader-0.17.0rc0.tar.gz

---
+++ packages/envoy-reader/envoy_reader-0.17.0rc0.tar.gz/envoy_reader-0.17.0rc0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="envoy_reader",
+    version="0.17.0c",
+    author="Jesse Rizzo",
+    author_email="jesse.rizzo@gmail.com",
+    description="A program to read from an Enphase Envoy on the local network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jesserizzo/envoy_reader",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "httpx >= 0.12.1",
+        "requests_async >= 0.6.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
