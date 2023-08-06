66b4ac47e1ddd4b7069a3cc085e42c4cf9e3ec17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:05 +0000

    Add package pomodoro-DEN-ik/0.0.1/pomodoro-DEN_ik-0.0.1.tar.gz

---
+++ packages/pomodoro-DEN-ik/pomodoro-DEN_ik-0.0.1.tar.gz/pomodoro-DEN_ik-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pomodoro-DEN_ik",
+    version="0.0.1",
+    author="Denys Zaluzhnyi",
+    author_email="denys.zaluzhnyi@gmail.com",
+    description="Simple concentrator based on pomodoro technique",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DarkDream99/WorkConcentrator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Topic :: Home Automation",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points="""
+        [console_scripts]
+        concentrator=work_concentrator.work_concentrator:run
+    """,
+)
