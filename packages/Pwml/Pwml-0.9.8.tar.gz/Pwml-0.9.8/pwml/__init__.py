d98c4209c6768c78dcc7dc96f4bad564c100e5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:29 +0000

    Add package Pwml/0.9.8/Pwml-0.9.8.tar.gz

---
+++ packages/Pwml/Pwml-0.9.8.tar.gz/Pwml-0.9.8/pwml/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from .classifiers import embedders
+from .classifiers import features
+from .classifiers import hierarchical
+
+from .utilities import filehelpers
+from .utilities import httphelpers
+from .utilities import imagehelpers
+from .utilities import mssqlhelpers
+from .utilities import classificationhelpers
+from .utilities import commonhelpers
+
+__version__ = '0.9.8'
