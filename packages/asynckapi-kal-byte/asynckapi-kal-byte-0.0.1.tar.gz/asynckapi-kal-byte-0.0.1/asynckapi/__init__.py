5330550b26a1dc24d860268563c465fd4a3aaa4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:18 +0000

    Add package asynckapi-kal-byte/0.0.1/asynckapi-kal-byte-0.0.1.tar.gz

---
+++ packages/asynckapi-kal-byte/asynckapi-kal-byte-0.0.1.tar.gz/asynckapi-kal-byte-0.0.1/asynckapi/__init__.py
@@ -0,0 +1,21 @@
+import aiohttp
+
+class Client:
+    def __init__(self):
+        self.session = aiohttp.ClientSession()
+        self.base_url = 'https://www.kal-byte.co.uk/api/'
+
+    async def password(self, length: int = None) -> str:
+        if length is None:
+            raise AttributeError('You must provide a length argument.')
+
+        extension = "password_generator"
+        argument = "?length={}".format(length)
+        async with self.session.get(self.base_url + extension + argument) as response:
+            data = await response.json()
+            password = data['password']
+
+        return password
+
+    async def close(self):
+        await self.session.close()
