4126c2755991d527e0e838232a67e7eb6a5d2414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:08 +0000

    Add package smars-library/0.3.7/smars_library-0.3.7.tar.gz

---
+++ packages/smars-library/smars_library-0.3.7.tar.gz/smars_library-0.3.7/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup
+
+# The directory contaoining this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="smars_library",
+    version="0.3.7",
+    description="SMARS Robot Python Library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kevinmcaleer/smars_library",
+    author="Kevin McAleer",
+    author_email="kevinmcaleer@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["smars_library"],
+    include_package_data=False,
+    install_requires=["adafruit-pca9685","pathlib"],
+    )
