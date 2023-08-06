e6bcf3cc6aa91b82e07e496920d7f00d80f57dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:14 +0000

    Add package bigjson/1.0.6/bigjson-1.0.6.tar.gz

---
+++ packages/bigjson/bigjson-1.0.6.tar.gz/bigjson-1.0.6/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+setup(
+    name='bigjson',
+    version='1.0.6',
+    packages=['bigjson'],
+    description='Python library that reads JSON files of any size.',
+    author='Henrik Heino',
+    author_email='henrik.heino@gmail.com',
+    url='https://henu.fi/bigjson',
+    license='MIT',
+)
