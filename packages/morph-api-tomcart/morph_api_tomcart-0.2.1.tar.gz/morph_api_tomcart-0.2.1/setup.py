2dbff984114bb2e12f0fc41627b054c0b1f373e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:56 +0000

    Add package morph-api-tomcart/0.2.1/morph_api_tomcart-0.2.1.tar.gz

---
+++ packages/morph-api-tomcart/morph_api_tomcart-0.2.1.tar.gz/morph_api_tomcart-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='morph_api_tomcart',
+    version='0.2.1',
+    packages=['morph_api_tomcart'],
+    url='https://github.com/BeepBoopRobit',
+    license='',
+    install_requires=[
+        'requests',
+        'isodate'
+    ],
+    author='Ricky Nelson, Michael Escobar',
+    author_email='rnelson@thetomcart.com, miescobar@deloitte.com',
+    description='Morpheus API Functions'
+)
