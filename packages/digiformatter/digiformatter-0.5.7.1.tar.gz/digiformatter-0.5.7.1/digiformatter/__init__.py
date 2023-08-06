36faa7a3d5d0bd96fe0bafdf377672ea74b5533a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:52 +0000

    Add package digiformatter/0.5.7.1/digiformatter-0.5.7.1.tar.gz

---
+++ packages/digiformatter/digiformatter-0.5.7.1.tar.gz/digiformatter-0.5.7.1/digiformatter/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.5.7.1"
+
+__all__ = ["cursorUp", "cursorDown", "cursorRight", "cursorLeft", "scrollUp", "scrollDown", "setWindowTitle", "overwriteLines", "createLoadBar", "truncate", "goto", "styles"]
+
+from digiformatter.formatter import cursorUp, cursorDown, cursorRight, cursorLeft, scrollUp, scrollDown, setWindowTitle, overwriteLines, createLoadBar, truncate, goto
+from digiformatter.styles import styles
