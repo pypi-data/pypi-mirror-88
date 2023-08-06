31fbe70b2c7d1683187c864d06a4062f55288f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:57 +0000

    Add package octomy-batch/0.0.8/octomy-batch-0.0.8.tar.gz

---
+++ packages/octomy-batch/octomy-batch-0.0.8.tar.gz/octomy-batch-0.0.8/fk/batch/Server.py
@@ -0,0 +1,13 @@
+from fk.batch.BatchProcessor import BatchProcessor
+
+
+class Server:
+    def __init__(self, config):
+        self.config = config
+        self.batch_processor = BatchProcessor(self.config)
+
+    # Start server and serve until it is aborted
+    def run(self):
+        while True:
+            # Do some work
+            self.batch_processor.process()
