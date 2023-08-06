ffc677b40ab3ad04b406190019d4087e49514cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:00 +0000

    Add package donald/0.3.1/Donald-0.3.1.tar.gz

---
+++ packages/donald/Donald-0.3.1.tar.gz/Donald-0.3.1/donald/__main__.py
@@ -0,0 +1,37 @@
+"""CLI Support."""
+
+import asyncio
+from importlib import import_module
+
+import click
+
+from donald import Donald
+
+
+async def run_donald(donald, listen=None, module=None):
+    """Start donald and import dependencies."""
+    if module:
+        try:
+            click.echo('Import module: %s' % module)
+            import_module(module)
+        except ImportError:
+            click.echo('Invalid module: %s' % module)
+    await donald.start()
+
+
+@click.command()
+@click.option('--listen', help="Listen the given AMPQ URL.")
+@click.option('--workers', default=4, help="Number of workers.")
+@click.option('--loglevel', default='INFO', help="Logging level.")
+@click.option('--module', help="Load module.")
+def main(workers=4, loglevel='info', **kw):
+    """Run Donald server."""
+    donald = Donald(num_workers=workers, loglevel=loglevel)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_donald(donald, **kw))
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
