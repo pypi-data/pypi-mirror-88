d3b207189e16099eba12b23fc75ea4c5ceb142f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package threedi-ws-client/0.0.3/threedi-ws-client-0.0.3.tar.gz

---
+++ packages/threedi-ws-client/threedi-ws-client-0.0.3.tar.gz/threedi-ws-client-0.0.3/tests/test_threedi_ws_client.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `threedi_ws_client` package."""
+
+
+import unittest
+
+from threedi_ws_client import threedi_ws_client
+
+
+class TestThreedi_ws_client(unittest.TestCase):
+    """Tests for `threedi_ws_client` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
