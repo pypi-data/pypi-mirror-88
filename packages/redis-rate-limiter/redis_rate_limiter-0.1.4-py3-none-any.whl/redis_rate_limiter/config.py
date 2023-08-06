51c60f47cb6bf09fa1820bf37deffb949d6709c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:25 +0000

    Add package redis-rate-limiter/0.1.4/redis_rate_limiter-0.1.4-py3-none-any.whl

---
+++ packages/redis-rate-limiter/redis_rate_limiter-0.1.4-py3-none-any.whl/redis_rate_limiter/config.py
@@ -0,0 +1,18 @@
+import socket
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Settings:
+    redis_url: str = ""
+    key_prefix: str = field(default_factory=socket.gethostname)
+
+
+settings = Settings()
+
+
+def basic_config(redis_url: str = "", key_prefix: str = ""):
+    if redis_url:
+        settings.redis_url = redis_url
+    if key_prefix:
+        settings.key_prefix = key_prefix
