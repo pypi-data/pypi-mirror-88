f545e5c1fe27a931c93e4e868f43f824fa6442c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:24 +0000

    Add package hgitaly/0.7.0/hgitaly-0.7.0.tar.gz

---
+++ packages/hgitaly/hgitaly-0.7.0.tar.gz/hgitaly-0.7.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('install-requirements.txt', 'r') as install_reqf:
+    install_req = [req.strip() for req in install_reqf]
+
+setup(
+    name='hgitaly',
+    version='0.7.0',
+    author='Georges Racinet',
+    author_email='georges.racinet@octobus.net',
+    url='https://foss.heptapod.net/heptapod/hgitaly',
+    description="Server-side implementation of Gitaly protocol for Mercurial",
+    long_description=open('README.md').read(),
+    keywords='hg mercurial heptapod gitlab',
+    license='GPLv2+',
+    packages=find_packages(),
+    install_requires=install_req,
+)
