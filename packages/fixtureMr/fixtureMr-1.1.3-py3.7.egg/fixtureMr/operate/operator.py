b2824d34776a62d78a972d13bc3757a44edee3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:09 +0000

    Add package fixtureMr/1.1.3/fixtureMr-1.1.3-py3.7.egg

---
+++ packages/fixtureMr/fixtureMr-1.1.3-py3.7.egg/fixtureMr/operate/operator.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# @Author: yongfanmao
+# @Date:   2020-12-11 16:44:14
+# @E-mail: maoyongfan@163.com
+# @Last Modified by:   yongfanmao
+# @Last Modified time: 2020-12-11 21:37:31
+import serial
+from fixtureMr.worker.worker import Worker
+
+class Operator(object):
+	def __init__(self):
+		pass
+
+	def closeLock(self,num):
+		"""
+		num 指定关闭哪台治具上的锁
+		"""
+		if num not in [1,2,3,4]:
+			raise Exception("你输入的序号不存在")
+
+		wk = Worker(num)
+		if wk.closeLock():
+			del wk
+			return True
+		else:
+			del wk
+			return False
+
+	def reback(self,num):
+		if num not in [1,2,3,4]:
+			raise Exception("你输入的序号不存在")
+
+		wk = Worker(num)
+		wk.reback()
+		del wk
+
+
+
+
+
