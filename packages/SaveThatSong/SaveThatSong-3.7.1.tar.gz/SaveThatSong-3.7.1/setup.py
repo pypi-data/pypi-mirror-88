bef07ba12d1e997863f012724b4651b57267fbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:49 +0000

    Add package SaveThatSong/3.7.1/SaveThatSong-3.7.1.tar.gz

---
+++ packages/SaveThatSong/SaveThatSong-3.7.1.tar.gz/SaveThatSong-3.7.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+        name='SaveThatSong',
+        version='3.7.1',
+         classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+                     ],
+        packages=setuptools.find_packages(),
+        install_requires=[
+                    'youtube_dl',
+                    'youtube_search',
+                    'spotipy',
+                    ],
+        description=('This is a python script that downloads songs and even playlists from Spotify and YouTube very easily'),
+        scripts=['sts'],
+        url='https://github.com/ArjunxyzSatish/SaveThatSong',
+        author='Arjun Satish'
+    )
