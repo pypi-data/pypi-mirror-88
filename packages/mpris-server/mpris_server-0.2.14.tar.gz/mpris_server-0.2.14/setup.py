ea55fd4f208a0668c6e7ecc618da2cc3ed657e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:49 +0000

    Add package mpris-server/0.2.14/mpris_server-0.2.14.tar.gz

---
+++ packages/mpris-server/mpris_server-0.2.14.tar.gz/mpris_server-0.2.14/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from pathlib import Path
+
+requirements = \
+  Path('requirements.txt') \
+    .read_text() \
+    .split('\n')
+readme = Path('README.md').read_text()
+
+setup(
+  name="mpris_server",
+  version="0.2.14",
+  description="Publish a MediaPlayer2 MPRIS device to D-BUS.",
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  url="https://alexdelorenzo.dev",
+  author="Alex DeLorenzo",
+  license="AGPL-3.0",
+  packages=['mpris_server'],
+  zip_safe=True,
+  install_requires=requirements,
+  python_requires='>=3.6',
+)
