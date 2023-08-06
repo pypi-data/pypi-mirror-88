b61ea26a65ab81de180407a382f4ec214985d64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:08 +0000

    Add package niltype/0.3.0/niltype-0.3.0.tar.gz

---
+++ packages/niltype/niltype-0.3.0.tar.gz/niltype-0.3.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+
+def find_required():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def find_dev_required():
+    with open("requirements-dev.txt") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="niltype",
+    version="0.3.0",
+    description="Like a None",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nikita Tsvetkov",
+    author_email="nikitanovosibirsk@yandex.com",
+    python_requires=">=3.6",
+    url="https://github.com/nikitanovosibirsk/niltype",
+    license="MIT",
+    packages=find_packages(exclude=("tests",)),
+    package_data={"niltype": ["py.typed"]},
+    install_requires=find_required(),
+    tests_require=find_dev_required(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development",
+        "Typing :: Typed",
+    ],
+)
