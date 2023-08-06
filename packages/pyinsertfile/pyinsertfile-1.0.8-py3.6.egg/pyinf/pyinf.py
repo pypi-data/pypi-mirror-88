c3abd543b18f62d6b89ec519312b9dc62417f7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:57 +0000

    Add package pyinsertfile/1.0.8/pyinsertfile-1.0.8-py3.6.egg

---
+++ packages/pyinsertfile/pyinsertfile-1.0.8-py3.6.egg/pyinf/pyinf.py
@@ -0,0 +1,21 @@
+import argparse 
+
+def main(): 
+
+    parser = argparse.ArgumentParser(prog ='pyinf', description ='easy file inserting') 
+
+    #parser.add_argument('mode',help='(pypi) for pypi package setup', required = False)
+    parser.add_argument('mode', action='store', type=str, required=False)
+
+    args = parser.parse_args()
+
+    print(parser.mode)
+
+    if parser.mode == "pypi":
+        print("demo")
+    
+        f = open("main.py","w+")
+
+        f.write("""
+        print("PyInsertFile")
+        """)
