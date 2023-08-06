6e5312c3dd4f0a2a628b82fa7e35ab7e41cacc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:24 +0000

    Add package diffusive-solver/3.0rc9/diffusive_solver-3.0rc9.tar.gz

---
+++ packages/diffusive-solver/diffusive_solver-3.0rc9.tar.gz/diffusive_solver-3.0rc9/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "diffusive_solver",
+    version = "3.0rc9",
+    author = "Iacopo Torre",
+    author_email = "iacopo.torre@icfo.eu",
+    description = "Package for solving systems of coupled diffusion equations",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://gitlab.com/itorre/diffusive_solver",
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research"
+    ],
+    python_requires = '>=3.6',
+    install_requires = ['numpy','matplotlib','meshio'],
+
+)
