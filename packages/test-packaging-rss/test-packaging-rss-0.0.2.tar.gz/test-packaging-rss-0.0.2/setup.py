6c6ba4b90888aa1d8edea205fee4edcd818c8df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:43 +0000

    Add package test-packaging-rss/0.0.2/test-packaging-rss-0.0.2.tar.gz

---
+++ packages/test-packaging-rss/test-packaging-rss-0.0.2.tar.gz/test-packaging-rss-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='test-packaging-rss',
+    version='0.0.2',
+    url='https://github.com/baobabtr33/test-packaging-rss',
+    license='',
+    author='Kim Jung Hwan',
+    packages=setuptools.find_packages(),
+    author_email='author@example.com',
+    description='test packaging',
+    setup_requires=['wheel']
+)
+
