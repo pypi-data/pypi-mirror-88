3f08bb46a8a7c75e3e58905598fd89246d58a808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:46 +0000

    Add package mat73/0.46/mat73-0.46.tar.gz

---
+++ packages/mat73/mat73-0.46.tar.gz/mat73-0.46/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+     name='mat73',  
+     version='0.46',
+     author="skjerns",
+     author_email="nomail@nomail.com",
+     description="Load MATLAB .mat 7.3 into Python native data types",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/skjerns/mat7.3",
+     download_url="https://github.com/skjerns/mat7.3/archive/v0.46.tar.gz",
+     install_requires=['h5py', 'numpy'],
+     license='MIT',
+     packages=['mat73'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
