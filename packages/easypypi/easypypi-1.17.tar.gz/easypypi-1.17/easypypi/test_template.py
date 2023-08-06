74ffd3756132b34dcb26759e47af51682d96a07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:25 +0000

    Add package easypypi/1.17/easypypi-1.17.tar.gz

---
+++ packages/easypypi/easypypi-1.17.tar.gz/easypypi-1.17/easypypi/test_template.py
@@ -0,0 +1,8 @@
+# Tests for {self.name}
+
+from .{self.name} import *
+import pytest
+class Test_Group_1:
+    def test_something(self):
+        """ Something should happen when you run something() """
+        assert something() == something_else
