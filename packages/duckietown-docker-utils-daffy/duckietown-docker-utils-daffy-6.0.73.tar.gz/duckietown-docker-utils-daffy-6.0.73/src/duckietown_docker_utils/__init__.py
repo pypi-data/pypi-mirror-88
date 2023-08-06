8b99babaa4b1bf0460a86cc059554b5ef916a33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:39 +0000

    Add package duckietown-docker-utils-daffy/6.0.73/duckietown-docker-utils-daffy-6.0.73.tar.gz

---
+++ packages/duckietown-docker-utils-daffy/duckietown-docker-utils-daffy-6.0.73.tar.gz/duckietown-docker-utils-daffy-6.0.73/src/duckietown_docker_utils/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "6.0.73"
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+import os
+
+path = os.path.dirname(os.path.dirname(__file__))
+
+logger.debug(f"duckietown_docker_utils version {__version__} path {path}")
+
+from .monitoring import *
+from .docker_run import *
+from .constants import *
