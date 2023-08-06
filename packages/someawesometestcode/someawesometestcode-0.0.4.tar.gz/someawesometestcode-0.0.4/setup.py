799bb7f7d7f6368dccc79f93b4ffa14b55a12495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:50 +0000

    Add package someawesometestcode/0.0.4/someawesometestcode-0.0.4.tar.gz

---
+++ packages/someawesometestcode/someawesometestcode-0.0.4.tar.gz/someawesometestcode-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="someawesometestcode", # Replace with your own username
+    version="0.0.4",
+    author="Tim Röthlisberger",
+    author_email="tim.roethlisberger@hotmail.com",
+    description="Testpackage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
