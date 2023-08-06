050e8a1f20fa63d2d714a121a3da9532920edc4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:38 +0000

    Add package projector-installer/0.1.14/projector-installer-0.1.14.tar.gz

---
+++ packages/projector-installer/projector-installer-0.1.14.tar.gz/projector-installer-0.1.14/projector_installer/apps.py
@@ -0,0 +1,336 @@
+# Copyright 2000-2020 JetBrains s.r.o.
+# Use of this source code is governed by the Apache 2.0 license that can be found
+# in the LICENSE file.
+
+
+"""Application management functions."""
+import io
+from os import listdir, chmod, stat, rename
+from os.path import join, expanduser, dirname, isfile, isdir
+from distutils.version import LooseVersion
+from typing import Optional, List, TextIO
+import json
+
+from .global_config import get_apps_dir, get_projector_server_dir, COMPATIBLE_APPS, \
+    CompatibleApp, RunConfig, is_password_protected, init_compatible_apps, is_secure, \
+    get_ssl_properties_file
+
+from .utils import unpack_tar_file
+
+IDEA_RUN_CLASS = 'com.intellij.idea.Main'
+PROJECTOR_RUN_CLASS = 'org.jetbrains.projector.server.ProjectorLauncher'
+IDEA_PLATFORM_PREFIX = 'idea.platform.prefix'
+IDEA_PATH_SELECTOR = 'idea.paths.selector'
+TOKEN_ENV_NAME = 'ORG_JETBRAINS_PROJECTOR_SERVER_HANDSHAKE_TOKEN'
+RO_TOKEN_ENV_NAME = 'ORG_JETBRAINS_PROJECTOR_SERVER_RO_HANDSHAKE_TOKEN'
+SSL_ENV_NAME = 'ORG_JETBRAINS_PROJECTOR_SERVER_SSL_PROPERTIES_PATH'
+
+
+def get_installed_apps(pattern: Optional[str] = None) -> List[str]:
+    """Returns sorted list of installed apps, matched given pattern."""
+    res = [file_name for file_name in listdir(get_apps_dir()) if
+           pattern is None or file_name.lower().find(pattern.lower()) != -1]
+    res.sort()
+    return res
+
+
+def get_compatible_apps(pattern: Optional[str] = None) -> List[CompatibleApp]:
+    """Returns list of compatible apps, matched given pattern."""
+    if not COMPATIBLE_APPS:
+        init_compatible_apps()
+
+    apps = [app for app in COMPATIBLE_APPS if
+            pattern is None or app.name.lower().find(pattern.lower()) != -1]
+
+    if pattern:
+        for app in apps:
+            if pattern.lower() == app.name.lower():
+                return [app]
+
+    return apps
+
+
+def get_compatible_app_names(pattern: Optional[str] = None) -> List[str]:
+    """Get sorted list of projector-compatible applications, matches given pattern."""
+    res = [app.name for app in get_compatible_apps(pattern)]
+    res.sort()
+    return res
+
+
+def get_app_path(app_name: str) -> str:
+    """Returns full path to given app."""
+    return join(get_apps_dir(), app_name)
+
+
+def write_run_script(run_config: RunConfig, src: TextIO, dst: TextIO) -> None:
+    """Writes run script from src to dst"""
+
+    for line in src:
+        if line.startswith("IDE_BIN_HOME"):
+            line = f'IDE_BIN_HOME={join(run_config.path_to_app, "bin")}\n'
+        elif line.find("classpath") != -1:
+            line = f' -classpath "$CLASSPATH:{get_projector_server_dir()}/*" \\\n'
+        elif line.find(IDEA_RUN_CLASS) != -1:
+            line = f' -Dorg.jetbrains.projector.server.port={run_config.projector_port} \\\n'
+            line += f' -Dorg.jetbrains.projector.server.classToLaunch={IDEA_RUN_CLASS} \\\n'
+
+            if is_secure(run_config):
+                line += f' -D{SSL_ENV_NAME}=\"{get_ssl_properties_file(run_config.name)}\" \\\n'
+
+            if is_password_protected(run_config):
+                line += f' -D{TOKEN_ENV_NAME}=\"{run_config.password}\" \\\n'
+                line += f' -D{RO_TOKEN_ENV_NAME}=\"{run_config.ro_password}\" \\\n'
+
+            line += f'  {PROJECTOR_RUN_CLASS}\\\n'
+
+        dst.write(line)
+
+
+def make_run_script(run_config: RunConfig, run_script: str) -> None:
+    """Creates run script from ide launch script."""
+    idea_script = get_launch_script(run_config.path_to_app)
+
+    with open(idea_script, 'r') as src, open(run_script, 'w') as dst:
+        write_run_script(run_config, src, dst)
+
+    stats = stat(run_script)
+    chmod(run_script, stats.st_mode | 0o0111)
+
+
+def check_run_script(run_config: RunConfig, run_script_name: str) -> bool:
+    """Check if run script corresponds to given config"""
+    idea_script = get_launch_script(run_config.path_to_app)
+    with open(idea_script, 'r') as src, open(run_script_name, 'r') as run_script:
+        dst = io.StringIO()
+        write_run_script(run_config, src, dst)
+        dst.seek(0)
+        return dst.read() == run_script.read()
+
+
+class ProductInfo:
+    """Ide product info"""
+
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, name: str, version: str, build_number: str, product_code: str,
+                 data_dir: str, svg_icon_path: str, os: str, launcher_path: str,
+                 java_exec_path: str, vm_options_path: str, startup_wm_class: str) -> None:
+        self.name: str = name
+        self.version: str = version
+        self.build_number: str = build_number
+        self.product_code: str = product_code
+        self.data_dir: str = data_dir
+        self.svg_icon_path: str = svg_icon_path
+        self.os: str = os
+        self.launcher_path: str = launcher_path
+        self.java_exec_path: str = java_exec_path
+        self.vm_options_path: str = vm_options_path
+        self.startup_wm_class: str = startup_wm_class
+
+
+PRODUCT_INFO = 'product-info.json'
+
+
+class Version:
+    """Three-parts version representation."""
+
+    def __init__(self, year: int, quart: int, last: int):
+        self.year: int = year
+        self.quart: int = quart
+        self.last: int = last
+
+
+def parse_version(version: str) -> Version:
+    """Parses version string to Version class."""
+    parsed = version.split(".")
+
+    try:
+        return Version(int(parsed[0]), int(parsed[1]), int(parsed[2] if len(parsed) > 2 else -1))
+    except ValueError:
+        return Version(0, 0, -1)
+
+
+def get_data_dir_from_script(run_script: str) -> str:
+    """Returns idea data dir from run script."""
+    with open(run_script, 'r') as file:
+        for line in file:
+            pos = line.find(IDEA_PATH_SELECTOR)
+
+            if pos != -1:
+                parts = line.split('=')
+
+                if len(parts) < 2:
+                    raise Exception(f'Unable to parse {IDEA_PATH_SELECTOR} line.')
+
+                return parts[1].split(' ')[0]
+
+    raise Exception('Unable to find data directory in the launch script.')
+
+
+def get_product_info(app_path: str) -> ProductInfo:
+    """Parses product info file to ProductInfo class."""
+    prod_info_path = join(app_path, PRODUCT_INFO)
+    with open(prod_info_path, "r") as file:
+        data = json.load(file)
+        java_exec_path = 'jre/bin/java'
+
+        if 'javaExecutablePath' in data['launch'][0]:
+            java_exec_path = data['launch'][0]['javaExecutablePath']
+
+        product_info = ProductInfo(data['name'], data['version'], data['buildNumber'],
+                                   data['productCode'], '', data['svgIconPath'],
+                                   data['launch'][0]['os'],
+                                   data['launch'][0]['launcherPath'],
+                                   java_exec_path,
+                                   data['launch'][0]['vmOptionsFilePath'],
+                                   data['launch'][0]['startupWmClass'])
+
+        version = parse_version(product_info.version)
+
+        if version.year >= 2020 and version.quart >= 2:
+            product_info.data_dir = data['dataDirectoryName']
+        else:
+            product_info.data_dir = get_data_dir_from_script(
+                join(app_path, product_info.launcher_path))
+
+        return product_info
+
+
+def get_launch_script(app_path: str) -> str:
+    """Returns full path to launch script by ide path."""
+    prod_info = get_product_info(app_path)
+    return join(app_path, prod_info.launcher_path)
+
+
+def get_bin_dir(app_path: str) -> str:
+    """Get full path to ide bin dir."""
+    run_script = get_launch_script(app_path)
+    return dirname(run_script)
+
+
+CONFIG_PREFIX = expanduser('~/')
+VER_2020_CONFIG_PREFIX = expanduser('~/.config/JetBrains')
+ANDROID_STUDIO_CONFIG_PREFIX = expanduser('~/.config/Google')
+
+
+def get_config_dir(app_path: str) -> str:
+    """Returns ide config directory."""
+    product_info = get_product_info(app_path)
+    version = parse_version(product_info.version)
+
+    if is_android_studio(product_info):
+        return join(ANDROID_STUDIO_CONFIG_PREFIX, product_info.data_dir)
+
+    if version.year >= 2020:
+        return join(VER_2020_CONFIG_PREFIX, product_info.data_dir)
+
+    return join(join(CONFIG_PREFIX, '.' + product_info.data_dir), 'config')
+
+
+PLUGIN_2020_PREFIX: str = expanduser('~/.local/share/JetBrains')
+ANDROID_STUDIO_PLUGIN_PREFIX = expanduser('~/.local/share/Google')
+
+
+def get_plugin_dir(app_path: str) -> str:
+    """Returns full path to application plugin directory."""
+    product_info = get_product_info(app_path)
+    version = parse_version(product_info.version)
+
+    if is_android_studio(product_info):
+        return join(ANDROID_STUDIO_PLUGIN_PREFIX, product_info.data_dir)
+
+    if version.year >= 2020:
+        return join(PLUGIN_2020_PREFIX, product_info.data_dir)
+
+    return join(get_config_dir(app_path), "plugins")
+
+
+def is_android_studio(product_info: ProductInfo) -> bool:
+    """Returns True if given product info corresponds to is from AS"""
+    return product_info.product_code == 'AI'
+
+
+def get_java_path(app_path: str) -> str:
+    """Returns full path to bundled java."""
+    product_info = get_product_info(app_path)
+    return join(app_path, product_info.java_exec_path)
+
+
+def unpack_app(file_path: str) -> str:
+    """Unpacks specified file to app directory."""
+    app_name = unpack_tar_file(file_path, get_apps_dir())
+
+    # For android studio - ensure that app directory has unique name
+    app_path = get_app_path(app_name)
+    product_info = get_product_info(app_path)
+
+    if is_android_studio(product_info):
+        versioned_name = app_name + "_" + product_info.version.replace(' ', '_')
+        new_path = get_app_path(versioned_name)
+        rename(app_path, new_path)
+        app_name = versioned_name
+
+    return app_name
+
+
+def get_jre_dir(path_to_app: str) -> str:
+    """Return path to dir with bundled jre"""
+    product_info = get_product_info(path_to_app)
+
+    if is_android_studio(product_info):
+        return join(path_to_app, 'jre')
+
+    return join(path_to_app, 'jbr')
+
+
+def is_path_to_app(app_path: str) -> bool:
+    """Checks app path validity"""
+    prod_info_path = join(app_path, PRODUCT_INFO)
+    return isfile(prod_info_path)
+
+
+# ~/.local/share/JetBrains/Toolbox/apps/AndroidStudio/ch-0
+def get_path_to_toolbox_channel(path: str) -> Optional[str]:
+    """"Returns path to toolbox channel"""
+    pos = path.find('JetBrains/Toolbox/apps')
+
+    if pos >= 0:
+        ch_path = path.rstrip('/')
+        ch_pos = ch_path.find('ch-')
+
+        if ch_pos > pos:
+            sep_pos = ch_path.find('/', ch_pos + 1)
+
+            if sep_pos < 0:
+                return ch_path
+
+            return ch_path[:sep_pos]
+
+    return None
+
+
+def is_toolbox_path(app_path: str) -> bool:
+    """Checks if given path is toolbox channel path"""
+    return get_path_to_toolbox_channel(app_path) is not None
+
+
+def get_path_to_latest_app(path: str) -> Optional[str]:
+    """Returns path to the app with latest version in channel"""
+    channel_path = get_path_to_toolbox_channel(path)
+
+    if channel_path is None:
+        return None
+
+    app_path = None
+    app_ver = None
+
+    for sub_dir in listdir(channel_path):
+        app_dir = join(channel_path, sub_dir)
+
+        if isdir(app_dir) and is_path_to_app(app_dir):
+            ver = LooseVersion(get_product_info(app_dir).version)
+
+            if app_path is None or app_ver < ver:
+                app_path = app_dir
+                app_ver = ver
+
+    return app_path
---
+++ packages/projector-installer/projector-installer-0.1.14.tar.gz/projector-installer-0.1.14/projector_installer/global_config.py
@@ -0,0 +1,188 @@
+# Copyright 2000-2020 JetBrains s.r.o.
+# Use of this source code is governed by the Apache 2.0 license that can be found
+# in the LICENSE file.
+
+
+"""
+Global configuration constants, variables and functions.
+"""
+
+import json
+import socket
+import sys
+from typing import List
+from shutil import rmtree
+from urllib.error import URLError
+from os.path import dirname, join, expanduser, abspath
+
+from .utils import create_dir_if_not_exist, download_file, get_file_name_from_url
+
+USER_HOME: str = expanduser('~')
+INSTALL_DIR: str = dirname(abspath(__file__))
+DEF_PROJECTOR_PORT: int = 9999
+COMPATIBLE_IDE_FILE: str = 'compatible_ide.json'
+DEF_CONFIG_DIR: str = '.projector'
+SSL_PROPERTIES_FILE = 'ssl.properties'
+BUNDLED_DIR: str = 'bundled'
+SERVER_DIR: str = 'server'
+config_dir: str = join(USER_HOME, DEF_CONFIG_DIR)
+cache_dir: str = ''
+CHANGELOG_URL = 'https://github.com/JetBrains/projector-installer/blob/master/CHANGELOG.md'
+
+
+def get_changelog_url(ver: str) -> str:
+    """Returns URL to changelog for specified version"""
+    ver_ref = ver.replace('.', '')
+    return f'{CHANGELOG_URL}#{ver_ref}'
+
+
+def get_path_to_license() -> str:
+    """Returns full path to license file"""
+    return join(INSTALL_DIR, 'LICENSE.txt')
+
+
+def get_apps_dir() -> str:
+    """Returns full path to applications directory."""
+    return join(config_dir, 'apps')
+
+
+def get_run_configs_dir() -> str:
+    """Returns full path to run configs directory."""
+    return join(config_dir, 'configs')
+
+
+def get_ssl_properties_file(config_name: str) -> str:
+    """Returns full path to ssl.properties file"""
+    return join(get_run_configs_dir(), config_name, SSL_PROPERTIES_FILE)
+
+
+def get_download_cache_dir() -> str:
+    """Returns full path to download cache directory."""
+
+    if cache_dir:
+        return cache_dir
+
+    return join(config_dir, 'cache')
+
+
+def get_ssl_dir() -> str:
+    """Returns full path to ssl directory."""
+    return join(config_dir, 'ssl')
+
+
+def get_compatible_ide_file() -> str:
+    """Returns full path to compatible ide file."""
+    return join(INSTALL_DIR, COMPATIBLE_IDE_FILE)
+
+
+class CompatibleApp:
+    """Compatible application entry."""
+
+    def __init__(self, name: str, url: str) -> None:
+        self.name: str = name
+        self.url: str = url
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, CompatibleApp):
+            return self.name == other.name and self.url == other.url
+
+        return False
+
+
+COMPATIBLE_APPS: List[CompatibleApp] = []
+
+
+class RunConfig:
+    """Run config dataclass"""
+
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, own_name: str, path_to_app: str, projector_port: int,
+                 token: str, password: str, ro_password: str, toolbox: bool) -> None:
+        self.name: str = own_name
+        self.path_to_app: str = path_to_app
+        self.projector_port: int = projector_port
+        self.token: str = token
+        self.password: str = password
+        self.ro_password: str = ro_password
+        self.toolbox = toolbox
+
+
+def is_secure(run_config: RunConfig) -> bool:
+    """Checks if secure configuration"""
+    return run_config.token != ''
+
+
+def is_password_protected(run_config: RunConfig) -> bool:
+    """Checks if run config is password protected"""
+    return run_config.password != ''
+
+
+def load_compatible_apps_from_file(file_name: str) -> None:
+    """Loads compatible apps file to memory."""
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+
+    for entry in data:
+        app = CompatibleApp(entry['name'], entry['url'])
+        if app not in COMPATIBLE_APPS:
+            COMPATIBLE_APPS.append(app)
+
+
+COMPATIBLE_IDE_FILE_URL: str = \
+    'https://raw.githubusercontent.com/JetBrains/projector-installer/master/' \
+    'projector_installer/compatible_ide.json'
+
+
+def download_compatible_apps() -> str:
+    """Downloads compatible ide json file from github repository."""
+    try:
+        download_file(COMPATIBLE_IDE_FILE_URL, get_download_cache_dir(), timeout=3, silent=True)
+        name = get_file_name_from_url(COMPATIBLE_IDE_FILE_URL)
+        file_name = join(get_download_cache_dir(), name)
+
+        return file_name
+    except (URLError, socket.timeout):
+        return ''
+
+
+def load_compatible_apps() -> None:
+    """Loads compatible apps dictionary from bundled file and github-stored Json"""
+    file_name = get_compatible_ide_file()
+    load_compatible_apps_from_file(file_name)
+    github_file = download_compatible_apps()
+
+    if github_file:
+        load_compatible_apps_from_file(github_file)
+
+
+def init_compatible_apps() -> None:
+    """Initializes compatible apps list."""
+    try:
+        load_compatible_apps()
+    except IOError as error:
+        print(f'Cannot load compatible ide file: {str(error)}. Exiting...')
+        sys.exit(2)
+
+
+def get_projector_server_dir() -> str:
+    """Returns directory with projector server jar"""
+    return join(INSTALL_DIR, BUNDLED_DIR, SERVER_DIR)
+
+
+def init_cache_dir() -> None:
+    """Initialize download cache dir"""
+    create_dir_if_not_exist(get_download_cache_dir())
+
+
+def init_config_dir() -> None:
+    """Initializes global config directory."""
+    # pylint: disable=W0703
+    try:
+        create_dir_if_not_exist(config_dir)
+        create_dir_if_not_exist(get_apps_dir())
+        create_dir_if_not_exist(get_run_configs_dir())
+        init_cache_dir()
+    except Exception as exception:
+        print(f'Error during initialization: {str(exception)}, cleanup ...')
+        rmtree(config_dir)
+        sys.exit(1)
