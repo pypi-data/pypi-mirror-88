2fe83f9ac536608564526ba94e98092ff92edd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:03 +0000

    Add package jpe-types/0.0.0.1.8/jpe_types-0.0.0.1.8.tar.gz

---
+++ packages/jpe-types/jpe_types-0.0.0.1.8.tar.gz/jpe_types-0.0.0.1.8/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "jpe_types",
+    packages=["jpe_types/paralel",
+              "jpe_types",
+              "jpe_types/conversions"],
+    version="0.0.0.1.8",
+    license='wtfpl',
+    description="slitly improved python types",
+    long_description="""
+    Threading
+    -   adds thread inharitance and return posibility
+    -   logging filteter for thread inheritance
+    -   a thread and a lock in the same class to make it slitly easyer to work with threads
+
+    conversions
+    - adds the abilty to convert ints to any base
+    """,
+    include_package_data = True,
+    author = 'Julian Wandhoven',                   # Type in your name
+    author_email = 'julian.wandhoven@fgz.ch',
+
+    url="https://github.com/JulianWww/jpe_types",
+    download_url="https://github.com/JulianWww/jpe_types/archive/0.tar.gz",
+    keywords=["dtypes", "jpe", "utils", "paralel", "logging"],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6' ,
+    'Programming Language :: Python :: 3'],#Specify which pyhton versions that you want to support
+)
