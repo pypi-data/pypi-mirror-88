04e718a7bc89b97893d15644336629bfc74bc81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:27 +0000

    Add package cnx-transforms/1.2.2/cnx_transforms-1.2.2-py2.py3-none-any.whl

---
+++ packages/cnx-transforms/cnx_transforms-1.2.2-py2.py3-none-any.whl/cnxtransforms/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2014, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+"""Transforms for CNXML <-> HTML."""
+from .converters import *  # noqa
+from .resolvers import *  # noqa
+from .producers import *  # noqa
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
