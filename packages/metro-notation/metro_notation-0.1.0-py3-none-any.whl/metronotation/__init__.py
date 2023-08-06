af5c3651c8513fa37f39cd978b97b544b1dd4ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:44 +0000

    Add package metro-notation/0.1.0/metro_notation-0.1.0-py3-none-any.whl

---
+++ packages/metro-notation/metro_notation-0.1.0-py3-none-any.whl/metronotation/__init__.py
@@ -0,0 +1,73 @@
+import math
+import sys
+
+from .canvas import Canvas
+from .renderer import Renderer
+
+VERSION = "0.1.0"
+
+
+def split_algorithm(algo_list):
+    algos_lists = []
+    index = 0
+
+    for i, algo in enumerate(algo_list + [None]):
+        if algo:
+            continue
+
+        alst = algo_list[index:i]
+        index = i + 1
+
+        if alst:
+            algos_lists.append(alst)
+
+    return algos_lists
+
+
+def load_algorithm(filename):
+    with open(filename, "r") as f:
+        lines = map(lambda s: s.strip(), f.read().splitlines())
+
+    algo_list = []
+    name = None
+
+    for line in lines:
+        if not line or line[0] == "#":
+            continue
+
+        if line[0] == "-":
+            algo_list.append(None)
+            continue
+
+        if line[0] == "[" and line[-1] == "]":
+            name = line[1:-1]
+            continue
+
+        algo_list.append((name or "NONAME", line))
+        name = None
+
+    return split_algorithm(algo_list)
+
+
+def render_algorithms(filename):
+    algo_lists = load_algorithm(filename)
+    renderers = [Renderer.from_algorithm(alst) for alst in algo_lists]
+
+    for renderer in renderers:
+        renderer.show()
+
+    if len(renderers) > 1:
+        merged_renderer = Renderer.merge(renderers)
+        merged_renderer.show()
+
+
+def run():
+    if len(sys.argv) != 2:
+        print("metro-notation [filename]")
+        return
+
+    render_algorithms(sys.argv[1])
+
+
+if __name__ == "__main__":
+    run()
