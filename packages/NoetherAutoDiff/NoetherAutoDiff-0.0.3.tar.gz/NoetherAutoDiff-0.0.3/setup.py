bcf6c8c8fa6f1261a99de7d8d304cd46deb535af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:54 +0000

    Add package NoetherAutoDiff/0.0.3/NoetherAutoDiff-0.0.3.tar.gz

---
+++ packages/NoetherAutoDiff/NoetherAutoDiff-0.0.3.tar.gz/NoetherAutoDiff-0.0.3/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+
+# current dir
+HERE = pathlib.Path(__file__).parent
+
+# add readme
+#README = (HERE / "README.md").read_text()
+
+setup(
+    name='NoetherAutoDiff',
+    packages=['NoetherAutoDiff'],
+    version='0.0.3',
+    license='MIT',
+    description='A Python wrapper for an efficient C++ library that is used for Auto Differentiation',
+    #long_description=README,
+    #long_description_content_type='text/markdown',
+    author='Sam Negassi',
+    author_email='samson.s.negassi@gmail.com',
+    url='https://github.com/cs107-noether/cs107-FinalProject',
+    download_url='https://github.com/cs107-noether/cs107-FinalProject/archive/v0.1.tar.gz',
+    keywords=['Auto', 'Differentiation', 'AutoDiff', 'Jacobian', 'Derivation'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
+
