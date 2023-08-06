9125fa07e5af52306beed41b36b473052975ac67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:46 +0000

    Add package dirhunt/0.8.0/dirhunt-0.8.0.tar.gz

---
+++ packages/dirhunt/dirhunt-0.8.0.tar.gz/dirhunt-0.8.0/dirhunt/tests/test_processors.py
@@ -0,0 +1,267 @@
+import unittest
+from dirhunt.tests._compat import patch
+
+import requests
+import requests_mock
+from bs4 import BeautifulSoup
+
+from dirhunt.crawler import Crawler
+from dirhunt.processors import ProcessHtmlRequest, ProcessIndexOfRequest, ProcessBlankPageRequest, ProcessNotFound, \
+    ProcessRedirect, Error, ProcessCssStyleSheet, ProcessJavaScript
+from dirhunt.tests.base import CrawlerTestBase
+from dirhunt.tests.test_directory_lists import TestCommonDirectoryList
+
+
+class TestError(CrawlerTestBase, unittest.TestCase):
+    def test_str(self):
+        e = Error(self.get_crawler_url(), Exception('Foo bar'))
+        self.assertIn('Foo bar', str(e))
+
+
+class TestProcessRedirect(CrawlerTestBase, unittest.TestCase):
+    html = ''
+
+    def test_is_applicable(self):
+        with requests_mock.mock() as m:
+            m.register_uri('GET', 'http://test.com', text=self.html, headers={'Content-Type': 'text/html'},
+                           status_code=300)
+            r = requests.get('http://test.com')
+            self.assertTrue(ProcessRedirect.is_applicable(r, None, None, None))
+
+    def test_process(self):
+        with requests_mock.mock() as m:
+            m.register_uri('GET', 'http://test.com', text=self.html, headers={'Location': 'http://foo/'},
+                           status_code=300)
+            r = requests.get('http://test.com')
+        with patch.object(Crawler, 'add_url') as mock_method:
+            p = ProcessRedirect(r, self.get_crawler_url())
+            p.process('')
+            urls = [crawler_url[0][0].url.url for crawler_url in mock_method.call_args_list]
+            self.assertEqual(urls, ['http://foo/'])
+
+    def test_str(self):
+        with requests_mock.mock() as m:
+            m.register_uri('GET', 'http://test.com', text=self.html, headers={'Location': 'http://foo/'},
+                           status_code=300)
+            r = requests.get('http://test.com')
+        with patch.object(Crawler, 'add_url'):
+            p = ProcessRedirect(r, self.get_crawler_url())
+            p.process('')
+        self.assertIn('http://foo/', str(p))
+
+
+class TestProcessNotFound(CrawlerTestBase, unittest.TestCase):
+    html = ''
+
+    def test_is_applicable(self):
+        with requests_mock.mock() as m:
+            m.register_uri('GET', 'http://test.com', text=self.html, headers={'Content-Type': 'text/html'},
+                           status_code=404)
+            r = requests.get('http://test.com')
+            self.assertTrue(ProcessNotFound.is_applicable(r, None, None, None))
+
+    def test_fake(self):
+        crawler_url = self.get_crawler_url()
+        crawler_url.exists = True
+        process = ProcessNotFound(None, crawler_url)
+        self.assertIn('{}.fake'.format(process.key_name), process.flags)
+
+    def test_str(self):
+        crawler_url = self.get_crawler_url()
+        crawler_url.exists = True
+        process = ProcessNotFound(None, crawler_url)
+        str(process)
+
+
+class TestProcessCssStyleSheet(CrawlerTestBase, unittest.TestCase):
+    css = 'body { background-image: url("img/foo.png") }'
+
+    def test_is_applicable(self):
+        crawler_url = self.get_crawler_url()
+        with requests_mock.mock() as m:
+            m.get(self.url, text=self.css, headers={'Content-Type': 'text/css'})
+            r = requests.get(self.url)
+            self.assertTrue(ProcessCssStyleSheet.is_applicable(r, self.css, crawler_url, None))
+
+    def test_process(self):
+        process = ProcessCssStyleSheet(None, self.get_crawler_url())
+        urls = process.process(self.css, None)
+        links = [link.url for link in urls]
+        self.assertEqual(links, ['http://domain.com/path/img/foo.png'])
+
+
+class TestProcessJavaScript(CrawlerTestBase, unittest.TestCase):
+    js = """
+    "http://example.com" "/wrong/file/test<>b" "api/create.php?user=test"
+    "index.html"
+    """
+
+    def test_is_applicable(self):
+        crawler_url = self.get_crawler_url()
+        with requests_mock.mock() as m:
+            m.get(self.url, text=self.js, headers={'Content-Type': 'application/javascript'})
+            r = requests.get(self.url)
+            self.assertTrue(ProcessJavaScript.is_applicable(r, self.js, crawler_url, None))
+
+    def test_process(self):
+        process = ProcessJavaScript(None, self.get_crawler_url())
+        urls = process.process(self.js, None)
+        links = [link.url for link in urls]
+        self.assertEqual(links, [
+            'http://example.com/',
+            'http://domain.com/path/api/create.php',
+            'http://domain.com/path/index.html',
+        ])
+
+
+class TestProcessHtmlRequest(CrawlerTestBase, unittest.TestCase):
+
+    def test_process(self):
+        html = """
+        <a href="dir/">dir</a>
+        <script src="myscripts.js"></script>        
+        """
+        with patch.object(Crawler, 'add_url') as mock_method:
+            process = ProcessHtmlRequest(None, self.get_crawler_url())
+            soup = BeautifulSoup(html, 'html.parser')
+            process.process(html, soup)
+            urls = [crawler_url[0][0].url.url for crawler_url in mock_method.call_args_list]
+            self.assertEqual(set(urls), {
+                'http://domain.com/path/myscripts.js',
+                'http://domain.com/path/dir/',
+                'http://domain.com/path/index.php',
+            })
+
+    def test_href_links(self):
+        html = """
+        <a href="..">Top</a>
+        <a href="dir/">dir</a>
+        <a href="foo.php">foo.php</a>
+        <a href="/spam/eggs">Eggs</a>
+        """
+        with patch.object(Crawler, 'add_url') as mock_method:
+            process = ProcessHtmlRequest(None, self.get_crawler_url())
+            soup = BeautifulSoup(html, 'html.parser')
+            process.links(soup)
+            urls = [crawler_url[0][0].url for crawler_url in mock_method.call_args_list]
+            self.assertEqual(urls, [
+                'http://domain.com/',
+                'http://domain.com/path/dir/',
+                'http://domain.com/path/foo.php',
+                'http://domain.com/spam/eggs',
+            ])
+
+    def test_refresh_links(self):
+        html = """
+        <meta http-equiv="refresh" content="0;URL=/someotherdirectory">
+        """
+        with patch.object(Crawler, 'add_url') as mock_method:
+            process = ProcessHtmlRequest(None, self.get_crawler_url())
+            soup = BeautifulSoup(html, 'html.parser')
+            process.links(soup)
+            args, kwargs = mock_method.call_args_list[0]
+            self.assertEqual(args[0].url, 'http://domain.com/someotherdirectory')
+
+    def test_assets(self):
+        html = """
+        <link rel="stylesheet" type="text/css" href="spam/theme.css">
+        <script src="myscripts.js"></script>
+        <script src="//cnd.extern.com/script.js"></script>        
+        <img src="/smiley.gif"> 
+        <img src="proto:invalid:url"> <!-- Ignore -->
+        """
+        with patch.object(Crawler, 'add_url') as mock_method:
+            process = ProcessHtmlRequest(None, self.get_crawler_url())
+            soup = BeautifulSoup(html, 'html.parser')
+            process.assets(soup)
+            urls = [crawler_url[0][0].url for crawler_url in mock_method.call_args_list]
+            self.assertEqual(urls, [
+                'http://domain.com/path/spam/theme.css',
+                'http://domain.com/path/myscripts.js',
+                'http://cnd.extern.com/script.js',
+                'http://domain.com/smiley.gif',
+            ])
+
+    def test_wordpress(self):
+        html = """
+        <script src="wp-content/myscripts.js"></script>
+        """
+        with patch.object(Crawler, 'add_url'):
+            process = ProcessHtmlRequest(None, self.get_crawler_url())
+            soup = BeautifulSoup(html, 'html.parser')
+            process.assets(soup)
+            self.assertIn('wordpress', process.crawler_url.flags)
+
+
+class TestProcessIndexOfRequest(CrawlerTestBase, unittest.TestCase):
+    html = """
+    <html><head><title>Index Of</title></head><body>
+    <a href="..">Top</a>
+    <a href="dir/">dir</a>
+    <a href="foo.php">foo.php</a>
+    <a href="error_log">error_log</a>
+    <a href="/spam/eggs">Eggs</a></body></html>
+    """
+
+    def test_is_applicable(self):
+        crawler_url = self.get_crawler_url()
+        with requests_mock.mock() as m:
+            m.get('http://test.com', text=self.html, headers={'Content-Type': 'text/html'})
+            r = requests.get('http://test.com')
+            soup = BeautifulSoup(self.html, 'html.parser')
+            self.assertTrue(ProcessIndexOfRequest.is_applicable(r, self.html, crawler_url, soup))
+
+    def test_process(self):
+        process = ProcessIndexOfRequest(None, self.get_crawler_url())
+        process.process(TestCommonDirectoryList.html,
+                        BeautifulSoup(TestCommonDirectoryList.html, 'html.parser'))
+        links = [link.url for link in process.files]
+        self.assertEqual(links, TestCommonDirectoryList.urls)
+
+    def test_interesting_ext_files(self):
+        process = ProcessIndexOfRequest(None, self.get_crawler_url())
+        process.process(self.html, BeautifulSoup(self.html, 'html.parser'))
+        self.assertEqual([file.url for file in process.interesting_ext_files()], ['http://domain.com/path/foo.php'])
+
+    def test_interesting_name_files(self):
+        process = ProcessIndexOfRequest(None, self.get_crawler_url())
+        process.process(self.html, BeautifulSoup(self.html, 'html.parser'))
+        self.assertEqual([file.url for file in process.interesting_name_files()], ['http://domain.com/path/error_log'])
+
+    def test_interesting_files(self):
+        process = ProcessIndexOfRequest(None, self.get_crawler_url())
+        process.process(self.html, BeautifulSoup(self.html, 'html.parser'))
+        self.assertEqual([file.url for file in process.interesting_files()], [
+            'http://domain.com/path/foo.php',
+            'http://domain.com/path/error_log'
+        ])
+
+    def test_flag_nothing(self):
+        process = ProcessIndexOfRequest(None, self.get_crawler_url())
+        process.process('', BeautifulSoup('', 'html.parser'))
+        self.assertEqual(process.flags, {'index_of', 'index_of.nothing'})
+
+    def test_str(self):
+        process = ProcessIndexOfRequest(None, self.get_crawler_url())
+        process.process(self.html, BeautifulSoup(self.html, 'html.parser'))
+        str(process)
+
+
+class TestProcessBlankPageRequest(CrawlerTestBase, unittest.TestCase):
+    def test_is_applicable(self):
+        html = '<html><!-- Foo --><head><title>Foo</title><script src="foo.js"></script></head><body>  </body></html>'
+        crawler_url = self.get_crawler_url()
+        with requests_mock.mock() as m:
+            m.get('http://test.com', text=html, headers={'Content-Type': 'text/html'})
+            r = requests.get('http://test.com')
+            soup = BeautifulSoup(html, 'html.parser')
+            self.assertTrue(ProcessBlankPageRequest.is_applicable(r, html, crawler_url, soup))
+
+    def test_not_applicable(self):
+        html = '<html><head><title>Foo</title></head><body>  Hello    </body></html>'
+        crawler_url = self.get_crawler_url()
+        with requests_mock.mock() as m:
+            m.get('http://test.com', text=html, headers={'Content-Type': 'text/html'})
+            r = requests.get('http://test.com')
+            soup = BeautifulSoup(html, 'html.parser')
+            self.assertFalse(ProcessBlankPageRequest.is_applicable(r, html, crawler_url, soup))
---
+++ packages/dirhunt/dirhunt-0.8.0.tar.gz/dirhunt-0.8.0/dirhunt/url_info.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+import re
+import socket
+import string
+import sys
+from operator import itemgetter
+from threading import Lock
+
+import six
+from bs4 import BeautifulSoup
+from click import get_terminal_size
+from colorama import Fore
+from requests import RequestException
+from urllib3.exceptions import ReadTimeoutError
+
+from dirhunt.cli import random_spinner
+from dirhunt.colors import status_code_colors
+from dirhunt.exceptions import EmptyError, RequestError
+from dirhunt.pool import Pool
+from dirhunt.utils import colored, remove_ansi_escape
+
+MAX_RESPONSE_SIZE = 1024 * 512
+DEFAULT_UNKNOWN_SIZE = '???'
+EXTRA_ORDER = ['created_at', 'filesize']
+
+def sizeof_fmt(num, suffix='B'):
+    if num is None:
+        return DEFAULT_UNKNOWN_SIZE
+    if isinstance(num, six.string_types):
+        num = int(num)
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%d%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%d%s%s" % (num, 'Yi', suffix)
+
+
+def format_extra(extra, length=0):
+    length = max(0, length - 2)
+    return ('[{:<%d}]' % length).format(' '.join(map(itemgetter(1), sorted(extra.items(),
+                                                                           key=lambda x: EXTRA_ORDER.index(x[0])))))
+
+
+class UrlInfo(object):
+    _data = None
+    _url_info = None
+    _text = None
+
+    def __init__(self, sessions, url, timeout=10):
+        self.sessions = sessions
+        self.url = url
+        self.timeout = timeout
+
+    def get_data(self):
+        session = self.sessions.get_session()
+        try:
+            resp = session.get(self.url.url, stream=True, verify=False, timeout=self.timeout, allow_redirects=False)
+        except RequestException:
+            raise RequestError
+        try:
+            text = resp.raw.read(MAX_RESPONSE_SIZE, decode_content=True)
+        except (RequestException, ReadTimeoutError, socket.timeout):
+            raise RequestError
+        try:
+            soup = BeautifulSoup(text, 'html.parser')
+        except NotImplementedError:
+            soup = title = body = None
+        else:
+            title = soup.select_one('title')
+            if title:
+                title = title.string
+            body = soup.select_one('body')
+            if body:
+                body = str(body)
+        if sys.version_info >= (3,):
+            text = text.decode('utf-8', errors='ignore')
+        return {
+            'resp': resp,
+            'text': text,
+            'soup': soup,
+            'title': title,
+            'body': body,
+        }
+
+    @property
+    def data(self):
+        if self._data is None:
+            self._data = self.get_data()
+        return self._data
+
+    def get_url_info(self):
+        size = self.data['resp'].headers.get('Content-Length')
+        size = len(self.data.get('text', '')) if size is None else size
+        status_code = int(self.data['resp'].status_code)
+        out = colored('({})'.format(status_code), status_code_colors(status_code)) + " "
+        out += colored('({:>6})'.format(sizeof_fmt(size)), Fore.LIGHTYELLOW_EX) + " "
+        return out
+
+    @property
+    def url_info(self):
+        if self._url_info is None:
+            self._url_info = self.get_url_info()
+        return self._url_info
+
+    def get_text(self):
+        text = self.data['title'] or self.data['body'] or self.data['text'] or ''
+        text = re.sub('[{}]'.format(string.whitespace), ' ', text)
+        return re.sub(' +', ' ', text).strip(' ')
+
+    @property
+    def text(self):
+        if self._text is None:
+            self._text = self.get_text()
+        return self._text
+
+    def line(self, line_size, url_column, extra_len):
+        if not len(self.text):
+            raise EmptyError
+        if len(self.url_info) + url_column + extra_len + 20 < line_size:
+            return self.one_line(line_size, url_column, extra_len)
+        else:
+            return self.multi_line(line_size, extra_len)
+
+    def one_line(self, line_size, url_column, extra_len):
+        text = self.text[:line_size-url_column-len(list(remove_ansi_escape(self.url_info)))-3]
+        out = self.url_info
+        out += colored(('{:<%d}' % url_column).format(self.url.url), Fore.LIGHTBLUE_EX) + "  "
+        if self.url.extra:
+            out += colored(' {} '.format(format_extra(self.url.extra, extra_len)), Fore.LIGHTBLUE_EX)
+        out += text
+        return out
+
+    def multi_line(self, line_size, extra_len):
+        out = colored('┏', Fore.LIGHTBLUE_EX) + ' {} {}\n'.format(
+            self.url_info, colored(self.url.url, Fore.LIGHTBLUE_EX)
+        )
+        out += colored('┗', Fore.LIGHTBLUE_EX)
+        if self.url.extra:
+            out += colored(' {}'.format(format_extra(self.url.extra, extra_len)), Fore.LIGHTBLUE_EX)
+        out += ' {}'.format(self.text[:line_size-2])
+        return out
+
+    def json(self):
+        return {
+            'text': self.text,
+            'url': self.url.json(),
+            'data': {
+                'text': self.data['text'],
+                'title': self.data['title'],
+                'body': self.data['body'],
+                'resp': {
+                    'headers': dict(self.data['resp'].headers),
+                    'status_code': self.data['resp'].status_code,
+                },
+            }
+        }
+
+
+class UrlsInfo(Pool):
+    url_len = 0
+    empty_files = 0
+    error_files = 0
+    count = 0
+    current = 0
+
+    def __init__(self, processors, sessions, std=None, max_workers=None, progress_enabled=True, timeout=10,
+                 save_info=False):
+        super(UrlsInfo, self).__init__(max_workers)
+        self.lock = Lock()
+        self.processors = processors
+        self.sessions = sessions
+        self.std = std
+        self.spinner = random_spinner()
+        self.progress_enabled = progress_enabled
+        self.timeout = timeout
+        self.urls_info = []
+        self.lines = []
+        self.save_info = save_info
+
+    def callback(self, url_len, extra_len, file):
+        url_info = None
+        try:
+            url_info = self._get_url_info(file)
+        except EmptyError:
+            self.empty_files += 1
+        except RequestError:
+            self.error_files += 1
+        self.lock.acquire()
+        self.erase()
+        if self.save_info:
+            self.urls_info.append(url_info)
+        if url_info:
+            size = get_terminal_size()
+            self.echo(url_info.line(size[0], url_len, extra_len))
+        self.print_progress()
+        self.lock.release()
+
+    def erase(self):
+        if self.std is None or not self.std.isatty():
+            return
+        CURSOR_UP_ONE = '\x1b[1A'
+        ERASE_LINE = '\x1b[2K'
+        # This can be improved. In the future we may want to define stdout/stderr with an cli option
+        # fn = sys.stderr.write if sys.stderr.isatty() else sys.stdout.write
+        self.std.write(CURSOR_UP_ONE + ERASE_LINE)
+
+    def echo(self, body):
+        if self.std is None:
+            return
+        # TODO: remove ANSI chars on is not tty
+        self.std.write(str(body))
+        self.std.write('\n')
+
+    def _get_url_info(self, file):
+        return UrlInfo(self.sessions, file, self.timeout)
+
+    def getted_interesting_files(self):
+        for processor in self.processors:
+            for file in processor.interesting_files():
+                yield file
+
+    def start(self):
+        self.echo('Starting...')
+        self.count = 0
+        url_len = 0
+        extra_len = 0
+        for file in self.getted_interesting_files():
+            url_len = max(url_len, len(file.url))
+            extra_len = max(extra_len, len(format_extra(file.extra)))
+            self.count += 1
+        for file in self.getted_interesting_files():
+            # TODO: issue #26. Añadir len() de contenido extra
+            self.submit(url_len, extra_len, file)
+        out = ''
+        if self.empty_files:
+            out += 'Empty files: {} '.format(self.empty_files)
+        if self.error_files:
+            out += 'Error files: {}'.format(self.error_files)
+        if out:
+            self.echo(out)
+
+    def print_progress(self):
+        if not self.progress_enabled:
+            # Cancel print progress on
+            return
+        self.current += 1
+        self.echo(('{} Interesting files {:>%d} / {}' % len(str(self.count))).format(
+            next(self.spinner),
+            self.current,
+            self.count,
+        ))
