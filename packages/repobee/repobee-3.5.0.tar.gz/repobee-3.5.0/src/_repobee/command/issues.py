c671c7be91c8ad322063a86485986b1c1e3f6df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:29 +0000

    Add package repobee/3.5.0/repobee-3.5.0.tar.gz

---
+++ packages/repobee/repobee-3.5.0.tar.gz/repobee-3.5.0/src/_repobee/command/issues.py
@@ -0,0 +1,239 @@
+"""Top-level commands for managing issues.
+
+This module contains the top-level functions for RepoBee's issue management
+functionality. Each public function in this module is to be treated as a
+self-contained program.
+
+.. module:: issues
+    :synopsis: Top-level commands for issue management.
+
+.. moduleauthor:: Simon Larsén
+"""
+import os
+import re
+from typing import Iterable, Optional, List, Tuple, Any, Mapping
+
+from colored import bg, fg, style  # type: ignore
+
+import repobee_plug as plug
+
+from _repobee.command import progresswrappers
+
+
+def list_issues(
+    repos: Iterable[plug.StudentRepo],
+    api: plug.PlatformAPI,
+    state: plug.IssueState = plug.IssueState.OPEN,
+    title_regex: str = "",
+    show_body: bool = False,
+    author: Optional[str] = None,
+) -> Mapping[str, List[plug.Result]]:
+    """List all issues in the specified repos.
+
+    Args:
+        repos: The repos from which to fetch issues.
+        api: An implementation of :py:class:`repobee_plug.PlatformAPI` used to
+            interface with the platform (e.g. GitHub or GitLab) instance.
+        state: state of the repo (open or closed). Defaults to open.
+        title_regex: If specified, only issues with titles matching the regex
+            are displayed. Defaults to the empty string (which matches
+            everything).
+        show_body: If True, the body of the issue is displayed along with the
+            default info.
+        author: Only show issues by this author.
+    """
+    # TODO optimize by not getting all repos at once
+    repos = list(repos)
+    repo_names = [repo.name for repo in repos]
+    max_repo_name_length = max(map(len, repo_names))
+
+    platform_repos = api.get_repos([repo.url for repo in repos])
+    issues_per_repo = _get_issue_generator(
+        platform_repos,
+        title_regex=title_regex,
+        author=author,
+        state=state,
+        api=api,
+    )
+
+    # _log_repo_issues exhausts the issues_per_repo iterator and
+    # returns a list with the same information. It's important to
+    # have issues_per_repo as an iterator as it greatly speeds
+    # up visual feedback to the user when fetching many issues
+    pers_issues_per_repo = _log_repo_issues(
+        issues_per_repo, show_body, max_repo_name_length + 6
+    )
+
+    # for writing to JSON
+    hook_result_mapping = {
+        repo_name: [
+            plug.Result(
+                name="list-issues",
+                status=plug.Status.SUCCESS,
+                msg="Fetched {} issues from {}".format(len(issues), repo_name),
+                data={issue.number: issue.to_dict() for issue in issues},
+            )
+        ]
+        for repo_name, issues in pers_issues_per_repo
+    }
+    # meta hook result
+    hook_result_mapping["list-issues"] = [
+        plug.Result(
+            name="meta",
+            status=plug.Status.SUCCESS,
+            msg="Meta info about the list-issues hook results",
+            data={"state": state.value},
+        )
+    ]
+    return hook_result_mapping
+
+
+def _get_issue_generator(
+    repos: Iterable[plug.Repo],
+    title_regex: str,
+    author: Optional[str],
+    state: plug.IssueState,
+    api: plug.PlatformAPI,
+) -> Iterable[Tuple[str, Iterable[plug.Issue]]]:
+    issues_per_repo = (
+        (
+            repo.name,
+            [
+                issue
+                for issue in api.get_repo_issues(repo)
+                if re.match(title_regex, issue.title)
+                and (state == plug.IssueState.ALL or state == issue.state)
+                and (not author or issue.author == author)
+            ],
+        )
+        for repo in repos
+    )
+    return issues_per_repo
+
+
+def _log_repo_issues(
+    issues_per_repo: Iterable[Tuple[str, Iterable[plug.Issue]]],
+    show_body: bool,
+    title_alignment: int,
+) -> List[Tuple[Any, list]]:
+    """Log repo issues.
+
+    Args:
+        issues_per_repo: (repo_name, issue generator) pairs
+        show_body: Include the body of the issue in the output.
+        title_alignment: Where the issue title should start counting from the
+            start of the line.
+    """
+    even = True
+    persistent_issues_per_repo = []
+    for repo_name, issues in issues_per_repo:
+        issues = list(issues)
+        persistent_issues_per_repo.append((repo_name, issues))
+
+        if not issues:
+            plug.log.warning("{}: No matching issues".format(repo_name))
+
+        for issue in issues:
+            color = (bg("grey_30") if even else bg("grey_15")) + fg("white")
+            even = not even  # cycle color
+            adjusted_alignment = title_alignment + len(
+                color
+            )  # color takes character space
+
+            id_ = "{}{}/#{}:".format(color, repo_name, issue.number).ljust(
+                adjusted_alignment
+            )
+            out = "{}{}{}{}created {!s} by {}".format(
+                id_,
+                issue.title,
+                style.RESET,
+                " ",
+                issue.created_at,
+                issue.author,
+            )
+            if show_body:
+                out += os.linesep * 2 + _limit_line_length(issue.body)
+            plug.echo(out)
+
+    return persistent_issues_per_repo
+
+
+def _limit_line_length(s: str, max_line_length: int = 100) -> str:
+    """Return the input string with lines no longer than max_line_length.
+
+    Args:
+        s: Any string.
+        max_line_length: Maximum allowed line length.
+    Returns:
+        the input string with lines no longer than max_line_length.
+    """
+    lines = s.split(os.linesep)
+    out = ""
+    for line in lines:
+        cur = 0
+        while len(line) - cur > max_line_length:
+            # find ws closest to the line length
+            idx = line.rfind(" ", cur, max_line_length + cur)
+            idx = max_line_length + cur if idx <= 0 else idx
+            if line[idx] == " ":
+                out += line[cur:idx]
+            else:
+                out += line[cur : idx + 1]
+            out += os.linesep
+            cur = idx + 1
+        out += line[cur : cur + max_line_length] + os.linesep
+    return out
+
+
+def open_issue(
+    issue: plug.Issue,
+    assignment_names: Iterable[str],
+    teams: Iterable[plug.StudentTeam],
+    api: plug.PlatformAPI,
+) -> None:
+    """Open an issue in student repos.
+
+    Args:
+        assignment_names: Names of assignments.
+        teams: Team objects specifying student groups.
+        issue: An issue to open.
+        api: An implementation of :py:class:`repobee_plug.PlatformAPI` used to
+            interface with the platform (e.g. GitHub or GitLab) instance.
+    """
+    repo_urls = api.get_repo_urls(
+        team_names=[t.name for t in teams], assignment_names=assignment_names
+    )
+    repos = progresswrappers.get_repos(repo_urls, api)
+    for repo in repos:
+        issue = api.create_issue(issue.title, issue.body, repo)
+        msg = f"Opened issue {repo.name}/#{issue.number}-'{issue.title}'"
+        repos.write(msg)  # type: ignore
+        plug.log.info(msg)
+
+
+def close_issue(
+    title_regex: str, repos: Iterable[plug.StudentRepo], api: plug.PlatformAPI
+) -> None:
+    """Close issues whose titles match the title_regex in student repos.
+
+    Args:
+        title_regex: A regex to match against issue titles.
+        assignment_names: Names of assignments.
+        teams: Team objects specifying student groups.
+        api: An implementation of :py:class:`repobee_plug.PlatformAPI` used to
+            interface with the platform (e.g. GitHub or GitLab) instance.
+    """
+    repo_urls = (repo.url for repo in repos)
+    platform_repos = progresswrappers.get_repos(repo_urls, api)
+    for repo in platform_repos:
+        to_close = [
+            issue
+            for issue in api.get_repo_issues(repo)
+            if re.match(title_regex, issue.title)
+            and issue.state == plug.IssueState.OPEN
+        ]
+        for issue in to_close:
+            api.close_issue(issue)
+            msg = f"Closed {repo.name}/#{issue.number}='{issue.title}'"
+            platform_repos.write(msg)  # type: ignore
+            plug.log.info(msg)
---
+++ packages/repobee/repobee-3.5.0.tar.gz/repobee-3.5.0/src/_repobee/command/peer.py
@@ -0,0 +1,224 @@
+"""Top-level commands for peer reviewing.
+
+This module contains the top-level functions for RepoBee's peer review
+functionality. Each public function in this module is to be treated as a
+self-contained program.
+
+.. module:: peer
+    :synopsis: Top-level commands for peer reviewing.
+
+.. moduleauthor:: Simon Larsén
+"""
+import itertools
+import collections
+import re
+from typing import Iterable, Optional
+
+import repobee_plug as plug
+
+import _repobee.command.teams
+from _repobee import formatters
+
+from _repobee.command import progresswrappers
+
+DEFAULT_REVIEW_ISSUE = plug.Issue(
+    title="Peer review",
+    body="You have been assigned to peer review this repo.",
+)
+
+
+def assign_peer_reviews(
+    assignment_names: Iterable[str],
+    teams: Iterable[plug.StudentTeam],
+    num_reviews: int,
+    issue: Optional[plug.Issue],
+    api: plug.PlatformAPI,
+) -> None:
+    """Assign peer reviewers among the students to each student repo. Each
+    student is assigned to review num_reviews repos, and consequently, each
+    repo gets reviewed by num_reviews reviewers.
+
+    In practice, each student repo has a review team generated (called
+    <student-repo-name>-review), to which num_reviews _other_ students are
+    assigned. The team itself is given pull-access to the student repo, so
+    that reviewers can view code and open issues, but cannot modify the
+    contents of the repo.
+
+    Args:
+        assignment_names: Names of assginments.
+        teams: Team objects specifying student groups.
+        num_reviews: Amount of reviews each student should perform
+            (consequently, the amount of reviews of each repo)
+        issue: An issue with review instructions to be opened in the considered
+            repos.
+        api: An implementation of :py:class:`repobee_plug.PlatformAPI` used to
+            interface with the platform (e.g. GitHub or GitLab) instance.
+    """
+    issue = issue or DEFAULT_REVIEW_ISSUE
+    expected_repo_names = plug.generate_repo_names(teams, assignment_names)
+    fetched_teams = progresswrappers.get_teams(
+        teams, api, desc="Fetching teams and repos"
+    )
+    fetched_repos = list(
+        itertools.chain.from_iterable(map(api.get_team_repos, fetched_teams))
+    )
+    fetched_repo_dict = {r.name: r for r in fetched_repos}
+
+    missing = set(expected_repo_names) - set(fetched_repo_dict.keys())
+    if missing:
+        raise plug.NotFoundError(f"Can't find repos: {', '.join(missing)}")
+
+    for assignment_name in assignment_names:
+        plug.echo("Allocating reviews")
+        allocations = plug.manager.hook.generate_review_allocations(
+            teams=teams, num_reviews=num_reviews
+        )
+        # adjust names of review teams
+        review_team_specs, reviewed_team_names = list(
+            zip(
+                *[
+                    (
+                        plug.StudentTeam(
+                            members=alloc.review_team.members,
+                            name=plug.generate_review_team_name(
+                                str(alloc.reviewed_team), assignment_name
+                            ),
+                        ),
+                        alloc.reviewed_team,
+                    )
+                    for alloc in allocations
+                ]
+            )
+        )
+
+        review_teams = _repobee.command.teams.create_teams(
+            review_team_specs, plug.TeamPermission.PULL, api
+        )
+        review_teams_progress = plug.cli.io.progress_bar(
+            review_teams,
+            desc="Creating review teams",
+            total=len(review_team_specs),
+        )
+
+        for review_team, reviewed_team_name in zip(
+            review_teams_progress, reviewed_team_names
+        ):
+            reviewed_repo = fetched_repo_dict[
+                plug.generate_repo_name(reviewed_team_name, assignment_name)
+            ]
+            review_teams_progress.write(  # type: ignore
+                f"Assigning {' and '.join(review_team.members)} "
+                f"to review {reviewed_repo.name}"
+            )
+            api.assign_repo(
+                review_team, reviewed_repo, plug.TeamPermission.PULL
+            )
+            api.create_issue(
+                issue.title,
+                issue.body,
+                reviewed_repo,
+                assignees=review_team.members,
+            )
+
+
+def purge_review_teams(
+    assignment_names: Iterable[str],
+    students: Iterable[plug.StudentTeam],
+    api: plug.PlatformAPI,
+) -> None:
+    """Delete all review teams associated with the given assignment names and
+    student teams.
+
+    Args:
+        assignment_names: Names of assignments.
+        students: An iterble of student teams.
+        api: An implementation of :py:class:`repobee_plug.PlatformAPI` used to
+            interface with the platform (e.g. GitHub or GitLab) instance.
+    """
+    review_team_names = [
+        plug.generate_review_team_name(student, assignment_name)
+        for student in students
+        for assignment_name in assignment_names
+    ]
+    teams = progresswrappers.get_teams(
+        review_team_names, api, desc="Deleting review teams"
+    )
+    for team in teams:
+        api.delete_team(team)
+        plug.log.info(f"Deleted {team.name}")
+
+
+def check_peer_review_progress(
+    assignment_names: Iterable[str],
+    teams: Iterable[plug.Team],
+    title_regex: str,
+    num_reviews: int,
+    api: plug.PlatformAPI,
+) -> None:
+    """Check which teams have opened peer review issues in their allotted
+    review repos
+
+    Args:
+        assignment_names: Names of assignments.
+        teams: An iterable of student teams.
+        title_regex: A regex to match against issue titles.
+        num_reviews: Amount of reviews each student is expected to have made.
+        api: An implementation of :py:class:`repobee_plug.PlatformAPI` used to
+            interface with the platform (e.g. GitHub or GitLab) instance.
+
+    """
+    teams = list(teams)
+    reviews = collections.defaultdict(list)
+
+    review_team_names = [
+        plug.generate_review_team_name(team, assignment_name)
+        for team in teams
+        for assignment_name in assignment_names
+    ]
+
+    review_teams = progresswrappers.get_teams(
+        review_team_names, api, desc="Processing review teams"
+    )
+    for review_team in review_teams:
+        repos = list(api.get_team_repos(review_team))
+        if len(repos) != 1:
+            plug.log.warning(
+                f"Expected {review_team.name} to have 1 associated "
+                f"repo, found {len(repos)}. "
+                f"Skipping..."
+            )
+            continue
+
+        reviewed_repo = repos[0]
+        expected_reviewers = set(review_team.members)
+        reviewing_teams = _extract_reviewing_teams(teams, expected_reviewers)
+
+        review_issue_authors = {
+            issue.author
+            for issue in api.get_repo_issues(reviewed_repo)
+            if re.match(title_regex, issue.title)
+        }
+
+        for team in reviewing_teams:
+            reviews[str(team)].append(
+                plug.Review(
+                    repo=reviewed_repo.name,
+                    done=any(
+                        map(review_issue_authors.__contains__, team.members)
+                    ),
+                )
+            )
+
+    plug.echo(
+        formatters.format_peer_review_progress_output(
+            reviews, [team.name for team in teams], num_reviews
+        )
+    )
+
+
+def _extract_reviewing_teams(teams, reviewers):
+    review_teams = []
+    for team in teams:
+        if any(map(team.members.__contains__, reviewers)):
+            review_teams.append(team)
+    return review_teams
