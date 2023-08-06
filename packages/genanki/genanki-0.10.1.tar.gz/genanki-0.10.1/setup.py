4331f7cd884138e2fcdf5c0b6e675a244e56d105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:59 +0000

    Add package genanki/0.10.1/genanki-0.10.1.tar.gz

---
+++ packages/genanki/genanki-0.10.1.tar.gz/genanki-0.10.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+version = {}
+with open('genanki/version.py') as fp:
+  exec(fp.read(), version)
+
+setup(name='genanki',
+      version=version['__version__'],
+      description='Generate Anki decks programmatically',
+      url='http://github.com/kerrickstaley/genanki',
+      author='Kerrick Staley',
+      author_email='k@kerrickstaley.com',
+      license='MIT',
+      packages=['genanki'],
+      zip_safe=False,
+      install_requires=[
+        'cached-property',
+        'frozendict',
+        'pystache',
+        'pyyaml',
+      ],
+      setup_requires=[
+          'pytest-runner',
+      ],
+      tests_require=[
+          'pytest>=6.0.2',
+      ],
+      keywords=[
+        'anki',
+        'flashcards',
+        'memorization',
+      ])
